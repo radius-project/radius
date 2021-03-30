5494b4334792d700f4333459ffcbb0762ac8b9c2
Ryan Nowak <nowakra@gmail.com>
2021-03-30 13:05:27 -0700

    Fix break in Dapr Microservices tutorial
    Oopsies :0

--- pkg/curp/deployment_processor.go
+++ pkg/curp/deployment_processor.go
@@ -146,7 +146,7 @@ func NewDeploymentProcessor(arm armauth.ArmConfig, k8s client.Client) Deployment
 	d := workloads.Dispatcher{
 		Renderers: map[string]workloads.WorkloadRenderer{
 			"dapr.io/Component@v1alpha1":          &daprcomponentv1alpha1.Renderer{},
-			"dapr.io/StateStore@v1alpha1@":        &daprstatestorev1alpha1.Renderer{},
+			"dapr.io/StateStore@v1alpha1":         &daprstatestorev1alpha1.Renderer{},
 			"azure.com/CosmosDocumentDb@v1alpha1": &cosmosdocumentdbv1alpha1.Renderer{Arm: arm},
 			"azure.com/Function@v1alpha1":         &dapr.Renderer{Inner: &functionv1alpha1.Renderer{}},
 			"azure.com/WebApp@v1alpha1":           &dapr.Renderer{Inner: &webappv1alpha1.Renderer{}},
