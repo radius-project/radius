65891af350372a8d3f3932835cbcf5e94461a86b
Ryan Nowak <nowakra@gmail.com>
2021-04-14 15:37:16 -0700

    Add "channel" concept to our asset publishing
    Part of: #231
    
    This change makes our version publishing logic more sophisticated. After
    this change we produce two version from each tagged release:
    
    - The release version (full version)
    - The channel (excludes patch)
    
    Things that get a version number baked in are stamped with the release
    version. That way a `rad --version` output will include the full build
    info including the patch level.
    
    Things that are downloaded by the user are uploaded based on a
    "channel". This allows us to patch things and for users to see the
    update. For instance we can patch the RP by uploading a new build with
    the same tag. Users can restart their RP and pick up the update.
    
    There's also built-in support for updating a version marker to point to
    the latest channel. This way we can push a `v0.2` tag and then `0.2`
    becomes the stable channel. Our install scripts and other assets will
    refer to this channel through the file.

--- .github/scripts/get_release_version.py
+++ .github/scripts/get_release_version.py
@@ -6,47 +6,95 @@
 # This script parses release version from Git tag and set the parsed version to
 # environment variable, REL_VERSION.
 
+# We set the environment variable REL_CHANNEL based on the kind of release. REL_CHANNEL is:
+# 'edge': for most builds
+# 'pr-<pr number>': for PR builds
+# '1.0.0-rc1' (the full version): for a tagged prerelease
+# '1.0' (major.minor): for a tagged release
+
+# We set the environment variable UPDATE_RELEASE if it's a full release (tagged and not prerelease)
+
+# REL_VERSION is used to stamp versions into binaries
+# REL_CHANNEL is used to upload assets to different paths
+
+# This way a 1.0 user can download 1.0.1, etc.
+
 import os
 import re
 import sys
 
 gitRef = os.getenv("GITHUB_REF")
-tagRefRegex = r"refs/tags/v(.*)"
-pullRefRegex = r"refs/pull/(.*)/(.*)"
+
+# From https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+# Group 'version' returns the whole version
+# other named groups return the components
+tagRefRegex = r"^refs/tags/v(?P<version>0|(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$"
+pullRefRegex = r"^refs/pull/(.*)/(.*)$"
 
 with open(os.getenv("GITHUB_ENV"), "a") as githubEnv:
     if gitRef is None:
         print("This is not running in github, GITHUB_REF is null. Assuming a local build...")
+
         version = "REL_VERSION=edge"
         print("Setting: {}".format(version))
         githubEnv.write(version + "\n")
+
+        channel = "REL_CHANNEL=edge"
+        print("Setting: {}".format(channel))
+        githubEnv.write(channel + "\n")
+
         sys.exit(0)
 
     match = re.search(pullRefRegex, gitRef)
     if match is not None:
         print("This is pull request {}...".format(match.group(1)))
+
         version = "REL_VERSION=pr-{}".format(match.group(1))
         print("Setting: {}".format(version))
         githubEnv.write(version + "\n")
+
+        channel = "REL_CHANNEL=pr-{}".format(match.group(1))
+        print("Setting: {}".format(channel))
+        githubEnv.write(channel + "\n")
+
         sys.exit(0)
 
     match = re.search(tagRefRegex, gitRef)
     if match is not None:
-        print("This is tagged as {}...".format(match.group(1)))
-        version = "REL_VERSION={}".format(match.group(1))
-        print("Setting: {}".format(version))
-        githubEnv.write(version + "\n")
+        print("This is tagged as {}...".format(match.group("version")))
 
-        if version.find("-rc") > 0:
-            print("Release Candidate build from {}...".format(version))
+        if match.group("prerelease") is None:
+            print("This is a full release...")
+
+            version = "REL_VERSION={}".format(match.group("version"))
+            print("Setting: {}".format(version))
+            githubEnv.write(version + "\n")
+
+            channel = "REL_CHANNEL={}.{}".format(match.group("major"), match.group("minor"))
+            print("Setting: {}".format(channel))
+            githubEnv.write(channel + "\n")
+
+            print("Setting: UPDATE_RELEASE=true")
+            githubEnv.write("UPDATE_RELEASE=true" + "\n")
+            sys.exit(0)
+
         else:
-            print("Release build from {}...".format(version))
-            githubEnv.write("LATEST_RELEASE=true\n")
-        sys.exit(0)
+            print("This is a prerelease...")
+        
+            version = "REL_VERSION={}".format(match.group("version"))
+            print("Setting: {}".format(version))
+            githubEnv.write(version + "\n")
 
-    githubEnv.write("\n")
-    print("This is daily build from {}...".format(gitRef))
+            channel = "REL_CHANNEL={}".format(match.group("version"))
+            print("Setting: {}".format(channel))
+            githubEnv.write(channel + "\n")
+            sys.exit(0)
+
+    print("This is a normal build")
     version = "REL_VERSION=edge"
     print("Setting: {}".format(version))
     githubEnv.write(version + "\n")
-    sys.exit(0)
+
+    channel = "REL_CHANNEL=edge"
+    print("Setting: {}".format(channel))
+    githubEnv.write(channel + "\n")
