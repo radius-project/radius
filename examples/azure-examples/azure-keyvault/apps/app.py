b6a2c5b4ffa1d41a210cb71608ba3540b7a9087a
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2021-04-21 16:16:06 -0700

    flush print

--- examples/azure-examples/azure-keyvault/apps/app.py
+++ examples/azure-examples/azure-keyvault/apps/app.py
@@ -11,15 +11,15 @@ from azure.identity import ManagedIdentityCredential
 from azure.core.exceptions import HttpResponseError
 
 # Test Application that tries to access Keyvault
-print("Getting vault url")
+print("Getting vault url", flush=True)
 VAULT_URL = os.environ["KV_URI"]
-print("Vault url: {0}".format(VAULT_URL))
+print("Vault url: {0}".format(VAULT_URL), flush=True)
 managed_identity = ManagedIdentityCredential()
 client = SecretClient(vault_url=VAULT_URL, credential=managed_identity)
 try:
     while True:
         # Create a secret
-        print("\n.. Create Secret")
+        print("\n.. Create Secret", flush=True)
         expires = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=365)
         secret_value = str(random.randint(1, 1000))
         secret_name = "mysecret-" + secret_value
@@ -27,16 +27,16 @@ try:
         print("Secret with name '{0}' created with value '{1}'".format(secret.name, secret.value))
 
         # Retrieve the secret just created
-        print("\n.. Get the Secret by name")
+        print("\n.. Get the Secret by name", flush=True)
         retrieved_secret = client.get_secret(secret_name)
-        print("Secret with name '{0}' was found with value '{1}'.".format(retrieved_secret.name, retrieved_secret.value))
+        print("Secret with name '{0}' was found with value '{1}'.".format(retrieved_secret.name, retrieved_secret.value), flush=True)
 
         # Delete secret
-        print("\n.. Deleting Secret...")
+        print("\n.. Deleting Secret...", flush=True)
         client.begin_delete_secret(retrieved_secret.name)
-        print("Secret with name '{0}' was deleted.".format(retrieved_secret.name))
+        print("Secret with name '{0}' was deleted.".format(retrieved_secret.name), flush=True)
 
         time.sleep(100)
 
 except HttpResponseError as e:
-    print("\nThis sample has caught an error. {0}".format(e.message))
+    print("\nThis sample has caught an error. {0}".format(e.message), flush=True)
