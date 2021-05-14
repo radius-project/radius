19434e1ac3425ae7f0e3df2b1d16cde2439a59d6
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2021-05-14 11:36:58 -0700

    fix bugs (#355)
    * fix bugs in test code

--- cmd/cli/cmd/envInitAzure.go
+++ cmd/cli/cmd/envInitAzure.go
@@ -405,7 +405,8 @@ func validateSubscription(ctx context.Context, authorizer autorest.Authorizer, s
 
 	group, err := rgc.Get(ctx, resourceGroup)
 	if group.StatusCode == 404 {
-		return nil, err
+		// Ignore the NotFound error for the resource group as it will get created if it does not exist.
+		return nil, nil
 	} else if err != nil {
 		return nil, err
 	}
