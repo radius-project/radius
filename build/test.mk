1a1cd3cd29e2b30834727d23c80698ff7f1275d2
Justin Kotalik <jukotali@microsoft.com>
2021-06-10 14:51:07 -0700

    Nit

--- build/test.mk
+++ build/test.mk
@@ -17,5 +17,8 @@ ENVTEST_ASSETS_DIR=$(shell pwd)/bin
 K8S_VERSION=1.19.2
 ENV_SETUP=$(GOBIN)/setup-envtest
 
-test-controller: generate-k8s-manifests generate-controller ## Runs controller tests, note arm64 version not available.
-		go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest; KUBEBUILDER_ASSETS="$(shell $(ENV_SETUP) use -p path ${K8S_VERSION} --arch amd64)" go test ./pkg/kubernetes/... 
+get-envtools:
+	go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+	
+test-controller: generate-k8s-manifests generate-controller get-envtools ## Runs controller tests, note arm64 version not available.
+	KUBEBUILDER_ASSETS="$(shell $(ENV_SETUP) use -p path ${K8S_VERSION} --arch amd64)" go test ./pkg/kubernetes/... 
