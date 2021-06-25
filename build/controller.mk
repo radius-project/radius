a5c9416e76fb003a5f77e3af09fd94ffad30159f
Justin Kotalik <jukotali@microsoft.com>
2021-06-25 14:21:41 -0700

    Don't create namespace

--- build/controller.mk
+++ build/controller.mk
@@ -8,17 +8,17 @@
 controller-run: generate-k8s-manifests generate-controller ## Run the controller locally
 	SKIP_WEBHOOKS=true go run ./cmd/k8s/main.go
 
-controller-install: generate-k8s-manifests
+controller-install: generate-k8s-manifests  ## Install CRDs into the K8s cluster specified in ~/.kube/config.
 	kubectl apply -f cmd/cli/cmd/Chart/templates/applications.radius.dev_applications.yaml \
 		-f cmd/cli/cmd/Chart/templates/applications.radius.dev_components.yaml \
 		-f cmd/cli/cmd/Chart/templates/applications.radius.dev_deployments.yaml
-controller-uninstall: generate-k8s-manifests
+controller-uninstall: generate-k8s-manifests  ## Uninstall CRDs from the K8s cluster specified in ~/.kube/config.
 	kubectl delete -f cmd/cli/cmd/Chart/templates/applications.radius.dev_applications.yaml \
 		-f cmd/cli/cmd/Chart/templates/applications.radius.dev_components.yaml \
 		-f cmd/cli/cmd/Chart/templates/applications.radius.dev_deployments.yaml
 
-controller-deploy: generate-k8s-manifests docker-build docker-push ## Install CRDs into the K8s cluster specified in ~/.kube/config.
+controller-deploy: generate-k8s-manifests docker-build docker-push ## Deploy controller to the K8s cluster specified in ~/.kube/config.
 	helm upgrade --wait --install --set container=$(DOCKER_REGISTRY)/radius-controller:$(DOCKER_TAG_VERSION) $(DOCKER_TAG_VERSION) cmd/cli/cmd/Chart/
 
-controller-undeploy: generate-k8s-manifests ## Uninstall CRDs from the K8s cluster specified in ~/.kube/config.
+controller-undeploy: generate-k8s-manifests ## Undeploy controller from the K8s cluster specified in ~/.kube/config.
 	helm uninstall $(DOCKER_TAG_VERSION)
