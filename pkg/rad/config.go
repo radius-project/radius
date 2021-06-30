radius-project/radius

commit c0e7c1fc264d22659c433c5a6abb097932cfe877

Stop using viper.GetViper()
Found this issue while doing some test cleanup. Right now we're using
viper.GetViper() which relies on global mutable state and is thus not
testable.

I was trying to figure out why our test code that reads config doesn't
do it the way we do in `rad`... and yeah that's why....

pkg/rad/config.go
@@ -23,8 +23,6 @@ import (
 	"golang.org/x/text/cases"
 )
 
-var CfgFile string
-
 // EnvironmentKey is the key used for the environment section
 const (
 	EnvironmentKey string = "environment"
@@ -82,13 +80,11 @@ func (env EnvironmentSection) GetEnvironment(name string) (environments.Environm
 	return env.decodeEnvironmentSection(name)
 }
 
-// initConfig reads in config file and ENV variables if set.
-func InitConfig() {
-	if CfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(CfgFile)
-	} else {
-		// Find home directory.
+func LoadConfig(configFilePath string) (*viper.Viper, error) {
+	config := viper.New()
+
+	if configFilePath == "" {
+		// Set config file using the HOME directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
@@ -96,39 +92,56 @@ func InitConfig() {
 		}
 
 		rad := path.Join(home, ".rad")
-		viper.AddConfigPath(rad)
-		viper.SetConfigName("config")
+		config.AddConfigPath(rad)
+		config.SetConfigName("config")
+	} else {
+		config.SetConfigFile(configFilePath)
 	}
 
-	// If a config file is found, read it in.
-	err := viper.ReadInConfig()
+	err := config.ReadInConfig()
 	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		// It's ok the config file is not found, this could be the first time the CLI
+		// is running. Commands that require configuration will check for the data they need.
+	} else if os.IsNotExist(err) {
+		// It's ok the config file is not found, this could be the first time the CLI
+		// is running. Commands that require configuration will check for the data they need.
+	} else if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func SaveConfig(v *viper.Viper) error {
+	configFilePath := v.ConfigFileUsed()
+	if configFilePath == "" {
+		// Set config file using the HOME directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Set the default config file so we can write to it if desired
-		CfgFile = path.Join(home, ".rad", "config.yaml")
-	} else if err == nil {
-		CfgFile = viper.ConfigFileUsed()
+		path.Join(home, ".rad", "config.yaml")
 	}
-}
 
-func SaveConfig() error {
-	dir := path.Dir(CfgFile)
+	dir := path.Dir(configFilePath)
 	_, err := os.Stat(dir)
 	if os.IsNotExist(err) {
-		_ = os.MkdirAll(dir, os.ModeDir|0755)
+		err := os.MkdirAll(dir, os.ModeDir|0755)
+		if err != nil {
+			return fmt.Errorf("failed to create directory '%s': %w", dir, err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("failed to find directory '%s': %w", dir, err)
 	}
 
-	err = viper.WriteConfigAs(CfgFile)
+	err = v.WriteConfigAs(configFilePath)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to write config to '%s': %w", configFilePath, err)
 	}
 
-	fmt.Printf("Successfully wrote configuration to %v\n", CfgFile)
+	fmt.Printf("Successfully wrote configuration to %v\n", configFilePath)
 	return nil
 }
 

cmd/cli/cmd/appDelete.go
@@ -35,7 +35,8 @@ func deleteApplication(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	env, err := rad.RequireEnvironment(cmd)
+	config := ConfigFromContext(cmd.Context())
+	env, err := rad.RequireEnvironment(cmd, config)
 	if err != nil {
 		return err
 	}
@@ -82,7 +83,7 @@ func deleteApplication(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	err = updateApplicationConfig(env, applicationName)
+	err = updateApplicationConfig(config, env, applicationName)
 	if err != nil {
 		return err
 	}
@@ -91,11 +92,10 @@ func deleteApplication(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func updateApplicationConfig(env environments.Environment, applicationName string) error {
+func updateApplicationConfig(config *viper.Viper, env environments.Environment, applicationName string) error {
 	// If the application we are deleting is the default application, remove it
 	if env.GetDefaultApplication() == applicationName {
-		v := viper.GetViper()
-		envSection, err := rad.ReadEnvironmentSection(v)
+		envSection, err := rad.ReadEnvironmentSection(config)
 		if err != nil {
 			return err
 		}
@@ -104,9 +104,9 @@ func updateApplicationConfig(env environments.Environment, applicationName strin
 
 		envSection.Items[env.GetName()][environments.EnvironmentKeyDefaultApplication] = ""
 
-		rad.UpdateEnvironmentSection(v, envSection)
+		rad.UpdateEnvironmentSection(config, envSection)
 
-		err = rad.SaveConfig()
+		err = rad.SaveConfig(config)
 		if err != nil {
 			return err
 		}

cmd/cli/cmd/appList.go
@@ -27,7 +27,8 @@ func init() {
 }
 
 func listApplications(cmd *cobra.Command, args []string) error {
-	env, err := rad.RequireEnvironment(cmd)
+	config := ConfigFromContext(cmd.Context())
+	env, err := rad.RequireEnvironment(cmd, config)
 	if err != nil {
 		return err
 	}

cmd/cli/cmd/appShow.go
@@ -26,7 +26,8 @@ func init() {
 }
 
 func showApplication(cmd *cobra.Command, args []string) error {
-	env, err := rad.RequireEnvironment(cmd)
+	config := ConfigFromContext(cmd.Context())
+	env, err := rad.RequireEnvironment(cmd, config)
 	if err != nil {
 		return err
 	}

cmd/cli/cmd/appSwitch.go
@@ -13,7 +13,6 @@ import (
 	"github.com/Azure/radius/pkg/rad/environments"
 	"github.com/Azure/radius/pkg/rad/logger"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // appSwitchCmd command to switch applications
@@ -50,8 +49,8 @@ func switchApplications(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("no application specified")
 	}
 
-	v := viper.GetViper()
-	env, err := rad.ReadEnvironmentSection(v)
+	config := ConfigFromContext(cmd.Context())
+	env, err := rad.ReadEnvironmentSection(config)
 	if err != nil {
 		return err
 	}
@@ -78,7 +77,7 @@ func switchApplications(cmd *cobra.Command, args []string) error {
 
 	env.Items[e.GetName()][environments.EnvironmentKeyDefaultApplication] = applicationName
 
-	rad.UpdateEnvironmentSection(v, env)
-	err = rad.SaveConfig()
+	rad.UpdateEnvironmentSection(config, env)
+	err = rad.SaveConfig(config)
 	return err
 }

cmd/cli/cmd/componentExpose.go
@@ -26,7 +26,8 @@ Press CTRL+C to exit the command and terminate the tunnel.`,
 # on local port 5000
 rad component expose --application icecream-store orders --port 5000 --remote-port 80`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		env, err := rad.RequireEnvironment(cmd)
+		config := ConfigFromContext(cmd.Context())
+		env, err := rad.RequireEnvironment(cmd, config)
 		if err != nil {
 			return err
 		}

cmd/cli/cmd/componentList.go
@@ -26,7 +26,8 @@ func init() {
 }
 
 func listComponents(cmd *cobra.Command, args []string) error {
-	env, err := rad.RequireEnvironment(cmd)
+	config := ConfigFromContext(cmd.Context())
+	env, err := rad.RequireEnvironment(cmd, config)
 	if err != nil {
 		return err
 	}

cmd/cli/cmd/componentLogs.go
@@ -38,7 +38,8 @@ rad component logs --application icecream-store orders --follow
 # read logs from the 'daprd' sidecare container of the 'orders' component of the 'icecream-store' application
 rad component logs --application icecream-store orders --container daprd`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		env, err := rad.RequireEnvironment(cmd)
+		config := ConfigFromContext(cmd.Context())
+		env, err := rad.RequireEnvironment(cmd, config)
 		if err != nil {
 			return err
 		}

commit 30135f11f5b3c2fcdd61a522bf99c3cd0447cd96

Fix writing config for the first time
Fixes: #650

This was a regression caused by #644

This should have set the variable instead of doing nothing.

pkg/rad/config.go
@@ -122,7 +122,7 @@ func SaveConfig(v *viper.Viper) error {
 			os.Exit(1)
 		}
 
-		path.Join(home, ".rad", "config.yaml")
+		configFilePath = path.Join(home, ".rad", "config.yaml")
 	}
 
 	dir := path.Dir(configFilePath)