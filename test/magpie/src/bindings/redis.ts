8a13f46d615a721a3cc1c6493e317f462dddfd84
Justin Kotalik <jukotali@microsoft.com>
2021-08-02 15:55:39 -0700

    Update redis client stuff

--- test/magpie/src/bindings/redis.ts
+++ test/magpie/src/bindings/redis.ts
@@ -26,62 +26,38 @@ export class RedisBinding implements Binding {
 
     public async status(): Promise<BindingStatus> {
         // from https://docs.microsoft.com/en-us/azure/azure-cache-for-redis/cache-nodejs-get-started
-        var cacheConnection = redis.createClient(+this.port, this.host, 
-            {
-                auth_pass: this.password,
-                // tls: {servername: this.host}
-            });
+        if (this.password) {
+            var cacheConnection = redis.createClient(+this.port, 
+                this.host, 
+                {
+                    auth_pass: this.password,
+                    tls: {servername: this.host}
+                });
+        } else {
+            var cacheConnection = redis.createClient(+this.port, this.host, {});
+        }
+
 
         cacheConnection.on("error", function(error) {
             console.error(error);
         });
         // Simple PING command
         console.log("\nCache command: PING");
-        var success = cacheConnection.ping(function(error, res) {
-            if (error != null) {
-                console.error("Error: " + error);
-            }
+        cacheConnection.ping(function(error, res) {
+            if (error) throw error;
             console.log("Cache response : " + res);
         });
-        if (!success) {
-            throw new Error("Could not ping redis cache");
-        }
 
-        // Simple get and put of integral data types into the cache
-        success = cacheConnection.get("Message");
-        console.log("\nCache command: GET Message");
-        console.log("Cache response : " + success);    
-        if (!success) {
-            throw new Error("Could not get on redis cache");
-        }
-
-        success = cacheConnection.set("Message", "Hello! The cache is working from Node.js!")
-        console.log("\nCache command: SET Message");
-        console.log("Cache response : " + success);    
-        if (!success) {
-            throw new Error("Could not set on redis cache");
-        }
+        cacheConnection.set("Message", "Hello! The cache is working from Node.js!", function(error, res) {
+            if (error) throw error;
+            console.log("Cache response : " + res);
+        });
 
         // Demonstrate "SET Message" executed as expected...
-        success = cacheConnection.get("Message", function(error, res) {
-            if (error != null) {
-                console.error("Error: " + error);
-            }
+        cacheConnection.get("Message", function(error, res) {
+            if (error) throw error;
             console.log("Cache response : " + res);
         });
-        console.log("\nCache command: GET Message");
-        console.log("Cache response : " + cacheConnection.get("Message"));    
-        if (!success) {
-            throw new Error("Could not get on redis cache");
-        }
-
-        // Get the client list, useful to see if connection list is growing...
-        success = cacheConnection.client("LIST")
-        console.log("\nCache command: CLIENT LIST");
-        console.log("Cache response : " + cacheConnection.client("LIST")); 
-        if (!success) {
-            throw new Error("Could not list on redis cache");
-        }
 
         return { ok: true, message: "connected"};
     }
