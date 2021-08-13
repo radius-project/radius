30c995737e3e8d0245e0b8f1136c3f14d7fa2e65
Justin Kotalik <jukotali@microsoft.com>
2021-08-13 12:43:00 -0700

    Still deciphering

---
+++ pkg/workloads/servicebusqueuev1alpha1/types.go
@@ -0,0 +1,8 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package servicebusqueuev1alpha1
+
+const Kind = "azure.com/ServiceBusQueue@v1alpha1"
