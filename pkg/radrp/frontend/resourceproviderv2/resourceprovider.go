4cb1d09f513d9142b1bf5935dd5af925f28b05d2
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2021-09-17 16:49:17 -0700

    linter

--- pkg/radrp/frontend/resourceproviderv2/resourceprovider.go
+++ pkg/radrp/frontend/resourceproviderv2/resourceprovider.go
@@ -537,9 +537,9 @@ func (r *rp) UpdateDeployment(ctx context.Context, d *rest.Deployment) (rest.Res
 
 func (r *rp) registerForHealthChecks(ctx context.Context, actions map[string]deployment.ComponentAction) error {
 	errs := []error{}
-	for _, action := range actions {
+	for c, action := range actions {
 		if action.Definition == nil || action.Operation == deployment.DeleteWorkload {
-			fmt.Printf("@@@@@ Ignoring handling of action: %s, op: %s, def: %v @@@@ \n", action.Name, action.Operation, action.Definition)
+			fmt.Printf("@@@@@ Ignoring handling of action app: %s, component: %s, op: %s, def: %v @@@@ \n", action.ApplicationName, c, action.Operation, action.Definition)
 			// Do nothing for delete workloads
 			continue
 		}
