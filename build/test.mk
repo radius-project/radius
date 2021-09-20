1b3904da1fd45bad410cd1040b6402eedb88d344
Karol Zadora-Przylecki <karolz@microsoft.com>
2021-09-20 11:18:51 -0700

    Include localdev tests in controller tests

--- build/test.mk
+++ build/test.mk
@@ -28,11 +28,10 @@ test-get-envtools:
 	go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
 
 test-controller: generate-k8s-manifests generate-controller test-get-envtools ## Runs controller tests, note arm64 version not available.
-	KUBEBUILDER_ASSETS="$(shell $(ENV_SETUP) use -p path ${K8S_VERSION} --arch amd64)" CGO_ENABLED=1 go test $(GOTEST_OPTS) -v ./test/integration/kubernetes/...
+	KUBEBUILDER_ASSETS="$(shell $(ENV_SETUP) use -p path ${K8S_VERSION} --arch amd64)" CGO_ENABLED=1 go test $(GOTEST_OPTS) -v ./test/integration/kubernetes/... ./test/integration/localdev/...
 
 test-validate-bicep: ## Validates that all .bicep files compile cleanly
 	BICEP_PATH="${HOME}/.rad/bin" ./build/validate-bicep.sh
 
-# These tests are no part of the build yet--local development experience is still in the early state of development
 test-localdev: test-get-envtools ## Runs local development environment tests
 	KUBEBUILDER_ASSETS="$(shell $(ENV_SETUP) use -p path ${K8S_VERSION} --arch amd64)" CGO_ENABLED=1 go test $(GOTEST_OPTS) -v ./test/integration/localdev/...
