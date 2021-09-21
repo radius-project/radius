0dc94900b2f1e614685da720aab78ceb2da21c43
Justin Kotalik <jukotali@microsoft.com>
2021-09-21 10:57:45 -0700

    No dapr for now

--- pkg/model/resourcesv1alpha3/json.go
+++ pkg/model/resourcesv1alpha3/json.go
@@ -14,40 +14,6 @@ const kindProperty = "kind"
 const nameProperty = "name"
 const idProperty = "id"
 
-// func (gb GenericBinding) MarshalJSON() ([]byte, error) {
-// 	properties := map[string]interface{}{}
-// 	for k, v := range gb.AdditionalProperties {
-// 		properties[k] = v
-// 	}
-
-// 	properties[kindProperty] = gb.Kind
-// 	return json.Marshal(properties)
-// }
-
-// func (gb *GenericBinding) UnmarshalJSON(b []byte) error {
-// 	properties := map[string]interface{}{}
-// 	err := json.Unmarshal(b, &properties)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	obj, ok := properties[kindProperty]
-// 	if !ok {
-// 		return errors.New("the 'kind' property is required")
-// 	}
-
-// 	kind, ok := obj.(string)
-// 	if !ok {
-// 		return errors.New("the 'kind' property must be a string")
-// 	}
-
-// 	delete(properties, kindProperty)
-
-// 	gb.Kind = kind
-// 	gb.AdditionalProperties = properties
-// 	return nil
-// }
-
 func (gt GenericTrait) MarshalJSON() ([]byte, error) {
 	properties := map[string]interface{}{}
 	for k, v := range gt.AdditionalProperties {
