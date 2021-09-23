8712f0f0b45ae4e6036bf9f3b648959da6fb2646
Ryan Nowak <nowakra@gmail.com>
2021-09-22 19:04:45 -0700

    Add adapter to renderers
    This change adds some missing things for the computed value construct
    that we discussed previous.
    
    Also this change demonstrates how we can adapt a subset of the  existing
    v1 renderers to use the new renderer contract and code path without
    rewriting it as a transitional measure. I can adapt the rest of these in
    a follow-up change after we review the pattern.
    
    This is preferrable to doing a 'big bang' rewrite because because we'll
    be able to migrate each scenario individually.

--- pkg/renderers/types.go
+++ pkg/renderers/types.go
@@ -36,12 +36,25 @@ type RendererOutput struct {
 	SecretValues   map[string]SecretValueReference
 }
 
+// ComputedValueReference represents a non-secret value that can accessed once the output resources
+// have been deployed.
 type ComputedValueReference struct {
-	LocalID       string
-	ValueSelector string
+	// ComputedValueReference might hold a static value in `.Value` or might be a reference
+	// that needs to be looked up. If `.Value` is set then treat this as a static value.
+	// If `.Value == nil` then use the `.PropertyReference` to look up a property in the property
+	// bag returned from deploying the resource via `handler.Put`.
+
+	LocalID           string
+	PropertyReference string
+	Value             interface{}
 }
 
+// SecretValueReference represents a secret value that can accessed on the output resources
+// have been deployed.
 type SecretValueReference struct {
+	// SecretValueReference always needs to be resolved against a deployed resource. These
+	// are secrets so we don't want to store them.
+
 	LocalID       string
 	Action        string
 	ValueSelector string
