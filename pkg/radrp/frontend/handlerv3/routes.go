8361779cdd82d6e58b589670ea33e9416ed41de4
Nghia Tran <tcnghia@gmail.com>
2021-09-25 17:37:55 -0700

    Disable item route for RadiusResource

--- pkg/radrp/frontend/handlerv3/routes.go
+++ pkg/radrp/frontend/handlerv3/routes.go
@@ -7,9 +7,11 @@ package handlerv3
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/Azure/radius/pkg/azure/azresources"
 	"github.com/Azure/radius/pkg/radrp/frontend/resourceproviderv3"
+	"github.com/Azure/radius/pkg/radrp/rest"
 	"github.com/Azure/radius/pkg/radrp/schemav3"
 	"github.com/gorilla/mux"
 )
@@ -27,9 +29,11 @@ func AddRoutes(rp resourceproviderv3.ResourceProvider, router *mux.Router, valid
 	var applicationItemPath = fmt.Sprintf("%s/{%s}", applicationCollectionPath, azresources.ApplicationNameKey)
 
 	var resourceCollectionPath = fmt.Sprintf("%s/{%s}", applicationItemPath, azresources.ResourceTypeKey)
-	var allResourceCollectionPath = fmt.Sprintf("%s/%s", applicationItemPath, schemav3.GenericResourceType)
 	var resourceItemPath = fmt.Sprintf("%s/{%s}", resourceCollectionPath, azresources.ResourceNameKey)
 	var operationItemPath = fmt.Sprintf("%s/{%s}/{%s}", resourceItemPath, "OperationResults", azresources.OperationIDKey)
+
+	var allResourceCollectionPath = fmt.Sprintf("%s/%s", applicationItemPath, schemav3.GenericResourceType)
+	var allResourceItemPath = fmt.Sprintf("%s/{%s}", allResourceCollectionPath, azresources.ResourceNameKey)
 
 	router.Path(applicationCollectionPath).Methods("GET").HandlerFunc(h.ListApplications)
 	subrouter = router.Path(applicationItemPath).Subrouter()
@@ -38,6 +42,8 @@ func AddRoutes(rp resourceproviderv3.ResourceProvider, router *mux.Router, valid
 	subrouter.Methods("DELETE").HandlerFunc(h.DeleteApplication)
 
 	router.Path(allResourceCollectionPath).Methods("GET").HandlerFunc(h.ListAllV3ResourcesByApplication)
+	subrouter.Path(allResourceItemPath).HandlerFunc(notSupported)
+
 	router.Path(resourceCollectionPath).Methods("GET").HandlerFunc(h.ListResources)
 	subrouter = router.Path(resourceItemPath).Subrouter()
 	subrouter.Methods("GET").HandlerFunc(h.GetResource)
@@ -46,4 +52,9 @@ func AddRoutes(rp resourceproviderv3.ResourceProvider, router *mux.Router, valid
 
 	subrouter = router.Path(operationItemPath).Subrouter()
 	subrouter.Methods("GET").HandlerFunc(h.GetOperation)
+}
+
+func notSupported(w http.ResponseWriter, req *http.Request) {
+	response := rest.NewBadRequestResponse(fmt.Sprintf("Route not suported: %v", req.URL.Path))
+	_ = response.Apply(req.Context(), w, req)
 }
