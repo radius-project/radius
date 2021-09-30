a40ab4293fa8a50b9a8d71dc2774c762d6ca126c
Ryan Nowak <nowakra@gmail.com>
2021-09-30 14:53:46 -0700

    Add support for secret values in connection in k8s
    This change enables the use of secrets in Kuberentes from inside the
    renderer. The current code only provides the renderer access to the
    non-secret values, so secrets would be omitted.
    
    Also fixed an issue with how we were transforming the 'defintion' when
    passing into the the renderer and added a unit test.

---
+++ pkg/kubernetes/controllers/radius/resource_controller_test.go
@@ -0,0 +1,133 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package controllers
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"testing"
+
+	"github.com/Azure/radius/pkg/azure/azresources"
+	radiusv1alpha3 "github.com/Azure/radius/pkg/kubernetes/api/radius/v1alpha3"
+	"github.com/Azure/radius/pkg/renderers"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+const (
+	Namespace       = "test-namespace"
+	ApplicationName = "test-application"
+	ResourceName    = "test-resource"
+)
+
+var resourceID = azresources.MakeID(
+	"kubernetes",
+	Namespace,
+	azresources.ResourceType{Type: "Microsoft.CustomProviders/resourceProviders", Name: "radiusv3"},
+	azresources.ResourceType{Type: "Application", Name: ApplicationName},
+	azresources.ResourceType{Type: "ContainerComponent", Name: ResourceName})
+
+func Test_GetRenderDependency(t *testing.T) {
+	scheme := runtime.NewScheme()
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(radiusv1alpha3.AddToScheme(scheme))
+
+	// Simulate a *rendered* component
+	resource := radiusv1alpha3.ContainerComponent{
+		TypeMeta: v1.TypeMeta{
+			APIVersion: radiusv1alpha3.GroupVersion.String(),
+			Kind:       "ContainerComponent",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      ResourceName,
+			Namespace: Namespace,
+		},
+		Spec: radiusv1alpha3.ResourceSpec{
+			Template: rawOrPanic(map[string]interface{}{
+				"properties": map[string]interface{}{
+					"definition-property": "definition-value",
+				},
+			}),
+		},
+		Status: radiusv1alpha3.ResourceStatus{
+			Resources: map[string]corev1.ObjectReference{
+				"SecretLocalID": {
+					Namespace:  Namespace,
+					Name:       "some-secret",
+					Kind:       "Secret",
+					APIVersion: "v1",
+				},
+			},
+			ComputedValues: rawOrPanic(map[string]renderers.ComputedValueReference{
+				"computed-property": {
+					Value: "computed-value",
+				},
+			}),
+			SecretValues: rawOrPanic(map[string]renderers.SecretValueReference{
+				"secret-property": {
+					LocalID:       "SecretLocalID",
+					ValueSelector: "secret-key",
+				},
+			}),
+		},
+	}
+
+	// And the secret that holds the secret values
+	secret := corev1.Secret{
+		TypeMeta: v1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Secret",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "some-secret",
+			Namespace: Namespace,
+		},
+		Data: map[string][]byte{
+			"secret-key": []byte(base64.RawStdEncoding.EncodeToString([]byte("secret-value"))),
+		},
+	}
+
+	c := fake.NewClientBuilder().WithScheme(scheme).WithObjects(&resource, &secret).Build()
+
+	controller := ResourceReconciler{
+		Client: c,
+	}
+
+	id, err := azresources.Parse(resourceID)
+	require.NoError(t, err)
+
+	dependency, err := controller.GetRenderDependency(context.Background(), Namespace, id)
+	require.NoError(t, err)
+	require.NotNil(t, dependency)
+
+	expected := renderers.RendererDependency{
+		ResourceID: id,
+		Definition: map[string]interface{}{
+			"definition-property": "definition-value",
+		},
+		ComputedValues: map[string]interface{}{
+			"computed-property": "computed-value",
+			"secret-property":   "secret-value",
+		},
+	}
+
+	require.Equal(t, expected, *dependency)
+}
+
+func rawOrPanic(obj interface{}) *runtime.RawExtension {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+
+	return &runtime.RawExtension{Raw: b}
+}
