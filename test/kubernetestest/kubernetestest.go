350dc7c64a60b0c7b754cab7e7f24807457a22ef
Justin Kotalik <jukotali@microsoft.com>
2021-09-30 13:23:28 -0700

    Mark application as owner of resources (#1126)

--- test/kubernetestest/kubernetestest.go
+++ test/kubernetestest/kubernetestest.go
@@ -35,7 +35,6 @@ type Step struct {
 	PostStepVerify func(ctx context.Context, t *testing.T, at ApplicationTest)
 	SkipComponents bool
 	SkipPods       bool
-	SkipCleanup    bool
 }
 
 type StepExecutor interface {
@@ -218,22 +217,20 @@ func (at ApplicationTest) Test(t *testing.T) {
 	// We run the validation code based on the final step
 	last := at.Steps[len(at.Steps)-1]
 
-	if !last.SkipCleanup {
-		// Cleanup code here will run regardless of pass/fail of subtests
-		t.Logf("deleting %s", at.Description)
-		err = cli.ApplicationDelete(ctx, at.Application)
-		require.NoErrorf(t, err, "failed to delete %s", at.Description)
-		t.Logf("finished deleting %s", at.Description)
+	// Cleanup code here will run regardless of pass/fail of subtests
+	t.Logf("deleting %s", at.Description)
+	err = cli.ApplicationDelete(ctx, at.Application)
+	require.NoErrorf(t, err, "failed to delete %s", at.Description)
+	t.Logf("finished deleting %s", at.Description)
 
-		if last.SkipPods {
-			t.Logf("skipping validation of pods...")
-		} else {
-			t.Logf("validating deletion of pods for %s", at.Description)
-			for _, ns := range at.CollectAllNamespaces() {
-				validation.ValidateNoPodsInNamespace(ctx, t, at.Options.K8sClient, ns)
-			}
-			t.Logf("finished deletion of pods for %s", at.Description)
+	if last.SkipPods {
+		t.Logf("skipping validation of pods...")
+	} else {
+		t.Logf("validating deletion of pods for %s", at.Description)
+		for _, ns := range at.CollectAllNamespaces() {
+			validation.ValidateNoPodsInNamespace(ctx, t, at.Options.K8sClient, ns)
 		}
+		t.Logf("finished deletion of pods for %s", at.Description)
 	}
 
 	// Custom verification is expected to use `t` to trigger its own assertions
