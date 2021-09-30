a1c5536416ce0fc94a29e1dacce5ae57bdbc1db1
Justin Kotalik <jukotali@microsoft.com>
2021-09-30 14:00:38 -0700

    nit

--- pkg/renderers/redisv1alpha3/kubernetesrender_test.go
+++ pkg/renderers/redisv1alpha3/kubernetesrender_test.go
@@ -57,7 +57,6 @@ func Test_Render_Managed_Kubernetes_Success(t *testing.T) {
 
 	t.Run("verify deployment", func(t *testing.T) {
 		require.Equal(t, "test-app.test-redis", deployment.Name)
-		require.Equal(t, "default", deployment.Namespace)
 		require.Equal(t, labels, deployment.Labels)
 		require.Empty(t, deployment.Annotations)
 
@@ -80,7 +79,6 @@ func Test_Render_Managed_Kubernetes_Success(t *testing.T) {
 
 	t.Run("verify service", func(t *testing.T) {
 		require.Equal(t, "test-app.test-redis", service.Name)
-		require.Equal(t, "default", service.Namespace)
 		require.Equal(t, labels, service.Labels)
 		require.Empty(t, service.Annotations)
 
