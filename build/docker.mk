44c147408d0a46d05ad0b4745cbba26f9975dfd9
Nghia Tran <tcnghia@gmail.com>
2021-10-10 11:53:43 -0700

    Use `ko` to build Go docker images

--- build/docker.mk
+++ build/docker.mk
@@ -29,21 +29,61 @@ docker-push-$(1):
 	docker push $(DOCKER_REGISTRY)/$(1)\:$(DOCKER_TAG_VERSION)
 endef
 
-# defines a target for each image
-DOCKER_IMAGES := radius-rp radius-controller
-$(foreach IMAGE,$(DOCKER_IMAGES),$(eval $(call generateDockerTargets,$(IMAGE),.,./deploy/images/$(IMAGE)/Dockerfile)))
-
 # magpie comes from our test directory.
 $(eval $(call generateDockerTargets,magpie,./test/magpie/,./test/magpie/Dockerfile))
 
 # list of 'outputs' to build all images
-DOCKER_BUILD_TARGETS:=$(foreach IMAGE,$(DOCKER_IMAGES),docker-build-$(IMAGE)) docker-build-magpie
+DOCKER_BUILD_TARGETS:=docker-build-magpie
 
 # list of 'outputs' to push all images
-DOCKER_PUSH_TARGETS:=$(foreach IMAGE,$(DOCKER_IMAGES),docker-push-$(IMAGE)) docker-push-magpie
+DOCKER_PUSH_TARGETS:=docker-push-magpie
 
 .PHONY: docker-build
 docker-build: $(DOCKER_BUILD_TARGETS) ## Builds all Docker images.
 
 .PHONY: docker-push
-docker-push: $(DOCKER_PUSH_TARGETS) ## Pushes all Docker images (without building).
+docker-push: $(KO_PUBLISH_TARGETS) docker-push-magpie ## Pushes all Docker images
+
+
+# Generate a target for each image we define
+# Params:
+# $(1): the image name for the target
+define generateKoTargets
+.PHONY: ko-publish-$(1)
+ko-publish-$(1): install-ko
+	@echo "$(ARROW) Building image $(DOCKER_REGISTRY)/$(1)\:$(DOCKER_TAG_VERSION) from $(2)"
+	$(eval $@_KO := $(shell mktemp -d))
+	@echo "builds:" > $($@_KO)/.ko.yaml
+	@echo "- id: $(1)" >> $($@_KO)/.ko.yaml
+	@echo "  main: ./cmd/$(1)" >> $($@_KO)/.ko.yaml
+	@echo "  ldflags:" >> $($@_KO)/.ko.yaml
+	@echo "  - $(LDFLAGS)" >> $($@_KO)/.ko.yaml
+
+	KO_DOCKER_REPO=$(DOCKER_REGISTRY) KO_CONFIG_PATH=$($@_KO) \
+      ko publish ./cmd/$(1) -B --tags $(DOCKER_TAG_VERSION)
+
+	@rm -rf ${$@_KO}
+endef
+
+# Generate an 'alias' for `ko` images
+# Params:
+# $(1): the image name for the target
+define generateAliasTargets
+.PHONY: docker-push-$(1)
+docker-push-$(1): ko-publish-$(1)
+endef
+
+# list of 'outputs' to push all images
+KO_IMAGES:=radius-rp radius-controller
+
+# Generate a ko-publish-x target for each image
+$(foreach IMAGE,$(KO_IMAGES),$(eval $(call generateKoTargets,$(IMAGE))))
+$(foreach IMAGE,$(KO_IMAGES),$(eval $(call generateAliasTargets,$(IMAGE))))
+KO_TARGETS=$(foreach IMAGE,$(KO_IMAGES),ko-publish-$(IMAGE))
+
+.PHONY: ko-publish
+ko-publish: $(KO_TARGETS)
+
+.PHONY: install-ko
+install-ko:
+go install github.com/google/ko:latest
