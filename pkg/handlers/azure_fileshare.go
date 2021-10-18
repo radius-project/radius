54dcae90010fc2e7dad27e9c2f2397e94cbe672a
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2021-10-18 14:00:32 -0700

    Using arm handler

--- pkg/handlers/azure_fileshare.go
+++ pkg/handlers/azure_fileshare.go
@@ -7,16 +7,10 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"fmt"
 
-	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2020-10-01/resources"
-	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2021-04-01/storage"
 	"github.com/Azure/radius/pkg/azure/armauth"
-	"github.com/Azure/radius/pkg/azure/clients"
 	"github.com/Azure/radius/pkg/healthcontract"
-	"github.com/Azure/radius/pkg/resourcemodel"
 )
 
 const (
@@ -44,83 +38,17 @@ func (handler *azureFileShareHandler) Put(ctx context.Context, options *PutOptio
 	if properties[FileShareIDKey] == "" {
 		// TODO
 		// Create managed resource
+		err = errors.New("Managed Azure File share is not yet supported")
 	} else {
-		_, err := handler.GetFileShareByID(ctx, options, properties[FileShareIDKey])
-		if err != nil {
-			return nil, err
-		}
-
-		options.Resource.Identity = resourcemodel.NewARMIdentity(properties[FileShareIDKey], clients.GetAPIVersionFromUserAgent(storage.UserAgent()))
+		armhandler := NewARMHandler(handler.arm)
+		properties, err = armhandler.Put(ctx, options)
 	}
-
-	return properties, nil
-}
-
-func (handler *azureFileShareHandler) GetFileShareByID(ctx context.Context, options *PutOptions, fileshareID string) (*storage.FileShare, error) {
-	// We only support user-managed resources. Do a GET just to validate that the resource exists.
-	if options.Resource.Managed {
-		return nil, fmt.Errorf("ARM handler only supports user-managed resources")
-	}
-
-	id, apiVersion, err := options.Resource.Identity.RequireARM()
-	if err != nil {
-		return nil, err
-	}
-
-	rc := clients.NewGenericResourceClient(handler.arm.SubscriptionID, handler.arm.Auth)
-	resource, err := rc.GetByID(ctx, id, apiVersion)
-	if err != nil {
-		return nil, fmt.Errorf("failed to access resource %q", id)
-	}
-
-	// Return the resource so renderers can use it for computed values.
-	serialized, err := handler.serializeResource(resource)
-	if err != nil {
-		return nil, err
-	}
-	return serialized, nil
-}
-
-func (handler *azureFileShareHandler) serializeResource(resource resources.GenericResource) (*storage.FileShare, error) {
-	b, err := json.Marshal(&resource)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal %T", resource)
-	}
-
-	data := storage.FileShare{}
-	err = json.Unmarshal(b, &data)
-	if err != nil {
-		return nil, errors.New("failed to umarshal resource data")
-	}
-
-	return &data, nil
-}
-
-func (handler *azureFileShareHandler) DeleteFileShare(ctx context.Context, accountName, fileshareName string) error {
-	fc := clients.NewFileSharesClient(handler.arm.SubscriptionID, handler.arm.Auth)
-	_, err := fc.Delete(ctx, handler.arm.ResourceGroup, accountName, fileshareName, "", "")
-	if err != nil {
-		return fmt.Errorf("failed to DELETE file share: %w", err)
-	}
-
-	return nil
+	return properties, err
 }
 
 func (handler *azureFileShareHandler) Delete(ctx context.Context, options DeleteOptions) error {
-	properties := options.ExistingOutputResource.PersistedProperties
-
-	if properties[ManagedKey] != "true" {
-		// For an 'unmanaged' resource we don't need to do anything, just forget it.
-		return nil
-	}
-
-	// Delete Azure File Share
-	err := handler.DeleteFileShare(ctx, properties[FileShareStorageAccountNameKey], properties[FileShareNameKey])
-	if err != nil {
-		return err
-	}
-
-	return nil
+	armHandler := NewARMHandler(handler.arm)
+	return armHandler.Delete(ctx, options)
 }
 
 func NewAzureFileShareHealthHandler(arm armauth.ArmConfig) HealthHandler {
