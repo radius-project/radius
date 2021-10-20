610e5f0303dfbb4366f00b477a21debc1bdcb2a7
Nghia Tran <tcnghia@gmail.com>
2021-10-20 16:03:04 -0700

    Don't build RP & magpie in `controller-deploy` (#1310)
    * Don't build RP & magpie in `controller-deploy`
    
    * Also wait for CRDs
    
    * Have Helm create the namespace if not exist

--- build/controller.mk
+++ build/controller.mk
@@ -10,17 +10,17 @@ controller-run: generate-k8s-manifests generate-controller ## Run the controller
 
 controller-crd-install: generate-k8s-manifests  ## Install CRDs into the K8s cluster specified in ~/.kube/config.
 	kubectl apply -f deploy/Chart/crds/
+	kubectl wait --for condition="established" -f deploy/Chart/crds/
 
 controller-crd-uninstall: generate-k8s-manifests  ## Uninstall CRDs from the K8s cluster specified in ~/.kube/config.
 	kubectl delete -f deploy/Chart/crds/
 
-create-namespace: # Ignore failures from creating a namespace as it may alreadye exist.
-	-kubectl create namespace radius-system
+controller-deploy: docker-build-radius-controller docker-push-radius-controller controller-deploy-existing ## Deploy controller to the K8s cluster specified in ~/.kube/config.
 
-controller-deploy: docker-build docker-push controller-deploy-existing ## Deploy controller to the K8s cluster specified in ~/.kube/config.
-
-controller-deploy-existing: generate-k8s-manifests create-namespace ## Deploy controller to the K8s cluster specified in ~/.kube/config.
-	helm upgrade --wait --install --set container=$(DOCKER_REGISTRY)/radius-controller --set tag=$(DOCKER_TAG_VERSION) radius deploy/Chart -n radius-system
+controller-deploy-existing: generate-k8s-manifests ## Deploy controller to the K8s cluster specified in ~/.kube/config.
+	helm upgrade radius deploy/Chart --wait --install -n radius-system --create-namespace \
+		--set container=$(DOCKER_REGISTRY)/radius-controller \
+		--set tag=$(DOCKER_TAG_VERSION) 
 
 controller-undeploy: generate-k8s-manifests ## Undeploy controller from the K8s cluster specified in ~/.kube/config.
 	helm uninstall radius
