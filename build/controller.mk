226368a8416364b22a8b14d137a018c136c3211c
Nghia Tran <tcnghia@gmail.com>
2021-10-20 13:37:12 -0700

    Also wait for CRDs

--- build/controller.mk
+++ build/controller.mk
@@ -10,6 +10,7 @@ controller-run: generate-k8s-manifests generate-controller ## Run the controller
 
 controller-crd-install: generate-k8s-manifests  ## Install CRDs into the K8s cluster specified in ~/.kube/config.
 	kubectl apply -f deploy/Chart/crds/
+	kubectl wait --for condition="established" -f deploy/Chart/crds/
 
 controller-crd-uninstall: generate-k8s-manifests  ## Uninstall CRDs from the K8s cluster specified in ~/.kube/config.
 	kubectl delete -f deploy/Chart/crds/
