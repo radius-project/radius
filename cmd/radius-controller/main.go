37c757111e6d02ba7f6369d80147c12dfe8cf7fa
Justin Kotalik <jukotali@microsoft.com>
2021-11-23 15:17:33 -0800

    idk...

--- cmd/radius-controller/main.go
+++ cmd/radius-controller/main.go
@@ -6,11 +6,10 @@ package main
 
 import (
 	"context"
-	"flag"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
@@ -21,13 +20,10 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	"github.com/Azure/radius/pkg/hosting"
 	bicepv1alpha3 "github.com/Azure/radius/pkg/kubernetes/api/bicep/v1alpha3"
 	radiusv1alpha3 "github.com/Azure/radius/pkg/kubernetes/api/radius/v1alpha3"
-	"github.com/Azure/radius/pkg/kubernetes/apiserver"
-	"github.com/go-logr/logr"
+	"github.com/gorilla/mux"
 	gatewayv1alpha1 "sigs.k8s.io/gateway-api/apis/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
@@ -49,155 +45,180 @@ func init() {
 }
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
-	// TODO may need to setup server up before setting up manager
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	// var metricsAddr string
+	// var enableLeaderElection bool
+	// var probeAddr string
+	// flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	// flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	// flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+	// 	"Enable leader election for controller manager. "+
+	// 		"Enabling this will ensure there is only one active controller manager.")
+	// opts := zap.Options{
+	// 	Development: true,
+	// }
+	// opts.BindFlags(flag.CommandLine)
+	// flag.Parse()
+	// // TODO may need to setup server up before setting up manager
 
-	// Get certificate from volume mounted environment variable
-	certDir := os.Getenv("TLS_CERT_DIR")
+	// ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-	// apiServerStarted = make(chan struct{})
-	apiServerReady := make(chan struct{})
+	// // Get certificate from volume mounted environment variable
+	// certDir := os.Getenv("TLS_CERT_DIR")
 
-	apiServerOptions := apiserver.APIServerExtensionOptions{
-		KubeConfig: ctrl.GetConfigOrDie(),
-		Scheme:     scheme,
-		Start:      apiServerReady,
-		TLSCertDir: certDir,
-	}
-	apiServer := apiserver.NewAPIServerExtension(setupLog, apiServerOptions)
+	// // apiServerStarted = make(chan struct{})
+	// apiServerReady := make(chan struct{})
 
-	host := hosting.Host{
-		Services: []hosting.Service{
-			apiServer,
-		},
-	}
+	// apiServerOptions := apiserver.APIServerExtensionOptions{
+	// 	KubeConfig: ctrl.GetConfigOrDie(),
+	// 	Scheme:     scheme,
+	// 	Start:      apiServerReady,
+	// 	TLSCertDir: certDir,
+	// }
+	// apiServer := apiserver.NewAPIServerExtension(setupLog, apiServerOptions)
+
+	// host := hosting.Host{
+	// 	Services: []hosting.Service{
+	// 		apiServer,
+	// 	},
+	// }
+
+	// // // Create a channel to handle the shutdown
+	// // exitCh := make(chan os.Signal, 1)
+	// // signal.Notify(exitCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	// // ctx, cancel := context.WithCancel(logr.NewContext(context.Background(), log))
+
+	// // err = kcpRunner.EnsureKcpExecutable(ctx)
+	// // if err != nil {
+	// // 	abort(err, "unable to ensure KCP executable", CannotDownloadKcpExecutable)
+	// // }
 
 	// // Create a channel to handle the shutdown
 	// exitCh := make(chan os.Signal, 1)
 	// signal.Notify(exitCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
-	// ctx, cancel := context.WithCancel(logr.NewContext(context.Background(), log))
+	// ctx, cancel := context.WithCancel(logr.NewContext(context.Background(), ctrl.Log))
 
-	// err = kcpRunner.EnsureKcpExecutable(ctx)
+	// setupLog.Info("Starting server...")
+	// stopped, serviceErrors := host.RunAsync(ctx)
+
+	// close(apiServerReady)
+
+	// // go func() {
+	// // 	<-apiServerStarted
+	// // 	log.Info("Applying CRDs...")
+	// // 	localenv.ApplyCRDs(ctx, getKubeConfigPath(), getCRDDir())
+	// // 	log.Info("CRDs Ready")
+	// // 	close(apiServerReady)
+	// // }()
+
+	// // Sleep TEMP
+	// time.Sleep(time.Second * 5)
+
+	// // mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	// // 	Scheme:                 scheme,
+	// // 	MetricsBindAddress:     metricsAddr,
+	// // 	Port:                   9443,
+	// // 	HealthProbeBindAddress: probeAddr,
+	// // 	LeaderElection:         enableLeaderElection,
+	// // 	LeaderElectionID:       "4fe3568c.radius.dev",
+	// // 	CertDir:                certDir,
+	// // })
+	// // if err != nil {
+	// // 	setupLog.Error(err, "unable to start manager")
+	// // 	os.Exit(1)
+	// // }
+
+	// // unstructuredClient, err := dynamic.NewForConfig(ctrl.GetConfigOrDie())
+	// // if err != nil {
+	// // 	setupLog.Error(err, "unable to create dynamic client")
+	// // 	os.Exit(1)
+	// // }
+
+	// // // Use discovery client to determine GVR for each resource type
+	// // dc, err := discovery.NewDiscoveryClientForConfig(ctrl.GetConfigOrDie())
+	// // if err != nil {
+	// // 	setupLog.Error(err, "unable to create discovery client")
+	// // 	os.Exit(1)
+	// // }
+	// // mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
+
+	// // options := radcontroller.Options{
+	// // 	AppModel:      kubernetesmodel.NewKubernetesModel(mgr.GetClient()),
+	// // 	Client:        mgr.GetClient(),
+	// // 	Dynamic:       unstructuredClient,
+	// // 	Scheme:        mgr.GetScheme(),
+	// // 	Log:           ctrl.Log,
+	// // 	Recorder:      mgr.GetEventRecorderFor("radius"),
+	// // 	RestConfig:    ctrl.GetConfigOrDie(),
+	// // 	RestMapper:    mapper,
+	// // 	ResourceTypes: radcontroller.DefaultResourceTypes,
+	// // 	WatchTypes:    radcontroller.DefaultWatchTypes,
+	// // 	SkipWebhooks:  os.Getenv("SKIP_WEBHOOKS") == "true",
+	// // }
+
+	// // controller := radcontroller.NewRadiusController(&options)
+	// // err = controller.SetupWithManager(mgr)
+	// // if err != nil {
+	// // 	setupLog.Error(err, "unable to create radius controller")
+	// // 	os.Exit(1)
+	// // }
+
+	// // //+kubebuilder:scaffold:builder
+
+	// // if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+	// // 	setupLog.Error(err, "unable to set up health check")
+	// // 	os.Exit(1)
+	// // }
+	// // if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+	// // 	setupLog.Error(err, "unable to set up ready check")
+	// // 	os.Exit(1)
+	// // }
+
+	// // setupLog.Info("starting manager")
+	// // if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	// // 	setupLog.Error(err, "problem running manager")
+	// // 	os.Exit(1)
+	// // }
+
+	// select {
+	// // Normal shutdown
+	// case <-exitCh:
+	// 	setupLog.Info("Shutdown requested..")
+	// 	cancel()
+	// // A service terminated with a failure. Details of the failure have already been logged.
+	// case <-serviceErrors:
+	// 	setupLog.Info("One of the services failed. Shutting down...")
+	// 	cancel()
+	// }
+
+	// // Finished shutting down. An error returned here is a failure to terminate
+	// // gracefully, so just crash if that happens.
+	// err := <-stopped
 	// if err != nil {
-	// 	abort(err, "unable to ensure KCP executable", CannotDownloadKcpExecutable)
+	// 	return
 	// }
+	r := mux.NewRouter()
 
-	// Create a channel to handle the shutdown
-	exitCh := make(chan os.Signal, 1)
-	signal.Notify(exitCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
-	ctx, cancel := context.WithCancel(logr.NewContext(context.Background(), ctrl.Log))
+	r.Path("/apis/api.radius.dev/v1alpha3").Methods("GET").HandlerFunc(reportVersion)
 
-	setupLog.Info("Starting server...")
-	stopped, serviceErrors := host.RunAsync(ctx)
-
-	close(apiServerReady)
-
-	// go func() {
-	// 	<-apiServerStarted
-	// 	log.Info("Applying CRDs...")
-	// 	localenv.ApplyCRDs(ctx, getKubeConfigPath(), getCRDDir())
-	// 	log.Info("CRDs Ready")
-	// 	close(apiServerReady)
-	// }()
-
-	// Sleep TEMP
-	time.Sleep(time.Second * 5)
-
-	// mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-	// 	Scheme:                 scheme,
-	// 	MetricsBindAddress:     metricsAddr,
-	// 	Port:                   9443,
-	// 	HealthProbeBindAddress: probeAddr,
-	// 	LeaderElection:         enableLeaderElection,
-	// 	LeaderElectionID:       "4fe3568c.radius.dev",
-	// 	CertDir:                certDir,
-	// })
-	// if err != nil {
-	// 	setupLog.Error(err, "unable to start manager")
-	// 	os.Exit(1)
-	// }
-
-	// unstructuredClient, err := dynamic.NewForConfig(ctrl.GetConfigOrDie())
-	// if err != nil {
-	// 	setupLog.Error(err, "unable to create dynamic client")
-	// 	os.Exit(1)
-	// }
-
-	// // Use discovery client to determine GVR for each resource type
-	// dc, err := discovery.NewDiscoveryClientForConfig(ctrl.GetConfigOrDie())
-	// if err != nil {
-	// 	setupLog.Error(err, "unable to create discovery client")
-	// 	os.Exit(1)
-	// }
-	// mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
-
-	// options := radcontroller.Options{
-	// 	AppModel:      kubernetesmodel.NewKubernetesModel(mgr.GetClient()),
-	// 	Client:        mgr.GetClient(),
-	// 	Dynamic:       unstructuredClient,
-	// 	Scheme:        mgr.GetScheme(),
-	// 	Log:           ctrl.Log,
-	// 	Recorder:      mgr.GetEventRecorderFor("radius"),
-	// 	RestConfig:    ctrl.GetConfigOrDie(),
-	// 	RestMapper:    mapper,
-	// 	ResourceTypes: radcontroller.DefaultResourceTypes,
-	// 	WatchTypes:    radcontroller.DefaultWatchTypes,
-	// 	SkipWebhooks:  os.Getenv("SKIP_WEBHOOKS") == "true",
-	// }
-
-	// controller := radcontroller.NewRadiusController(&options)
-	// err = controller.SetupWithManager(mgr)
-	// if err != nil {
-	// 	setupLog.Error(err, "unable to create radius controller")
-	// 	os.Exit(1)
-	// }
-
-	// //+kubebuilder:scaffold:builder
-
-	// if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-	// 	setupLog.Error(err, "unable to set up health check")
-	// 	os.Exit(1)
-	// }
-	// if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-	// 	setupLog.Error(err, "unable to set up ready check")
-	// 	os.Exit(1)
-	// }
-
-	// setupLog.Info("starting manager")
-	// if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-	// 	setupLog.Error(err, "problem running manager")
-	// 	os.Exit(1)
-	// }
-
-	select {
-	// Normal shutdown
-	case <-exitCh:
-		setupLog.Info("Shutdown requested..")
-		cancel()
-	// A service terminated with a failure. Details of the failure have already been logged.
-	case <-serviceErrors:
-		setupLog.Info("One of the services failed. Shutting down...")
-		cancel()
+	s := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: r,
+		BaseContext: func(ln net.Listener) context.Context {
+			return context.TODO()
+		},
 	}
 
-	// Finished shutting down. An error returned here is a failure to terminate
-	// gracefully, so just crash if that happens.
-	err := <-stopped
+	err := s.ListenAndServe()
+
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
+	fmt.Println("server stopped")
+}
+
+func reportVersion(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Add("Content-Type", "application/json")
+	_, _ = w.Write([]byte(`{"return":"1.0.0"}`))
 }
