8d11f5e58cce32b2a2350b9a3afbb069d5346286
Ariel Silverman <asilverman@users.noreply.github.com>
2022-01-02 18:38:50 -0800

    retire usage of gopkg.in/yaml.v2 dependency in favor of v3 (#1598)

--- pkg/renderers/volumev1alpha3/azure_keyvault.go
+++ pkg/renderers/volumev1alpha3/azure_keyvault.go
@@ -17,7 +17,7 @@ import (
 	"github.com/Azure/radius/pkg/renderers"
 	"github.com/Azure/secrets-store-csi-driver-provider-azure/pkg/provider"
 	"github.com/gofrs/uuid"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	csidriver "sigs.k8s.io/secrets-store-csi-driver/apis/v1alpha1"
 )
