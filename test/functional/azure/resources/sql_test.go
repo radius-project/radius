5e3ee6bd9a9a8bedf76d9f00c5dfc9bb7ea7b813
Justin Kotalik <jukotali@microsoft.com>
2022-01-27 14:44:55 -0800

    Issue with deploying two sql servers at the same time

--- test/functional/azure/resources/sql_test.go
+++ test/functional/azure/resources/sql_test.go
@@ -18,65 +18,6 @@ import (
 	"github.com/project-radius/radius/test/validation"
 )
 
-func Test_MicrosoftSQL_Unmanaged(t *testing.T) {
-	application := "azure-resources-microsoft-sql-unmanaged"
-	template := "testdata/azure-resources-microsoft-sql-unmanaged.bicep"
-	test := azuretest.NewApplicationTest(t, application, []azuretest.Step{
-		{
-			Executor: azuretest.NewDeployStepExecutor(template),
-			AzureResources: &validation.AzureResourceSet{
-				Resources: []validation.ExpectedResource{
-					{
-						Type: azresources.SqlServers,
-						Tags: map[string]string{
-							"radiustest": "azure-resources-microsoft-sql-unmanaged",
-						},
-						Children: []validation.ExpectedChildResource{
-							{
-								Type:        azresources.SqlServersDatabases,
-								Name:        "cool-database",
-								UserManaged: true,
-							},
-						},
-						UserManaged: true,
-					},
-				},
-			},
-			RadiusResources: &validation.ResourceSet{
-				Resources: []validation.RadiusResource{
-					{
-						ApplicationName: application,
-						ResourceName:    "todoapp",
-						ResourceType:    containerv1alpha3.ResourceType,
-						OutputResources: map[string]validation.ExpectedOutputResource{
-							outputresource.LocalIDDeployment: validation.NewOutputResource(outputresource.LocalIDDeployment, outputresource.TypeKubernetes, resourcekinds.Kubernetes, true, false, rest.OutputResourceStatus{}),
-							outputresource.LocalIDSecret:     validation.NewOutputResource(outputresource.LocalIDSecret, outputresource.TypeKubernetes, resourcekinds.Kubernetes, true, false, rest.OutputResourceStatus{}),
-						},
-					},
-					{
-						ApplicationName: application,
-						ResourceName:    "db",
-						ResourceType:    microsoftsqlv1alpha3.ResourceType,
-						OutputResources: map[string]validation.ExpectedOutputResource{
-							outputresource.LocalIDAzureSqlServer:         validation.NewOutputResource(outputresource.LocalIDAzureSqlServer, outputresource.TypeARM, resourcekinds.AzureSqlServer, false, false, rest.OutputResourceStatus{}),
-							outputresource.LocalIDAzureSqlServerDatabase: validation.NewOutputResource(outputresource.LocalIDAzureSqlServerDatabase, outputresource.TypeARM, resourcekinds.AzureSqlServerDatabase, false, false, rest.OutputResourceStatus{}),
-						},
-					},
-				},
-			},
-			Pods: &validation.K8sObjectSet{
-				Namespaces: map[string][]validation.K8sObject{
-					application: {
-						validation.NewK8sObjectForResource(application, "todoapp"),
-					},
-				},
-			},
-		},
-	})
-
-	test.Test(t)
-}
-
 func Test_MicrosoftSQL_UnmanagedWithoutResourceID(t *testing.T) {
 	application := "azure-resources-microsoft-sql-unmanaged-withoutresourceid"
 	template := "testdata/azure-resources-microsoft-sql-unmanaged-withoutresourceid.bicep"
