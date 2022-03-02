685434ee93868a7c42b942d7ac156a2ab6a5d734
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2022-03-02 12:54:14 -0800

    Update install link (#2002)

--- pkg/cli/k3d/install.go
+++ pkg/cli/k3d/install.go
@@ -14,7 +14,7 @@ import (
 func RequireK3dInstalled() error {
 	_, err := exec.LookPath("k3d")
 	if errors.Is(err, exec.ErrNotFound) {
-		return errors.New("could not find k3d on the system path. Follow installation instructions at: https://rancher.com/docs/k3s/latest/en/installation/install-options/")
+		return errors.New("could not find k3d on the system path. Follow installation instructions at: https://k3d.io/#installation")
 	} else if err != nil {
 		return fmt.Errorf("failed to find k3d: %w", err)
 	}
