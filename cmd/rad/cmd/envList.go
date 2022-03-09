f312a6aabbba87e5aafb6c643af1967f2eefbe59
Bharath Joginapally <bharath@Bharaths-MBP.guest.corp.microsoft.com>
2022-03-09 10:34:50 -0800

    Add json support for env list command
    issue: https://github.com/project-radius/radius/issues/2034

--- cmd/rad/cmd/envList.go
+++ cmd/rad/cmd/envList.go
@@ -6,6 +6,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/project-radius/radius/pkg/cli"
@@ -17,28 +18,38 @@ var envListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List environments",
 	Long:  `List environments`,
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: getEnvConfigs,
+}
 
-		config := ConfigFromContext(cmd.Context())
-		env, err := cli.ReadEnvironmentSection(config)
-		if err != nil {
-			return err
-		}
+func getEnvConfigs(cmd *cobra.Command, args [] string) error {
+	config := ConfigFromContext(cmd.Context())
+	env, err := cli.ReadEnvironmentSection(config)
+	if err != nil {
+		return err
+	}
 
-		if len(env.Items) == 0 {
-			fmt.Println("No environments found. Use 'rad env init' to initialize.")
-			return nil
-		}
-
-		b, err := yaml.Marshal(&env)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println(string(b))
-		fmt.Println()
+	if len(env.Items) == 0 {
+		fmt.Println("No environments found. Use 'rad env init' to initialize.")
 		return nil
-	},
+	}
+
+	printFlag, err := cmd.Flags().GetString("output")
+	if err != nil {
+		return err
+	}
+	var b []byte
+	if(printFlag=="json") {
+		b, err = json.MarshalIndent(&env, "", "  ")
+	} else {
+		b, err = yaml.Marshal(&env)
+	}
+
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+	fmt.Println()
+	return nil
 }
 
 func init() {
