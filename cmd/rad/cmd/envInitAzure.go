3433798b9ed75bd1d58f55ca9ec6c1d796448efa
Mohammad Haveliwala <mhaveliwala@microsoft.com>
2022-03-17 12:31:13 -0500

    updated workflows for -n

--- cmd/rad/cmd/envInitAzure.go
+++ cmd/rad/cmd/envInitAzure.go
@@ -67,7 +67,7 @@ rad env init azure -i
 # Create a Radius environment using flags
 ## If an environment of the same name, resource group, and subscription
 ## already exists Radius will connect to it instead of deploying a new one.
-rad env init azure -e myenv --subscription-id SUB-ID-GUID --resource-group RG-NAME --location westus2
+rad env init azure -n myenv --subscription-id SUB-ID-GUID --resource-group RG-NAME --location westus2
 `,
 	RunE: initAzureRadEnvironment,
 }
