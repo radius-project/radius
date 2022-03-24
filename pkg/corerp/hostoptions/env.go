6ce49903c1b0c7359ad2aa0d4ed4722ac0c3f8b2
Young Bu Park <youngp@microsoft.com>
2022-03-23 18:20:48 -0700

    add more comments

--- pkg/corerp/hostoptions/env.go
+++ pkg/corerp/hostoptions/env.go
@@ -11,8 +11,6 @@ import (
 )
 
 const (
-	ConfigFilePrefix = "rp-setting"
-
 	RadiusDevEnvironment       = "dev"
 	RadiusDogfood              = "df-westus3"
 	RadiusCanaryEastUS2EUAP    = "prod-eastus2euap"
@@ -22,23 +20,28 @@ const (
 
 var currentEnv = RadiusDevEnvironment
 
+// Environment returns the current environment name.
 func Environment() string {
 	return currentEnv
 }
 
+// IsDevelopment returns true if the current environment is development environment.
 func IsDevelopment() bool {
 	return strings.HasPrefix(Environment(), RadiusDevEnvironment)
 }
 
+// IsDogfood returns true if the current environment is dogfood environment.
 func IsDogfood() bool {
 	return Environment() == RadiusDogfood
 }
 
+// IsCanary returns true if the current environment is canary region.
 func IsCanary() bool {
 	env := Environment()
 	return env == RadiusCanaryEastUS2EUAP || env == RadiusCanaryCentralUS2EUAP
 }
 
+// IsProduction returns true if the current environment is production, but not canary.
 func IsProduction() bool {
 	return !IsCanary() && strings.HasPrefix(Environment(), RadiusProdPrefix)
 }
