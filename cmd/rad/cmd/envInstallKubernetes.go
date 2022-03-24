be39ece6707c5193b669d1934d258d41e418543d
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2022-03-24 11:25:08 -0700

    fix tests

--- cmd/rad/cmd/envInstallKubernetes.go
+++ cmd/rad/cmd/envInstallKubernetes.go
@@ -25,4 +25,12 @@ func init() {
 	envInstallKubernetesCmd.Flags().StringP("chart", "", "", "Specify a file path to a helm chart to install radius from")
 	envInstallKubernetesCmd.Flags().String("image", "", "Specify the radius controller image to use")
 	envInstallKubernetesCmd.Flags().String("tag", "", "Specify the radius controller tag to use")
+
+	// Parameters to configure Azure provider for cloud resources
+	envInstallKubernetesCmd.Flags().BoolP("provider-azure", "", false, "Add Azure provider for cloud resources")
+	envInstallKubernetesCmd.Flags().String("provider-azure-subscription", "", "Azure subscription for cloud resources")
+	envInstallKubernetesCmd.Flags().String("provider-azure-resource-group", "", "Azure resource-group for cloud resources")
+	envInstallKubernetesCmd.Flags().StringP("provider-azure-client-id", "", "", "The client id for the service principal")
+	envInstallKubernetesCmd.Flags().StringP("provider-azure-client-secret", "", "", "The client secret for the service principal")
+	envInstallKubernetesCmd.Flags().StringP("provider-azure-tenant-id", "", "", "The tenant id for the service principal")
 }
