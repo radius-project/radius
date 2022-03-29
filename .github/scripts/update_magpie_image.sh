399241c41aec6969da19e08d631dccecbff5c4fe
mishrapratikshya <pratmishra@microsoft.com>
2022-03-29 10:56:58 -0700

    update the command to be GNU compatible

--- .github/scripts/update_magpie_image.sh
+++ .github/scripts/update_magpie_image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -name "*.bicep" -exec sed -i '' -e "s/magpiego:latest/magpiego:${REL_VERSION}/g" {} +
+find . -type f -name "*.bicep" -exec sed -i'' -e "s/magpiego:latest/magpiego:${REL_VERSION}/g" {} +
