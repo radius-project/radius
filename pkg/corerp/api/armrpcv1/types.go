3f908da7485cd4ce28f128281bbcd9cf75e2571a
Young Bu Park <youngp@microsoft.com>
2022-04-04 14:04:35 -0700

    wip

--- pkg/corerp/api/armrpcv1/types.go
+++
@@ -1,9 +0,0 @@
-// ------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-// ------------------------------------------------------------
-
-package armrpcv1
-
-// ResourceTags is the type of ARM tags.
-type ResourceTags map[string]string
