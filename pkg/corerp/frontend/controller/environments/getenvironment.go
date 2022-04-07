a911b9caa8f87d007c963da2711fbe7ae4bc8734
Young Bu Park <youngp@microsoft.com>
2022-04-06 19:29:43 -0700

    cleanups

--- pkg/corerp/frontend/controller/environments/getenvironment.go
+++ pkg/corerp/frontend/controller/environments/getenvironment.go
@@ -9,9 +9,7 @@ import (
 	"context"
 	"net/http"
 
-	v20220315 "github.com/project-radius/radius/pkg/corerp/api/v20220315"
 	ctrl "github.com/project-radius/radius/pkg/corerp/frontend/controller"
-	"github.com/project-radius/radius/pkg/corerp/servicecontext"
 	"github.com/project-radius/radius/pkg/radrp/backend/deployment"
 	"github.com/project-radius/radius/pkg/radrp/db"
 	"github.com/project-radius/radius/pkg/radrp/rest"
@@ -35,26 +33,6 @@ func NewGetEnvironment(db db.RadrpDB, jobEngine deployment.DeploymentProcessor) 
 }
 
 func (e *GetEnvironment) Run(ctx context.Context, req *http.Request) (rest.Response, error) {
-	serviceCtx := servicecontext.ARMRequestContextFromContext(ctx)
-	e.Validate(ctx, req)
-	rID := serviceCtx.ResourceID
-	// TODO: Get the environment resource from datastorage. now return fake data.
-	m := &v20220315.Environment{
-		ID:         rID.ID,
-		Name:       rID.Name(),
-		Type:       rID.Type(),
-		Location:   "West US",
-		SystemData: *serviceCtx.SystemData(),
-		Properties: v20220315.EnvironmentProperties{
-			Compute: v20220315.EnvironmentCompute{
-				Kind:       v20220315.KubernetesComputeKind,
-				ResourceID: "fakeID",
-			},
-		},
-	}
-	return rest.NewOKResponse(m), nil
-}
-
-func (e *GetEnvironment) Validate(ctx context.Context, req *http.Request) error {
-	return nil
+	// TODO: implement create or update environment operation.
+	return rest.NewOKResponse("not implemented"), nil
 }
