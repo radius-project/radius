9abf759a8be688b3cfcedf51593a2608ffc3d746
Young Bu Park <youngp@microsoft.com>
2022-04-11 09:15:38 -0700

    wip

--- pkg/corerp/store/util.go
+++ pkg/corerp/store/util.go
@@ -27,23 +27,25 @@ func NormalizeLocation(location string) string {
 	return NormalizeLocation(location)
 }
 
+var escapedStorageKeys = []string{
+	":00", ":01", ":02", ":03", ":04", ":05", ":06", ":07", ":08", ":09", ":0A", ":0B", ":0C", ":0D", ":0E", ":0F",
+	":10", ":11", ":12", ":13", ":14", ":15", ":16", ":17", ":18", ":19", ":1A", ":1B", ":1C", ":1D", ":1E", ":1F",
+	":20", ":21", ":22", ":23", ":24", ":25", ":26", ":27", ":28", ":29", ":2A", ":2B", ":2C", ":2D", ":2E", ":2F",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":3A", ":3B", ":3C", ":3D", ":3E", ":3F",
+	":40", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+	"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ":5B", ":5C", ":5D", ":5E", ":5F",
+	":60", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+	"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ":7B", ":7C", ":7D", ":7E", ":7F",
+}
+
+const keyDelimit = "-"
+
+func EscapedStorageKey(key string) string {
+
+}
+
 /*
 https://msazure.visualstudio.com/DefaultCollection/One/_git/AzureUX-AZSaaS?path=%2Fsrc%2Fcommon%2Fstorage%2Fhelpers%2Fstorageutility.cs&_a=contents&version=GBmaster
-
-        /// <summary>
-        /// The escaped storage keys.
-        /// </summary>
-        private static readonly string[] EscapedStorageKeys = new string[128]
-        {
-            ":00", ":01", ":02", ":03", ":04", ":05", ":06", ":07", ":08", ":09", ":0A", ":0B", ":0C", ":0D", ":0E", ":0F",
-            ":10", ":11", ":12", ":13", ":14", ":15", ":16", ":17", ":18", ":19", ":1A", ":1B", ":1C", ":1D", ":1E", ":1F",
-            ":20", ":21", ":22", ":23", ":24", ":25", ":26", ":27", ":28", ":29", ":2A", ":2B", ":2C", ":2D", ":2E", ":2F",
-            "0",   "1",   "2",   "3",   "4",   "5",   "6",   "7",   "8",   "9", ":3A", ":3B", ":3C", ":3D", ":3E", ":3F",
-            ":40",   "A",   "B",   "C",   "D",   "E",   "F",   "G",   "H",   "I",   "J",   "K",   "L",   "M",   "N",   "O",
-            "P",   "Q",   "R",   "S",   "T",   "U",   "V",   "W",   "X",   "Y",   "Z", ":5B", ":5C", ":5D", ":5E", ":5F",
-            ":60",   "a",   "b",   "c",   "d",   "e",   "f",   "g",   "h",   "i",   "j",   "k",   "l",   "m",   "n",   "o",
-            "p",   "q",   "r",   "s",   "t",   "u",   "v",   "w",   "x",   "y",   "z", ":7B", ":7C", ":7D", ":7E", ":7F",
-        };
 
 		      /// <summary>
         /// Escapes the storage key.
