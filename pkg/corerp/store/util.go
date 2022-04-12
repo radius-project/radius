d13c3296e8fce864b59b194b34242f51f819d43b
Young Bu Park <youngp@microsoft.com>
2022-04-12 09:03:22 -0700

    wip

--- pkg/corerp/store/util.go
+++ pkg/corerp/store/util.go
@@ -1,8 +1,35 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
 package store
 
-import "strings"
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"unicode"
+)
 
-// https://msazure.visualstudio.com/DefaultCollection/One/_git/AzureUX-ResourceStack?path=/src/common/core/Utilities/NormalizationUtility.cs&_a=contents&version=GBmaster
+var (
+	ErrInvalidKey = errors.New("key includes invalid character")
+)
+
+var escapedStorageKeys = []string{
+	":00", ":01", ":02", ":03", ":04", ":05", ":06", ":07", ":08", ":09", ":0A", ":0B", ":0C", ":0D", ":0E", ":0F",
+	":10", ":11", ":12", ":13", ":14", ":15", ":16", ":17", ":18", ":19", ":1A", ":1B", ":1C", ":1D", ":1E", ":1F",
+	":20", ":21", ":22", ":23", ":24", ":25", ":26", ":27", ":28", ":29", ":2A", ":2B", ":2C", ":2D", ":2E", ":2F",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":3A", ":3B", ":3C", ":3D", ":3E", ":3F",
+	":40", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+	"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ":5B", ":5C", ":5D", ":5E", ":5F",
+	":60", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+	"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ":7B", ":7C", ":7D", ":7E", ":7F",
+}
+
+const (
+	keyDelimiter = "-"
+)
 
 // NormalizeLetterOrDigitToUpper normalizes the value to only letter or digit to upper invariant.
 func NormalizeLetterOrDigitToUpper(s string) string {
@@ -12,8 +39,11 @@ func NormalizeLetterOrDigitToUpper(s string) string {
 
 	sb := strings.Builder{}
 	for _, ch := range s {
-		sb.WriteByte(byte(ch))
+		if unicode.IsDigit(ch) || unicode.IsLetter(ch) {
+			sb.WriteRune(ch)
+		}
 	}
+
 	return strings.ToUpper(sb.String())
 }
 
@@ -27,93 +57,31 @@ func NormalizeLocation(location string) string {
 	return NormalizeLocation(location)
 }
 
-var escapedStorageKeys = []string{
-	":00", ":01", ":02", ":03", ":04", ":05", ":06", ":07", ":08", ":09", ":0A", ":0B", ":0C", ":0D", ":0E", ":0F",
-	":10", ":11", ":12", ":13", ":14", ":15", ":16", ":17", ":18", ":19", ":1A", ":1B", ":1C", ":1D", ":1E", ":1F",
-	":20", ":21", ":22", ":23", ":24", ":25", ":26", ":27", ":28", ":29", ":2A", ":2B", ":2C", ":2D", ":2E", ":2F",
-	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":3A", ":3B", ":3C", ":3D", ":3E", ":3F",
-	":40", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-	"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ":5B", ":5C", ":5D", ":5E", ":5F",
-	":60", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-	"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ":7B", ":7C", ":7D", ":7E", ":7F",
-}
-
-const keyDelimit = "-"
-
 func EscapedStorageKey(key string) string {
-
+	sb := strings.Builder{}
+	for _, ch := range key {
+		if ch < 128 {
+			sb.WriteString(escapedStorageKeys[ch])
+		} else if unicode.IsDigit(ch) || unicode.IsLetter(ch) {
+			sb.WriteRune(ch)
+		} else if ch < 0x100 {
+			sb.WriteRune(':')
+			sb.WriteString(fmt.Sprintf("%02d", ch))
+		} else {
+			sb.WriteRune(':')
+			sb.WriteRune(':')
+			sb.WriteString(fmt.Sprintf("%04d", ch))
+		}
+	}
+	return sb.String()
 }
 
-/*
-https://msazure.visualstudio.com/DefaultCollection/One/_git/AzureUX-AZSaaS?path=%2Fsrc%2Fcommon%2Fstorage%2Fhelpers%2Fstorageutility.cs&_a=contents&version=GBmaster
+func CombineStorageKeys(keys ...string) (string, error) {
+	for _, key := range keys {
+		if strings.Contains(key, keyDelimiter) {
+			return "", ErrInvalidKey
+		}
+	}
 
-		      /// <summary>
-        /// Escapes the storage key.
-        /// </summary>
-        /// <param name="storageKey">The storage key.</param>
-        public static string EscapeStorageKey(string storageKey)
-        {
-            var sb = new StringBuilder(storageKey.Length);
-            for (var index = 0; index < storageKey.Length; ++index)
-            {
-                var c = storageKey[index];
-                if (c < 128)
-                {
-                    sb.Append(StorageUtility.EscapedStorageKeys[c]);
-                }
-                else if (char.IsLetterOrDigit(c))
-                {
-                    sb.Append(c);
-                }
-                else if (c < 0x100)
-                {
-                    sb.Append(':');
-                    sb.Append(((int)c).ToString("X2", CultureInfo.InvariantCulture));
-                }
-                else
-                {
-                    sb.Append(':');
-                    sb.Append(':');
-                    sb.Append(((int)c).ToString("X4", CultureInfo.InvariantCulture));
-                }
-            }
-
-            return sb.ToString();
-
-			/// <summary>
-        /// Combines the storage keys.
-        /// </summary>
-        /// <param name="keys">The storage keys.</param>
-        public static string CombineStorageKeys(params string[] keys)
-        {
-            foreach (var key in keys)
-            {
-                if (key.Contains('-', StringComparison.InvariantCulture))
-                {
-                    throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "The storage key '{0}' is not properly encoded. Use CloudTableExtensions.EscapeStorageKey for encoding.", key), nameof(keys));
-                }
-            }
-
-            return string.Join("-", keys);
-        }
-
-        /// <summary>
-        /// Combines the storage keys.
-        /// </summary>
-        /// <param name="keys0">The keys0.</param>
-        /// <param name="keys1">The keys1.</param>
-        public static string CombineStorageKeys(string keys0, string keys1)
-        {
-            if (keys0.Contains('-', StringComparison.InvariantCulture))
-            {
-                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "The storage key '{0}' is not properly encoded. Use CloudTableExtensions.EscapeStorageKey for encoding.", keys0), nameof(keys0));
-            }
-
-            if (keys1.Contains('-', StringComparison.InvariantCulture))
-            {
-                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "The storage key '{0}' is not properly encoded. Use CloudTableExtensions.EscapeStorageKey for encoding.", keys1), nameof(keys1));
-            }
-
-            return string.Concat(keys0, "-", keys1);
-        }
-*/
+	return strings.Join(keys, keyDelimiter), nil
+}
