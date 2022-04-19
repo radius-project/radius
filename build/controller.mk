7f48f46e86be3b6d84f17a4baff4fdd9a227eef5
Will Smith <willsmith@microsoft.com>
2022-04-19 09:18:24 -0700

    Removing changes from controller makefile

--- build/controller.mk
+++ build/controller.mk
@@ -5,9 +5,9 @@
 
 ##@ Controller
 
-kubernetes-deploy: docker-build-radius-rp docker-push-radius-rp kubernetes-deploy-existing ## Deploy controller to the K8s cluster specified in ~/.kube/config.
+kubernetes-deploy: docker-build-radius-rp docker-push-radius-rp controller-deploy-existing ## Deploy controller to the K8s cluster specified in ~/.kube/config.
 
-kubernetes-deploy-existing: ## Deploy controller to the K8s cluster specified in ~/.kube/config.
+kubernetes-deploy-existing: generate-k8s-manifests ## Deploy controller to the K8s cluster specified in ~/.kube/config.
 	go run ./cmd/rad/main.go env init kubernetes --chart deploy/Chart/ --image $(DOCKER_REGISTRY)/radius-rp --tag $(DOCKER_TAG_VERSION) 
 
 kubernetes-undeploy: ## Uninstall controller from the K8s cluster specified in ~/.kube/config.
