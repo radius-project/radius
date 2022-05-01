7351f4d71748141a4e50aa23cc77696cbf97e2ed
Bharath Joginapally <bharath@fkarakus-ws2.redmond.corp.microsoft.com>
2022-05-01 15:45:45 -0700

    Fix unit test

--- pkg/corerp/dataprovider/storageprovider_test.go
+++ pkg/corerp/dataprovider/storageprovider_test.go
@@ -16,7 +16,7 @@ func TestNormalizeResourceType(t *testing.T) {
 		in  string
 		out string
 	}{
-		{"applications.core/environments", "applicationscoÒre-environments"},
+		{"applications.core/environments", "applicationscore-environments"},
 		{"applications.core/provider", "applicationscore-provider"},
 		{"applications.connector/provider", "applicationsconnector-provider"},
 	}
