f6b3f49d49803660ac81b8327b854dadf0fcf157
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-05-18 11:03:13 -0700

    Adding data model, conversion tests

---
+++ pkg/corerp/datamodel/converter/gateway_converter_test.go
@@ -0,0 +1,88 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package converter
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/project-radius/radius/pkg/basedatamodel"
+	"github.com/project-radius/radius/pkg/corerp/api/v20220315privatepreview"
+	"github.com/project-radius/radius/pkg/corerp/datamodel"
+	"github.com/stretchr/testify/require"
+)
+
+// NOTE: this test is to validate the type conversion between versioned model and data model.
+// Converted content must be tested in ConvertFrom and ConvertTo tests in api models under /pkg/api/[api-version].
+
+func TestGatewayDataModelToVersioned(t *testing.T) {
+	testset := []struct {
+		dataModelFile string
+		apiVersion    string
+		apiModelType  interface{}
+		err           error
+	}{
+		{
+			"../../api/v20220315privatepreview/testdata/gatewayresourcedatamodel.json",
+			"2022-03-15-privatepreview",
+			&v20220315privatepreview.GatewayResource{},
+			nil,
+		},
+		{
+			"",
+			"unsupported",
+			nil,
+			basedatamodel.ErrUnsupportedAPIVersion,
+		},
+	}
+
+	for _, tc := range testset {
+		t.Run(tc.apiVersion, func(t *testing.T) {
+			c := loadTestData(tc.dataModelFile)
+			dm := &datamodel.Gateway{}
+			_ = json.Unmarshal(c, dm)
+			am, err := GatewayDataModelToVersioned(dm, tc.apiVersion)
+			if tc.err != nil {
+				require.ErrorAs(t, tc.err, &err)
+			} else {
+				require.NoError(t, err)
+				require.IsType(t, tc.apiModelType, am)
+			}
+		})
+	}
+}
+
+func TestGatewayDataModelFromVersioned(t *testing.T) {
+	testset := []struct {
+		versionedModelFile string
+		apiVersion         string
+		err                error
+	}{
+		{
+			"../../api/v20220315privatepreview/testdata/gatewayresource.json",
+			"2022-03-15-privatepreview",
+			nil,
+		},
+		{
+			"",
+			"unsupported",
+			basedatamodel.ErrUnsupportedAPIVersion,
+		},
+	}
+
+	for _, tc := range testset {
+		t.Run(tc.apiVersion, func(t *testing.T) {
+			c := loadTestData(tc.versionedModelFile)
+			dm, err := GatewayDataModelFromVersioned(c, tc.apiVersion)
+			if tc.err != nil {
+				require.ErrorAs(t, tc.err, &err)
+			} else {
+				require.NoError(t, err)
+				require.IsType(t, tc.apiVersion, dm.InternalMetadata.UpdatedAPIVersion)
+			}
+		})
+	}
+}
