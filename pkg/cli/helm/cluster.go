a22c4d7254a30b5ce440aa2f72e61d68a89b94a1
Ruokun (Tommy) Niu <t-tommyniu@microsoft.com>
2022-05-24 09:25:50 -0700

    Install Open Service Mesh as part of rad env init kubernetes (#2364)
    * Created osmclient.go, setup imports, consts and osmOptions struct
    
    * Completed ApplyOsmHelmChart function
    
    Next: work on runOsmHelmInstall
    Double-check with Justin on some of the details/logic
    
    * changed all namespaces to "radius-system"
    
    * Completed the install and uninstall functions for osm
    
    * modified cluster.go so that rad env init will use the osm client to install osm control plane
    
    * Updated the correct chart version for OSM
    
    * Updated the uninstallation function for osm
    
    * Deleted the commented/unused imports in osmclient.go
    
    * Updated the OSM variable names and modified output
    
    Since OSM is an acronym, we need to change all 'Osm' instances into 'OSM'
    
    * Modified the files based on the reviews
    
    - Removed unused "_embed" imports from all clients
    - Removed unnecessary comment
    - Ensured the consistency in output logs/error messages
    
    * Decreased the resource limits and requests for multiple pods
    
    Currently, some of the checks are failing due to insufficient CPU. I am decreasing the cpu limit and request of some other pods in this commit.
    
    * Removed the configuration that caused parsing error
    
    * A test commit to see if resource allocation was modified
    
    * Implemented a function that modifies OSM's resource values (cpu and memory) using the MergeMaps function
    
    * Updated the units in resource definition;
    
    * Reverted the previous changes to the resources
    
    * Reverted the yaml files correctly (modified based on the main branch)
    
    * Delet helm_test.go. This file was used for local testing only
    
    * Attempt to use the upgrade command (with -install flag) instead of the install command
    
    * Modified the installation function
    
    * Was previously running into a memory issue. Changed the memory for the requests to be the same as the limit

--- pkg/cli/helm/cluster.go
+++ pkg/cli/helm/cluster.go
@@ -20,6 +20,7 @@ import (
 const (
 	ContourChartDefaultVersion = "7.4.6"
 	DaprDefaultVersion         = "1.6.0"
+	OSMDefaultVersion          = "0.11.1"
 )
 
 type ClusterOptions struct {
@@ -27,6 +28,7 @@ type ClusterOptions struct {
 	Dapr      DaprOptions
 	Contour   ContourOptions
 	Radius    RadiusOptions
+	OSM       OSMOptions
 }
 
 func NewDefaultClusterOptions() ClusterOptions {
@@ -53,6 +55,9 @@ func NewDefaultClusterOptions() ClusterOptions {
 		Radius: RadiusOptions{
 			ChartVersion: chartVersion,
 			Tag:          tag,
+		},
+		OSM: OSMOptions{
+			ChartVersion: OSMDefaultVersion,
 		},
 	}
 }
@@ -129,6 +134,11 @@ func InstallOnCluster(ctx context.Context, options ClusterOptions, client client
 		return err
 	}
 
+	err = ApplyOSMHelmChart(options.OSM)
+	if err != nil {
+		return err
+	}
+
 	return err
 }
 
@@ -157,6 +167,11 @@ func UninstallOnCluster(kubeContext string) error {
 	}
 
 	err = RunRadiusHelmUninstall(helmConf)
+	if err != nil {
+		return err
+	}
+
+	err = RunOSMHelmUninstall(helmConf)
 	if err != nil {
 		return err
 	}
