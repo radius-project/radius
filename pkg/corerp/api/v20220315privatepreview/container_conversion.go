ab3e3424c8621e474c5402092ac762f4e94e5b0c
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-05-25 12:37:03 -0700

    conversion changes

--- pkg/corerp/api/v20220315privatepreview/container_conversion.go
+++ pkg/corerp/api/v20220315privatepreview/container_conversion.go
@@ -16,6 +16,41 @@ import (
 // ConvertTo converts from the versioned Container resource to version-agnostic datamodel.
 func (src *ContainerResource) ConvertTo() (api.DataModelInterface, error) {
 	// Note: SystemData conversion isn't required since this property comes ARM and datastore.
+
+	connections := make(map[string]datamodel.ConnectionProperties)
+	for key, val := range src.Properties.Connections {
+		roles := []string{}
+		var kind datamodel.Kind
+		if val != nil {
+			if val.Iam != nil {
+				for _, r := range val.Iam.Roles {
+					roles = append(roles, to.String(r))
+				}
+
+				kind = toKindDataModel(val.Iam.Kind)
+			}
+
+			connections[key] = datamodel.ConnectionProperties{
+				Source:                to.String(val.Source),
+				DisableDefaultEnvVars: to.Bool(val.DisableDefaultEnvVars),
+				Iam: datamodel.IamProperties{
+					Kind:  kind,
+					Roles: roles,
+				},
+			}
+		}
+	}
+	/*
+		probe := ConvertToHealthProbePropertiesClassification(src.Properties.Container.LivenessProbe)
+
+		converted_probe := datamodel.HealthProbeProperties{
+			Kind:                to.String(probe.Kind),
+			FailureThreshold:    to.Float32(probe.FailureThreshold),
+			InitialDelaySeconds: to.Float32(probe.InitialDelaySeconds),
+			PeriodSeconds:       to.Float32(probe.PeriodSeconds),
+		}
+	*/
+
 	converted := &datamodel.ContainerResource{
 		TrackedResource: basedatamodel.TrackedResource{
 			ID:       to.String(src.ID),
@@ -27,6 +62,11 @@ func (src *ContainerResource) ConvertTo() (api.DataModelInterface, error) {
 		Properties: datamodel.ContainerProperties{
 			ProvisioningState: toProvisioningStateDataModel(src.Properties.ProvisioningState),
 			Application:       to.String(src.Properties.Application),
+			Connections:       connections,
+			Container: datamodel.Container{
+				Image: to.String(src.Properties.Container.Image),
+				Env:   to.StringMap(src.Properties.Container.Env),
+			},
 		},
 		InternalMetadata: basedatamodel.InternalMetadata{
 			UpdatedAPIVersion: Version,
@@ -54,4 +94,30 @@ func (dst *ContainerResource) ConvertFrom(src api.DataModelInterface) error {
 	}
 
 	return nil
+}
+
+/*
+func ConvertToHealthProbePropertiesClassification(i interface{}) datamodel.HealthProbePropertiesClassification {
+	var p datamodel.HealthProbePropertiesClassification
+	switch i.(type) {
+	case ExecHealthProbeProperties:
+	case HTTPGetHealthProbeProperties:
+	case TCPHealthProbeProperties:
+	default:
+	}
+	return p
+}
+*/
+
+func toKindDataModel(kind *Kind) datamodel.Kind {
+	if kind == nil {
+		return datamodel.KindAzure // TODO: need to define default kind
+	}
+
+	switch *kind {
+	case KindAzure:
+		return datamodel.KindAzure
+	default:
+		return datamodel.KindAzure
+	}
 }
