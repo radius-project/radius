9b47da368d0ddc5f905a1f6c82944d3663ffe44e
Lakshmi Javadekar <lakshmi@Lakshmis-MacBook-Pro.local>
2022-05-26 11:12:36 -0700

    updated per review comments

--- cmd/rad/cmd/envDelete.go
+++ cmd/rad/cmd/envDelete.go
@@ -76,7 +76,7 @@ func deleteEnv(cmd *cobra.Command, args []string) error {
 		// 2. Delete all radius resources in the customer/user resource group (ex custom resource provider)
 		// 3. Delete control plane resource group
 		if err = deleteAllApplications(cmd.Context(), az); err != nil {
-			if envErr, ok := err.(*radclient.RadiusError); ok && envErr.Code == "EnvironmentNotFound" {
+			if err, ok := err.(*radclient.RadiusError); ok && err.Code == "EnvironmentNotFound" {
 				output.LogInfo("Environment '%s' not found", az.Name)
 
 				errDelConfig := deleteEnvFromConfig(cmd.Context(), config, env.GetName())
