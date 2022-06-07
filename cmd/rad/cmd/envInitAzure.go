ba076adffd26b87c1445a35c207e0313447bcf28
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2022-06-06 22:36:36 -0700

    remove ucp in azure pipeline

--- cmd/rad/cmd/envInitAzure.go
+++ cmd/rad/cmd/envInitAzure.go
@@ -107,8 +107,6 @@ func initAzureRadEnvironment(cmd *cobra.Command, args []string) error {
 			ChartPath: a.ChartPath,
 			Image:     a.Image,
 			Tag:       a.Tag,
-			UCPImage:  a.UCPImage,
-			UCPTag:    a.UCPTag,
 		},
 	}
 	clusterOptions := helm.NewClusterOptions(cliOptions)
@@ -143,8 +141,6 @@ func init() {
 	envInitAzureCmd.Flags().StringP("chart", "", "", "Specify a file path to a helm chart to install radius from")
 	envInitAzureCmd.Flags().String("image", "", "Specify the radius controller image to use")
 	envInitAzureCmd.Flags().String("tag", "", "Specify the radius controller tag to use")
-	envInitAzureCmd.Flags().String("ucp-image", "", "specify the UCP image to use")
-	envInitAzureCmd.Flags().String("ucp-tag", "", "specify the UCP tag to use")
 
 	// development support
 	envInitAzureCmd.Flags().StringP("deployment-template", "t", "", "The file path to the deployment template - this can be used to override a custom build of the environment deployment ARM template for testing")
@@ -163,8 +159,6 @@ type arguments struct {
 	Namespace               string
 	Image                   string
 	Tag                     string
-	UCPImage                string
-	UCPTag                  string
 }
 
 func validate(cmd *cobra.Command, args []string) (arguments, error) {
@@ -249,16 +243,6 @@ func validate(cmd *cobra.Command, args []string) (arguments, error) {
 		return arguments{}, err
 	}
 
-	ucpImage, err := cmd.Flags().GetString("ucp-image")
-	if err != nil {
-		return arguments{}, err
-	}
-
-	ucpTag, err := cmd.Flags().GetString("ucp-tag")
-	if err != nil {
-		return arguments{}, err
-	}
-
 	if location != "" && !isSupportedLocation(location) {
 		return arguments{}, fmt.Errorf("the location '%s' is not supported. choose from: %s", location, strings.Join(supportedLocations[:], ", "))
 	}
@@ -276,8 +260,6 @@ func validate(cmd *cobra.Command, args []string) (arguments, error) {
 		ChartPath:               chartPath,
 		Image:                   image,
 		Tag:                     tag,
-		UCPImage:                ucpImage,
-		UCPTag:                  ucpTag,
 	}, nil
 }
 
