21c75d4fdc39fd3dedc98d3eabd941bd43a6242e
Young Bu Park <youngp@microsoft.com>
2022-06-07 16:41:57 -0700

    update connector rp

--- pkg/connectorrp/frontend/handler/routes.go
+++ pkg/connectorrp/frontend/handler/routes.go
@@ -27,18 +27,18 @@ func AddRoutes(ctx context.Context, sp dataprovider.DataStorageProvider, router 
 		root = router.PathPrefix(pathBase).Subrouter()
 	}
 
-	subscriptionRt := router
+	scopeRoute := router
 	if !hostoptions.IsSelfHosted() {
-		subscriptionRt = router.PathPrefix(pathBase + "/subscriptions/{subscriptionID}").Subrouter()
+		scopeRoute = router.PathPrefix(pathBase + "/subscriptions/{subscriptionID}").Subrouter()
 	}
 
 	// Configure the default ARM handlers.
-	err := server.ConfigureDefaultHandlers(ctx, sp, root, subscriptionRt, ProviderNamespaceName, NewGetOperations)
+	err := server.ConfigureDefaultHandlers(ctx, sp, root, scopeRoute, hostoptions.IsSelfHosted(), ProviderNamespaceName, NewGetOperations)
 	if err != nil {
 		return err
 	}
 
-	mongoRTSubrouter := subscriptionRt.PathPrefix("/resourcegroups/{resourceGroup}/providers/applications.connector/mongodatabases").
+	mongoRTSubrouter := scopeRoute.PathPrefix("/resourcegroups/{resourceGroup}/providers/applications.connector/mongodatabases").
 		Queries(server.APIVersionParam, "{"+server.APIVersionParam+"}").Subrouter()
 	mongoResourceRouter := mongoRTSubrouter.PathPrefix("/{mongoDatabases}").Subrouter()
 	handlerOptions := []server.HandlerOptions{
