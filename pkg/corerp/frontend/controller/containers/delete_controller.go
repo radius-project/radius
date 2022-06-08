53fb2f624ae6d26bbb5504f3e3af1aac46c4527a
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-06-08 11:45:39 -0700

    Adding Delete container operation controller

---
+++ pkg/corerp/frontend/controller/containers/delete_controller.go
@@ -0,0 +1,61 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package environments
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	manager "github.com/project-radius/radius/pkg/armrpc/asyncoperation/statusmanager"
+	ctrl "github.com/project-radius/radius/pkg/armrpc/frontend/controller"
+	"github.com/project-radius/radius/pkg/armrpc/servicecontext"
+	"github.com/project-radius/radius/pkg/corerp/datamodel"
+	"github.com/project-radius/radius/pkg/radrp/rest"
+	"github.com/project-radius/radius/pkg/ucp/store"
+)
+
+var _ ctrl.Controller = (*DeleteController)(nil)
+
+// DeleteController is the controller implementation to delete container resource.
+type DeleteController struct {
+	ctrl.BaseController
+}
+
+// NewDeleteController creates a new Delete Container Controller.
+func NewDeleteController(ds store.StorageClient, sm manager.StatusManager) (ctrl.Controller, error) {
+	return &DeleteController{ctrl.NewBaseController(ds, sm)}, nil
+}
+
+func (e *DeleteController) Run(ctx context.Context, req *http.Request) (rest.Response, error) {
+	serviceCtx := servicecontext.ARMRequestContextFromContext(ctx)
+
+	// Read resource metadata from the storage
+	existingResource := &datamodel.Container{}
+	etag, err := e.GetResource(ctx, serviceCtx.ResourceID.String(), existingResource)
+	if err != nil && !errors.Is(&store.ErrNotFound{}, err) {
+		return nil, err
+	}
+
+	if etag == "" {
+		return rest.NewNoContentResponse(), nil
+	}
+
+	err = ctrl.ValidateETag(*serviceCtx, etag)
+	if err != nil {
+		return rest.NewPreconditionFailedResponse(serviceCtx.ResourceID.String(), err.Error()), nil
+	}
+
+	err = e.DataStore.Delete(ctx, serviceCtx.ResourceID.String())
+	if err != nil {
+		if errors.Is(&store.ErrNotFound{}, err) {
+			return rest.NewNoContentResponse(), nil
+		}
+		return nil, err
+	}
+
+	return rest.NewOKResponse(nil), nil
+}
