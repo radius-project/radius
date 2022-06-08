eafc38b1773119c3955cc62f39f50164fef0ef45
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-06-08 11:47:55 -0700

    Adding Get container operation controller

---
+++ pkg/corerp/frontend/controller/containers/get_controller.go
@@ -0,0 +1,45 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package environments
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	manager "github.com/project-radius/radius/pkg/armrpc/asyncoperation/statusmanager"
+	ctrl "github.com/project-radius/radius/pkg/armrpc/frontend/controller"
+	"github.com/project-radius/radius/pkg/armrpc/servicecontext"
+	"github.com/project-radius/radius/pkg/corerp/datamodel"
+	"github.com/project-radius/radius/pkg/corerp/datamodel/converter"
+	"github.com/project-radius/radius/pkg/radrp/rest"
+	"github.com/project-radius/radius/pkg/ucp/store"
+)
+
+var _ ctrl.Controller = (*GetController)(nil)
+
+// GetController is the controller implementation to get the container resource.
+type GetController struct {
+	ctrl.BaseController
+}
+
+// NewGetController creates a new GetController.
+func NewGetController(ds store.StorageClient, sm manager.StatusManager) (ctrl.Controller, error) {
+	return &GetController{ctrl.NewBaseController(ds, sm)}, nil
+}
+
+func (e *GetController) Run(ctx context.Context, req *http.Request) (rest.Response, error) {
+	serviceCtx := servicecontext.ARMRequestContextFromContext(ctx)
+
+	existingResource := &datamodel.Container{}
+	_, err := e.GetResource(ctx, serviceCtx.ResourceID.String(), existingResource)
+	if err != nil && errors.Is(&store.ErrNotFound{}, err) {
+		return rest.NewNotFoundResponse(serviceCtx.ResourceID), nil
+	}
+
+	versioned, _ := converter.ContainerDataModelToVersioned(existingResource, serviceCtx.APIVersion)
+	return rest.NewOKResponse(versioned), nil
+}
