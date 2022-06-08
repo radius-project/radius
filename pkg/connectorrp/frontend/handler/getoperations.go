8b1ce5491a698ed7d847215151d8e0135a93dbc1
Vishwanath Hiremath <vhiremath@microsoft.com>
2022-06-08 13:24:25 -0700

    Adding Controllers for dapr resources

--- pkg/connectorrp/frontend/handler/getoperations.go
+++ pkg/connectorrp/frontend/handler/getoperations.go
@@ -106,6 +106,96 @@ func (opctrl *GetOperations) availableOperationsV1() *v1.PaginatedList {
 				},
 				IsDataAction: false,
 			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprInvokeHttpRoutes/read",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprInvokeHttpRoutes",
+					Operation:   "Get/List daprInvokeHttpRoutes",
+					Description: "Gets/Lists daprInvokeHttpRoutes connector(s).",
+				},
+				IsDataAction: false,
+			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprInvokeHttpRoutes/write",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprInvokeHttpRoutes",
+					Operation:   "Create/Update daprInvokeHttpRoutes",
+					Description: "Creates or updates a mdaprInvokeHttpRoute connector.",
+				},
+				IsDataAction: false,
+			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprInvokeHttpRoutes/delete",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprInvokeHttpRoutes",
+					Operation:   "Delete daprInvokeHttpRoute",
+					Description: "Deletes a daprInvokeHttpRoute connector.",
+				},
+				IsDataAction: false,
+			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprSecretStores/read",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprSecretStores",
+					Operation:   "Get/List daprSecretStores",
+					Description: "Gets/Lists daprSecretStore connector(s).",
+				},
+				IsDataAction: false,
+			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprSecretStores/write",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprSecretStores",
+					Operation:   "Create/Update daprSecretStores",
+					Description: "Creates or updates a daprSecretStore connector.",
+				},
+				IsDataAction: false,
+			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprSecretStores/delete",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprSecretStores",
+					Operation:   "Delete daprSecretStore",
+					Description: "Deletes a daprSecretStore connector.",
+				},
+				IsDataAction: false,
+			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprStateStores/read",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprStateStores",
+					Operation:   "Get/List daprStateStores",
+					Description: "Gets/Lists daprStateStore connector(s).",
+				},
+				IsDataAction: false,
+			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprStateStores/write",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprStateStores",
+					Operation:   "Create/Update daprStateStores",
+					Description: "Creates or updates a daprStateStore connector.",
+				},
+				IsDataAction: false,
+			},
+			&v1.Operation{
+				Name: "Applications.Connector/daprStateStores/delete",
+				Display: &v1.OperationDisplayProperties{
+					Provider:    ProviderNamespaceName,
+					Resource:    "daprStateStores",
+					Operation:   "Delete daprStateStore",
+					Description: "Deletes a daprStateStore connector.",
+				},
+				IsDataAction: false,
+			},
 		},
 	}
 }
