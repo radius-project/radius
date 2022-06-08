847a3fa463402bc8954996b3491bf1fa31349c76
Young Bu Park <youngp@microsoft.com>
2022-06-07 19:50:57 -0700

    support appcore-rp images and tags

--- cmd/rad/cmd/envInitDev.go
+++ cmd/rad/cmd/envInitDev.go
@@ -59,6 +59,16 @@ func initDevRadEnvironment(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	appcoreImage, err := cmd.Flags().GetString("appcore-image")
+	if err != nil {
+		return err
+	}
+
+	appcoreTag, err := cmd.Flags().GetString("appcore-tag")
+	if err != nil {
+		return err
+	}
+
 	ucpImage, err := cmd.Flags().GetString("ucp-image")
 	if err != nil {
 		return err
@@ -105,11 +115,13 @@ func initDevRadEnvironment(cmd *cobra.Command, args []string) error {
 	cliOptions := helm.ClusterOptions{
 		Namespace: namespace,
 		Radius: helm.RadiusOptions{
-			ChartPath: chartPath,
-			Image:     image,
-			Tag:       tag,
-			UCPImage:  ucpImage,
-			UCPTag:    ucpTag,
+			ChartPath:    chartPath,
+			Image:        image,
+			Tag:          tag,
+			UCPImage:     ucpImage,
+			UCPTag:       ucpTag,
+			AppCoreImage: appcoreImage,
+			AppCoreTag:   appcoreTag,
 		},
 	}
 	options := helm.NewClusterOptions(cliOptions)
