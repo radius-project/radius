7347e44bf69f0f180a86f9beaf6bd8d437d6cdd6
Young Bu Park <youngp@microsoft.com>
2022-06-08 18:16:50 -0700

    add more tests

--- pkg/connectorrp/frontend/handler/routes_test.go
+++ pkg/connectorrp/frontend/handler/routes_test.go
@@ -17,62 +17,78 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestHandlers(t *testing.T) {
-	handlerTests := []struct {
-		url    string
-		method string
-	}{
-		{
-			url:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/testrg/providers/applications.connector/mongodatabases?api-version=2022-03-15-privatepreview",
-			method: http.MethodGet,
-		}, {
-			url:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo0?api-version=2022-03-15-privatepreview",
-			method: http.MethodPut,
-		}, {
-			url:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo0?api-version=2022-03-15-privatepreview",
-			method: http.MethodPatch,
-		}, {
-			url:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo0?api-version=2022-03-15-privatepreview",
-			method: http.MethodDelete,
-		}, {
-			url:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo0?api-version=2022-03-15-privatepreview",
-			method: http.MethodDelete,
-		}, {
-			url:    "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo0/listsecrets?api-version=2022-03-15-privatepreview",
-			method: http.MethodPost,
-		}, {
-			url:    "/providers/applications.connector/operations?api-version=2022-03-15-privatepreview",
-			method: http.MethodGet,
-		}, {
-			url:    "/subscriptions/00000000-0000-0000-0000-000000000000?api-version=2.0",
-			method: http.MethodPut,
-		},
-	}
+var handlerTests = []struct {
+	url        string
+	method     string
+	isAzureAPI bool
+}{
+	{
+		url:        "/resourcegroups/testrg/providers/applications.connector/mongodatabases?api-version=2022-03-15-privatepreview",
+		method:     http.MethodGet,
+		isAzureAPI: false,
+	}, {
+		url:        "/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo?api-version=2022-03-15-privatepreview",
+		method:     http.MethodPut,
+		isAzureAPI: false,
+	}, {
+		url:        "/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo?api-version=2022-03-15-privatepreview",
+		method:     http.MethodPatch,
+		isAzureAPI: false,
+	}, {
+		url:        "/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo?api-version=2022-03-15-privatepreview",
+		method:     http.MethodDelete,
+		isAzureAPI: false,
+	}, {
+		url:        "/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo?api-version=2022-03-15-privatepreview",
+		method:     http.MethodDelete,
+		isAzureAPI: false,
+	}, {
+		url:        "/resourcegroups/testrg/providers/applications.connector/mongodatabases/mongo/listsecrets?api-version=2022-03-15-privatepreview",
+		method:     http.MethodPost,
+		isAzureAPI: false,
+	}, {
+		url:        "/providers/applications.connector/operations?api-version=2022-03-15-privatepreview",
+		method:     http.MethodGet,
+		isAzureAPI: true,
+	}, {
+		url:        "/subscriptions/00000000-0000-0000-0000-000000000000?api-version=2.0",
+		method:     http.MethodPut,
+		isAzureAPI: true,
+	},
+}
 
+func TestHandlers(t *testing.T) {
 	mctrl := gomock.NewController(t)
 	defer mctrl.Finish()
 
 	mockSP := dataprovider.NewMockDataStorageProvider(mctrl)
 	mockSC := store.NewMockStorageClient(mctrl)
 
-	mockSC.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Return(&store.Object{
-		Data: map[string]interface{}{
-			"name": "mongo0",
-			"properties": map[string]interface{}{
-				"provisioningState": "Updating",
-			},
-		},
-	}, nil).AnyTimes()
+	mockSC.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Return(&store.Object{}, nil).AnyTimes()
 	mockSC.EXPECT().Save(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
 	mockSP.EXPECT().GetStorageClient(gomock.Any(), gomock.Any()).Return(store.StorageClient(mockSC), nil).AnyTimes()
 
+	assertRouters(t, "", true, mockSP)
+	assertRouters(t, "/api.ucp.dev", false, mockSP)
+}
+
+func assertRouters(t *testing.T, pathBase string, isARM bool, mockSP *dataprovider.MockDataStorageProvider) {
 	r := mux.NewRouter()
-	err := AddRoutes(context.Background(), mockSP, r, "", true)
+	err := AddRoutes(context.Background(), mockSP, r, pathBase, isARM)
 	require.NoError(t, err)
 
 	for _, tt := range handlerTests {
-		t.Run(tt.url, func(t *testing.T) {
-			req, _ := http.NewRequestWithContext(context.Background(), tt.method, "http://localhost"+tt.url, nil)
+		if !isARM && tt.isAzureAPI {
+			continue
+		}
+
+		uri := "http://localhost" + pathBase + tt.url
+		if isARM && !tt.isAzureAPI {
+			uri = "http://localhost" + pathBase + "/subscriptions/00000000-0000-0000-0000-000000000000" + tt.url
+		}
+
+		t.Run(uri, func(t *testing.T) {
+			req, _ := http.NewRequestWithContext(context.Background(), tt.method, uri, nil)
 			var match mux.RouteMatch
 			require.True(t, r.Match(req, &match))
 		})
