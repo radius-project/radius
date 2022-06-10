99b0e15064d5257469693cb416533f8cd0d00d4a
Young Bu Park <youngp@microsoft.com>
2022-06-10 09:56:27 -0700

    Create QueueProvider and refactor service (#2562)
    * Create QueueProvider to support multiple queues
    * Refactor Service by creating BaseService to remove the duplicated initialization codes

--- pkg/armrpc/frontend/server/handler.go
+++ pkg/armrpc/frontend/server/handler.go
@@ -36,13 +36,13 @@ type HandlerOptions struct {
 	HandlerFactory ControllerFunc
 }
 
-func RegisterHandler(ctx context.Context, sp dataprovider.DataStorageProvider, opts HandlerOptions) error {
+func RegisterHandler(ctx context.Context, sp dataprovider.DataStorageProvider, sm manager.StatusManager, opts HandlerOptions) error {
 	sc, err := sp.GetStorageClient(ctx, opts.ResourceType)
 	if err != nil {
 		return err
 	}
 
-	ctrl, err := opts.HandlerFactory(sc, nil)
+	ctrl, err := opts.HandlerFactory(sc, sm)
 	if err != nil {
 		return err
 	}
@@ -70,17 +70,18 @@ func RegisterHandler(ctx context.Context, sp dataprovider.DataStorageProvider, o
 func ConfigureDefaultHandlers(
 	ctx context.Context,
 	sp dataprovider.DataStorageProvider,
+	sm manager.StatusManager,
 	rootRouter *mux.Router,
 	pathBase string,
 	isAzureProvider bool,
 	providerNamespace string,
 	operationCtrlFactory ControllerFunc) error {
 	providerNamespace = strings.ToLower(providerNamespace)
-	rt := fmt.Sprintf("%s/provider", providerNamespace)
+	rt := providerNamespace + "/provider"
 
 	if isAzureProvider {
 		// https://github.com/Azure/azure-resource-manager-rpc/blob/master/v1.0/proxy-api-reference.md#exposing-available-operations
-		err := RegisterHandler(ctx, sp, HandlerOptions{
+		err := RegisterHandler(ctx, sp, sm, HandlerOptions{
 			ParentRouter:   rootRouter.Path(fmt.Sprintf("/providers/%s/operations", providerNamespace)).Queries(APIVersionParam, "{"+APIVersionParam+"}").Subrouter(),
 			ResourceType:   rt,
 			Method:         v1.OperationGet,
@@ -90,7 +91,7 @@ func ConfigureDefaultHandlers(
 			return err
 		}
 		// https://github.com/Azure/azure-resource-manager-rpc/blob/master/v1.0/subscription-lifecycle-api-reference.md#creating-or-updating-a-subscription
-		err = RegisterHandler(ctx, sp, HandlerOptions{
+		err = RegisterHandler(ctx, sp, sm, HandlerOptions{
 			ParentRouter:   rootRouter.Path(pathBase).Queries(APIVersionParam, "{"+APIVersionParam+"}").Subrouter(),
 			ResourceType:   rt,
 			Method:         v1.OperationPut,
@@ -101,10 +102,11 @@ func ConfigureDefaultHandlers(
 		}
 	}
 
+	statusRT := providerNamespace + "/operationstatuses"
 	opStatus := fmt.Sprintf("%s/providers/%s/locations/{location}/operationstatuses/{operationId}", pathBase, providerNamespace)
-	err := RegisterHandler(ctx, sp, HandlerOptions{
+	err := RegisterHandler(ctx, sp, sm, HandlerOptions{
 		ParentRouter:   rootRouter.Path(opStatus).Queries(APIVersionParam, "{"+APIVersionParam+"}").Subrouter(),
-		ResourceType:   rt,
+		ResourceType:   statusRT,
 		Method:         v1.OperationGetOperationStatuses,
 		HandlerFactory: default_ctrl.NewGetOperationStatus,
 	})
@@ -113,9 +115,9 @@ func ConfigureDefaultHandlers(
 	}
 
 	opResult := fmt.Sprintf("%s/providers/%s/locations/{location}/operationresults/{operationId}", pathBase, providerNamespace)
-	err = RegisterHandler(ctx, sp, HandlerOptions{
+	err = RegisterHandler(ctx, sp, sm, HandlerOptions{
 		ParentRouter:   rootRouter.Path(opResult).Queries(APIVersionParam, "{"+APIVersionParam+"}").Subrouter(),
-		ResourceType:   rt,
+		ResourceType:   statusRT,
 		Method:         v1.OperationGetOperationResult,
 		HandlerFactory: default_ctrl.NewGetOperationResult,
 	})
--- pkg/corerp/backend/service.go
+++ pkg/corerp/backend/service.go
@@ -9,31 +9,29 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/go-logr/logr"
 	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
-	sm "github.com/project-radius/radius/pkg/armrpc/asyncoperation/statusmanager"
 	"github.com/project-radius/radius/pkg/armrpc/asyncoperation/worker"
 	"github.com/project-radius/radius/pkg/armrpc/hostoptions"
-	"github.com/project-radius/radius/pkg/queue/inmemory"
-	"github.com/project-radius/radius/pkg/ucp/dataprovider"
 
 	containers_ctrl "github.com/project-radius/radius/pkg/corerp/backend/controller/containers"
 )
 
 const (
-	providerName         = "Applications.Core"
-	providerResourceType = providerName + "/provider"
+	providerName = "Applications.Core"
 )
 
 // Service is a service to run AsyncReqeustProcessWorker.
 type Service struct {
-	options hostoptions.HostOptions
+	worker.Service
 }
 
 // NewService creates new service instance to run AsyncReqeustProcessWorker.
 func NewService(options hostoptions.HostOptions) *Service {
 	return &Service{
-		options: options,
+		worker.Service{
+			ProviderName: providerName,
+			Options:      options,
+		},
 	}
 }
 
@@ -44,39 +42,19 @@ func (w *Service) Name() string {
 
 // Run starts the service and worker.
 func (w *Service) Run(ctx context.Context) error {
-	logger := logr.FromContextOrDiscard(ctx)
+	if err := w.Init(ctx); err != nil {
+		return err
+	}
 
-	sp := dataprovider.NewStorageProvider(w.options.Config.StorageProvider)
-	ctx = hostoptions.WithContext(ctx, w.options.Config)
-
-	// Register async operation controllers.
-	controllers := worker.NewControllerRegistry(sp)
-	err := controllers.Register(
+	// Register controllers
+	err := w.Controllers.Register(
 		ctx,
-		v1.OperationType{Type: containers_ctrl.ResourceTypeName, Method: v1.OperationGet},
+		containers_ctrl.ResourceTypeName,
+		v1.OperationPut,
 		containers_ctrl.NewUpdateContainer)
 	if err != nil {
 		panic(err)
 	}
 
-	// Create Async operation manager.
-	sc, err := sp.GetStorageClient(ctx, providerResourceType)
-	if err != nil {
-		panic(err)
-	}
-	asyncOpManager := sm.New(sc, nil, providerName, w.options.Config.Env.RoleLocation)
-
-	// TODO: Make it configurable.
-	queue := inmemory.NewClient(nil)
-
-	// Create and start worker.
-	worker := worker.New(worker.Options{}, asyncOpManager, queue, controllers)
-
-	logger.Info("Start Worker...")
-	if err := worker.Start(ctx); err != nil {
-		logger.Error(err, "failed to start worker...")
-	}
-
-	logger.Info("Sorker stopped...")
-	return nil
+	return w.Start(ctx, worker.Options{})
 }
