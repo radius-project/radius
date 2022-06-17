b7df6333a486654552b2a8185e21343d6dec17ef
Bharath Joginapally <bharath@Bharaths-MacBook-Pro.local>
2022-06-17 11:01:30 -0700

    Remove transformer for cli output

--- pkg/cli/objectformats/objectformats.go
+++ pkg/cli/objectformats/objectformats.go
@@ -74,17 +74,6 @@ func GetResourceTableFormat() output.FormatterOptions {
 			{
 				Heading:  "TYPE",
 				JSONPath: "{ .Type }",
-				Transformer: func(t string) string {
-					tokens := strings.Split(t, "/")
-					// For Radius resource types only show last part of the resource type. Example: mongo.com.MongoDatabase instead of Microsoft.CustomProviders/mongo.com.MongoDatabase
-					// For non-Radius resources types, show full resource type, Microsoft.ServiceBus/namespaces for example.
-					// TODO: "Microsoft.CustomProviders" should be updated to reflect Radius RP name once we move out of custom RP mode:
-					// https://github.com/project-radius/radius/issues/1534
-					if tokens[0] == "Microsoft.CustomProviders" {
-						return tokens[len(tokens)-1]
-					}
-					return t
-				},
 			},
 		},
 	}
