0811ddab70194ba24c87be12ec907607f2cca904
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2022-06-21 15:45:15 -0700

    temp fix

--- pkg/renderers/redisv1alpha3/render_test.go
+++ pkg/renderers/redisv1alpha3/render_test.go
@@ -10,9 +10,13 @@ import (
 	"testing"
 
 	"github.com/go-logr/logr"
+	"github.com/project-radius/radius/pkg/handlers"
 	"github.com/project-radius/radius/pkg/radlogger"
+	"github.com/project-radius/radius/pkg/radrp/outputresource"
 	"github.com/project-radius/radius/pkg/renderers"
+	"github.com/project-radius/radius/pkg/resourcekinds"
 	"github.com/stretchr/testify/require"
+	"gotest.tools/assert"
 )
 
 const (
@@ -29,147 +33,147 @@ func createContext(t *testing.T) context.Context {
 	return logr.NewContext(context.Background(), logger)
 }
 
-// func Test_Render_Kubernetes_Success(t *testing.T) {
-// 	ctx := createContext(t)
-// 	renderer := Renderer{}
+func Test_Render_Kubernetes_Success(t *testing.T) {
+	ctx := createContext(t)
+	renderer := Renderer{}
 
-// 	input := renderers.RendererResource{
-// 		ApplicationName: "test-app",
-// 		ResourceName:    "test-redis",
-// 		ResourceType:    ResourceType,
-// 		Definition: map[string]interface{}{
-// 			"host": "hello.com",
-// 			"port": 1234,
-// 			"secrets": map[string]interface{}{
-// 				"connectionString": "cs***",
-// 				"password":         "pwd***",
-// 			},
-// 		},
-// 	}
-// 	output, err := renderer.Render(ctx, renderers.RenderOptions{
-// 		Resource:     input,
-// 		Dependencies: map[string]renderers.RendererDependency{},
-// 	})
-// 	require.NoError(t, err)
+	input := renderers.RendererResource{
+		ApplicationName: "test-app",
+		ResourceName:    "test-redis",
+		ResourceType:    ResourceType,
+		Definition: map[string]interface{}{
+			"host": "hello.com",
+			"port": 1234,
+			"secrets": map[string]interface{}{
+				"connectionString": "cs***",
+				"password":         "pwd***",
+			},
+		},
+	}
+	output, err := renderer.Render(ctx, renderers.RenderOptions{
+		Resource:     input,
+		Dependencies: map[string]renderers.RendererDependency{},
+	})
+	require.NoError(t, err)
 
-// 	expected := renderers.RendererOutput{
-// 		Resources: []outputresource.OutputResource{},
-// 		ComputedValues: map[string]renderers.ComputedValueReference{
-// 			"host": {
-// 				Value: to.StringPtr("hello.com"),
-// 			},
-// 			"port": {
-// 				Value: to.Int32Ptr(1234),
-// 			},
-// 			"username": {
-// 				Value: "",
-// 			},
-// 		},
-// 		SecretValues: map[string]renderers.SecretValueReference{
-// 			"password": {
-// 				Value: "pwd***",
-// 			},
-// 			"connectionString": {
-// 				Value: "cs***",
-// 			},
-// 		},
-// 	}
-// 	assert.DeepEqual(t, expected, output)
-// }
+	expected := renderers.RendererOutput{
+		Resources: []outputresource.OutputResource{},
+		ComputedValues: map[string]renderers.ComputedValueReference{
+			"host": {
+				Value: "hello.com",
+			},
+			"port": {
+				Value: "1234",
+			},
+			"username": {
+				Value: "",
+			},
+		},
+		SecretValues: map[string]renderers.SecretValueReference{
+			"password": {
+				Value: "pwd***",
+			},
+			"connectionString": {
+				Value: "cs***",
+			},
+		},
+	}
+	assert.DeepEqual(t, expected, output)
+}
 
-// func Test_Render_Azure_Success(t *testing.T) {
-// 	ctx := createContext(t)
-// 	renderer := Renderer{}
+func Test_Render_Azure_Success(t *testing.T) {
+	ctx := createContext(t)
+	renderer := Renderer{}
 
-// 	resource := renderers.RendererResource{
-// 		ApplicationName: applicationName,
-// 		ResourceName:    resourceName,
-// 		ResourceType:    ResourceType,
-// 		Definition: map[string]interface{}{
-// 			"resource": "/subscriptions/test-sub/resourceGroups/test-group/providers/Microsoft.Cache/Redis/test-redis",
-// 			"host":     "localhost",
-// 			"port":     42,
-// 		},
-// 	}
+	resource := renderers.RendererResource{
+		ApplicationName: applicationName,
+		ResourceName:    resourceName,
+		ResourceType:    ResourceType,
+		Definition: map[string]interface{}{
+			"resource": "/subscriptions/test-sub/resourceGroups/test-group/providers/Microsoft.Cache/Redis/test-redis",
+			"host":     "localhost",
+			"port":     42,
+		},
+	}
 
-// 	output, err := renderer.Render(ctx, renderers.RenderOptions{Resource: resource, Dependencies: map[string]renderers.RendererDependency{}})
-// 	require.NoError(t, err)
+	output, err := renderer.Render(ctx, renderers.RenderOptions{Resource: resource, Dependencies: map[string]renderers.RendererDependency{}})
+	require.NoError(t, err)
 
-// 	require.Len(t, output.Resources, 1)
+	require.Len(t, output.Resources, 1)
 
-// 	require.Equal(t, outputresource.LocalIDAzureRedis, output.Resources[0].LocalID)
-// 	require.Equal(t, resourcekinds.AzureRedis, output.Resources[0].ResourceType.Type)
+	require.Equal(t, outputresource.LocalIDAzureRedis, output.Resources[0].LocalID)
+	require.Equal(t, resourcekinds.AzureRedis, output.Resources[0].ResourceType.Type)
 
-// 	expectedProperties := map[string]string{
-// 		handlers.RedisResourceIdKey: "/subscriptions/test-sub/resourceGroups/test-group/providers/Microsoft.Cache/Redis/test-redis",
-// 		handlers.RedisNameKey:       resourceName,
-// 	}
-// 	require.Equal(t, expectedProperties, output.Resources[0].Resource)
+	expectedProperties := map[string]string{
+		handlers.RedisResourceIdKey: "/subscriptions/test-sub/resourceGroups/test-group/providers/Microsoft.Cache/Redis/test-redis",
+		handlers.RedisNameKey:       resourceName,
+	}
+	require.Equal(t, expectedProperties, output.Resources[0].Resource)
 
-// 	expectedComputedValues := map[string]renderers.ComputedValueReference{
-// 		"host": {
-// 			Value: to.StringPtr("localhost"),
-// 		},
-// 		"port": {
-// 			Value: to.Int32Ptr(42),
-// 		},
-// 		"username": {
-// 			LocalID:           "AzureRedis",
-// 			PropertyReference: "redisusername",
-// 		},
-// 	}
-// 	require.Equal(t, expectedComputedValues, output.ComputedValues)
-// 	require.Equal(t, "/primaryKey", output.SecretValues[renderers.PasswordStringHolder].ValueSelector)
-// 	require.Equal(t, "listKeys", output.SecretValues[renderers.PasswordStringHolder].Action)
-// }
+	expectedComputedValues := map[string]renderers.ComputedValueReference{
+		"host": {
+			Value: "localhost",
+		},
+		"port": {
+			Value: "42",
+		},
+		"username": {
+			LocalID:           "AzureRedis",
+			PropertyReference: "redisusername",
+		},
+	}
+	require.Equal(t, expectedComputedValues, output.ComputedValues)
+	require.Equal(t, "/primaryKey", output.SecretValues[renderers.PasswordStringHolder].ValueSelector)
+	require.Equal(t, "listKeys", output.SecretValues[renderers.PasswordStringHolder].Action)
+}
 
-// func Test_Render_Azure_User_Secrets(t *testing.T) {
-// 	ctx := createContext(t)
-// 	renderer := Renderer{}
+func Test_Render_Azure_User_Secrets(t *testing.T) {
+	ctx := createContext(t)
+	renderer := Renderer{}
 
-// 	expectedComputedValues := map[string]renderers.ComputedValueReference{
-// 		"host": {
-// 			Value: to.StringPtr("localhost"),
-// 		},
-// 		"port": {
-// 			Value: to.Int32Ptr(42),
-// 		},
-// 		"username": {
-// 			Value: "",
-// 		},
-// 	}
+	expectedComputedValues := map[string]renderers.ComputedValueReference{
+		"host": {
+			Value: "localhost",
+		},
+		"port": {
+			Value: "42",
+		},
+		"username": {
+			Value: "",
+		},
+	}
 
-// 	expectedSecretValues := map[string]renderers.SecretValueReference{
-// 		renderers.PasswordStringHolder: {
-// 			Value: "deadbeef",
-// 		},
-// 		renderers.ConnectionStringValue: {
-// 			Value: "admin:deadbeef@localhost:42",
-// 		},
-// 	}
+	expectedSecretValues := map[string]renderers.SecretValueReference{
+		renderers.PasswordStringHolder: {
+			Value: "deadbeef",
+		},
+		renderers.ConnectionStringValue: {
+			Value: "admin:deadbeef@localhost:42",
+		},
+	}
 
-// 	resource := renderers.RendererResource{
-// 		ApplicationName: applicationName,
-// 		ResourceName:    resourceName,
-// 		ResourceType:    ResourceType,
-// 		Definition: map[string]interface{}{
-// 			"host": "localhost",
-// 			"port": 42,
-// 			"secrets": map[string]string{
-// 				renderers.PasswordStringHolder:  "deadbeef",
-// 				renderers.ConnectionStringValue: "admin:deadbeef@localhost:42",
-// 			},
-// 		},
-// 	}
+	resource := renderers.RendererResource{
+		ApplicationName: applicationName,
+		ResourceName:    resourceName,
+		ResourceType:    ResourceType,
+		Definition: map[string]interface{}{
+			"host": "localhost",
+			"port": 42,
+			"secrets": map[string]string{
+				renderers.PasswordStringHolder:  "deadbeef",
+				renderers.ConnectionStringValue: "admin:deadbeef@localhost:42",
+			},
+		},
+	}
 
-// 	output, err := renderer.Render(ctx, renderers.RenderOptions{Resource: resource, Dependencies: map[string]renderers.RendererDependency{}})
-// 	require.NoError(t, err)
+	output, err := renderer.Render(ctx, renderers.RenderOptions{Resource: resource, Dependencies: map[string]renderers.RendererDependency{}})
+	require.NoError(t, err)
 
-// 	require.Len(t, output.Resources, 0)
+	require.Len(t, output.Resources, 0)
 
-// 	require.Equal(t, expectedComputedValues, output.ComputedValues)
-// 	require.Equal(t, expectedSecretValues, output.SecretValues)
-// }
+	require.Equal(t, expectedComputedValues, output.ComputedValues)
+	require.Equal(t, expectedSecretValues, output.SecretValues)
+}
 
 func Test_Render_Azure_NoResourceSpecified(t *testing.T) {
 	ctx := createContext(t)
