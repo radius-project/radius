34e0c87747ff22be786877396ec954d137d5bd26
Vishwanath Hiremath <100623239+vishwahiremat@users.noreply.github.com>
2022-06-21 11:00:01 -0700

    Connector controllers for Dapr resources (#2578)
    * Adding Controllers for dapr resources
    
    * adding routes and routing tests
    
    * fix for unit test failure

---
+++ pkg/connectorrp/frontend/controller/daprstatestores/deletedaprstatestore.go
@@ -0,0 +1,64 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package daprstatestores
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	manager "github.com/project-radius/radius/pkg/armrpc/asyncoperation/statusmanager"
+	ctrl "github.com/project-radius/radius/pkg/armrpc/frontend/controller"
+	"github.com/project-radius/radius/pkg/armrpc/servicecontext"
+	"github.com/project-radius/radius/pkg/connectorrp/datamodel"
+	"github.com/project-radius/radius/pkg/radrp/rest"
+	"github.com/project-radius/radius/pkg/ucp/store"
+)
+
+var _ ctrl.Controller = (*DeleteDaprStateStore)(nil)
+
+// DeleteDaprStateStore is the controller implementation to delete daprStateStore connector resource.
+type DeleteDaprStateStore struct {
+	ctrl.BaseController
+}
+
+// NewDeleteDaprStateStore creates a new instance DeleteDaprStateStore.
+func NewDeleteDaprStateStore(ds store.StorageClient, sm manager.StatusManager) (ctrl.Controller, error) {
+	return &DeleteDaprStateStore{ctrl.NewBaseController(ds, sm)}, nil
+}
+
+func (daprStateStore *DeleteDaprStateStore) Run(ctx context.Context, req *http.Request) (rest.Response, error) {
+	serviceCtx := servicecontext.ARMRequestContextFromContext(ctx)
+
+	// Read resource metadata from the storage
+	existingResource := &datamodel.DaprStateStore{}
+	etag, err := daprStateStore.GetResource(ctx, serviceCtx.ResourceID.String(), existingResource)
+	if err != nil {
+		if errors.Is(&store.ErrNotFound{}, err) {
+			return rest.NewNoContentResponse(), nil
+		}
+		return nil, err
+	}
+
+	if etag == "" {
+		return rest.NewNoContentResponse(), nil
+	}
+
+	err = ctrl.ValidateETag(*serviceCtx, etag)
+	if err != nil {
+		return rest.NewPreconditionFailedResponse(serviceCtx.ResourceID.String(), err.Error()), nil
+	}
+
+	err = daprStateStore.DataStore.Delete(ctx, serviceCtx.ResourceID.String())
+	if err != nil {
+		if errors.Is(&store.ErrNotFound{}, err) {
+			return rest.NewNoContentResponse(), nil
+		}
+		return nil, err
+	}
+
+	return rest.NewOKResponse(nil), nil
+}
--- pkg/connectorrp/frontend/handler/routes.go
+++ pkg/connectorrp/frontend/handler/routes.go
@@ -14,6 +14,10 @@ import (
 	"github.com/project-radius/radius/pkg/armrpc/frontend/server"
 	"github.com/project-radius/radius/pkg/ucp/dataprovider"
 
+	daprHttpRoute_ctrl "github.com/project-radius/radius/pkg/connectorrp/frontend/controller/daprinvokehttproutes"
+	daprPubSub_ctrl "github.com/project-radius/radius/pkg/connectorrp/frontend/controller/daprpubsubbrokers"
+	daprSecretStore_ctrl "github.com/project-radius/radius/pkg/connectorrp/frontend/controller/daprsecretstores"
+	daprStateStore_ctrl "github.com/project-radius/radius/pkg/connectorrp/frontend/controller/daprstatestores"
 	mongo_ctrl "github.com/project-radius/radius/pkg/connectorrp/frontend/controller/mongodatabases"
 	rabbitmq_ctrl "github.com/project-radius/radius/pkg/connectorrp/frontend/controller/rabbitmqmessagequeues"
 	redis_ctrl "github.com/project-radius/radius/pkg/connectorrp/frontend/controller/rediscaches"
@@ -38,6 +42,22 @@ func AddRoutes(ctx context.Context, sp dataprovider.DataStorageProvider, sm mana
 	mongoRTSubrouter := router.NewRoute().PathPrefix(pathBase+"/resourcegroups/{resourceGroup}/providers/applications.connector/mongodatabases").
 		Queries(server.APIVersionParam, "{"+server.APIVersionParam+"}").Subrouter()
 	mongoResourceRouter := mongoRTSubrouter.PathPrefix("/{mongoDatabases}").Subrouter()
+
+	daprHttpRouteRTSubrouter := router.NewRoute().PathPrefix(pathBase+"/resourcegroups/{resourceGroup}/providers/applications.connector/daprinvokehttproutes").
+		Queries(server.APIVersionParam, "{"+server.APIVersionParam+"}").Subrouter()
+	daprHttpRouteResourceRouter := daprHttpRouteRTSubrouter.PathPrefix("/{daprInvokeHttpRoutes}").Subrouter()
+
+	daprPubSubRTSubrouter := router.NewRoute().PathPrefix(pathBase+"/resourcegroups/{resourceGroup}/providers/applications.connector/daprpubsubbrokers").
+		Queries(server.APIVersionParam, "{"+server.APIVersionParam+"}").Subrouter()
+	daprPubSubResourceRouter := daprPubSubRTSubrouter.PathPrefix("/{daprPubSubBrokers}").Subrouter()
+
+	daprSecretStoreRTSubrouter := router.NewRoute().PathPrefix(pathBase+"/resourcegroups/{resourceGroup}/providers/applications.connector/daprsecretstores").
+		Queries(server.APIVersionParam, "{"+server.APIVersionParam+"}").Subrouter()
+	daprSecretStoreResourceRouter := daprSecretStoreRTSubrouter.PathPrefix("/{daprSecretStores}").Subrouter()
+
+	daprStateStoreRTSubrouter := router.NewRoute().PathPrefix(pathBase+"/resourcegroups/{resourceGroup}/providers/applications.connector/daprstatestores").
+		Queries(server.APIVersionParam, "{"+server.APIVersionParam+"}").Subrouter()
+	daprStateStoreResourceRouter := daprStateStoreRTSubrouter.PathPrefix("/{daprStateStores}").Subrouter()
 
 	redisRTSubrouter := router.NewRoute().PathPrefix(pathBase+"/resourcegroups/{resourceGroup}/providers/applications.connector/rediscaches").
 		Queries(server.APIVersionParam, "{"+server.APIVersionParam+"}").Subrouter()
@@ -86,6 +106,126 @@ func AddRoutes(ctx context.Context, sp dataprovider.DataStorageProvider, sm mana
 			ResourceType:   mongo_ctrl.ResourceTypeName,
 			Method:         mongo_ctrl.OperationListSecret,
 			HandlerFactory: mongo_ctrl.NewListSecretsMongoDatabase,
+		},
+		{
+			ParentRouter:   daprHttpRouteRTSubrouter,
+			ResourceType:   daprHttpRoute_ctrl.ResourceTypeName,
+			Method:         v1.OperationList,
+			HandlerFactory: daprHttpRoute_ctrl.NewListDaprInvokeHttpRoutes,
+		},
+		{
+			ParentRouter:   daprHttpRouteResourceRouter,
+			ResourceType:   daprHttpRoute_ctrl.ResourceTypeName,
+			Method:         v1.OperationGet,
+			HandlerFactory: daprHttpRoute_ctrl.NewGetDaprInvokeHttpRoute,
+		},
+		{
+			ParentRouter:   daprHttpRouteResourceRouter,
+			ResourceType:   daprHttpRoute_ctrl.ResourceTypeName,
+			Method:         v1.OperationPut,
+			HandlerFactory: daprHttpRoute_ctrl.NewCreateOrUpdateDaprInvokeHttpRoute,
+		},
+		{
+			ParentRouter:   daprHttpRouteResourceRouter,
+			ResourceType:   daprHttpRoute_ctrl.ResourceTypeName,
+			Method:         v1.OperationPatch,
+			HandlerFactory: daprHttpRoute_ctrl.NewCreateOrUpdateDaprInvokeHttpRoute,
+		},
+		{
+			ParentRouter:   daprHttpRouteResourceRouter,
+			ResourceType:   daprHttpRoute_ctrl.ResourceTypeName,
+			Method:         v1.OperationDelete,
+			HandlerFactory: daprHttpRoute_ctrl.NewDeleteDaprInvokeHttpRoute,
+		},
+		{
+			ParentRouter:   daprPubSubRTSubrouter,
+			ResourceType:   daprPubSub_ctrl.ResourceTypeName,
+			Method:         v1.OperationList,
+			HandlerFactory: daprPubSub_ctrl.NewListDaprPubSubBrokers,
+		},
+		{
+			ParentRouter:   daprPubSubResourceRouter,
+			ResourceType:   daprPubSub_ctrl.ResourceTypeName,
+			Method:         v1.OperationGet,
+			HandlerFactory: daprPubSub_ctrl.NewGetDaprPubSubBroker,
+		},
+		{
+			ParentRouter:   daprPubSubResourceRouter,
+			ResourceType:   daprPubSub_ctrl.ResourceTypeName,
+			Method:         v1.OperationPut,
+			HandlerFactory: daprPubSub_ctrl.NewCreateOrUpdateDaprPubSubBroker,
+		},
+		{
+			ParentRouter:   daprPubSubResourceRouter,
+			ResourceType:   daprPubSub_ctrl.ResourceTypeName,
+			Method:         v1.OperationPatch,
+			HandlerFactory: daprPubSub_ctrl.NewCreateOrUpdateDaprPubSubBroker,
+		},
+		{
+			ParentRouter:   daprPubSubResourceRouter,
+			ResourceType:   daprPubSub_ctrl.ResourceTypeName,
+			Method:         v1.OperationDelete,
+			HandlerFactory: daprPubSub_ctrl.NewDeleteDaprPubSubBroker,
+		},
+		{
+			ParentRouter:   daprSecretStoreRTSubrouter,
+			ResourceType:   daprSecretStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationList,
+			HandlerFactory: daprSecretStore_ctrl.NewListDaprSecretStores,
+		},
+		{
+			ParentRouter:   daprSecretStoreResourceRouter,
+			ResourceType:   daprSecretStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationGet,
+			HandlerFactory: daprSecretStore_ctrl.NewGetDaprSecretStore,
+		},
+		{
+			ParentRouter:   daprSecretStoreResourceRouter,
+			ResourceType:   daprSecretStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationPut,
+			HandlerFactory: daprSecretStore_ctrl.NewCreateOrUpdateDaprSecretStore,
+		},
+		{
+			ParentRouter:   daprSecretStoreResourceRouter,
+			ResourceType:   daprSecretStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationPatch,
+			HandlerFactory: daprSecretStore_ctrl.NewCreateOrUpdateDaprSecretStore,
+		},
+		{
+			ParentRouter:   daprSecretStoreResourceRouter,
+			ResourceType:   daprSecretStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationDelete,
+			HandlerFactory: daprSecretStore_ctrl.NewDeleteDaprSecretStore,
+		},
+		{
+			ParentRouter:   daprStateStoreRTSubrouter,
+			ResourceType:   daprStateStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationList,
+			HandlerFactory: daprStateStore_ctrl.NewListDaprStateStores,
+		},
+		{
+			ParentRouter:   daprStateStoreResourceRouter,
+			ResourceType:   daprStateStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationGet,
+			HandlerFactory: daprStateStore_ctrl.NewGetDaprStateStore,
+		},
+		{
+			ParentRouter:   daprStateStoreResourceRouter,
+			ResourceType:   daprStateStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationPut,
+			HandlerFactory: daprStateStore_ctrl.NewCreateOrUpdateDaprStateStore,
+		},
+		{
+			ParentRouter:   daprStateStoreResourceRouter,
+			ResourceType:   daprStateStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationPatch,
+			HandlerFactory: daprStateStore_ctrl.NewCreateOrUpdateDaprStateStore,
+		},
+		{
+			ParentRouter:   daprStateStoreResourceRouter,
+			ResourceType:   daprStateStore_ctrl.ResourceTypeName,
+			Method:         v1.OperationDelete,
+			HandlerFactory: daprStateStore_ctrl.NewDeleteDaprStateStore,
 		},
 		{
 			ParentRouter:   redisRTSubrouter,
