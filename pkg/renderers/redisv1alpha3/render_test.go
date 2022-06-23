fe88768da2692a7c0a8113b969a61fd6ec4947f4
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2022-06-22 17:02:24 -0700

    fix renderer bug (#2226)
    * fix renderer bug
    
    * fix comments
    
    * fix rabbitmq renderer
    
    * fix comments
    
    * fix comments
    
    * e2e test changes
    
    * test changes

--- pkg/renderers/redisv1alpha3/render_test.go
+++ pkg/renderers/redisv1alpha3/render_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/project-radius/radius/pkg/renderers"
 	"github.com/project-radius/radius/pkg/resourcekinds"
 	"github.com/stretchr/testify/require"
+	"gotest.tools/assert"
 )
 
 const (
@@ -32,7 +33,55 @@ func createContext(t *testing.T) context.Context {
 	return logr.NewContext(context.Background(), logger)
 }
 
-func Test_Render_Success(t *testing.T) {
+func Test_Render_Kubernetes_Success(t *testing.T) {
+	ctx := createContext(t)
+	renderer := Renderer{}
+
+	input := renderers.RendererResource{
+		ApplicationName: "test-app",
+		ResourceName:    "test-redis",
+		ResourceType:    ResourceType,
+		Definition: map[string]interface{}{
+			"host": "hello.com",
+			"port": 1234,
+			"secrets": map[string]interface{}{
+				"connectionString": "cs***",
+				"password":         "pwd***",
+			},
+		},
+	}
+	output, err := renderer.Render(ctx, renderers.RenderOptions{
+		Resource:     input,
+		Dependencies: map[string]renderers.RendererDependency{},
+	})
+	require.NoError(t, err)
+
+	expected := renderers.RendererOutput{
+		Resources: []outputresource.OutputResource{},
+		ComputedValues: map[string]renderers.ComputedValueReference{
+			"host": {
+				Value: "hello.com",
+			},
+			"port": {
+				Value: "1234",
+			},
+			"username": {
+				Value: "",
+			},
+		},
+		SecretValues: map[string]renderers.SecretValueReference{
+			"password": {
+				Value: "pwd***",
+			},
+			"connectionString": {
+				Value: "cs***",
+			},
+		},
+	}
+	assert.DeepEqual(t, expected, output)
+}
+
+func Test_Render_Azure_Success(t *testing.T) {
 	ctx := createContext(t)
 	renderer := Renderer{}
 
@@ -78,9 +127,30 @@ func Test_Render_Success(t *testing.T) {
 	require.Equal(t, "listKeys", output.SecretValues[renderers.PasswordStringHolder].Action)
 }
 
-func Test_Render_User_Secrets(t *testing.T) {
+func Test_Render_Azure_User_Secrets(t *testing.T) {
 	ctx := createContext(t)
 	renderer := Renderer{}
+
+	expectedComputedValues := map[string]renderers.ComputedValueReference{
+		"host": {
+			Value: "localhost",
+		},
+		"port": {
+			Value: "42",
+		},
+		"username": {
+			Value: "",
+		},
+	}
+
+	expectedSecretValues := map[string]renderers.SecretValueReference{
+		renderers.PasswordStringHolder: {
+			Value: "deadbeef",
+		},
+		renderers.ConnectionStringValue: {
+			Value: "admin:deadbeef@localhost:42",
+		},
+	}
 
 	resource := renderers.RendererResource{
 		ApplicationName: applicationName,
@@ -101,12 +171,11 @@ func Test_Render_User_Secrets(t *testing.T) {
 
 	require.Len(t, output.Resources, 0)
 
-	expectedComputedValues, expectedSecretValues := expectedComputedAndSecretValues()
 	require.Equal(t, expectedComputedValues, output.ComputedValues)
 	require.Equal(t, expectedSecretValues, output.SecretValues)
 }
 
-func Test_Render_NoResourceSpecified(t *testing.T) {
+func Test_Render_Azure_NoResourceSpecified(t *testing.T) {
 	ctx := createContext(t)
 	renderer := Renderer{}
 
@@ -122,7 +191,7 @@ func Test_Render_NoResourceSpecified(t *testing.T) {
 	require.Equal(t, 0, len(rendererOutput.Resources))
 }
 
-func Test_Render_InvalidResourceType(t *testing.T) {
+func Test_Render_Azure_InvalidResourceType(t *testing.T) {
 	ctx := createContext(t)
 	renderer := Renderer{}
 
@@ -137,30 +206,4 @@ func Test_Render_InvalidResourceType(t *testing.T) {
 	_, err := renderer.Render(ctx, renderers.RenderOptions{Resource: resource, Dependencies: map[string]renderers.RendererDependency{}})
 	require.Error(t, err)
 	require.Equal(t, "the 'resource' field must refer to a Redis Cache", err.Error())
-}
-
-func expectedComputedAndSecretValues() (map[string]renderers.ComputedValueReference, map[string]renderers.SecretValueReference) {
-	expectedComputedValues := map[string]renderers.ComputedValueReference{
-		"host": {
-			Value: "localhost",
-		},
-		"port": {
-			Value: "42",
-		},
-		renderers.UsernameStringValue: {
-			LocalID:           outputresource.LocalIDAzureRedis,
-			PropertyReference: handlers.RedisUsernameKey,
-		},
-	}
-
-	expectedSecretValues := map[string]renderers.SecretValueReference{
-		renderers.PasswordStringHolder: {
-			Value: "deadbeef",
-		},
-		renderers.ConnectionStringValue: {
-			Value: "admin:deadbeef@localhost:42",
-		},
-	}
-
-	return expectedComputedValues, expectedSecretValues
 }
