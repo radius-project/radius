b85e58b12068e4d64bb9e1562098351e6ad77f6a
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-06-24 15:41:33 -0700

    Adding explicit interface methods for identity

--- pkg/corerp/handlers/resource_handler.go
+++ pkg/corerp/handlers/resource_handler.go
@@ -24,6 +24,7 @@ const (
 // ResourceHandler interface defines the methods that every output resource will implement
 //go:generate mockgen -destination=./mock_resource_handler.go -package=handlers -self_package github.com/project-radius/radius/pkg/handlers github.com/project-radius/radius/pkg/handlers ResourceHandler
 type ResourceHandler interface {
-	Put(ctx context.Context, resource outputresource.OutputResource) (resourcemodel.ResourceIdentity, map[string]string, error)
-	Delete(ctx context.Context) error
+	Put(ctx context.Context, resource outputresource.OutputResource) error
+	GetResourceIdentity(ctx context.Context, resource outputresource.OutputResource) (resourcemodel.ResourceIdentity, error)
+	GetResourceProperties(ctx context.Context, resource outputresource.OutputResource) (map[string]string, error)
 }
