faa2172ee43674fd3a767340a57ba409ee756e06
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-06-24 15:10:49 -0700

    Adding common resource handlers keys

--- pkg/corerp/handlers/resource_handler.go
+++ pkg/corerp/handlers/resource_handler.go
@@ -12,6 +12,15 @@ import (
 	"github.com/project-radius/radius/pkg/resourcemodel"
 )
 
+const (
+	// Common Keys
+	KubernetesAPIVersionKey = "kubernetesapiversion"
+	KubernetesKindKey       = "kuberneteskind"
+	KubernetesNamespaceKey  = "kubernetesnamespace"
+	KubernetesNameKey       = "kubernetesname"
+	ResourceName            = "resourcename"
+)
+
 // ResourceHandler interface defines the methods that every output resource will implement
 //go:generate mockgen -destination=./mock_resource_handler.go -package=handlers -self_package github.com/project-radius/radius/pkg/handlers github.com/project-radius/radius/pkg/handlers ResourceHandler
 type ResourceHandler interface {
