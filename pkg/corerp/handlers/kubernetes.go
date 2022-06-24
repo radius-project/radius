d8fe7a5dc2a58b2b1bbb9893e154c6ac83cb7a2a
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-06-24 15:42:20 -0700

    Adding k8s handler

---
+++ pkg/corerp/handlers/kubernetes.go
@@ -0,0 +1,134 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package handlers
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/project-radius/radius/pkg/kubernetes"
+	"github.com/project-radius/radius/pkg/providers"
+	"github.com/project-radius/radius/pkg/radrp/outputresource"
+	"github.com/project-radius/radius/pkg/resourcemodel"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func NewKubernetesHandler(k8s client.Client) ResourceHandler {
+	return &kubernetesHandler{k8s: k8s}
+}
+
+type kubernetesHandler struct {
+	k8s client.Client
+}
+
+func (handler *kubernetesHandler) Put(ctx context.Context, resource outputresource.OutputResource) error {
+	item, err := convertToUnstructured(resource)
+	if err != nil {
+		return err
+	}
+
+	err = handler.PatchNamespace(ctx, item.GetNamespace())
+	if err != nil {
+		return err
+	}
+
+	if resource.Deployed {
+		// This resource is deployed in the Render process
+		// TODO: This will eventually change
+		// For now, no need to process any further
+		return nil
+	}
+	err = handler.k8s.Patch(ctx, &item, client.Apply, &client.PatchOptions{FieldManager: kubernetes.FieldManager})
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (handler *kubernetesHandler) GetResourceIdentity(ctx context.Context, resource outputresource.OutputResource) (resourcemodel.ResourceIdentity, error) {
+	item, err := convertToUnstructured(resource)
+	if err != nil {
+		return resourcemodel.ResourceIdentity{}, err
+	}
+
+	identity := resourcemodel.ResourceIdentity{
+		ResourceType: &resourcemodel.ResourceType{
+			Type:     resource.ResourceType.Type,
+			Provider: providers.ProviderKubernetes,
+		},
+		Data: resourcemodel.KubernetesIdentity{
+			Name:       item.GetName(),
+			Namespace:  item.GetNamespace(),
+			Kind:       item.GetKind(),
+			APIVersion: item.GetAPIVersion(),
+		},
+	}
+
+	return identity, err
+}
+
+func (handler *kubernetesHandler) GetResourceProperties(ctx context.Context, resource outputresource.OutputResource) (map[string]string, error) {
+	item, err := convertToUnstructured(resource)
+	if err != nil {
+		return nil, err
+	}
+
+	// For a Kubernetes resource we only need to store the ObjectMeta and TypeMeta data
+	properties := map[string]string{
+		KubernetesKindKey:       item.GetKind(),
+		KubernetesAPIVersionKey: item.GetAPIVersion(),
+		KubernetesNamespaceKey:  item.GetNamespace(),
+		ResourceName:            item.GetName(),
+	}
+
+	return properties, err
+}
+
+func (handler *kubernetesHandler) PatchNamespace(ctx context.Context, namespace string) error {
+	// Ensure that the namespace exists that we're able to operate upon.
+	ns := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "Namespace",
+			"metadata": map[string]interface{}{
+				"name": namespace,
+				"labels": map[string]interface{}{
+					kubernetes.LabelManagedBy: kubernetes.LabelManagedByRadiusRP,
+				},
+			},
+		},
+	}
+
+	err := handler.k8s.Patch(ctx, ns, client.Apply, &client.PatchOptions{FieldManager: kubernetes.FieldManager})
+	if err != nil {
+		// we consider this fatal - without a namespace we won't be able to apply anything else
+		return fmt.Errorf("error applying namespace: %w", err)
+	}
+
+	return nil
+}
+
+func convertToUnstructured(resource outputresource.OutputResource) (unstructured.Unstructured, error) {
+	if resource.ResourceType.Provider != providers.ProviderKubernetes {
+		return unstructured.Unstructured{}, errors.New("wrong resource type")
+	}
+
+	obj, ok := resource.Resource.(runtime.Object)
+	if !ok {
+		return unstructured.Unstructured{}, errors.New("inner type was not a runtime.Object")
+	}
+
+	c, err := runtime.DefaultUnstructuredConverter.ToUnstructured(resource.Resource)
+	if err != nil {
+		return unstructured.Unstructured{}, fmt.Errorf("could not convert object %v to unstructured: %w", obj.GetObjectKind(), err)
+	}
+
+	return unstructured.Unstructured{Object: c}, nil
+}
