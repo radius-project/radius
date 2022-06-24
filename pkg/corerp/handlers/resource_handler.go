e07070bb2b4d91368b47fca06b6ff4fdbf162a93
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-06-24 15:09:07 -0700

    Adding the corerp resource handler interface

---
+++ pkg/corerp/handlers/resource_handler.go
@@ -0,0 +1,20 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package handlers
+
+import (
+	"context"
+
+	"github.com/project-radius/radius/pkg/radrp/outputresource"
+	"github.com/project-radius/radius/pkg/resourcemodel"
+)
+
+// ResourceHandler interface defines the methods that every output resource will implement
+//go:generate mockgen -destination=./mock_resource_handler.go -package=handlers -self_package github.com/project-radius/radius/pkg/handlers github.com/project-radius/radius/pkg/handlers ResourceHandler
+type ResourceHandler interface {
+	Put(ctx context.Context, resource outputresource.OutputResource) (resourcemodel.ResourceIdentity, map[string]string, error)
+	Delete(ctx context.Context) error
+}
