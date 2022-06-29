86b5b3cb59ea7b6f0700beceb2641434148e051b
mishrapratikshya <pratmishra@microsoft.com>
2022-06-28 19:09:37 -0700

    Initial commit for rabbitmqmessagequeues renderer

--- pkg/connectorrp/renderers/rabbitmqmessagequeues/renderer_test.go
+++ pkg/connectorrp/renderers/rabbitmqmessagequeues/renderer_test.go
@@ -18,6 +18,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+<<<<<<< HEAD
+=======
+const (
+	applicationName = "test-app"
+	resourceName    = "test-rabbitmq"
+)
+
+>>>>>>> a28a48bf (Initial commit for rabbitmqmessagequeues renderer)
 func createContext(t *testing.T) context.Context {
 	logger, err := radlogger.NewTestLogger(t)
 	if err != nil {
