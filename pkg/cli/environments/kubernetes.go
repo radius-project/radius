c144b926413485873ed2068d46b5f65fa2047a68
Justin Kotalik <jukotali@microsoft.com>
2022-06-29 08:13:16 -0700

    Always set the right tags

--- pkg/cli/environments/kubernetes.go
+++ pkg/cli/environments/kubernetes.go
@@ -121,8 +121,8 @@ func (e *KubernetesEnvironment) CreateLegacyDeploymentClient(ctx context.Context
 	return &azure.ResouceDeploymentClient{
 		Client:           dc,
 		OperationsClient: op,
-		SubscriptionID:   e.Namespace,
-		ResourceGroup:    e.Namespace,
+		SubscriptionID:   subscriptionId,
+		ResourceGroup:    resourceGroup,
 		Tags:             tags,
 		EnableUCP:        e.EnableUCP,
 	}, nil
