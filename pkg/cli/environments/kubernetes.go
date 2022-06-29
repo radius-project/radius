9455f3ddbbf6087035ca72cde72852aa317932a1
Justin Kotalik <jukotali@microsoft.com>
2022-06-28 21:07:57 -0700

    fixing default env requirements

--- pkg/cli/environments/kubernetes.go
+++ pkg/cli/environments/kubernetes.go
@@ -24,8 +24,6 @@ import (
 // KubernetesEnvironment represents a Kubernetes Radius environment.
 type KubernetesEnvironment struct {
 	RadiusEnvironment `mapstructure:",squash"`
-	ClusterName       string     `mapstructure:"clustername" validate:"required"`
-	Registry          *Registry  `mapstructure:"registry,omitempty"`
 	Providers         *Providers `mapstructure:"providers"`
 }
 
