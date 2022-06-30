701cc880114dadf8cc815a420c14f16e25e8e5cd
Ruokun (Tommy) Niu <t-tommyniu@microsoft.com>
2022-06-30 14:27:34 -0700

    Configure osm namespace <add> using k8s API (#2732)

--- pkg/handlers/kubernetes.go
+++ pkg/handlers/kubernetes.go
@@ -83,7 +83,11 @@ func (handler *kubernetesHandler) PatchNamespace(ctx context.Context, namespace 
 			"metadata": map[string]interface{}{
 				"name": namespace,
 				"labels": map[string]interface{}{
-					kubernetes.LabelManagedBy: kubernetes.LabelManagedByRadiusRP,
+					kubernetes.LabelManagedBy:         kubernetes.LabelManagedByRadiusRP,
+					"openservicemesh.io/monitored-by": "osm",
+				},
+				"annotations": map[string]interface{}{
+					"openservicemesh.io/sidecar-injection": "enabled",
 				},
 			},
 		},
