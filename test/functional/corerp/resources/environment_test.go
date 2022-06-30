5cd9a55bf9ef8227f27f136493fc0ce2297e0f72
Will Smith <willdavsmith@gmail.com>
2022-06-29 20:30:03 -0700

    Adding CoreRP test suite (#2690)
    * Adding WIP coreRP test suite
    
    * Wake up CI
    
    * [TEMP] add validate-bicep back
    
    * test
    
    * Fixing extra tag in azure pipeline
    
    * [TEMP] removing ci for appcore
    
    * Using CLI commands instead of cluster proxy
    
    * Changing resource group for tests to default
    
    * Removing bicep validation
    
    * Fixing linting issue
    
    * Moving rad bicep download feature flag
    
    * enabling nested modules test
    
    * Removing parallel testing for corerp
    
    * Updating tests to use rad deploy and application clients
    
    * Updating corerp test
    
    * Create resource group for deployments
    
    * slash
    
    * Long day lol
    
    Co-authored-by: Will Smith <willsmith@microsoft.com>
    Co-authored-by: Justin Kotalik <jukotali@microsoft.com>

---
+++ test/functional/corerp/resources/environment_test.go
@@ -0,0 +1,33 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package resource_test
+
+import (
+	"testing"
+
+	"github.com/project-radius/radius/test/functional/corerp"
+	"github.com/project-radius/radius/test/step"
+	"github.com/project-radius/radius/test/validation"
+)
+
+func Test_Environment(t *testing.T) {
+	template := "testdata/corerp-resources-environment.bicep"
+	name := "corerp-resources-environment"
+
+	test := corerp.NewCoreRPTest(t, name, []corerp.TestStep{
+		{
+			Executor: step.NewDeployExecutor(template),
+			Resources: []validation.Resource{
+				{
+					Name: "corerp-resources-environment-env",
+					Type: validation.EnvironmentsResource,
+				},
+			},
+		},
+	})
+
+	test.Test(t)
+}
