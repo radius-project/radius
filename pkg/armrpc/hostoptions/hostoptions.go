522009de7156a8ac3ed3cd5c01c83599d0589472
Shalabh Mohan Shrivastava <shalabhms@gmail.com>
2022-07-01 14:16:44 -0700

    Updating app-core helm chart to inject env vars for adding k8s and arm host options (#2767)
    * adding arm, k8s hostOptions , helm chart
    
    * Adding K8S_CLUSTER env variable to appcorerp chart
    
    * Adding SKIP_AUTH to appcore-rp env
    
    * [TEMP] Skipping environment test
    
    * Changing rp values to appcorerp in corerp chart
    
    * Re-enabling environment tests
    
    * Revert "Changing rp values to appcorerp in corerp chart"
    
    This reverts commit 35942139d84fde840d36c3b0bf007dcdb8867a21.
    
    * upgrade mariner image to 2.0
    
    * add -y
    
    * add wget
    
    * wip
    
    Co-authored-by: willdavsmith <willdavsmith@gmail.com>
    Co-authored-by: Justin Kotalik <jukotali@microsoft.com>
    Co-authored-by: Young Bu Park <youngp@microsoft.com>

--- pkg/armrpc/hostoptions/hostoptions.go
+++ pkg/armrpc/hostoptions/hostoptions.go
@@ -13,22 +13,24 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/project-radius/radius/pkg/armrpc/servicecontext"
+	"github.com/project-radius/radius/pkg/azure/armauth"
+	"github.com/project-radius/radius/pkg/radrp/k8sauth"
 	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/kubectl/pkg/scheme"
+	controller_runtime "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // HostOptions defines all of the settings that our RP's execution environment provides.
 type HostOptions struct {
 	// Config is the bootstrap configuration loaded from config file.
-	Config *ProviderConfig
-
-	// DBClientFactory func(ctx context.Context) (*mongo.Database, error)
-
-	// HealthChannels supports loosely-coupled communication between the Resource Provider
-	// backend and the Health Monitor. This is part of the options for now because it's
-	// based on in-memory communication.
-	// HealthChannels healthcontract.HealthChannels
+	Config    *ProviderConfig
+	Arm       *armauth.ArmConfig
+	K8sConfig *rest.Config
 }
 
 func NewHostOptionsFromEnvironment(configPath string) (HostOptions, error) {
@@ -37,8 +39,20 @@ func NewHostOptionsFromEnvironment(configPath string) (HostOptions, error) {
 		return HostOptions{}, err
 	}
 
+	arm, err := getArm()
+	if err != nil {
+		return HostOptions{}, err
+	}
+
+	k8s, err := getKubernetes()
+	if err != nil {
+		return HostOptions{}, err
+	}
+
 	return HostOptions{
-		Config: conf,
+		Config:    conf,
+		Arm:       arm,
+		K8sConfig: k8s,
 	}, nil
 }
 
@@ -79,4 +93,44 @@ func FromContext(ctx context.Context) *ProviderConfig {
 // WithContext injects ProviderConfig into the given http context.
 func WithContext(ctx context.Context, cfg *ProviderConfig) context.Context {
 	return context.WithValue(ctx, servicecontext.HostingConfigContextKey, cfg)
+}
+
+func getArm() (*armauth.ArmConfig, error) {
+	skipARM, ok := os.LookupEnv("SKIP_ARM")
+	if ok && strings.EqualFold(skipARM, "true") {
+		return nil, nil
+	}
+
+	arm, err := armauth.GetArmConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build ARM config: %w", err)
+	}
+
+	if arm != nil {
+		fmt.Println("Initializing RP with the provided ARM credentials")
+	}
+
+	return arm, nil
+}
+
+func getKubernetes() (*rest.Config, error) {
+	cfg, err := k8sauth.GetConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get kubernetes config: %w", err)
+	}
+
+	// Verify that we can connect to the cluster before handing out the config
+	s := scheme.Scheme
+	c, err := controller_runtime.New(cfg, controller_runtime.Options{Scheme: s})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kubernetes client: %w", err)
+	}
+
+	ns := &corev1.NamespaceList{}
+	err = c.List(context.Background(), ns, &controller_runtime.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to kubernetes: %w", err)
+	}
+
+	return cfg, nil
 }
