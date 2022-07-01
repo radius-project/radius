1191eed33bc38b250f81d0a7e99577bf0a1e9043
mishrapratikshya <pratmishra@microsoft.com>
2022-07-01 10:54:07 -0700

    remove the types.go

--- pkg/corerp/renderers/daprextension/types.go
+++
@@ -1,8 +0,0 @@
-// ------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-// ------------------------------------------------------------
-
-package daprextension
-
-const Kind = "Applications.Core/Sidecar"
