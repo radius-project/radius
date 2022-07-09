f15c4e9dbd4dc51592fcbbb7135263201d68ae38
Young Bu Park <youngp@microsoft.com>
2022-07-08 22:32:45 -0700

    Create KubeClient for RP (#2852)

--- pkg/armrpc/asyncoperation/worker/service.go
+++ pkg/armrpc/asyncoperation/worker/service.go
@@ -11,16 +11,13 @@ import (
 	"github.com/go-logr/logr"
 	manager "github.com/project-radius/radius/pkg/armrpc/asyncoperation/statusmanager"
 	"github.com/project-radius/radius/pkg/armrpc/hostoptions"
+	kubeclient "github.com/project-radius/radius/pkg/kubernetes/client"
 	"github.com/project-radius/radius/pkg/renderers"
 	"github.com/project-radius/radius/pkg/ucp/dataprovider"
 	queue "github.com/project-radius/radius/pkg/ucp/queue/client"
 	qprovider "github.com/project-radius/radius/pkg/ucp/queue/provider"
 
-	contourv1 "github.com/projectcontour/contour/apis/projectcontour/v1"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	controller_runtime "sigs.k8s.io/controller-runtime/pkg/client"
-	csidriver "sigs.k8s.io/secrets-store-csi-driver/apis/v1alpha1"
 )
 
 // Service is the base worker service implementation to initialize and start worker.
@@ -58,16 +55,10 @@ func (s *Service) Init(ctx context.Context) error {
 	s.OperationStatusManager = manager.New(opSC, s.RequestQueue, s.ProviderName, s.Options.Config.Env.RoleLocation)
 	s.Controllers = NewControllerRegistry(s.StorageProvider)
 
-	scheme := clientgoscheme.Scheme
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(csidriver.AddToScheme(scheme))
-	utilruntime.Must(contourv1.AddToScheme(scheme))
-
-	k8s, err := controller_runtime.New(s.Options.K8sConfig, controller_runtime.Options{Scheme: scheme})
+	s.KubeClient, err = kubeclient.CreateKubeClient(s.Options.K8sConfig)
 	if err != nil {
 		return err
 	}
-	s.KubeClient = k8s
 
 	if s.Options.Arm != nil {
 		s.SecretClient = renderers.NewSecretValueClient(*s.Options.Arm)
