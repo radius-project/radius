2270e3e87ea9c60527be647dbd5387560ff9c154
nithyatsu <98416062+nithyatsu@users.noreply.github.com>
2022-07-13 12:45:17 -0700

    introduce provider config (#2737)

--- pkg/azure/clients/resourcedeploymentclient_test.go
+++ pkg/azure/clients/resourcedeploymentclient_test.go
@@ -9,14 +9,14 @@ import (
 	"context"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2020-10-01/resources"
+	"github.com/project-radius/radius/pkg/providers"
 	"github.com/stretchr/testify/require"
 )
 
 func TestResourceClientPrepare(t *testing.T) {
 	resourceClient := NewResourceDeploymentClientWithBaseURI("http://localhost:5050")
 
-	request, err := resourceClient.ResourceCreateOrUpdatePreparer(context.TODO(), "/resourcegroups/my-rg/providers/Microsoft.Resources/deployments/my-deployment", resources.Deployment{})
+	request, err := resourceClient.ResourceCreateOrUpdatePreparer(context.TODO(), "/resourcegroups/my-rg/providers/Microsoft.Resources/deployments/my-deployment", providers.Deployment{})
 	require.NoError(t, err)
 
 	require.Equal(t, "/resourcegroups/my-rg/providers/Microsoft.Resources/deployments/my-deployment", request.URL.Path)
