a36d6853aae86b2ed35e0104c95bcaaf4b8ce743
Ryan Nowak <nowakra@gmail.com>
2022-07-14 13:32:32 -0700

    Fix version output for Bicep in rad version
    Fixes: #2869
    
    This change fixes the version output for Bicep. This regressed recently
    when the signature of the `runBicep`function was changed to include JSON
    unmarshalling. The output of `bicep --version` is not JSON.

--- test/functional/corerp/cli/cli_test.go
+++ test/functional/corerp/cli/cli_test.go
@@ -6,11 +6,18 @@
 package resource_test
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"testing"
 
+	"github.com/project-radius/radius/pkg/cli/bicep"
+	"github.com/project-radius/radius/pkg/cli/objectformats"
+	"github.com/project-radius/radius/test"
 	"github.com/project-radius/radius/test/functional/corerp"
+	"github.com/project-radius/radius/test/functional/kubernetes"
+	"github.com/project-radius/radius/test/radcli"
 	"github.com/project-radius/radius/test/step"
 	"github.com/project-radius/radius/test/validation"
 	"github.com/stretchr/testify/require"
@@ -96,4 +103,25 @@ func Test_CLI_DeploymentParameters(t *testing.T) {
 	})
 
 	test.Test(t)
+}
+
+func Test_CLI_version(t *testing.T) {
+	ctx, cancel := test.GetContext(t)
+	defer cancel()
+
+	options := kubernetes.NewTestOptions(t)
+	cli := radcli.NewCLI(t, options.ConfigFilePath)
+
+	output, err := cli.Version(ctx)
+	require.NoError(t, err)
+
+	// Matching logic:
+	//
+	// Release: any word or number characters or hyphen or dot
+	// Version: any work or number characters or hyphen or dot
+	// Bicep: Semver
+	// Commit: any lowercase word or number characters
+	matcher := fmt.Sprintf(`RELEASE\s+VERSION\s+BICEP\s+COMMIT\s*([a-zA-Z0-9-\.]+)\s+([a-zA-Z0-9-\.]+)\s+(%s)\s+([a-z0-9]+)`, bicep.SemanticVersionRegex)
+	expected := regexp.MustCompile(matcher)
+	require.Regexp(t, expected, objectformats.TrimSpaceMulti(output))
 }
