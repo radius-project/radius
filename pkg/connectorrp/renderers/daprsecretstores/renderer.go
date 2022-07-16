70f851f4084046b64c1d7edf492d6564f363fe6a
Vishwanath Hiremath <100623239+vishwahiremat@users.noreply.github.com>
2022-07-15 18:01:47 -0700

    Removing the applicationID check if applicationId is empty (#2971)
    * removing the applicationID check if applicationId is empty
    
    * Addressing review comments

--- pkg/connectorrp/renderers/daprsecretstores/renderer.go
+++ pkg/connectorrp/renderers/daprsecretstores/renderer.go
@@ -46,13 +46,16 @@ func (r Renderer) Render(ctx context.Context, dm conv.DataModelInterface, option
 	if secretStoreFunc == nil {
 		return renderers.RendererOutput{}, fmt.Errorf("%s is not supported. Supported kind values: %s", properties.Kind, getAlphabeticallySortedKeys(r.SecretStores))
 	}
-
-	applicationID, err := resources.Parse(resource.Properties.Application)
-	if err != nil {
-		return renderers.RendererOutput{}, errors.New("the 'application' field must be a valid resource id")
+	var applicationName string
+	if resource.Properties.Application != "" {
+		applicationID, err := resources.Parse(resource.Properties.Application)
+		if err != nil {
+			return renderers.RendererOutput{}, errors.New("the 'application' field must be a valid resource id")
+		}
+		applicationName = applicationID.Name()
 	}
 
-	resoures, err := secretStoreFunc(*resource, applicationID.Name(), options.Namespace)
+	resoures, err := secretStoreFunc(*resource, applicationName, options.Namespace)
 	if err != nil {
 		return renderers.RendererOutput{}, err
 	}
