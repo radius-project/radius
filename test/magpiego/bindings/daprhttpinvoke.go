a0bd3db31f7893338b5903502d92b080d1413a8a
mishrapratikshya <pratmishra@microsoft.com>
2022-07-22 20:24:13 -0700

    add E2E test to validate daprInvokeHttpRoutes

---
+++ test/magpiego/bindings/daprhttpinvoke.go
@@ -0,0 +1,36 @@
+package bindings
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	dapr "github.com/dapr/go-sdk/client"
+)
+
+// Use this with a values like:
+// - CONNECTION_DAPRHTTPROUTE_APPID
+// - DAPR_GRPC_PORT
+func DaprHttpRouteBinding(envParams map[string]string) BindingStatus {
+	appID := envParams["APPID"]
+	if appID == "" {
+		log.Println("APPID is required")
+		return BindingStatus{false, "APPID is required"}
+	}
+	client, err := dapr.NewClientWithPort(os.Getenv("DAPR_GRPC_PORT"))
+	if err != nil {
+		log.Println("Error creating dapr client", err)
+		return BindingStatus{false, fmt.Sprintf("Error creating dapr client - %s", err.Error())}
+	}
+	defer client.Close()
+	ctx := context.Background()
+	//Using Dapr SDK to invoke a method
+	result, err := client.InvokeMethod(ctx, appID, "/backend", "get")
+	if err != nil {
+		log.Println("Error invoking dapr InvokeMethod - ", err)
+		return BindingStatus{false, fmt.Sprintf("Error invoking dapr InvokeMethod  - %s", err.Error())}
+	}
+	log.Printf("successfully invoked dapr InvokeMethod %s", string(result))
+	return BindingStatus{true, "invoked dapr InvokeMethod"}
+}
