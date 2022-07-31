cb1ac4b63f47570e36a132d49817e855ccdf8d28
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2022-07-31 12:10:07 -0700

    Update CLI docs to reflect change from "Container" to "containers" (#3243)
    * Update resourceExpose.go
    
    * Update logs from Container to containers

--- cmd/rad/cmd/resourceExpose.go
+++ cmd/rad/cmd/resourceExpose.go
@@ -26,7 +26,7 @@ This command is useful for testing resources that accept network traffic but are
 Press CTRL+C to exit the command and terminate the tunnel.`,
 	Example: `# expose port 80 on the 'orders' resource of the 'icecream-store' application
 # on local port 5000
-rad resource expose --application icecream-store Container orders --port 5000 --remote-port 80`,
+rad resource expose --application icecream-store containers orders --port 5000 --remote-port 80`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		config := ConfigFromContext(cmd.Context())
 		workspace, err := cli.RequireWorkspace(cmd, config)
