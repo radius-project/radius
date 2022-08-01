4af2d7abc6c85d9b4e74cbed90e679ee4c5121d3
Vishwanath Hiremath <vhiremath@microsoft.com>
2022-08-01 10:55:58 -0700

    updated the arguements

--- cmd/rad/cmd/resourceDelete.go
+++ cmd/rad/cmd/resourceDelete.go
@@ -13,7 +13,7 @@ import (
 
 // resourceDeleteCmd is the command to delete a resource
 var resourceDeleteCmd = &cobra.Command{
-	Use:   "delete type resource",
+	Use:   "delete resource_type resource_name",
 	Short: "Delete a RAD resource",
 	Long:  "Deletes a RAD resource with the given name",
 	RunE:  deleteResource,
