0f44036f7ed22aa5593c9de3c549992626b4b08b
nithyatsu <nithyasu@microsoft.com>
2022-08-02 13:24:45 -0700

    pass the right env value to deployment template

--- cmd/rad/cmd/deploy.go
+++ cmd/rad/cmd/deploy.go
@@ -109,7 +109,7 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	_, err = cli.RequireEnvironmentNameArgs(cmd, args, *workspace)
+	environment, err := cli.RequireEnvironmentNameArgs(cmd, args, *workspace)
 	if err != nil {
 		return err
 	}
@@ -139,7 +139,7 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 	}
 	output.CompleteStep(step)
 
-	err = bicep.InjectEnvironmentParam(template, parameters, cmd.Context(), workspace.Environment)
+	err = bicep.InjectEnvironmentParam(template, parameters, cmd.Context(), environment)
 	if err != nil {
 		return err
 	}
