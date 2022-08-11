13d3abd015b88fe071712e5e76b904be875682d9
Aman Bhardwaj <amanbha@microsoft.com>
2022-08-11 15:25:54 -0700

    Revert "Add shorthand rad list usage example (#3337)"
    This reverts commit 6aa6b59ec526e949b3fea22e80d08a4ef2de7ee4.

--- cmd/rad/cmd/resourceShow.go
+++ cmd/rad/cmd/resourceShow.go
@@ -16,24 +16,10 @@ import (
 
 // resourceShowCmd command to show details of a resource
 var resourceShowCmd = &cobra.Command{
-	Use:     "show [resourceType] [resourceName]",
+	Use:     "show [type] [resource]",
 	Short:   "Show RAD resource details",
 	Long:    "Show details of the specified Radius resource",
-	Example: `
-	sample list of resourceType: containers, gateways, httpRoutes, daprPubSubBrokers, daprInvokeHttpRoutes, extenders, mongoDatabases, rabbitMQMessageQueues, redisCaches, sqlDatabases, daprStateStores, daprSecretStores
-
-	# show details of a specified resource in the default environment
-
-	rad resource show containers orders
-	rad resource show gateways orders_gateways
-	rad resource show httpRoutes orders_routes
-
-	# show details of a specified resource in an application
-	rad resource show containers orders --application icecream-store
-	
-	# show details of a specified resource in an application (shorthand flag)
-	rad resource show containers orders -a icecream-store 
-	`,
+	Example: `rad resource show --application icecream-store containers orders`,
 	RunE:    showResource,
 }
 
