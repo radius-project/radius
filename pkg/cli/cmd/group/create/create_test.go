187eb1210b4617d78542a5e8a5a09995c96424df
nithyatsu <nithyasu@microsoft.com>
2022-09-01 14:30:38 -0700

    rad group create

--- pkg/cli/cmd/group/create/create_test.go
+++ pkg/cli/cmd/group/create/create_test.go
@@ -6,10 +6,13 @@
 package create
 
 import (
+	"context"
 	"testing"
 
 	"github.com/project-radius/radius/pkg/cli/framework"
+	"github.com/project-radius/radius/pkg/cli/workspaces"
 	"github.com/project-radius/radius/test/radcli"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -49,5 +52,32 @@ func Test_Validate(t *testing.T) {
 }
 
 func Test_Run(t *testing.T) {
+
+	t.Run("Validate rad group create", func(t *testing.T) {
+
+		/*ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		appManagementClient := clients.NewMockApplicationsManagementClient(ctrl)
+		appManagementClient.EXPECT().
+			ShowResource(gomock.Any(), "containers", "foo").
+			Return(CreateContainerResource(), nil).Times(1)*/
+
+		workspace := &workspaces.Workspace{
+			Connection: map[string]interface{}{
+				"kind":    "kubernetes",
+				"context": "kind-kind",
+			},
+
+			Name: "kind-kind",
+		}
+
+		runner := &Runner{
+			Workspace: workspace,
+		}
+
+		err := runner.Run(context.Background())
+		require.NoError(t, err)
+	})
 
 }
