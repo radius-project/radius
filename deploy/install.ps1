6c7378264d07500500db9181c381f490f09ac71e
Ariel Silverman <asilverman@users.noreply.github.com>
2022-09-08 09:27:19 -0700

    Revert "fix destructive changes to users PATH and registry (#3507)"
    This reverts commit 259c55bd0197fb6832e4cd62d5a83d8061709443.

--- deploy/install.ps1
+++ deploy/install.ps1
@@ -34,8 +34,7 @@ if ((Get-ExecutionPolicy) -gt 'RemoteSigned' -or (Get-ExecutionPolicy) -eq 'ByPa
 # Check if Radius CLI is installed.
 if (Test-Path $RadiusCliFilePath -PathType Leaf) {
     Write-Warning "Radius is detected - $RadiusCliFilePath"
-    Write-Output "Current version:"
-    Write-OUtput $(Invoke-Expression "$RadiusCliFilePath version -o json | ConvertFrom-JSON | Format-List")
+    #TODO Invoke-Expression "$RadiusCliFilePath --version"
     Write-Output "Reinstalling Radius..."
 }
 else {
@@ -49,7 +48,8 @@ if (!(Test-Path $RadiusRoot -PathType Container)) {
     throw "Cannot create $RadiusRoot"
 }
 
-if ($Version -eq "") {
+if($Version -eq "")
+{
     $Version = Invoke-WebRequest $StableVersionUrl -UseBasicParsing
     $Version = $Version.Trim()
 }
@@ -59,54 +59,46 @@ $urlParts = @(
     $OsArch,
     $RadiusCliFileName
 )
-$binaryUrl = $urlParts -join "/"
+$binaryUrl =  $urlParts -join "/"
 
 $binaryFilePath = $RadiusRoot + "\" + $RadiusCliFileName
 Write-Output "Downloading $binaryUrl ..."
 
-try {
+$uri = [uri]$binaryUrl
+try
+{
     $ProgressPreference = "SilentlyContinue" # Do not show progress bar
     Invoke-WebRequest -Uri $binaryUrl -OutFile $binaryFilePath -UseBasicParsing
     if (!(Test-Path $binaryFilePath -PathType Leaf)) {
         throw "Failed to download Radius Cli binary - $binaryFilePath"
     }
 }
-catch [Net.WebException] {
+catch [Net.WebException]
+{
     throw "ERROR: The specified release version: $Version does not exist."
 }
 
-# Print the version string of the installed CLI
-Write-Output "Radius version:"
-Write-OUtput $(Invoke-Expression "$RadiusCliFilePath version -o json | ConvertFrom-JSON | Format-List")
-
+# TODO Check the Radius CLI version: Invoke-Expression "$RadiusCliFilePath --version"
 
 # Add RadiusRoot directory to User Path environment variable
 Write-Output "Try to add $RadiusRoot to User Path Environment variable..."
-$UserPathEnvironmentVar = (Get-Item -Path HKCU:\Environment).GetValue(
-    'PATH', # the registry-value name
-    $null, # the default value to return if no such value exists.
-    'DoNotExpandEnvironmentNames' # the option that suppresses expansion
-)
-  
+$UserPathEnvironmentVar = [Environment]::GetEnvironmentVariable("PATH", "User")
 if ($UserPathEnvironmentVar -like '*radius*') {
     Write-Output "Skipping to add $RadiusRoot to User Path - $UserPathEnvironmentVar"
 }
 else {
-    Write-Host "Adding $InstallFolder to PATH"
-    # [Environment]::SetEnvironmentVariable sets the value kind as REG_SZ, use the function below to set a value of kind REG_EXPAND_SZ
-    Set-ItemProperty HKCU:\Environment "PATH" "$UserPathEnvironmentVar$RadiusRoot" -Type ExpandString
-    # Also add the path to the current session
-    $env:PATH += ";$RadiusRoot"
+    [System.Environment]::SetEnvironmentVariable("PATH", $UserPathEnvironmentVar + ";$RadiusRoot", "User")
+    $UserPathEnvironmentVar = [Environment]::GetEnvironmentVariable("PATH", "User")
+    Write-Output "Added $RadiusRoot to User Path - $UserPathEnvironmentVar"
 }
-Write-Output "Added $RadiusRoot to User Path - $env:PATH"
+
 Write-Output "`r`nRadius CLI is installed successfully."
 
 Write-Output "Installing rad-bicep (""rad bicep download"")..."
 $cmd = (Start-Process -NoNewWindow -FilePath $RadiusCliFilePath -ArgumentList "bicep download" -PassThru -Wait)
 if ($cmd.ExitCode -ne 0) {
     Write-Warning "`r`nFailed to install rad-bicep"
-}
-else {
+} else {
     Write-Output "`r`nrad-bicep installed successfully"
 }
 
