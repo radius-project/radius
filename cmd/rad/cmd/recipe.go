3ca56bcf60e00c6aaf238a397ae7d089be86cbc0
Vishwanath Hiremath <100623239+vishwahiremat@users.noreply.github.com>
2022-09-19 17:15:42 -0700

    Implementation of CLI design for Connector Recipe linking to environments (#3753)
    * cli implementation for rad recipe create

---
+++ cmd/rad/cmd/recipe.go
@@ -0,0 +1,22 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var recipeCmd = &cobra.Command{
+	Use:   "recipe",
+	Short: "Manage connector recipes",
+	Long:  `Manage connector recipes`,
+}
+
+func init() {
+	RootCmd.AddCommand(recipeCmd)
+	recipeCmd.PersistentFlags().StringP("environment", "e", "", "The environment name")
+	recipeCmd.PersistentFlags().StringP("workspace", "w", "", "The workspace name")
+}
