cca10b71f72f41a3ed9807029b84bc91c8ece5ff
Young Bu Park <youngp@microsoft.com>
2022-10-03 09:57:43 -0700

    Upgrade pkgs to the latest

--- pkg/telemetry/metrics/provider/prometheusexporter.go
+++ pkg/telemetry/metrics/provider/prometheusexporter.go
@@ -5,13 +5,30 @@
 
 package provider
 
-import "go.opentelemetry.io/otel/exporters/metric/prometheus"
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	otelprom "go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/metric/view"
+)
 
 // NewPrometheusMetricsExporter returns prometheus exporter used for metrics collection
-func NewPrometheusMetricsExporter(config prometheus.Config) (*prometheus.Exporter, error) {
-	exporter, err := prometheus.InstallNewPipeline(config)
-	if err != nil {
-		return nil, err
+func NewPrometheusMetricsExporter() (*metric.MeterProvider, http.Handler, error) {
+	exporter := otelprom.New()
+	registry := prometheus.NewRegistry()
+	if err := registry.Register(exporter.Collector); err != nil {
+		return nil, nil, err
 	}
-	return exporter, nil
+
+	defaultView, err := view.New(view.MatchInstrumentName("*"))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	provider := metric.NewMeterProvider(metric.WithReader(exporter, defaultView))
+
+	return provider, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}), nil
 }
