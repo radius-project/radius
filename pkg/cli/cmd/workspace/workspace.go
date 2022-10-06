7eafeff34c5bcd22859cc4fe430185855834563f
nithyatsu <98416062+nithyatsu@users.noreply.github.com>
2022-10-06 10:28:20 -0700

    Update pkg/cli/cmd/workspace/workspace.go
    Co-authored-by: Aaron Crawfis <Aaron.Crawfis@microsoft.com>

--- pkg/cli/cmd/workspace/workspace.go
+++ pkg/cli/cmd/workspace/workspace.go
@@ -21,24 +21,12 @@ func NewCommand(factory framework.Factory) *cobra.Command {
 		You can easily define and switch between workspaces to deploy and manage applications across local, test, and production environments.
 		`,
 		Example: `
-		# List all local workspaces
-		rad workspace list
+# Create workspace with no default resource group or environment set
+rad workspace create kubernetes myworkspace --context kind-kind
 
-		# Create workspace with no default resource group or environment set
-		rad workspace create kubernetes myworkspace --context kind-kind
-
-		# Create workspace with default resource group and environment set
-		rad workspace create kubernetes myworkspace --context kind-kind --group myrg --environment myenv
-
-		# Delete workspace
-		rad workspace delete myworkspace
-
-		# Show details of workspace
-		rad workspace show myWorkspace
-
-		#switch workspace
-		rad workspace switch otherWorkspace
-		`,
+# Create workspace with default resource group and environment set
+rad workspace create kubernetes myworkspace --context kind-kind --group myrg --environment myenv
+`,
 	}
 
 	create, _ := workspace_create.NewCommand(factory)
