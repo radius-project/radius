cb910641bdb9d3d68c4d49b9573a0805c3e3ea64
Bharath Joginapally <bharath@DESKTOP-CP9SIGQ.redmond.corp.microsoft.com>
2022-10-13 18:42:36 -0700

    Correct mock package

--- pkg/ucp/secret/client.go
+++ pkg/ucp/secret/client.go
@@ -9,7 +9,7 @@ import (
 	"context"
 )
 
-//go:generate mockgen -destination=./mock_client.go -package=client -self_package github.com/project-radius/radius/pkg/ucp/secret github.com/project-radius/radius/pkg/ucp/secret Client
+//go:generate mockgen -destination=./mock_client.go -package=secret -self_package github.com/project-radius/radius/pkg/ucp/secret github.com/project-radius/radius/pkg/ucp/secret Client
 
 // Client is an interface to implement secret operations.
 type Client interface {
