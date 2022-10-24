073e011c348fc1eacedac02ba83746d9a3f1138a
Young Bu Park <youngp@microsoft.com>
2022-10-24 09:18:57 -0700

    fix typo

--- pkg/azure/clientv2/clients.go
+++ pkg/azure/clientv2/clients.go
@@ -12,7 +12,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/msi/armmsi"
 )
 
-var defaultClientOption = &arm.ClientOptions{
+var defaultClientOptions = &arm.ClientOptions{
 	ClientOptions: azcore.ClientOptions{
 		Retry: policy.RetryOptions{
 			MaxRetries: 10, // TODO: Find the better retry number.
@@ -29,11 +29,11 @@ type AzureClientOption struct {
 // NewFederatedIdentityClient creates new federated identity client.
 func NewFederatedIdentityClient(subscriptionID string, option *AzureClientOption) (*armmsi.FederatedIdentityCredentialsClient, error) {
 	// TODO: Add LRU cache to maintain the clients.
-	return armmsi.NewFederatedIdentityCredentialsClient(subscriptionID, option.Cred, defaultClientOption)
+	return armmsi.NewFederatedIdentityCredentialsClient(subscriptionID, option.Cred, defaultClientOptions)
 }
 
 // NewUserAssignedIdentityClient creates new user assigned managed identity client.
 func NewUserAssignedIdentityClient(subscriptionID string, option *AzureClientOption) (*armmsi.UserAssignedIdentitiesClient, error) {
 	// TODO: Add LRU cache to maintain the clients.
-	return armmsi.NewUserAssignedIdentitiesClient(subscriptionID, option.Cred, defaultClientOption)
+	return armmsi.NewUserAssignedIdentitiesClient(subscriptionID, option.Cred, defaultClientOptions)
 }
