2ea9d515753701d3b05d84a2cda82f907107d11b
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2022-10-31 12:10:18 -0700

    fix review comments

--- test/functional/ucp/aws_test.go
+++ test/functional/ucp/aws_test.go
@@ -16,7 +16,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	awsgo "github.com/aws/aws-sdk-go-v2/aws"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudcontrol"
@@ -27,57 +26,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func initializeAWS(t *testing.T, ctx context.Context) aws.AWSCloudControlClient {
-	cfg, err := awsconfig.LoadDefaultConfig(ctx)
-	require.NoError(t, err)
-	var awsClient aws.AWSCloudControlClient = cloudcontrol.NewFromConfig(cfg)
-	return awsClient
-}
+var streamName = "my-stream" + uuid.NewString()
+var resourceType = "AWS::Kinesis::Stream"
 
 func Test_AWS_DeleteResource(t *testing.T) {
 	ctx := context.Background()
-	resourceType := "AWS::Kinesis::Stream"
-	streamName := "my-stream" + uuid.NewString()
-
-	// Test setup - Create AWS resource using AWS APIs
-	awsClient := initializeAWS(t, ctx)
-
-	desiredState := map[string]interface{}{
-		"Name":                 streamName,
-		"RetentionPeriodHours": 180,
-		"ShardCount":           4,
-	}
-	desiredStateBytes, err := json.Marshal(desiredState)
-
-	response, err := awsClient.CreateResource(ctx, &cloudcontrol.CreateResourceInput{
-		TypeName:     &resourceType,
-		DesiredState: awsgo.String(string(desiredStateBytes)),
-	})
-	require.NoError(t, err)
-	// Wait till the create is complete
-	maxWaitTime := 300 * time.Second
-	waiter := cloudcontrol.NewResourceRequestSuccessWaiter(awsClient)
-	err = waiter.Wait(ctx, &cloudcontrol.GetResourceRequestStatusInput{
-		RequestToken: response.ProgressEvent.RequestToken,
-	}, maxWaitTime)
-	require.NoError(t, err)
-
-	t.Cleanup(func() {
-		// Just in case delete fails
-		deleteOutput, err := awsClient.DeleteResource(ctx, &cloudcontrol.DeleteResourceInput{
-			Identifier: &streamName,
-			TypeName:   to.Ptr("AWS::Kinesis::Stream"),
-		})
-		require.NoError(t, err)
-
-		// Wait till the delete is complete
-		maxWaitTime := 300 * time.Second
-		waiter := cloudcontrol.NewResourceRequestSuccessWaiter(awsClient)
-		err = waiter.Wait(ctx, &cloudcontrol.GetResourceRequestStatusInput{
-			RequestToken: deleteOutput.ProgressEvent.RequestToken,
-		}, maxWaitTime)
-	})
-	// End of test setup
+	setupTestAWSResource(t, ctx)
 
 	test := NewUCPTest(t, "Test_AWS_DeleteResource", func(t *testing.T, url string, roundTripper http.RoundTripper) {
 		// Call UCP Delete AWS Resource API
@@ -100,6 +54,7 @@ func Test_AWS_DeleteResource(t *testing.T) {
 		deleteRequest, err := http.NewRequest(http.MethodPost, deleteURL, bytes.NewBuffer(deleteBody))
 		require.NoError(t, err)
 		deleteResponse, err := roundTripper.RoundTrip(deleteRequest)
+		require.NoError(t, err)
 		require.Equal(t, http.StatusAccepted, deleteResponse.StatusCode)
 
 		// Get the operation status url from the Azure-Asyncoperation header
@@ -130,4 +85,57 @@ func Test_AWS_DeleteResource(t *testing.T) {
 	})
 	test.Test(t)
 
+}
+
+func setupTestAWSResource(t *testing.T, ctx context.Context) {
+	// Test setup - Create AWS resource using AWS APIs
+	cfg, err := awsconfig.LoadDefaultConfig(ctx)
+	require.NoError(t, err)
+	var awsClient aws.AWSCloudControlClient = cloudcontrol.NewFromConfig(cfg)
+	desiredState := map[string]interface{}{
+		"Name":                 streamName,
+		"RetentionPeriodHours": 180,
+		"ShardCount":           4,
+	}
+	desiredStateBytes, err := json.Marshal(desiredState)
+	require.NoError(t, err)
+
+	response, err := awsClient.CreateResource(ctx, &cloudcontrol.CreateResourceInput{
+		TypeName:     &resourceType,
+		DesiredState: awsgo.String(string(desiredStateBytes)),
+	})
+	require.NoError(t, err)
+	waitForSuccess(t, ctx, awsClient, response.ProgressEvent.RequestToken)
+
+	t.Cleanup(func() {
+		// Check if resource exists before issuing a delete because the AWS SDK async delete operation
+		// seems to fail if the resource does not exist
+		_, err := awsClient.GetResource(ctx, &cloudcontrol.GetResourceInput{
+			Identifier: &streamName,
+			TypeName:   &resourceType,
+		})
+		if aws.IsAWSResourceNotFound(err) {
+			return
+		}
+		// Just in case delete fails
+		deleteOutput, err := awsClient.DeleteResource(ctx, &cloudcontrol.DeleteResourceInput{
+			Identifier: &streamName,
+			TypeName:   &resourceType,
+		})
+		require.NoError(t, err)
+
+		// Ignoring status of delete since AWS command fails if the resource does not already exist
+		waitForSuccess(t, ctx, awsClient, deleteOutput.ProgressEvent.RequestToken)
+	})
+	// End of test setup
+}
+
+func waitForSuccess(t *testing.T, ctx context.Context, awsClient aws.AWSCloudControlClient, requestToken *string) {
+	// Wait till the create is complete
+	maxWaitTime := 300 * time.Second
+	waiter := cloudcontrol.NewResourceRequestSuccessWaiter(awsClient)
+	err := waiter.Wait(ctx, &cloudcontrol.GetResourceRequestStatusInput{
+		RequestToken: requestToken,
+	}, maxWaitTime)
+	require.NoError(t, err)
 }
