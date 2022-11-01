f0c350638c09e7688df9ca93496e8afc208a45e1
Bharath <99223262+bjoginapally@users.noreply.github.com>
2022-10-31 19:22:36 -0700

    Add etcd implementation of secret resource ucp (#4425)
    # Description
    
    Adding etcd implementation to secret resource in ucp
    
    ## Issue reference
    
    Fixes: #4379 
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [x] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [ ] Extended the documentation / Created issue for it
    
    Co-authored-by: Bharath Joginapally <bharath@Bharaths-MacBook-Pro.local>

---
+++ pkg/ucp/secret/etcd/client.go
@@ -0,0 +1,77 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package etcd
+
+import (
+	"context"
+
+	"github.com/project-radius/radius/pkg/ucp/secret"
+	"github.com/project-radius/radius/pkg/ucp/util"
+	etcdclient "go.etcd.io/etcd/client/v3"
+)
+
+const (
+	secretResourcePrefix = "secret|"
+)
+
+var _ secret.Client = (*Client)(nil)
+
+// Client represents radius secret client to manage radius secret.
+type Client struct {
+	ETCDClient *etcdclient.Client
+}
+
+// Save saves the secret in the data store.
+func (c *Client) Save(ctx context.Context, name string, value []byte) error {
+	if name == "" {
+		return &secret.ErrInvalid{Message: "invalid argument. 'name' is required"}
+	}
+
+	if value == nil {
+		return &secret.ErrInvalid{Message: "invalid argument. 'value' is required"}
+	}
+	secretName := generateSecretResourceName(name)
+
+	// We don't care about response while save, only if the operation is successful or not
+	_, err := c.ETCDClient.Put(ctx, secretName, string(value))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete deletes the secrets corresponding to the name in etcd.
+func (c *Client) Delete(ctx context.Context, name string) error {
+	secretName := generateSecretResourceName(name)
+	resp, err := c.ETCDClient.Delete(ctx, secretName)
+	if err != nil {
+		return err
+	}
+	if resp.Deleted == 0 {
+		return &secret.ErrNotFound{}
+	}
+	return nil
+}
+
+// Get returns the secret object if it exists or returns an error.
+func (c *Client) Get(ctx context.Context, name string) ([]byte, error) {
+	if name == "" {
+		return nil, &secret.ErrInvalid{Message: "invalid argument. 'name' is required"}
+	}
+	secretName := generateSecretResourceName(name)
+	resp, err := c.ETCDClient.Get(ctx, secretName)
+	if err != nil {
+		return nil, err
+	}
+	if resp.Count == 0 {
+		return nil, &secret.ErrNotFound{}
+	}
+	return resp.Kvs[0].Value, nil
+}
+
+func generateSecretResourceName(name string) string {
+	return secretResourcePrefix + util.NormalizeStringToLower(name)
+}
