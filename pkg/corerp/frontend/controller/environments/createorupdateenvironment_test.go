375ff7d9421cf034ce5b877de545d2f4df65d45a
snehabandla <snehab@microsoft.com>
2022-11-02 12:47:31 -0700

    removing unnecessary typing

--- pkg/corerp/frontend/controller/environments/createorupdateenvironment_test.go
+++ pkg/corerp/frontend/controller/environments/createorupdateenvironment_test.go
@@ -606,7 +606,7 @@ func TestGetDevRecipes(t *testing.T) {
 		devRecipes, err := getDevRecipes(ctx)
 		require.NoError(t, err)
 		expectedRecipes := map[string]datamodel.EnvironmentRecipeProperties{
-			"mongo-azure": datamodel.EnvironmentRecipeProperties{
+			"mongo-azure": {
 				LinkType:     "Applications.Link/mongoDatabases",
 				TemplatePath: "radiusdev.azurecr.io/recipes/mongodatabases/azure:1.0",
 			},
@@ -666,21 +666,21 @@ func TestParseRepoPathForMetadata(t *testing.T) {
 func TestEnsureUserRecipesHaveValidNames(t *testing.T) {
 	t.Run("No overlap between user and dev recipes", func(t *testing.T) {
 		devRecipes := map[string]datamodel.EnvironmentRecipeProperties{
-			"mongo-azure": datamodel.EnvironmentRecipeProperties{
+			"mongo-azure": {
 				LinkType:     "Applications.Link/mongoDatabases",
 				TemplatePath: "radiusdev.azurecr.io/recipes/mongodatabases/azure:1.0",
 			},
-			"redis-azure": datamodel.EnvironmentRecipeProperties{
+			"redis-azure": {
 				LinkType:     "Applications.Link/redisCache",
 				TemplatePath: "radiusdev.azurecr.io/recipes/redis/azure:1.0",
 			},
 		}
 		userRecipes := map[string]datamodel.EnvironmentRecipeProperties{
-			"user-mongo-azure": datamodel.EnvironmentRecipeProperties{
+			"user-mongo-azure": {
 				LinkType:     "Applications.Link/mongoDatabases",
 				TemplatePath: "radiusdev.azurecr.io/mongo:1.0",
 			},
-			"user-redis-azure": datamodel.EnvironmentRecipeProperties{
+			"user-redis-azure": {
 				LinkType:     "Applications.Link/redisCache",
 				TemplatePath: "radiusdev.azurecr.io/redis:1.0",
 			},
@@ -691,21 +691,21 @@ func TestEnsureUserRecipesHaveValidNames(t *testing.T) {
 	})
 	t.Run("Single recipe overlap between user and dev recipes", func(t *testing.T) {
 		devRecipes := map[string]datamodel.EnvironmentRecipeProperties{
-			"mongo-azure": datamodel.EnvironmentRecipeProperties{
+			"mongo-azure": {
 				LinkType:     "Applications.Link/mongoDatabases",
 				TemplatePath: "radiusdev.azurecr.io/recipes/mongodatabases/azure:1.0",
 			},
-			"redis-azure": datamodel.EnvironmentRecipeProperties{
+			"redis-azure": {
 				LinkType:     "Applications.Link/redisCache",
 				TemplatePath: "radiusdev.azurecr.io/recipes/redis/azure:1.0",
 			},
 		}
 		userRecipes := map[string]datamodel.EnvironmentRecipeProperties{
-			"mongo-azure": datamodel.EnvironmentRecipeProperties{
+			"mongo-azure": {
 				LinkType:     "Applications.Link/mongoDatabases",
 				TemplatePath: "radiusdev.azurecr.io/mongo:1.0",
 			},
-			"user-redis-azure": datamodel.EnvironmentRecipeProperties{
+			"user-redis-azure": {
 				LinkType:     "Applications.Link/redisCache",
 				TemplatePath: "radiusdev.azurecr.io/redis:1.0",
 			},
@@ -720,21 +720,21 @@ func TestEnsureUserRecipesHaveValidNames(t *testing.T) {
 	})
 	t.Run("Multiple recipe overlap between user and dev recipes", func(t *testing.T) {
 		devRecipes := map[string]datamodel.EnvironmentRecipeProperties{
-			"mongo-azure": datamodel.EnvironmentRecipeProperties{
+			"mongo-azure": {
 				LinkType:     "Applications.Link/mongoDatabases",
 				TemplatePath: "radiusdev.azurecr.io/recipes/mongodatabases/azure:1.0",
 			},
-			"redis-azure": datamodel.EnvironmentRecipeProperties{
+			"redis-azure": {
 				LinkType:     "Applications.Link/redisCache",
 				TemplatePath: "radiusdev.azurecr.io/recipes/redis/azure:1.0",
 			},
 		}
 		userRecipes := map[string]datamodel.EnvironmentRecipeProperties{
-			"mongo-azure": datamodel.EnvironmentRecipeProperties{
+			"mongo-azure": {
 				LinkType:     "Applications.Link/mongoDatabases",
 				TemplatePath: "radiusdev.azurecr.io/mongo:1.0",
 			},
-			"redis-azure": datamodel.EnvironmentRecipeProperties{
+			"redis-azure": {
 				LinkType:     "Applications.Link/redisCache",
 				TemplatePath: "radiusdev.azurecr.io/redis:1.0",
 			},
