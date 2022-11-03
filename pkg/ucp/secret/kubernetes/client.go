ffebf546b2716db70bddfe566e924912c95fc64e
Bharath <99223262+bjoginapally@users.noreply.github.com>
2022-11-03 14:52:41 -0700

    Add k8s secret implementation for ucp (#4463)
    # Description
    This change implements ability to create secrets as k8s secret in ucp
    
    ## Issue reference
    
    <!--
    We strive to have all PR being opened based on an issue, where the
    problem or feature have been discussed prior to implementation.
    -->
    
    Fixes: #4386 #4389 
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [x] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [ ] Extended the documentation / Created issue for it
    
    Co-authored-by: Bharath Joginapally <bharath@Bharaths-MacBook-Pro.local>

---
+++ pkg/ucp/secret/kubernetes/client.go
@@ -0,0 +1,116 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package kubernetes
+
+import (
+	"context"
+
+	"github.com/project-radius/radius/pkg/ucp/secret"
+	"github.com/project-radius/radius/pkg/ucp/util"
+	corev1 "k8s.io/api/core/v1"
+	k8s_error "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	controller_runtime "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	SecretKey       = "ucp_secret"
+	RadiusNamespace = "radius-system"
+)
+
+var _ secret.Client = (*Client)(nil)
+
+// Client implements secret storage for k8s.
+type Client struct {
+	K8sClient controller_runtime.Client
+}
+
+// Save saves the secret as a k8s secret resource.
+func (c *Client) Save(ctx context.Context, name string, value []byte) error {
+	if name == "" {
+		return &secret.ErrInvalid{Message: "invalid argument. 'name' is required"}
+	}
+
+	if value == nil {
+		return &secret.ErrInvalid{Message: "invalid argument. 'value' is required"}
+	}
+	secretName := util.NormalizeStringToLower(name)
+	secretObjectKey := controller_runtime.ObjectKey{
+		Name:      secretName,
+		Namespace: RadiusNamespace,
+	}
+	// build secret object
+	data := make(map[string][]byte)
+	data[SecretKey] = value
+	secret := &corev1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      secretName,
+			Namespace: RadiusNamespace,
+		},
+		Data: data,
+	}
+	// check if secret already exists or not
+	res := &corev1.Secret{}
+	err := c.K8sClient.Get(ctx, secretObjectKey, res)
+	if err != nil {
+		if is404Error(err) {
+			return c.K8sClient.Create(ctx, secret)
+		}
+		return err
+	}
+	return c.K8sClient.Update(ctx, secret)
+}
+
+// Delete deletes the secret resource with id.
+func (c *Client) Delete(ctx context.Context, name string) error {
+	if name == "" {
+		return &secret.ErrInvalid{Message: "invalid argument. 'name' is required"}
+	}
+	secretName := util.NormalizeStringToLower(name)
+	secretObject := &corev1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      secretName,
+			Namespace: RadiusNamespace,
+		},
+	}
+	err := c.K8sClient.Delete(ctx, secretObject)
+	if err != nil {
+		if is404Error(err) {
+			return &secret.ErrNotFound{}
+		}
+		return err
+	}
+	return nil
+}
+
+// Get returns the id if secret exists otherwise error.
+func (c *Client) Get(ctx context.Context, name string) ([]byte, error) {
+	if name == "" {
+		return nil, &secret.ErrInvalid{Message: "invalid argument. 'name' is required"}
+	}
+	secretName := util.NormalizeStringToLower(name)
+	res := &corev1.Secret{}
+	secretObjectKey := controller_runtime.ObjectKey{
+		Name:      secretName,
+		Namespace: RadiusNamespace,
+	}
+	err := c.K8sClient.Get(ctx, secretObjectKey, res)
+	if err != nil {
+		if is404Error(err) {
+			return nil, &secret.ErrNotFound{}
+		}
+		return nil, err
+	}
+	return res.Data[SecretKey], nil
+}
+
+func is404Error(err error) bool {
+	statusErr, ok := err.(*k8s_error.StatusError)
+	if ok && statusErr.ErrStatus.Code == 404 {
+		return true
+	}
+	return false
+}
---
+++ pkg/ucp/secret/kubernetes/client_test.go
@@ -0,0 +1,136 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package kubernetes
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	rp_testing "github.com/project-radius/radius/pkg/corerp/testing"
+	"github.com/project-radius/radius/pkg/ucp/secret"
+	"github.com/stretchr/testify/require"
+	"k8s.io/kubectl/pkg/scheme"
+)
+
+const (
+	SecretName = "test_secret_name"
+)
+
+func Test_Save(t *testing.T) {
+	k8sFakeClient := Client{
+		K8sClient: rp_testing.NewFakeKubeClient(scheme.Scheme),
+	}
+	ctx := context.Background()
+	secretValue, err := json.Marshal("test_secret_value")
+	require.NoError(t, err)
+	updatedSecretValue, err := json.Marshal("updated_secret_value")
+	require.NoError(t, err)
+	tests := []struct {
+		testName    string
+		secretName  string
+		secretValue []byte
+		update      bool
+		err         error
+	}{
+		{"save-new-secret", SecretName, secretValue, false, nil},
+		{"update-secret", SecretName, secretValue, true, nil},
+		{"save-with-invalid-name", "", secretValue, false, &secret.ErrInvalid{Message: "invalid argument. 'name' is required"}},
+		{"save-with-empty-secret", SecretName, nil, false, &secret.ErrInvalid{Message: "invalid argument. 'value' is required"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			err := k8sFakeClient.Save(ctx, tt.secretName, tt.secretValue)
+			require.Equal(t, err, tt.err)
+			if tt.update {
+				err := k8sFakeClient.Save(ctx, tt.secretName, updatedSecretValue)
+				require.Equal(t, err, tt.err)
+			}
+			// if save is expected to succeed, then compare saved secret and delete after test
+			if tt.err == nil {
+				res, err := k8sFakeClient.Get(ctx, tt.secretName)
+				require.NoError(t, err)
+				if tt.update {
+					require.Equal(t, res, updatedSecretValue)
+				} else {
+					require.Equal(t, res, secretValue)
+				}
+				err = k8sFakeClient.Delete(ctx, tt.secretName)
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func Test_Get(t *testing.T) {
+	k8sFakeClient := Client{
+		K8sClient: rp_testing.NewFakeKubeClient(scheme.Scheme),
+	}
+	ctx := context.Background()
+	secretValue, err := json.Marshal("test_secret_value")
+	require.NoError(t, err)
+	tests := []struct {
+		testName   string
+		secretName string
+		save       bool
+		err        error
+	}{
+		{"get-secret", SecretName, true, nil},
+		{"get-non-existent-secret", SecretName, false, &secret.ErrNotFound{}},
+		{"get-with-invalid-name", "", false, &secret.ErrInvalid{Message: "invalid argument. 'name' is required"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			if tt.save {
+				err := k8sFakeClient.Save(ctx, tt.secretName, secretValue)
+				require.NoError(t, err)
+			}
+			res, err := k8sFakeClient.Get(ctx, tt.secretName)
+			require.Equal(t, err, tt.err)
+			// if the get is successful then compare for values
+			if tt.err == nil {
+				require.Equal(t, res, secretValue)
+			}
+			// if secret is saved, cleanup secret at the end
+			if tt.save {
+				err = k8sFakeClient.Delete(ctx, tt.secretName)
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func Test_Delete(t *testing.T) {
+	k8sFakeClient := Client{
+		K8sClient: rp_testing.NewFakeKubeClient(scheme.Scheme),
+	}
+	ctx := context.Background()
+	secretValue, err := json.Marshal("test_secret_value")
+	require.NoError(t, err)
+	tests := []struct {
+		testName   string
+		secretName string
+		save       bool
+		err        error
+	}{
+		{"delete-secret", SecretName, true, nil},
+		{"delete-non-existent-secret", SecretName, false, &secret.ErrNotFound{}},
+		{"delete-with-invalid-name", "", false, &secret.ErrInvalid{Message: "invalid argument. 'name' is required"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			if tt.save {
+				err := k8sFakeClient.Save(ctx, tt.secretName, secretValue)
+				require.NoError(t, err)
+			}
+			err = k8sFakeClient.Delete(ctx, tt.secretName)
+			require.Equal(t, err, tt.err)
+		})
+	}
+}
