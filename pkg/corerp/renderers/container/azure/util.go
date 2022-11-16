6b09257cc533cc30902a01b40b3eaebff21467a3
Young Bu Park <youngp@microsoft.com>
2022-11-15 22:26:23 -0800

    added comment

--- pkg/corerp/renderers/container/azure/util.go
+++ pkg/corerp/renderers/container/azure/util.go
@@ -7,7 +7,12 @@ package azure
 
 import "strings"
 
-// MakeResourceName builds resource name based on name with prefix for azure resources.
+// MakeResourceName builds resource name with prefix for azure resources.
+// For instance, when user uses keyvault persistent volume, RP will
+// auto-provision per-container managed identity in the resource group
+// which is specified by environment resource. In this case,
+// RP uses application name as prefix to avoid the name conflict in the same
+// resource group.
 func MakeResourceName(name string, prefix ...string) string {
 	var sb strings.Builder
 
