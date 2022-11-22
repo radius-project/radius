d7da821ce6068d838da682d8be6608fea18bb7ac
Young Bu Park <youngp@microsoft.com>
2022-11-22 10:30:55 -0800

    fix

--- pkg/rp/kube/resources_test.go
+++ pkg/rp/kube/resources_test.go
@@ -6,15 +6,20 @@
 package kube
 
 import (
+	"context"
 	"encoding/json"
+	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/project-radius/radius/pkg/armrpc/api/conv"
+	"github.com/project-radius/radius/pkg/corerp/datamodel"
+	"github.com/project-radius/radius/pkg/ucp/dataprovider"
 	"github.com/project-radius/radius/pkg/ucp/store"
+	"github.com/stretchr/testify/require"
 )
 
 const (
 	testEnvID = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/radius-test-rg/providers/Applications.Core/environments/env"
-	testAppID = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/radius-test-rg/providers/Applications.Core/applications/app"
 )
 
 func fakeStoreObject(dm conv.DataModelInterface) *store.Object {
@@ -28,4 +33,52 @@ func fakeStoreObject(dm conv.DataModelInterface) *store.Object {
 		return nil
 	}
 	return &store.Object{Data: r}
+}
+
+func TestFindNamespaceByEnvID(t *testing.T) {
+	mctrl := gomock.NewController(t)
+
+	nsTests := []struct {
+		desc string
+		prop datamodel.KubernetesComputeProperties
+		id   string
+		out  string
+	}{
+		{
+			desc: "namespace is defined",
+			prop: datamodel.KubernetesComputeProperties{
+				Namespace: "default-ns",
+			},
+			id:  testEnvID,
+			out: "default-ns",
+		},
+		{
+			desc: "undefined namespace",
+			prop: datamodel.KubernetesComputeProperties{},
+			id:   testEnvID,
+			out:  "env",
+		},
+	}
+
+	for _, tc := range nsTests {
+		t.Run(tc.desc, func(t *testing.T) {
+			envdm := &datamodel.Environment{
+				Properties: datamodel.EnvironmentProperties{
+					Compute: datamodel.EnvironmentCompute{
+						Kind:              datamodel.KubernetesComputeKind,
+						KubernetesCompute: tc.prop,
+					},
+				},
+			}
+
+			mockSP := dataprovider.NewMockDataStorageProvider(mctrl)
+			mockSC := store.NewMockStorageClient(mctrl)
+
+			mockSP.EXPECT().GetStorageClient(gomock.Any(), gomock.Any()).Return(store.StorageClient(mockSC), nil).Times(1)
+			mockSC.EXPECT().Get(gomock.Any(), tc.id, gomock.Any()).Return(fakeStoreObject(envdm), nil).Times(1)
+			ns, err := FindNamespaceByEnvID(context.Background(), mockSP, testEnvID)
+			require.NoError(t, err)
+			require.Equal(t, tc.out, ns)
+		})
+	}
 }
