319430dbb428d55831b2db7c6872d9ca2d9b3ed3
nithyatsu <nithyasu@microsoft.com>
2022-11-29 11:52:41 -0800

    add magpie and test sample for https gateway

--- test/magpiego/main.go
+++ test/magpiego/main.go
@@ -4,21 +4,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	log.Println(fmt.Sprintf("Server running at http://localhost:%s", port))
 	log.Println(fmt.Sprintf("Check http://localhost:%s/healthz for status", port))
-	crt := os.Getenv("CERT")
-	key := os.Getenv("KEY")
 
-	if crt == "" {
-		err := startMagpieServer()
+	crt := strings.TrimSpace(os.Getenv("CERT"))
+	key := strings.TrimSpace(os.Getenv("KEY"))
+	var err error
+	if crt == "" && key == "" {
+		log.Println("Starting magpie in http mode")
+		err = startMagpieServer()
+	} else if crt == "" || key == "" {
+		log.Println("certificate or key is not provided. Starting magpie in http mode")
+		err = startMagpieServer()
 	} else {
-		err := startSecureMagpieServer([]byte(crt), []byte(key))
+		log.Println("Starting magpie in https mode")
+		err = startSecureMagpieServer([]byte(crt), []byte(key))
 	}
 	if err != nil {
-		log.Println("Terminating Magpie")
+		log.Println("Terminating Magpie. Encountered error - ", err.Error())
 		os.Exit(1)
 	}
 }
