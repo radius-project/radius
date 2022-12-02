4b49355b1011644494bf8149b75bf5c8ff44ffe2
snehabandla <snehab@microsoft.com>
2022-12-02 13:00:47 -0800

    Splitting the two tests apart

--- test/functional/corerp/resources/mongodb_test.go
+++ test/functional/corerp/resources/mongodb_test.go
@@ -112,6 +112,55 @@ func Test_MongoDBUserSecrets(t *testing.T) {
 // container using the mongoDB link to connect to the mongoDB resource
 func Test_MongoDB_Recipe(t *testing.T) {
 
+	template := "testdata/corerp-resources-mongodb-recipe.bicep"
+	name := "corerp-resources-mongodb-recipe"
+
+	requiredSecrets := map[string]map[string]string{}
+
+	test := corerp.NewCoreRPTest(t, name, []corerp.TestStep{
+		{
+			Executor: step.NewDeployExecutor(template, functional.GetMagpieImage()),
+			CoreRPResources: &validation.CoreRPResourceSet{
+				Resources: []validation.CoreRPResource{
+					{
+						Name: "corerp-resources-environment-recipes-env",
+						Type: validation.EnvironmentsResource,
+					},
+					{
+						Name: "corerp-resources-mongodb-recipe",
+						Type: validation.ApplicationsResource,
+						App:  name,
+					},
+					{
+						Name: "mongodb-recipe-app-ctnr",
+						Type: validation.ContainersResource,
+						App:  name,
+					},
+					{
+						Name: "mongo-recipe-db",
+						Type: validation.MongoDatabasesResource,
+						App:  name,
+					},
+				},
+			},
+			K8sObjects: &validation.K8sObjectSet{
+				Namespaces: map[string][]validation.K8sObject{
+					"corerp-resources-environment-recipes-env": {
+						validation.NewK8sPodForResource(name, "mongodb-recipe-app-ctnr"),
+					},
+				},
+			},
+		},
+	}, requiredSecrets)
+
+	test.Test(t)
+}
+
+// Test_MongoDB_Recipe validates:
+// the creation of a mongoDB from a devrecipe that is linked to the environment when created with useDevRecipes = true
+// the container using the mongoDB link to connect to the mongoDB resource
+func Test_MongoDB_DevRecipe(t *testing.T) {
+
 	template := "testdata/corerp-resources-mongodb-devrecipe.bicep"
 	name := "corerp-resources-mongodb-devrecipe"
 
