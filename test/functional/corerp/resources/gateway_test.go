5cc3fc538d34d2f2c4d8e7b78d19ce8bf7b4d42e
nithyatsu <nithyasu@microsoft.com>
2022-12-01 21:28:20 -0800

    Revert "one last fix"
    This reverts commit cdadd6c3fae6b1284c5363a8451e45db43a3692d.

--- test/functional/corerp/resources/gateway_test.go
+++ test/functional/corerp/resources/gateway_test.go
@@ -126,7 +126,9 @@ func testGatewayWithPortForward(t *testing.T, ctx context.Context, at corerp.Cor
 		return fmt.Errorf("portforward failed with error: %s", err)
 	case localPort := <-portChan:
 		baseURL := fmt.Sprintf("http://localhost:%d", localPort)
-
+		if isHttps {
+			baseURL = fmt.Sprintf("http://hostname:%d", localPort)
+		}
 		t.Logf("Portforward session active at %s", baseURL)
 
 		if isHttps {
@@ -235,7 +237,9 @@ func testGatewayAvailability(t *testing.T, hostname, baseURL, path string, expec
 		return err
 	}
 
-	req.Host = hostname
+	if !isHttps {
+		req.Host = hostname
+	}
 
 	// Send requests to backing container via port-forward
 	response, err := autorest.SendWithSender(
