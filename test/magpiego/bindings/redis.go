ea6d1b68741e20380f814614f8751ecfd273db7c
Young Bu Park <youngp@microsoft.com>
2022-12-02 20:26:07 -0800

    fix redis option

--- test/magpiego/bindings/redis.go
+++ test/magpiego/bindings/redis.go
@@ -2,6 +2,7 @@ package bindings
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"time"
@@ -18,13 +19,18 @@ func RedisBinding(envParams map[string]string) BindingStatus {
 		return BindingStatus{false, "Redis HOST and PORT are required"}
 	}
 	redisPassword := envParams["PASSWORD"]
-	op := &redis.Options{Addr: redisHost, Password: redisPassword, WriteTimeout: 5 * time.Second}
+	op := &redis.Options{
+		Addr:         redisHost,
+		Password:     redisPassword,
+		WriteTimeout: 5 * time.Second,
+		TLSConfig:    &tls.Config{},
+	}
 	client := redis.NewClient(op)
 
 	ctx := context.Background()
 	err = client.Ping(ctx).Err()
 	if err != nil {
-		log.Println(fmt.Sprintf("failed to connect with redis instance at %s - %v", redisHost, err.Error()))
+		log.Printf("failed to connect with redis instance at %s - %v\n", redisHost, err.Error()))
 		return BindingStatus{false, "not connected"}
 	}
 	return BindingStatus{true, "connected"}
