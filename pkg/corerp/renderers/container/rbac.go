6ee4c04fc975e54a0b3eba0fafba8bc3c26067b6
Young Bu Park <youngp@microsoft.com>
2022-12-05 13:53:54 -0800

    Create per container role (#4809)
    # Description
    
    This is to create role and rolebinding for container when user uses
    non-default namespace.
    
    ## Issue reference
    
    <!--
    We strive to have all PR being opened based on an issue, where the
    problem or feature have been discussed prior to implementation.
    -->
    
    Fixes: #4808
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [x] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [ ] Extended the documentation / Created issue for it

---
+++ pkg/corerp/renderers/container/rbac.go
@@ -0,0 +1,88 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package container
+
+import (
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/project-radius/radius/pkg/corerp/datamodel"
+	"github.com/project-radius/radius/pkg/kubernetes"
+	"github.com/project-radius/radius/pkg/resourcekinds"
+	"github.com/project-radius/radius/pkg/rp/outputresource"
+)
+
+func makeRBACRole(appName, name, namespace string, resource *datamodel.ContainerResource) *outputresource.OutputResource {
+	labels := kubernetes.MakeDescriptiveLabels(appName, resource.Name, resource.Type)
+
+	role := &rbacv1.Role{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Role",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    labels,
+		},
+		// At this time, we support only secret rbac permission for the namespace.
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{""},
+				Resources: []string{"secrets"},
+				Verbs:     []string{"get", "list"},
+			},
+		},
+	}
+
+	or := outputresource.NewKubernetesOutputResource(
+		resourcekinds.KubernetesRole,
+		outputresource.LocalIDKubernetesRole,
+		role,
+		role.ObjectMeta)
+
+	return &or
+}
+
+func makeRBACRoleBinding(appName, name, saName, namespace string, resource *datamodel.ContainerResource) *outputresource.OutputResource {
+	labels := kubernetes.MakeDescriptiveLabels(appName, resource.Name, resource.Type)
+
+	bindings := &rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    labels,
+		},
+		RoleRef: rbacv1.RoleRef{
+			Kind:     "Role",
+			Name:     name,
+			APIGroup: "rbac.authorization.k8s.io",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind: "ServiceAccount",
+				Name: saName,
+			},
+		},
+	}
+
+	or := outputresource.NewKubernetesOutputResource(
+		resourcekinds.KubernetesRoleBinding,
+		outputresource.LocalIDKubernetesRoleBinding,
+		bindings,
+		bindings.ObjectMeta)
+
+	or.Dependencies = []outputresource.Dependency{
+		{
+			LocalID: outputresource.LocalIDKubernetesRole,
+		},
+	}
+	return &or
+}
