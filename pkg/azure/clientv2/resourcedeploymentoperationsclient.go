e9d5d1887b66efa43b0c692fec7e25395330b75f
Yetkin Timocin <ytimocin@gmail.com>
2022-12-13 12:42:06 -0800

    Updating customAction - wip

--- pkg/azure/clientv2/resourcedeploymentoperationsclient.go
+++ pkg/azure/clientv2/resourcedeploymentoperationsclient.go
@@ -7,13 +7,12 @@ package clientv2
 
 import (
 	"context"
-	"net/http"
 
-	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/resources"
-	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/to"
 )
 
 // ResourceDeploymentOperationsClient is an operations client which takes in a resourceID as the destination to query.
@@ -22,15 +21,31 @@ type ResourceDeploymentOperationsClient struct {
 	armresources.DeploymentOperationsClient
 }
 
-// NewResourceDeploymentOperationsClientWithBaseURI creates an instance of the ResourceOperations client using a custom endpoint.  Use
-// this when interacting with UCP resources that uses a non-standard base URI.
-func NewResourceDeploymentOperationsClientWithBaseURI(subscriptionID string, baseURI string) (*ResourceDeploymentOperationsClient, error) {
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
+// NewResourceDeploymentOperationsClient creates an instance of the ResourceDeploymentOperations client using the default endpoint.
+func NewResourceDeploymentOperationsClient(cred azcore.TokenCredential, subscriptionID string) (*ResourceDeploymentOperationsClient, error) {
+	client, err := NewResourceDeploymentOperationsClientWithBaseURI(cred, subscriptionID, DefaultBaseURI)
 	if err != nil {
 		return nil, err
 	}
 
-	client, err := armresources.NewDeploymentOperationsClient(subscriptionID, cred, nil)
+	return client, err
+}
+
+// NewResourceDeploymentOperationsClientWithBaseURI creates an instance of the ResourceDeploymentOperations client using a custom endpoint.
+// Use this when interacting with UCP resources that uses a non-standard base URI.
+func NewResourceDeploymentOperationsClientWithBaseURI(cred azcore.TokenCredential, subscriptionID string, baseURI string) (*ResourceDeploymentOperationsClient, error) {
+	options := &arm.ClientOptions{
+		ClientOptions: azcore.ClientOptions{
+			Cloud: cloud.Configuration{
+				Services: map[cloud.ServiceName]cloud.ServiceConfiguration{
+					cloud.ResourceManager: {
+						Endpoint: baseURI,
+					},
+				},
+			},
+		},
+	}
+	client, err := armresources.NewDeploymentOperationsClient(subscriptionID, cred, options)
 	if err != nil {
 		return nil, err
 	}
@@ -45,7 +60,7 @@ func NewResourceDeploymentOperationsClientWithBaseURI(subscriptionID string, bas
 func (client *ResourceDeploymentOperationsClient) List(ctx context.Context, resourceGroupName string, deploymentName string, resourceID string, top *int32) (*armresources.DeploymentOperationsListResult, error) {
 	result := &armresources.DeploymentOperationsListResult{
 		Value:    make([]*armresources.DeploymentOperation, 0),
-		NextLink: to.StringPtr(""),
+		NextLink: to.Ptr(""),
 	}
 	// TODO: Validate resourceID
 	pager := client.NewListPager(resourceGroupName, deploymentName,
@@ -59,62 +74,8 @@ func (client *ResourceDeploymentOperationsClient) List(ctx context.Context, reso
 			return result, err
 		}
 		deploymentOperationsList := nextPage.Value
-		for _, deploymentOperation := range deploymentOperationsList {
-			result.Value = append(result.Value, *&deploymentOperation)
-		}
-	}
-	return nil, nil
-}
-
-// ListPreparer prepares the List request.
-func (client ResourceDeploymentOperationsClient) ListPreparer(ctx context.Context, resourceId string, top *int32) (*http.Request, error) {
-	const APIVersion = "2020-10-01"
-	queryParameters := map[string]interface{}{
-		"api-version": APIVersion,
-	}
-	if top != nil {
-		queryParameters["$top"] = autorest.Encode("query", *top)
+		result.Value = append(result.Value, deploymentOperationsList...)
 	}
 
-	preparer := autorest.CreatePreparer(
-		autorest.AsGet(),
-		// autorest.WithBaseURL(client.BaseURI),
-		autorest.WithPath(resourceId+"/operations"),
-		autorest.WithQueryParameters(queryParameters))
-	return preparer.Prepare((&http.Request{}).WithContext(ctx))
-}
-
-// listNextResults retrieves the next set of results, if any.
-func (client ResourceDeploymentOperationsClient) listNextResults(ctx context.Context, lastResults resources.DeploymentOperationsListResult) (result resources.DeploymentOperationsListResult, err error) {
-	req, err := deploymentOperationsListResultPreparer(lastResults, ctx)
-	if err != nil {
-		return result, autorest.NewErrorWithError(err, "resources.DeploymentOperationsClient", "listNextResults", nil, "Failure preparing next results request")
-	}
-	if req == nil {
-		return
-	}
-
-	// TODO: Find out what to use here.
-	// resp, err := client.ListSender(req)
-	// if err != nil {
-	// 	result.Response = autorest.Response{Response: resp}
-	// 	return result, autorest.NewErrorWithError(err, "resources.DeploymentOperationsClient", "listNextResults", resp, "Failure sending next results request")
-	// }
-	// result, err = client.ListResponder(resp)
-	// if err != nil {
-	// 	err = autorest.NewErrorWithError(err, "resources.DeploymentOperationsClient", "listNextResults", resp, "Failure responding to next results request")
-	// }
-	return
-}
-
-// deploymentOperationsListResultPreparer prepares a request to retrieve the next set of results.
-// It returns nil if no more results exist.
-func deploymentOperationsListResultPreparer(dolr resources.DeploymentOperationsListResult, ctx context.Context) (*http.Request, error) {
-	if dolr.NextLink == nil || len(*dolr.NextLink) == 0 {
-		return nil, nil
-	}
-	return autorest.Prepare((&http.Request{}).WithContext(ctx),
-		autorest.AsJSON(),
-		autorest.AsGet(),
-		autorest.WithBaseURL(to.String(dolr.NextLink)))
+	return result, nil
 }
--- pkg/azure/clientv2/unfold.go
+++ pkg/azure/clientv2/unfold.go
@@ -11,9 +11,9 @@ import (
 	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
-	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/google/go-cmp/cmp"
 	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
+	"github.com/project-radius/radius/pkg/cli/clients_new/generated"
 )
 
 // ServiceError conforms to the OData v4 error format.
@@ -26,9 +26,9 @@ type ServiceError struct {
 	Code           string                   `json:"code,omitempty" yaml:"code,omitempty"`
 	Message        string                   `json:"message,omitempty" yaml:"message,omitempty"`
 	Target         *string                  `json:"target,omitempty" yaml:"target,omitempty"`
-	Details        []*v1.ErrorDetails       `json:"details,omitempty" yaml:"details,omitempty"`
-	InnerError     map[string]interface{}   `json:"innererror,omitempty" yaml:"innererror,omitempty"`
-	AdditionalInfo []map[string]interface{} `json:"additionalInfo,omitempty" yaml:"additionalInfo,omitempty"`
+	Details        []*generated.ErrorDetail `json:"details,omitempty" yaml:"details,omitempty"`
+	InnerError     map[string]any           `json:"innererror,omitempty" yaml:"innererror,omitempty"`
+	AdditionalInfo []map[string]any         `json:"additionalInfo,omitempty" yaml:"additionalInfo,omitempty"`
 }
 
 // UnfoldServiceError unfolds the Details field in the given azure.ServiceError,
@@ -37,31 +37,31 @@ type ServiceError struct {
 //
 // This is needed because for custom RP, errors are not treated as structured
 // JSON, even if we follow the error format from ARM.
-func UnfoldServiceError(in *azure.ServiceError) *ServiceError {
+func UnfoldServiceError(in *generated.ErrorDetail) *ServiceError {
 	out := &ServiceError{
-		Code:           in.Code,
-		Message:        in.Message,
-		Target:         in.Target,
-		InnerError:     in.InnerError,
-		AdditionalInfo: in.AdditionalInfo,
+		Code:    *in.Code,
+		Message: *in.Message,
+		Target:  in.Target,
+		// InnerError:     in.Details,
+		// AdditionalInfo: in.AdditionalInfo,
 	}
 	// Now, the details defined in azure.ServiceError is unstructured, but
 	// it is in fact has structure based on OData V4. We will reparse.
 	if in.Details == nil {
 		return out
 	}
-	out.Details = make([]*v1.ErrorDetails, len(in.Details))
+	out.Details = make([]*generated.ErrorDetail, len(in.Details))
 	for i, d := range in.Details {
-		out.Details[i] = &v1.ErrorDetails{}
+		out.Details[i] = &generated.ErrorDetail{}
 		// First we attempt to deserialize this raw form to the format
 		// of armerrors.ErrorDetail.
 		if err := roundTripJSON(d, out.Details[i]); err != nil {
 			// If the deserialization didn't work, we fall back to
 			// just extracting out the fields using the contract in OData V4 error.
-			*out.Details[i] = v1.ErrorDetails{
-				Code:    *extractString(d["code"]),
-				Message: *extractString(d["message"]),
-				Target:  *extractString(d["target"]),
+			*out.Details[i] = generated.ErrorDetail{
+				Code:    extractString(d.Code),
+				Message: extractString(d.Message),
+				Target:  extractString(d.Target),
 			}
 		}
 		// Since these Details may have raw JSON in their Message field,
@@ -75,67 +75,60 @@ func UnfoldServiceError(in *azure.ServiceError) *ServiceError {
 
 // UnfoldErrorDetails extract the Message field of a given *radclient.ErrorDetail
 // into its correspoding Details field, which is structured.
-func UnfoldErrorDetails(d *v1.ErrorDetails) v1.ErrorDetails {
+func UnfoldErrorDetails(d *generated.ErrorDetail) generated.ErrorDetail {
 	if d == nil {
-		return v1.ErrorDetails{}
+		return generated.ErrorDetail{}
 	}
 
 	new := *d
-	if new.Target != "" {
-		new.Target = ""
+	if new.Target != nil {
+		// FIXME: Why do we have this logic?
+		new.Target = nil
 	}
 
 	for i := range new.Details {
-		new.Details[i] = UnfoldErrorDetails(&new.Details[i])
+		errorDetails := UnfoldErrorDetails(new.Details[i])
+		new.Details[i] = &errorDetails
 	}
 
-	if new.Message == "" {
+	if new.Message == nil {
 		return new
 	}
 
-	resp := v1.ErrorResponse{}
-	err := json.Unmarshal([]byte(d.Message), &resp)
+	resp := &generated.ErrorResponse{}
+	err := json.Unmarshal([]byte(*d.Message), &resp)
 	if err != nil || cmp.Equal(resp.Error, v1.ErrorDetails{}) {
 		return new
 	}
 
 	// We successfully parse an v1.ErrorResponse from the message.
 	// Let's move that information into the structured details.
-	new.Message = ""
-	new.Details = append(new.Details, UnfoldErrorDetails(&resp.Error))
+	new.Message = nil
+	errorDetails := UnfoldErrorDetails(resp.Error)
+	new.Details = append(new.Details, &errorDetails)
 	return new
 }
 
 type WrappedErrorResponse struct {
-	ErrorResponse v1.ErrorResponse
+	ErrorResponse generated.ErrorResponse
 }
 
 func (w WrappedErrorResponse) Error() string {
-	return w.ErrorResponse.Error.Message
+	return *w.ErrorResponse.Error.Message
 }
 
 // TryUnfoldErrorResponse takes an error that wrapped a radclient.ErrorResponse
 // and unfold nested JSON messages into structured radclient.ErrorDetail field.
 //
 // If the given error isn't wrapping a *radclient.ErrorResponse, nil is returned.
-func TryUnfoldErrorResponse(err error) *v1.ErrorDetails {
+func TryUnfoldErrorResponse(err error) *generated.ErrorDetail {
 	inner, ok := errors.Unwrap(err).(WrappedErrorResponse)
 	if cmp.Equal(inner.ErrorResponse.Error, v1.ErrorDetails{}) || !ok {
 		return nil
 	}
 
-	errDetails := UnfoldErrorDetails(&inner.ErrorResponse.Error)
+	errDetails := UnfoldErrorDetails(inner.ErrorResponse.Error)
 	return &errDetails
-}
-
-// TryUnfoldServiceError calls UnfoldServiceError if the given error is
-// of the type ServiceError.  Otherwise, returns nil.
-func TryUnfoldServiceError(err error) *ServiceError {
-	svcError, ok := err.(*azure.ServiceError)
-	if !ok {
-		return nil
-	}
-	return UnfoldServiceError(svcError)
 }
 
 func roundTripJSON(input interface{}, output interface{}) error {
