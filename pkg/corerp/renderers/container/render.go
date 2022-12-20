398850dfbd497cef216db01aebfc392daef6a88e
Ryan Nowak <nowakra@gmail.com>
2022-12-20 10:46:14 -0800

    Disable K8s Service Links (#4870)
    Fixes: #3002
    
    This change disables Kubernetes service links, a feature that injects
    environment variables for each service into pods. We're disabling this
    feature because it has plenty of issues and it's platform-specific
    (kubernetes). See the issues mentioned in comments for context.
    
    Radius provides its own service-discovery mechanism without the same
    problems (connections).

--- pkg/corerp/renderers/container/render.go
+++ pkg/corerp/renderers/container/render.go
@@ -20,6 +20,7 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/project-radius/radius/pkg/armrpc/api/conv"
 	"github.com/project-radius/radius/pkg/corerp/datamodel"
 	"github.com/project-radius/radius/pkg/corerp/handlers"
@@ -490,6 +491,13 @@ func (r Renderer) makeDeployment(ctx context.Context, applicationName string, op
 					Annotations: map[string]string{},
 				},
 				Spec: corev1.PodSpec{
+					// See: https://github.com/kubernetes/kubernetes/issues/92226 and
+					// 		https://github.com/project-radius/radius/issues/3002
+					//
+					// Service links are a flawed and Kubernetes-only feature that we don't
+					// want to leak into Radius containers.
+					EnableServiceLinks: to.Ptr(false),
+
 					ServiceAccountName: serviceAccountName,
 					Containers:         []corev1.Container{container},
 					Volumes:            volumes,
