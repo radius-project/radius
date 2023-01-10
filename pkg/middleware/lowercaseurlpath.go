7acf77ad250f1bc6d91770da0cbedf03942c48d1
Young Bu Park <youngp@microsoft.com>
2023-01-10 10:50:19 -0800

    updated comment

--- pkg/middleware/lowercaseurlpath.go
+++ pkg/middleware/lowercaseurlpath.go
@@ -15,7 +15,7 @@ import (
 // LowercaseURLPath is the middelware to lowercase the incoming request url path.
 func LowercaseURLPath(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		// ARM populates "Referer" header in the request which can be used for FQDN of the resource.
+		// UCP/ARM populates "Referer" header in the request which can be used for FQDN of the resource.
 		// This is the fallback setting "Referer" header to save the original URL for UCP scenario.
 		// https://github.com/Azure/azure-resource-manager-rpc/blob/master/v1.0/common-api-details.md#proxy-request-header-modifications
 		if r.Header.Get(v1.RefererHeader) == "" {
