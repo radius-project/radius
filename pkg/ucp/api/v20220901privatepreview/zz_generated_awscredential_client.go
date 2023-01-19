8a7eb747a0a7803f9b7b59faef04f345f6c28918
Bharath <99223262+bjoginapally@users.noreply.github.com>
2023-01-19 11:51:13 -0800

    Move credential clients into single file and refactor example names (#4985)
    # Description
    
    - Right now there are two credential clients one for put and other for
    get, list, delete. This change gets a single client for all credential
    operations
    issue: NA
    
    ## Issue reference
    
    <!--
    We strive to have all PR being opened based on an issue, where the
    problem or feature have been discussed prior to implementation.
    -->
    
    Fixes: Bug Fix
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [ ] Extended the documentation / Created issue for it
    
    Co-authored-by: Bharath Joginapally <bharath@Bharaths-MacBook-Pro.local>

--- pkg/ucp/api/v20220901privatepreview/zz_generated_awscredential_client.go
+++ pkg/ucp/api/v20220901privatepreview/zz_generated_awscredential_client.go
@@ -111,3 +111,160 @@ func (client *AWSCredentialClient) createOrUpdateHandleResponse(resp *http.Respo
 	return result, nil
 }
 
+// Delete - Delete credential resource for this plane instance
+// If the operation fails it returns an *azcore.ResponseError type.
+// Generated from API version 2022-09-01-privatepreview
+// planeType - The type of the plane
+// planeName - The name of the plane
+// credentialName - The name of the credential
+// options - AWSCredentialClientDeleteOptions contains the optional parameters for the AWSCredentialClient.Delete method.
+func (client *AWSCredentialClient) Delete(ctx context.Context, planeType string, planeName string, credentialName string, options *AWSCredentialClientDeleteOptions) (AWSCredentialClientDeleteResponse, error) {
+	req, err := client.deleteCreateRequest(ctx, planeType, planeName, credentialName, options)
+	if err != nil {
+		return AWSCredentialClientDeleteResponse{}, err
+	}
+	resp, err := client.pl.Do(req)
+	if err != nil {
+		return AWSCredentialClientDeleteResponse{}, err
+	}
+	if !runtime.HasStatusCode(resp, http.StatusOK, http.StatusNoContent) {
+		return AWSCredentialClientDeleteResponse{}, runtime.NewResponseError(resp)
+	}
+	return AWSCredentialClientDeleteResponse{}, nil
+}
+
+// deleteCreateRequest creates the Delete request.
+func (client *AWSCredentialClient) deleteCreateRequest(ctx context.Context, planeType string, planeName string, credentialName string, options *AWSCredentialClientDeleteOptions) (*policy.Request, error) {
+	urlPath := "/planes/{planeType}/{planeName}/providers/System.AWS/credentials/{credentialName}"
+	if planeType == "" {
+		return nil, errors.New("parameter planeType cannot be empty")
+	}
+	urlPath = strings.ReplaceAll(urlPath, "{planeType}", url.PathEscape(planeType))
+	if planeName == "" {
+		return nil, errors.New("parameter planeName cannot be empty")
+	}
+	urlPath = strings.ReplaceAll(urlPath, "{planeName}", url.PathEscape(planeName))
+	if credentialName == "" {
+		return nil, errors.New("parameter credentialName cannot be empty")
+	}
+	urlPath = strings.ReplaceAll(urlPath, "{credentialName}", url.PathEscape(credentialName))
+	req, err := runtime.NewRequest(ctx, http.MethodDelete, runtime.JoinPaths(client.host, urlPath))
+	if err != nil {
+		return nil, err
+	}
+	reqQP := req.Raw().URL.Query()
+	reqQP.Set("api-version", "2022-09-01-privatepreview")
+	req.Raw().URL.RawQuery = reqQP.Encode()
+	req.Raw().Header["Accept"] = []string{"application/json"}
+	return req, nil
+}
+
+// Get - Get name of the secret that is holding credentials for the plane instance
+// If the operation fails it returns an *azcore.ResponseError type.
+// Generated from API version 2022-09-01-privatepreview
+// planeType - The type of the plane
+// planeName - The name of the plane
+// credentialName - The name of the credential
+// options - AWSCredentialClientGetOptions contains the optional parameters for the AWSCredentialClient.Get method.
+func (client *AWSCredentialClient) Get(ctx context.Context, planeType string, planeName string, credentialName string, options *AWSCredentialClientGetOptions) (AWSCredentialClientGetResponse, error) {
+	req, err := client.getCreateRequest(ctx, planeType, planeName, credentialName, options)
+	if err != nil {
+		return AWSCredentialClientGetResponse{}, err
+	}
+	resp, err := client.pl.Do(req)
+	if err != nil {
+		return AWSCredentialClientGetResponse{}, err
+	}
+	if !runtime.HasStatusCode(resp, http.StatusOK) {
+		return AWSCredentialClientGetResponse{}, runtime.NewResponseError(resp)
+	}
+	return client.getHandleResponse(resp)
+}
+
+// getCreateRequest creates the Get request.
+func (client *AWSCredentialClient) getCreateRequest(ctx context.Context, planeType string, planeName string, credentialName string, options *AWSCredentialClientGetOptions) (*policy.Request, error) {
+	urlPath := "/planes/{planeType}/{planeName}/providers/System.AWS/credentials/{credentialName}"
+	if planeType == "" {
+		return nil, errors.New("parameter planeType cannot be empty")
+	}
+	urlPath = strings.ReplaceAll(urlPath, "{planeType}", url.PathEscape(planeType))
+	if planeName == "" {
+		return nil, errors.New("parameter planeName cannot be empty")
+	}
+	urlPath = strings.ReplaceAll(urlPath, "{planeName}", url.PathEscape(planeName))
+	if credentialName == "" {
+		return nil, errors.New("parameter credentialName cannot be empty")
+	}
+	urlPath = strings.ReplaceAll(urlPath, "{credentialName}", url.PathEscape(credentialName))
+	req, err := runtime.NewRequest(ctx, http.MethodGet, runtime.JoinPaths(client.host, urlPath))
+	if err != nil {
+		return nil, err
+	}
+	reqQP := req.Raw().URL.Query()
+	reqQP.Set("api-version", "2022-09-01-privatepreview")
+	req.Raw().URL.RawQuery = reqQP.Encode()
+	req.Raw().Header["Accept"] = []string{"application/json"}
+	return req, nil
+}
+
+// getHandleResponse handles the Get response.
+func (client *AWSCredentialClient) getHandleResponse(resp *http.Response) (AWSCredentialClientGetResponse, error) {
+	result := AWSCredentialClientGetResponse{}
+	if err := runtime.UnmarshalAsJSON(resp, &result.CredentialResource); err != nil {
+		return AWSCredentialClientGetResponse{}, err
+	}
+	return result, nil
+}
+
+// List - List the credentials for this plane instance
+// If the operation fails it returns an *azcore.ResponseError type.
+// Generated from API version 2022-09-01-privatepreview
+// planeType - The type of the plane
+// planeName - The name of the plane
+// options - AWSCredentialClientListOptions contains the optional parameters for the AWSCredentialClient.List method.
+func (client *AWSCredentialClient) List(ctx context.Context, planeType string, planeName string, options *AWSCredentialClientListOptions) (AWSCredentialClientListResponse, error) {
+	req, err := client.listCreateRequest(ctx, planeType, planeName, options)
+	if err != nil {
+		return AWSCredentialClientListResponse{}, err
+	}
+	resp, err := client.pl.Do(req)
+	if err != nil {
+		return AWSCredentialClientListResponse{}, err
+	}
+	if !runtime.HasStatusCode(resp, http.StatusOK) {
+		return AWSCredentialClientListResponse{}, runtime.NewResponseError(resp)
+	}
+	return client.listHandleResponse(resp)
+}
+
+// listCreateRequest creates the List request.
+func (client *AWSCredentialClient) listCreateRequest(ctx context.Context, planeType string, planeName string, options *AWSCredentialClientListOptions) (*policy.Request, error) {
+	urlPath := "/planes/{planeType}/{planeName}/providers/System.AWS/credentials"
+	if planeType == "" {
+		return nil, errors.New("parameter planeType cannot be empty")
+	}
+	urlPath = strings.ReplaceAll(urlPath, "{planeType}", url.PathEscape(planeType))
+	if planeName == "" {
+		return nil, errors.New("parameter planeName cannot be empty")
+	}
+	urlPath = strings.ReplaceAll(urlPath, "{planeName}", url.PathEscape(planeName))
+	req, err := runtime.NewRequest(ctx, http.MethodGet, runtime.JoinPaths(client.host, urlPath))
+	if err != nil {
+		return nil, err
+	}
+	reqQP := req.Raw().URL.Query()
+	reqQP.Set("api-version", "2022-09-01-privatepreview")
+	req.Raw().URL.RawQuery = reqQP.Encode()
+	req.Raw().Header["Accept"] = []string{"application/json"}
+	return req, nil
+}
+
+// listHandleResponse handles the List response.
+func (client *AWSCredentialClient) listHandleResponse(resp *http.Response) (AWSCredentialClientListResponse, error) {
+	result := AWSCredentialClientListResponse{}
+	if err := runtime.UnmarshalAsJSON(resp, &result.CredentialResourceList); err != nil {
+		return AWSCredentialClientListResponse{}, err
+	}
+	return result, nil
+}
+
