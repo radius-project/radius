ba539ff68250f9664a4b6da81c4727c58664d02c
sk593 <shruthikumar@microsoft.com>
2023-01-20 10:20:44 -0800

    logs

--- pkg/middleware/lowercaseurlpath.go
+++ pkg/middleware/lowercaseurlpath.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/go-logr/logr"
 	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
 )
 
@@ -18,11 +19,13 @@ func LowercaseURLPath(next http.Handler) http.Handler {
 		// UCP/ARM populates "Referer" header in the request which can be used for FQDN of the resource.
 		// This is the fallback setting "Referer" header to save the original URL for UCP scenario.
 		// https://github.com/Azure/azure-resource-manager-rpc/blob/master/v1.0/common-api-details.md#proxy-request-header-modifications
+		logger := logr.FromContextOrDiscard(r.Response.Request.Context())
 		if r.Header.Get(v1.RefererHeader) == "" {
 			if r.URL.Host == "" {
 				r.URL.Host = r.Host
 			}
 			r.Header.Set(v1.RefererHeader, r.URL.String())
+			logger.Info("Referer in middleware: " + r.URL.String())
 		}
 
 		r.URL.Path = strings.ToLower(r.URL.Path)
