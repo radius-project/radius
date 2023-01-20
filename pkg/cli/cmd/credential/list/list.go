e1e75de5ab3d95c1a2ab3c1c0b95a6a540d6c3b5
Bharath Joginapally <bharath@Bharaths-MacBook-Pro.local>
2023-01-19 19:02:10 -0800

    Address comments

--- pkg/cli/cmd/credential/list/list.go
+++ pkg/cli/cmd/credential/list/list.go
@@ -83,7 +83,7 @@ func (r *Runner) Validate(cmd *cobra.Command, args []string) error {
 
 // Run runs the `rad provider list` command.
 func (r *Runner) Run(ctx context.Context) error {
-	r.Output.LogInfo("Listing credential for all cloud providers for Radius installation %q...", r.Workspace.FmtConnection())
+	r.Output.LogInfo("Listing credentials for all cloud providers for Radius installation %q...", r.Workspace.FmtConnection())
 	client, err := r.ConnectionFactory.CreateCloudProviderManagementClient(ctx, *r.Workspace)
 	if err != nil {
 		return err
