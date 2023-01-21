5231f36610447caec4a903faca18b666dfd5310d
Ryan Nowak <nowakra@gmail.com>
2023-01-20 16:35:12 -0800

    Skip flaky test (#4993)
    This PR skips a flaky test so that it does not impact other work. 
    
    See: https://github.com/project-radius/radius/issues/4992

--- test/functional/corerp/resources/mongodb_test.go
+++ test/functional/corerp/resources/mongodb_test.go
@@ -116,6 +116,8 @@ func Test_MongoDBUserSecrets(t *testing.T) {
 // the creation of a mongoDB from recipe
 // container using the mongoDB link to connect to the mongoDB resource
 func Test_MongoDB_Recipe(t *testing.T) {
+	t.Skip("This test is flaky, see issue: https://github.com/project-radius/radius/issues/4992")
+
 	template := "testdata/corerp-resources-mongodb-recipe.bicep"
 	name := "corerp-resources-mongodb-recipe"
 	appNamespace := "corerp-resources-mongodb-recipe-app"
