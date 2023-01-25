f31947f13772519bab396d5638e64fb8ed7b6378
sk593 <shruthikumar@microsoft.com>
2023-01-25 14:52:30 -0800

    moving referer

--- pkg/ucp/proxy/types.go
+++ pkg/ucp/proxy/types.go
@@ -110,8 +110,8 @@ func (p *armProxy) processAsyncResponse(resp *http.Response) error {
 		logger := logr.FromContextOrDiscard(ctx)
 		// As per https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/async-operations,
 		// first check for Azure-AsyncOperation header and if not found, check for LocationHeader
-		logger.Info(fmt.Sprintf("Async header from response : %s", resp.Header[AzureAsyncOperationHeader]))
-		logger.Info(fmt.Sprintf("Location header from response : %s", resp.Header[LocationHeader]))
+		logger.Info(fmt.Sprintf("#### Async header from response : %s", resp.Header[AzureAsyncOperationHeader]))
+		logger.Info(fmt.Sprintf("#### Location header from response : %s", resp.Header[LocationHeader]))
 		// if azureAsyncOperationHeader, ok := resp.Header[AzureAsyncOperationHeader]; ok {
 
 		// 	// This is an Async Response with a Azure-AsyncOperation Header
