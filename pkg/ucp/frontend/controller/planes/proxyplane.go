f530582660492aa8fd84d1dc4be8139ffa0b87a3
sk593 <shruthikumar@microsoft.com>
2023-01-25 14:52:30 -0800

    moving referer

--- pkg/ucp/frontend/controller/planes/proxyplane.go
+++ pkg/ucp/frontend/controller/planes/proxyplane.go
@@ -49,7 +49,17 @@ func (p *ProxyPlane) Run(ctx context.Context, w http.ResponseWriter, req *http.R
 		logger.V(ucplog.Debug).Info("incoming request header", "key", key, "value", value)
 	}
 
+	logger.Info("#### req url before get relative: " + req.URL.Path)
 	req.URL.Path = p.GetRelativePath(req.URL.Path)
+
+	refererURL := url.URL{
+		Scheme:   req.URL.Scheme,
+		Host:     req.Host,
+		Path:     req.URL.Path,
+		RawQuery: req.URL.RawQuery,
+	}
+	logger.Info("#### req url after get relative" + req.URL.String())
+	logger.Info(fmt.Sprintf("###### Referer in UCP : %s", req.Header.Get(v1.RefererHeader)))
 
 	// Make a copy of the incoming URL and trim the base path
 	newURL := *req.URL
@@ -169,12 +179,12 @@ func (p *ProxyPlane) Run(ctx context.Context, w http.ResponseWriter, req *http.R
 	req.Header.Set("X-Forwarded-Proto", httpScheme)
 
 	// Set Referer header
-	refererURL := url.URL{
-		Scheme:   httpScheme,
-		Host:     req.Host,
-		Path:     p.Options.BasePath + newURL.Path,
-		RawQuery: uri.RawQuery,
-	}
+	// refererURL := url.URL{
+	// 	Scheme:   httpScheme,
+	// 	Host:     req.Host,
+	// 	Path:     p.Options.BasePath + newURL.Path,
+	// 	RawQuery: uri.RawQuery,
+	// }
 	req.Header.Set(v1.RefererHeader, refererURL.String())
 	logger = logr.FromContextOrDiscard(ctx)
 	logger.Info(fmt.Sprintf("###### Referer in UCP : %s", req.Header.Get(v1.RefererHeader)))
