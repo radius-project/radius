9c3f2bd9c0fa0a78cbf4832e44a1650709dc1cc1
mishrapratikshya <pratmishra@microsoft.com>
2023-01-30 10:33:38 -0800

    rebased and resolved conflict

--- pkg/linkrp/renderers/rediscaches/renderer.go
+++ pkg/linkrp/renderers/rediscaches/renderer.go
@@ -69,9 +69,9 @@ func (r Renderer) Render(ctx context.Context, dm v1.ResourceDataModel, options r
 }
 
 func renderAzureRecipe(resource *datamodel.RedisCache, options renderers.RenderOptions, secretValues map[string]rpv1.SecretValueReference, computedValues map[string]renderers.ComputedValueReference) (renderers.RendererOutput, error) {
-	if options.RecipeProperties.LinkType != resource.ResourceTypeName() {
-		return renderers.RendererOutput{}, v1.NewClientErrInvalidRequest(fmt.Sprintf("link type %q of provided recipe %q is incompatible with %q resource type. Recipe link type must match link resource type.",
-			options.RecipeProperties.LinkType, options.RecipeProperties.Name, linkrp.RedisCachesResourceType))
+	err := renderers.ValidateLinkType(resource, options)
+	if err != nil {
+		return renderers.RendererOutput{}, err
 	}
 
 	recipeData := linkrp.RecipeData{
