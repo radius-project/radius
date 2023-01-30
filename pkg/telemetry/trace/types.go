df4043c1e81985f61824089faf19a2b993c296f6
nithyatsu <98416062+nithyatsu@users.noreply.github.com>
2023-01-30 14:31:53 -0800

    Initial commit of enabling traces in cli (#5032)
    # Description
    
    This PR enables tracing on cli. We are introducing the traces using open
    telemetry and added zipkin exports.
    
    ## Issue reference
    
    
    Fixes: https://dev.azure.com/azure-octo/Incubations/_workitems/edit/5764
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [ ] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [ ] Unit tests passing
    * [ ] Extended the documentation / Created issue for it

---
+++ pkg/telemetry/trace/types.go
@@ -0,0 +1,11 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package trace
+
+type TracerOptions struct {
+	ServiceName string
+	URL         string
+}
