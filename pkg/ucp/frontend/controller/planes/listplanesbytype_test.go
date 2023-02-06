116197657aecb5ab641eda51d999a9e979af06de
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2023-02-06 12:09:20 -0800

    Fix issue: Per-plane list endpoint in UCP does not function (#5061)
    # Description
    
    Fixed the swagger and added a new controller with an appropriate query
    to list planes per plane type
    
    #4878 
    
    <!--
    We strive to have all PR being opened based on an issue, where the
    problem or feature have been discussed prior to implementation.
    -->
    
    Fixes: #4878 
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [ ] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [ ] Unit tests passing
    * [ ] Extended the documentation / Created issue for it
    
    ---------
    
    Co-authored-by: Justin Kotalik <jukotali@microsoft.com>

---
+++ pkg/ucp/frontend/controller/planes/listplanesbytype_test.go
@@ -0,0 +1,54 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+package planes
+
+import (
+	http "net/http"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
+	armrpc_rest "github.com/project-radius/radius/pkg/armrpc/rest"
+	ctrl "github.com/project-radius/radius/pkg/ucp/frontend/controller"
+	"github.com/project-radius/radius/pkg/ucp/store"
+	"github.com/project-radius/radius/pkg/ucp/util/testcontext"
+	"github.com/stretchr/testify/require"
+	"gotest.tools/assert"
+)
+
+func Test_ListPlanesByType(t *testing.T) {
+	ctx, cancel := testcontext.New(t)
+	defer cancel()
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockStorageClient := store.NewMockStorageClient(mockCtrl)
+
+	planesCtrl, err := NewListPlanesByType(ctrl.Options{
+		DB: mockStorageClient,
+	})
+	require.NoError(t, err)
+
+	rootScope := "/planes/radius"
+	url := rootScope + "?api-version=2022-09-01-privatepreview"
+	var query store.Query
+	query.RootScope = "/planes"
+	query.IsScopeQuery = true
+	query.ResourceType = "radius"
+
+	expectedPlaneList := []any{}
+	expectedResponse := armrpc_rest.NewOKResponse(
+		&v1.PaginatedList{
+			Value: expectedPlaneList,
+		})
+
+	mockStorageClient.EXPECT().Query(gomock.Any(), query).Return(&store.ObjectQueryResult{}, nil)
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+	actualResponse, err := planesCtrl.Run(ctx, nil, request)
+	require.NoError(t, err)
+	assert.DeepEqual(t, expectedResponse, actualResponse)
+}
