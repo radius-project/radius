3283b51675ba7345807b3b0bd8f2cdd0c4124a6a
Karishma Chawla <kachawla@microsoft.com>
2023-02-06 21:11:16 -0800

    Update dapr recipe renderer to include all output resources (#5124)
    Updating dapr recipe renderer to include all output resources so that
    these resources can be deleted in order.
    
    https://github.com/project-radius/radius/issues/5115
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [x] Adds necessary unit tests for change
    * [x] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [x] Extended the documentation / Created issue for it
    
    Co-authored-by: karishma-chawla <74574173+karishma-chawla@users.noreply.github.com>

--- pkg/linkrp/renderers/daprstatestores/recipe.go
+++ pkg/linkrp/renderers/daprstatestores/recipe.go
@@ -72,6 +72,26 @@ func GetDaprStateStoreRecipe(resource *datamodel.DaprStateStore, applicationName
 			RadiusManaged:        to.Ptr(false), // Deleting storage account will delete all the underlying resources
 			Dependencies:         []rpv1.Dependency{{LocalID: rpv1.LocalIDAzureStorageTableService}},
 		},
+		{
+			LocalID: rpv1.LocalIDAzureStorageTableService,
+			ResourceType: resourcemodel.ResourceType{
+				Type:     resourcekinds.DaprStateStoreAzureTableService,
+				Provider: resourcemodel.ProviderAzure,
+			},
+			ProviderResourceType: azresources.StorageStorageAccounts + "/" + azresources.StorageStorageTableServices,
+			RadiusManaged:        to.BoolPtr(false), // Deleting storage account will delete all the underlying resources
+			Dependencies:         []rpv1.Dependency{{LocalID: rpv1.LocalIDDaprStateStoreAzureStorage}},
+		},
+		{
+			LocalID: rpv1.LocalIDAzureStorageTable,
+			ResourceType: resourcemodel.ResourceType{
+				Type:     resourcekinds.DaprStateStoreAzureTable,
+				Provider: resourcemodel.ProviderAzure,
+			},
+			ProviderResourceType: azresources.StorageStorageAccounts + "/" + azresources.StorageStorageTableServices + "/" + azresources.StorageStorageAccountsTables,
+			RadiusManaged:        to.BoolPtr(false), // Deleting storage account will delete all the underlying resources
+			Dependencies:         []rpv1.Dependency{{LocalID: rpv1.LocalIDAzureStorageTableService}},
+		},
 	}
 
 	return renderers.RendererOutput{
