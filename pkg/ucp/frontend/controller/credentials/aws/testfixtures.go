fb1c68c188eabe89dee8da4ed23c472b21a69c28
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2023-02-10 16:52:27 -0800

    Use operation controller for credentials UCP resource

---
+++ pkg/ucp/frontend/controller/credentials/aws/testfixtures.go
@@ -0,0 +1,12 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package aws
+
+var (
+	testHeaderFile                      = "requestheaders20220901privatepreview.json"
+	testHeaderFileWithBadAPIVersion     = "requestheaders20220901privatepreview_badapiversion.json"
+	testHeaderFileWithInvalidCredential = "requestheaders20220901privatepreview_invalidcredential.json"
+)
--- pkg/ucp/frontend/controller/credentials/azure/deletecredential.go
+++ pkg/ucp/frontend/controller/credentials/azure/deletecredential.go
@@ -10,63 +10,75 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-logr/logr"
 	armrpc_controller "github.com/project-radius/radius/pkg/armrpc/frontend/controller"
 	"github.com/project-radius/radius/pkg/armrpc/rest"
 	armrpc_rest "github.com/project-radius/radius/pkg/armrpc/rest"
 	"github.com/project-radius/radius/pkg/middleware"
 	"github.com/project-radius/radius/pkg/ucp/datamodel"
+	"github.com/project-radius/radius/pkg/ucp/datamodel/converter"
 	ctrl "github.com/project-radius/radius/pkg/ucp/frontend/controller"
 	"github.com/project-radius/radius/pkg/ucp/frontend/controller/credentials"
 	"github.com/project-radius/radius/pkg/ucp/resources"
 	"github.com/project-radius/radius/pkg/ucp/secret"
 	"github.com/project-radius/radius/pkg/ucp/store"
-	"github.com/project-radius/radius/pkg/ucp/ucplog"
 )
 
 var _ armrpc_controller.Controller = (*DeleteCredential)(nil)
 
 // DeleteCredential is the controller implementation to delete a UCP credential.
 type DeleteCredential struct {
-	ctrl.BaseController
+	ctrl.Operation[*datamodel.Credential, datamodel.Credential]
 }
 
 // NewDeleteCredential creates a new DeleteCredential.
 func NewDeleteCredential(opts ctrl.Options) (armrpc_controller.Controller, error) {
-	return &DeleteCredential{ctrl.NewBaseController(opts)}, nil
+	return &DeleteCredential{
+		ctrl.NewOperation(opts,
+			ctrl.ResourceOptions[datamodel.Credential]{
+				RequestConverter:  converter.CredentialDataModelFromVersioned,
+				ResponseConverter: converter.CredentialDataModelToVersioned,
+			},
+		),
+	}, nil
 }
 
-func (p *DeleteCredential) Run(ctx context.Context, w http.ResponseWriter, req *http.Request) (armrpc_rest.Response, error) {
-	path := middleware.GetRelativePath(p.Options.BasePath, req.URL.Path)
-	logger := ucplog.FromContextOrDiscard(ctx)
+func (c *DeleteCredential) Run(ctx context.Context, w http.ResponseWriter, req *http.Request) (armrpc_rest.Response, error) {
+	path := middleware.GetRelativePath(c.BasePath(), req.URL.Path)
+	logger := logr.FromContextOrDiscard(ctx)
 
 	resourceID, err := resources.ParseResource(path)
 	if err != nil {
 		return armrpc_rest.NewBadRequestResponse(err.Error()), nil
 	}
-	// Check if the resource being deleted exists or not.
-	existingResource := datamodel.Credential{}
-	etag, err := p.GetResource(ctx, resourceID.String(), &existingResource)
-	if errors.Is(err, &store.ErrNotFound{}) {
-		return armrpc_rest.NewNoContentResponse(), nil
-	} else if err != nil {
+
+	old, etag, err := c.GetResource(ctx, resourceID)
+	if err != nil {
 		return nil, err
+	}
+
+	if old == nil {
+		return rest.NewNoContentResponse(), nil
 	}
 
 	secretName := credentials.GetSecretName(resourceID)
 
 	// Delete the credential secret.
-	err = p.Options.SecretClient.Delete(ctx, secretName)
+	err = c.Options().SecretClient.Delete(ctx, secretName)
 	if errors.Is(err, &secret.ErrNotFound{}) {
 		return armrpc_rest.NewNoContentResponse(), nil
 	} else if err != nil {
 		return nil, err
 	}
 
-	// Delete resource from radius.
-	err = p.DeleteResource(ctx, resourceID.String(), etag)
-	if errors.Is(err, &store.ErrNotFound{}) {
-		return armrpc_rest.NewNoContentResponse(), nil
-	} else if err != nil {
+	if r, err := c.PrepareResource(ctx, req, nil, old, etag); r != nil || err != nil {
+		return r, err
+	}
+
+	if err := c.StorageClient().Delete(ctx, resourceID.String()); err != nil {
+		if errors.Is(&store.ErrNotFound{}, err) {
+			return rest.NewNoContentResponse(), nil
+		}
 		return nil, err
 	}
 
