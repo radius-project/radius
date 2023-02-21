05b9c208a2877ff2b92dcaaae4f286ef5be1da37
Vishwanath Hiremath <100623239+vishwahiremat@users.noreply.github.com>
2023-02-21 01:14:26 -0800

    Fix bug for not setting dev recipe flag through rad init (#5133) (#5149)
    # Description
    - SkipDevRecipes isn't being set on env when user does rad init
    - This change sets the flag on the env <img width="1084" alt="Screenshot
    2023-02-07 at 1 36 22 PM"
    src="https://user-images.githubusercontent.com/99223262/217371400-58e826d8-0181-4fd0-ab21-06e8e45f66ec.png">
    
    ## Issue reference
    
    <!--
    We strive to have all PR being opened based on an issue, where the
    problem or feature have been discussed prior to implementation. -->
    
    Fixes: #5130 
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [ ] Extended the documentation / Created issue for it
    
    ---------
    
    Co-authored-by: Bharath Joginapally <bharath@Bharaths-MacBook-Pro.local>
    
    # Description
    
    _Please explain the changes you've made._
    
    ## Issue reference
    
    <!--
    We strive to have all PR being opened based on an issue, where the
    problem or feature have been discussed prior to implementation.
    -->
    
    Fixes: #issue_number
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [x] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [ ] Extended the documentation / Created issue for it
    
    ---------
    
    Co-authored-by: Bharath <99223262+bjoginapally@users.noreply.github.com>
    Co-authored-by: Bharath Joginapally <bharath@Bharaths-MacBook-Pro.local>

--- pkg/cli/cmd/radInit/init_test.go
+++ pkg/cli/cmd/radInit/init_test.go
@@ -442,7 +442,7 @@ func Test_Validate(t *testing.T) {
 	radcli.SharedValidateValidation(t, NewCommand, testcases)
 }
 
-func Test_Run_InstallAndCreateEnvironment_WithAzureProvider(t *testing.T) {
+func Test_Run_InstallAndCreateEnvironment_WithAzureProvider_WithRecipes(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	configFileInterface := framework.NewMockConfigFileInterface(ctrl)
 	configFileInterface.EXPECT().
@@ -456,8 +456,9 @@ func Test_Run_InstallAndCreateEnvironment_WithAzureProvider(t *testing.T) {
 	appManagementClient.EXPECT().
 		CreateUCPGroup(context.Background(), "deployments", "local", "default", gomock.Any()).
 		Return(true, nil).Times(1)
+	skipRecipes := false
 	appManagementClient.EXPECT().
-		CreateEnvironment(context.Background(), "default", v1.LocationGlobal, "defaultNamespace", "kubernetes", gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+		CreateEnvironment(context.Background(), "default", v1.LocationGlobal, "defaultNamespace", "kubernetes", gomock.Any(), gomock.Any(), gomock.Any(), !skipRecipes).
 		Return(true, nil).Times(1)
 
 	credentialManagementClient := cli_credential.NewMockCredentialManagementClient(ctrl)
@@ -491,6 +492,7 @@ func Test_Run_InstallAndCreateEnvironment_WithAzureProvider(t *testing.T) {
 		Namespace:           "defaultNamespace",
 		RadiusInstalled:     true, // We're testing the reinstall case
 		Reinstall:           true,
+		SkipDevRecipes:      skipRecipes,
 		AzureCloudProvider: &azure.Provider{
 			SubscriptionID: "test-subscription",
 			ResourceGroup:  "test-rg",
@@ -567,7 +569,7 @@ func Test_Run_InstallAndCreateEnvironment_WithAWSProvider(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func Test_Run_InstallAndCreateEnvironment_WithoutAzureProvider(t *testing.T) {
+func Test_Run_InstallAndCreateEnvironment_WithoutAzureProvider_WithSkipRecipes(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	configFileInterface := framework.NewMockConfigFileInterface(ctrl)
 	configFileInterface.EXPECT().
@@ -581,8 +583,9 @@ func Test_Run_InstallAndCreateEnvironment_WithoutAzureProvider(t *testing.T) {
 	appManagementClient.EXPECT().
 		CreateUCPGroup(context.Background(), "deployments", "local", "default", gomock.Any()).
 		Return(true, nil).Times(1)
+	skipRecipes := true
 	appManagementClient.EXPECT().
-		CreateEnvironment(context.Background(), "default", v1.LocationGlobal, "defaultNamespace", "kubernetes", gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+		CreateEnvironment(context.Background(), "default", v1.LocationGlobal, "defaultNamespace", "kubernetes", gomock.Any(), gomock.Any(), gomock.Any(), !skipRecipes).
 		Return(true, nil).Times(1)
 
 	configFileInterface.EXPECT().
@@ -604,6 +607,7 @@ func Test_Run_InstallAndCreateEnvironment_WithoutAzureProvider(t *testing.T) {
 		Output:              outputSink,
 		Workspace:           &workspaces.Workspace{Name: "defaultWorkspace"},
 		KubeContext:         "kind-kind",
+		SkipDevRecipes:      skipRecipes,
 		RadiusInstalled:     false,
 		Namespace:           "defaultNamespace",
 		EnvName:             "default",
