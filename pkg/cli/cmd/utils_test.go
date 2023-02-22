f33e2ddb8792dbd6028982c3eefdbc088a8768eb
Pratikshya Mishra <99209503+mishrapratikshya@users.noreply.github.com>
2023-02-22 14:51:47 -0800

    patch release for 0.17.2 (#5195)
    # Description
    
    cherry picking commits for fixing
    1. rad init
    2. rad recipe register
    
    ## Issue reference
    
    <!--
    We strive to have all PR being opened based on an issue, where the
    problem or feature have been discussed prior to implementation.
    -->
    
    Fixes: #issue_number
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [ ] Unit tests passing
    * [ ] Extended the documentation / Created issue for it
    
    ---------
    
    Co-authored-by: Young Bu Park <youngp@microsoft.com>

---
+++ pkg/cli/cmd/utils_test.go
@@ -0,0 +1,138 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package cmd
+
+import (
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/project-radius/radius/pkg/cli"
+	"github.com/project-radius/radius/pkg/cli/aws"
+	"github.com/project-radius/radius/pkg/cli/azure"
+	corerp "github.com/project-radius/radius/pkg/corerp/api/v20220315privatepreview"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateEnvProviders(t *testing.T) {
+	var nilAzureProvider *azure.Provider = nil
+	var nilAWSProvider *aws.Provider = nil
+
+	providersTests := []struct {
+		name      string
+		providers []any
+		out       corerp.Providers
+		err       error
+	}{
+		{
+			name:      "empty providers",
+			providers: []any{},
+			out:       corerp.Providers{},
+			err:       nil,
+		},
+		{
+			name:      "invalid provider types",
+			providers: []any{azure.Provider{}},
+			out:       corerp.Providers{},
+			err:       &cli.FriendlyError{Message: "Internal error: cannot create environment with invalid type 'azure.Provider'"},
+		},
+		{
+			name: "skip nil provider",
+			providers: []any{
+				nil,
+				&azure.Provider{SubscriptionID: "testSubs", ResourceGroup: "testRG"},
+			},
+			out: corerp.Providers{
+				Azure: &corerp.ProvidersAzure{
+					Scope: to.Ptr("/subscriptions/testSubs/resourceGroups/testRG"),
+				},
+			},
+			err: nil,
+		},
+		{
+			name: "only azure provider",
+			providers: []any{
+				&azure.Provider{SubscriptionID: "testSubs", ResourceGroup: "testRG"},
+			},
+			out: corerp.Providers{
+				Azure: &corerp.ProvidersAzure{
+					Scope: to.Ptr("/subscriptions/testSubs/resourceGroups/testRG"),
+				},
+			},
+			err: nil,
+		},
+		{
+			name: "multiple azure providers",
+			providers: []any{
+				&azure.Provider{SubscriptionID: "testSubs", ResourceGroup: "testRG"},
+				&azure.Provider{SubscriptionID: "testSub2", ResourceGroup: "testRG2"},
+			},
+			out: corerp.Providers{
+				Azure: &corerp.ProvidersAzure{
+					Scope: to.Ptr("/subscriptions/testSubs/resourceGroups/testRG"),
+				},
+			},
+			err: &cli.FriendlyError{Message: "Only one azure provider can be configured to a scope"},
+		},
+		{
+			name: "only aws provider",
+			providers: []any{
+				&aws.Provider{AccountId: "0", TargetRegion: "westus"},
+			},
+			out: corerp.Providers{
+				Aws: &corerp.ProvidersAws{
+					Scope: to.Ptr("/planes/aws/aws/accounts/0/regions/westus"),
+				},
+			},
+			err: nil,
+		},
+		{
+			name: "multiple aws providers",
+			providers: []any{
+				&aws.Provider{AccountId: "0", TargetRegion: "westus"},
+				&aws.Provider{AccountId: "1", TargetRegion: "eastus"},
+			},
+			out: corerp.Providers{
+				Aws: &corerp.ProvidersAws{
+					Scope: to.Ptr("/planes/aws/aws/accounts/0/regions/westus"),
+				},
+			},
+			err: &cli.FriendlyError{Message: "Only one aws provider can be configured to a scope"},
+		},
+		{
+			name: "azure and aws providers",
+			providers: []any{
+				&azure.Provider{SubscriptionID: "testSubs", ResourceGroup: "testRG"},
+				&aws.Provider{AccountId: "0", TargetRegion: "westus"},
+			},
+			out: corerp.Providers{
+				Azure: &corerp.ProvidersAzure{
+					Scope: to.Ptr("/subscriptions/testSubs/resourceGroups/testRG"),
+				},
+				Aws: &corerp.ProvidersAws{
+					Scope: to.Ptr("/planes/aws/aws/accounts/0/regions/westus"),
+				},
+			},
+			err: nil,
+		},
+		{
+			name: "skip typed nil value",
+			providers: []any{
+				nilAzureProvider,
+				nilAWSProvider,
+			},
+			out: corerp.Providers{},
+			err: nil,
+		},
+	}
+
+	for _, tc := range providersTests {
+		t.Run(tc.name, func(t *testing.T) {
+			provider, err := CreateEnvProviders(tc.providers)
+			require.Equal(t, tc.out, provider)
+			require.ErrorIs(t, err, tc.err)
+		})
+	}
+}
