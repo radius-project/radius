dc97d41ba33a400fb887210e978f4b58a7bdc3b0
mishrapratikshya <pratmishra@microsoft.com>
2023-03-02 17:55:34 -0800

    update test resource id var names and fix devRecipe UT failing

--- test/executeCoreRPFunctionalTest.sh
+++ test/executeCoreRPFunctionalTest.sh
@@ -20,12 +20,12 @@ adminUser='coolUser'
 adminPassword=$(uuidgen)
 resp=`az deployment group create --resource-group $resourcegroup --template-file createAzureTestResources.bicep --parameters 'adminUsername=$adminUser' --parameters 'adminPassword=$adminPassword'`
 cat resp
-export SERVICEBUS_RESOURCE_ID=$( jq -r '.properties.outputs.namespaceId.value' <<< "${resp}" ) 
-export MSSQL_RESOURCE_ID=$( jq -r '.properties.outputs.sqlServerId.value' <<< "${resp}" )
-export MSSQL_USERNAME=$adminUser
-export MSSQL_PASSWORD=$adminPassword
-export MONGODB_RESOURCE_ID=$( jq -r '.properties.outputs.mongoDatabaseId.value' <<< "${resp}" )
-export REDIS_RESOURCE_ID=$( jq -r '.properties.outputs.redisCacheId.value' <<< "${resp}" )
-export TABLESTORAGE_RESOURCE_ID=$( jq -r '.properties.outputs.tableStorageAccId.value' <<< "${resp}" )
-export DOCUMENTDB_RESOURCE_ID=$( jq -r '.properties.outputs.cosmosMongoAccountID.value' <<< "${resp}" )
+export AZURE_SERVICEBUS_RESOURCE_ID=$( jq -r '.properties.outputs.namespaceId.value' <<< "${resp}" ) 
+export AZURE_MSSQL_RESOURCE_ID=$( jq -r '.properties.outputs.sqlServerId.value' <<< "${resp}" )
+export AZURE_MSSQL_USERNAME=$adminUser
+export AZURE_MSSQL_PASSWORD=$adminPassword
+export AZURE_MONGODB_RESOURCE_ID=$( jq -r '.properties.outputs.mongoDatabaseId.value' <<< "${resp}" )
+export AZURE_REDIS_RESOURCE_ID=$( jq -r '.properties.outputs.redisCacheId.value' <<< "${resp}" )
+export AZURE_TABLESTORAGE_RESOURCE_ID=$( jq -r '.properties.outputs.tableStorageAccId.value' <<< "${resp}" )
+export AZURE_COSMOS_MONGODB_ACCOUNT_ID=$( jq -r '.properties.outputs.cosmosMongoAccountID.value' <<< "${resp}" )
 make test-functional-corerp
