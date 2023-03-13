91b05e53b8627c229ad5ba5e59cf78b15d36b605
sk593 <shruthikumar@microsoft.com>
2023-03-13 11:19:20 -0700

    adding configloader tests

--- pkg/recipes/configloader/environment_test.go
+++ pkg/recipes/configloader/environment_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 
 	model "github.com/project-radius/radius/pkg/corerp/api/v20220315privatepreview"
-	"github.com/project-radius/radius/pkg/corerp/datamodel"
+	"github.com/project-radius/radius/pkg/resourcemodel"
 	"github.com/project-radius/radius/pkg/to"
 	"github.com/stretchr/testify/require"
 )
@@ -17,22 +17,21 @@ import (
 const (
 	kind          = "kubernetes"
 	namespace     = "default"
+	appNamespace  = "app-default"
 	envResourceId = "/subscriptions/test-sub/resourceGroups/test-group/providers/Applications.Core/environments/env0"
-	scope         = "/subscriptions/testSubs/resourceGroups/testRG"
+	appResourceId = "/subscriptions/test-sub/resourceGroups/test-group/providers/Applications.Core/applications/app0"
+	azureScope    = "/subscriptions/testSubs/resourceGroups/testRG"
+	awsScope      = "/planes/aws/aws/accounts/000/regions/cool-region"
 )
 
-func Test_GetConfiguration(t *testing.T) {
+func Test_GetConfigurationAzure(t *testing.T) {
 	envConfig := &Configuration{
 		Runtime: RuntimeConfiguration{
 			Kubernetes: &KubernetesRuntime{
 				Namespace: "default",
 			},
 		},
-		Providers: datamodel.Providers{
-			Azure: datamodel.ProvidersAzure{
-				Scope: scope,
-			},
-		},
+		Providers: createAzureProvider(),
 	}
 	envResource := model.EnvironmentResource{
 		Properties: &model.EnvironmentProperties{
@@ -43,13 +42,100 @@ func Test_GetConfiguration(t *testing.T) {
 			},
 			Providers: &model.Providers{
 				Azure: &model.ProvidersAzure{
-					Scope: to.Ptr(scope),
+					Scope: to.Ptr(azureScope),
 				},
 			},
 		},
 	}
-
 	result, err := getConfiguration(&envResource, nil)
 	require.NoError(t, err)
 	require.Equal(t, envConfig, result)
+}
+
+func Test_GetConfigurationAWS(t *testing.T) {
+	envConfig := &Configuration{
+		Runtime: RuntimeConfiguration{
+			Kubernetes: &KubernetesRuntime{
+				Namespace: "default",
+			},
+		},
+		Providers: createAWSProvider(),
+	}
+	envResource := model.EnvironmentResource{
+		Properties: &model.EnvironmentProperties{
+			Compute: &model.KubernetesCompute{
+				Kind:       to.Ptr(kind),
+				Namespace:  to.Ptr(namespace),
+				ResourceID: to.Ptr(envResourceId),
+			},
+			Providers: &model.Providers{
+				Aws: &model.ProvidersAws{
+					Scope: to.Ptr(awsScope),
+				},
+			},
+		},
+	}
+	result, err := getConfiguration(&envResource, nil)
+	require.NoError(t, err)
+	require.Equal(t, envConfig, result)
+
+	appConfig := &Configuration{
+		Runtime: RuntimeConfiguration{
+			Kubernetes: &KubernetesRuntime{
+				Namespace: "app-default",
+			},
+		},
+		Providers: createAWSProvider(),
+	}
+	appResource := model.ApplicationResource{
+		Properties: &model.ApplicationProperties{
+			Status: &model.ResourceStatus{
+				Compute: &model.KubernetesCompute{
+					Kind:       to.Ptr(kind),
+					Namespace:  to.Ptr(appNamespace),
+					ResourceID: to.Ptr(appResourceId),
+				},
+			},
+		},
+	}
+	result, err = getConfiguration(&envResource, &appResource)
+	require.NoError(t, err)
+	require.Equal(t, appConfig, result)
+}
+
+func Test_InvalidApplicationError(t *testing.T) {
+	envResource := model.EnvironmentResource{
+		Properties: &model.EnvironmentProperties{
+			Compute: &model.KubernetesCompute{
+				Kind:       to.Ptr(kind),
+				Namespace:  to.Ptr(namespace),
+				ResourceID: to.Ptr(envResourceId),
+			},
+		},
+	}
+	// Invalid app model
+	appResource := model.ApplicationResource{
+		Properties: &model.ApplicationProperties{
+			Status: &model.ResourceStatus{
+				Compute: &model.EnvironmentCompute{},
+			},
+		},
+	}
+	_, err := getConfiguration(&envResource, &appResource)
+	require.Error(t, err)
+	require.Equal(t, err.Error(), "invalid model conversion")
+}
+
+func createAzureProvider() map[string]map[string]any {
+	return map[string]map[string]any{
+		resourcemodel.ProviderAzure: {
+			"scope": azureScope,
+		}}
+}
+
+func createAWSProvider() map[string]map[string]any {
+	return map[string]map[string]any{
+		resourcemodel.ProviderAWS: {
+			"scope": awsScope,
+		}}
 }
--- pkg/rp/kube/resources_test.go
+++ pkg/rp/kube/resources_test.go
@@ -12,15 +12,19 @@ import (
 
 	"github.com/golang/mock/gomock"
 	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
+	model "github.com/project-radius/radius/pkg/corerp/api/v20220315privatepreview"
 	"github.com/project-radius/radius/pkg/corerp/datamodel"
 	rpv1 "github.com/project-radius/radius/pkg/rp/v1"
+	"github.com/project-radius/radius/pkg/to"
 	"github.com/project-radius/radius/pkg/ucp/dataprovider"
 	"github.com/project-radius/radius/pkg/ucp/store"
 	"github.com/stretchr/testify/require"
 )
 
 const (
-	testEnvID = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/radius-test-rg/providers/Applications.Core/environments/env"
+	testEnvID    = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/radius-test-rg/providers/Applications.Core/environments/env"
+	namespace    = "default"
+	appNamespace = "app-default"
 )
 
 func fakeStoreObject(dm v1.DataModelInterface) *store.Object {
@@ -82,4 +86,44 @@ func TestFindNamespaceByEnvID(t *testing.T) {
 			require.Equal(t, tc.out, ns)
 		})
 	}
+}
+
+func TestFetchNameSpaceFromEnvironmentResource(t *testing.T) {
+	envResource := model.EnvironmentResource{
+		Properties: &model.EnvironmentProperties{
+			Compute: &model.KubernetesCompute{
+				Namespace: to.Ptr(namespace),
+			},
+		},
+	}
+
+	ns, err := FetchNameSpaceFromEnvironmentResource(&envResource)
+	require.NoError(t, err)
+	require.Equal(t, namespace, ns)
+	// Invalid env model
+	envResource.Properties.Compute = &model.EnvironmentCompute{}
+	_, err = FetchNameSpaceFromEnvironmentResource(&envResource)
+	require.Error(t, err)
+	require.Equal(t, err.Error(), "invalid model conversion")
+}
+
+func TestFetchNameSpaceFromApplicationResource(t *testing.T) {
+	appResource := model.ApplicationResource{
+		Properties: &model.ApplicationProperties{
+			Status: &model.ResourceStatus{
+				Compute: &model.KubernetesCompute{
+					Namespace: to.Ptr(appNamespace),
+				},
+			},
+		},
+	}
+
+	ns, err := FetchNameSpaceFromApplicationResource(&appResource)
+	require.NoError(t, err)
+	require.Equal(t, appNamespace, ns)
+	// Invalid app model
+	appResource.Properties.Status.Compute = &model.EnvironmentCompute{}
+	_, err = FetchNameSpaceFromApplicationResource(&appResource)
+	require.Error(t, err)
+	require.Equal(t, err.Error(), "invalid model conversion")
 }
