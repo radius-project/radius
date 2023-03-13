32a326dfe100d36940a16d8a93fff45c3b0d87fb
Lakshmi Javadekar <lakshmi@Lakshmis-MacBook-Pro.local>
2023-03-13 15:20:59 -0700

    adding unit tests, updated to tblfmt still rebasing..

--- pkg/corerp/renderers/httproute/render_test.go
+++ pkg/corerp/renderers/httproute/render_test.go
@@ -1,8 +1,3 @@
-// ------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-// ------------------------------------------------------------
-
 package httproute
 
 import (
