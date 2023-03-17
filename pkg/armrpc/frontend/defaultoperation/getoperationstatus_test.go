b92be61cb908ad56da956f9f48a4966f5dfd42f1
Young Bu Park <youngp@microsoft.com>
2023-03-16 23:13:25 -0700

    Revert "test"
    This reverts commit 37fed12c8d8d5d0985bd59efe30652f34b49a468.

---
+++ pkg/armrpc/frontend/defaultoperation/getoperationstatus_test.go
@@ -0,0 +1,93 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package defaultoperation
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
+	manager "github.com/project-radius/radius/pkg/armrpc/asyncoperation/statusmanager"
+	ctrl "github.com/project-radius/radius/pkg/armrpc/frontend/controller"
+	"github.com/project-radius/radius/pkg/ucp/store"
+
+	"github.com/golang/mock/gomock"
+	"github.com/project-radius/radius/test/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetOperationStatusRun(t *testing.T) {
+	mctrl := gomock.NewController(t)
+	defer mctrl.Finish()
+
+	mStorageClient := store.NewMockStorageClient(mctrl)
+	ctx := context.Background()
+
+	rawDataModel := testutil.ReadFixture("operationstatus_datamodel.json")
+	osDataModel := &manager.Status{}
+	_ = json.Unmarshal(rawDataModel, osDataModel)
+
+	rawExpectedOutput := testutil.ReadFixture("operationstatus_output.json")
+	expectedOutput := &v1.AsyncOperationStatus{}
+	_ = json.Unmarshal(rawExpectedOutput, expectedOutput)
+
+	t.Run("get non-existing resource", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		req, _ := testutil.GetARMTestHTTPRequest(ctx, http.MethodGet, operationStatusTestHeaderFile, nil)
+		ctx := testutil.ARMTestContextFromRequest(req)
+
+		mStorageClient.
+			EXPECT().
+			Get(gomock.Any(), gomock.Any()).
+			DoAndReturn(func(ctx context.Context, id string, _ ...store.GetOptions) (*store.Object, error) {
+				return nil, &store.ErrNotFound{}
+			})
+
+		ctl, err := NewGetOperationStatus(ctrl.Options{
+			StorageClient: mStorageClient,
+		})
+
+		require.NoError(t, err)
+		resp, err := ctl.Run(ctx, w, req)
+		require.NoError(t, err)
+		_ = resp.Apply(ctx, w, req)
+		require.Equal(t, http.StatusNotFound, w.Result().StatusCode)
+	})
+
+	t.Run("get existing resource", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		req, _ := testutil.GetARMTestHTTPRequest(ctx, http.MethodGet, operationStatusTestHeaderFile, nil)
+		ctx := testutil.ARMTestContextFromRequest(req)
+
+		mStorageClient.
+			EXPECT().
+			Get(gomock.Any(), gomock.Any()).
+			DoAndReturn(func(ctx context.Context, id string, _ ...store.GetOptions) (*store.Object, error) {
+				return &store.Object{
+					Metadata: store.Metadata{ID: id},
+					Data:     osDataModel,
+				}, nil
+			})
+
+		ctl, err := NewGetOperationStatus(ctrl.Options{
+			StorageClient: mStorageClient,
+		})
+
+		require.NoError(t, err)
+		resp, err := ctl.Run(ctx, w, req)
+		require.NoError(t, err)
+		_ = resp.Apply(ctx, w, req)
+		require.Equal(t, http.StatusOK, w.Result().StatusCode)
+
+		actualOutput := &v1.AsyncOperationStatus{}
+		_ = json.Unmarshal(w.Body.Bytes(), actualOutput)
+
+		require.Equal(t, expectedOutput, actualOutput)
+	})
+}
