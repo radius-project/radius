613419a275e15f53211ae2cb3a0c577c2c58cabe
Yetkin Timocin <ytimocin@microsoft.com>
2023-03-17 18:16:46 -0700

    Using async generic controllers for Dapr State Store resource type (#5279)
    # Description
    Making Dapr state store resource type use generic async controllers.
    
    ## Issue reference
    Fixes: #5269 
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [x] Extended the documentation / Created issue for it

--- pkg/armrpc/asyncoperation/controller/request_test.go
+++ pkg/armrpc/asyncoperation/controller/request_test.go
@@ -6,9 +6,14 @@
 package controller
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
+	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
+	"github.com/project-radius/radius/pkg/ucp/resources"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,4 +24,74 @@ func TestTimeout(t *testing.T) {
 	testTimeout := time.Duration(200) * time.Minute
 	r = Request{OperationTimeout: &testTimeout}
 	require.Equal(t, testTimeout, r.Timeout())
+}
+
+func TestRequest_ARMRequestContext(t *testing.T) {
+	opID := uuid.New()
+	subscriptionID := uuid.New()
+	resourceGroup := "test-resource-group"
+	provider := "applications.core"
+	resourceID := fmt.Sprintf("/subscriptions/%s/resourcegroups/%s/providers/%s/environments/test-environment", subscriptionID, resourceGroup, provider)
+	parsedResourceID, err := resources.Parse(resourceID)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name    string
+		req     *Request
+		want    *v1.ARMRequestContext
+		wantErr bool
+	}{
+		{
+			name:    "empty request",
+			req:     &Request{},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid id",
+			req: &Request{
+				ResourceID: "invalid",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "happy path",
+			req: &Request{
+				ResourceID:     resourceID,
+				CorrelationID:  "test-correlation-id",
+				OperationID:    opID,
+				OperationType:  "APPLICATIONS.CORE/ENVIRONMENTS|PUT",
+				TraceparentID:  "test-traceparent-id",
+				HomeTenantID:   "test-home-tenant-id",
+				ClientObjectID: "test-client-object-id",
+				APIVersion:     "2021-01-01",
+				AcceptLanguage: "en-US",
+			},
+			want: &v1.ARMRequestContext{
+				ResourceID:     parsedResourceID,
+				CorrelationID:  "test-correlation-id",
+				OperationID:    opID,
+				OperationType:  "APPLICATIONS.CORE/ENVIRONMENTS|PUT",
+				Traceparent:    "test-traceparent-id",
+				HomeTenantID:   "test-home-tenant-id",
+				ClientObjectID: "test-client-object-id",
+				APIVersion:     "2021-01-01",
+				AcceptLanguage: "en-US",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.req.ARMRequestContext()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Request.ARMRequestContext() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Request.ARMRequestContext() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
