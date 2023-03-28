34e69f6f952081a5adad8b53ba66630333b3a709
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2023-03-28 10:38:33 -0700

    Use generic operation controllers for UCP (#5302)
    # Description
    
    Use generic operation controllers for UCP
    
    #4634 
    
    <!--
    We strive to have all PR being opened based on an issue, where the
    problem or feature have been discussed prior to implementation.
    -->
    
    Fixes: #4634 
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [ ] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [ ] Unit tests passing
    * [ ] Extended the documentation / Created issue for it

---
+++ pkg/armrpc/frontend/defaultoperation/defaultsyncput.go
@@ -0,0 +1,62 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package defaultoperation
+
+import (
+	"context"
+	"net/http"
+
+	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
+	ctrl "github.com/project-radius/radius/pkg/armrpc/frontend/controller"
+	"github.com/project-radius/radius/pkg/armrpc/rest"
+)
+
+// DefaultSyncPut is the controller implementation to create or update resource synchronously.
+type DefaultSyncPut[P interface {
+	*T
+	v1.ResourceDataModel
+}, T any] struct {
+	ctrl.Operation[P, T]
+}
+
+// NewDefaultSyncPut creates a new DefaultSyncPut.
+func NewDefaultSyncPut[P interface {
+	*T
+	v1.ResourceDataModel
+}, T any](opts ctrl.Options, resourceOpts ctrl.ResourceOptions[T]) (ctrl.Controller, error) {
+	return &DefaultSyncPut[P, T]{ctrl.NewOperation[P](opts, resourceOpts)}, nil
+}
+
+// Run executes DefaultSyncPut operation.
+func (e *DefaultSyncPut[P, T]) Run(ctx context.Context, w http.ResponseWriter, req *http.Request) (rest.Response, error) {
+	serviceCtx := v1.ARMRequestContextFromContext(ctx)
+	newResource, err := e.GetResourceFromRequest(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	old, etag, err := e.GetResource(ctx, serviceCtx.ResourceID)
+	if err != nil {
+		return nil, err
+	}
+
+	if r, err := e.PrepareResource(ctx, req, newResource, old, etag); r != nil || err != nil {
+		return r, err
+	}
+
+	for _, filter := range e.UpdateFilters() {
+		if resp, err := filter(ctx, newResource, old, e.Options()); resp != nil || err != nil {
+			return resp, err
+		}
+	}
+
+	P(newResource).SetProvisioningState(v1.ProvisioningStateSucceeded)
+	newEtag, err := e.SaveResource(ctx, serviceCtx.ResourceID.String(), newResource, etag)
+	if err != nil {
+		return nil, err
+	}
+
+	return e.ConstructSyncResponse(ctx, req.Method, newEtag, newResource)
+}
---
+++ pkg/armrpc/frontend/defaultoperation/defaultsyncput_test.go
@@ -0,0 +1,229 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package defaultoperation
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	v1 "github.com/project-radius/radius/pkg/armrpc/api/v1"
+	ctrl "github.com/project-radius/radius/pkg/armrpc/frontend/controller"
+	"github.com/project-radius/radius/pkg/ucp/store"
+	"github.com/project-radius/radius/test/testutil"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDefaultSyncPut_Create(t *testing.T) {
+	createCases := []struct {
+		desc    string
+		getErr  error
+		saveErr error
+		rCode   int
+		rErr    error
+	}{
+		{
+			"sync-create-new-resource-success",
+			&store.ErrNotFound{},
+			nil,
+			http.StatusOK,
+			nil,
+		},
+		{
+			"sync-create-new-resource-concurrency-error",
+			&store.ErrConcurrency{},
+			nil,
+			http.StatusOK,
+			&store.ErrConcurrency{},
+		},
+	}
+
+	for _, tt := range createCases {
+		t.Run(tt.desc, func(t *testing.T) {
+			teardownTest, mds, msm := setupTest(t)
+			defer teardownTest(t)
+
+			reqModel, _, _ := loadTestResurce()
+
+			w := httptest.NewRecorder()
+			req, err := testutil.GetARMTestHTTPRequest(context.Background(), http.MethodPut, resourceTestHeaderFile, reqModel)
+			require.NoError(t, err)
+
+			ctx := testutil.ARMTestContextFromRequest(req)
+
+			mds.EXPECT().Get(gomock.Any(), gomock.Any()).
+				Return(&store.Object{}, tt.getErr).
+				Times(1)
+
+			if tt.getErr == nil || errors.Is(&store.ErrNotFound{}, tt.getErr) {
+				mds.EXPECT().Save(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(tt.saveErr).
+					Times(1)
+			}
+
+			opts := ctrl.Options{
+				StorageClient: mds,
+				StatusManager: msm,
+			}
+
+			resourceOpts := ctrl.ResourceOptions[TestResourceDataModel]{
+				RequestConverter:  testResourceDataModelFromVersioned,
+				ResponseConverter: testResourceDataModelToVersioned,
+				UpdateFilters: []ctrl.UpdateFilter[TestResourceDataModel]{
+					testValidateRequest,
+				},
+			}
+
+			ctl, err := NewDefaultSyncPut(opts, resourceOpts)
+			require.NoError(t, err)
+
+			resp, err := ctl.Run(ctx, w, req)
+
+			if tt.rErr != nil {
+				require.Error(t, tt.rErr)
+			} else {
+				require.NoError(t, err)
+
+				_ = resp.Apply(ctx, w, req)
+				require.Equal(t, tt.rCode, w.Result().StatusCode)
+			}
+		})
+	}
+}
+
+func TestDefaultSyncPut_Update(t *testing.T) {
+	updateCases := []struct {
+		desc               string
+		versionedInputFile string
+		datamodelFile      string
+		getErr             error
+		skipSave           bool
+		saveErr            error
+		qErr               error
+		rbErr              error
+		rCode              int
+		rErr               error
+	}{
+		{
+			"sync-update-existing-resource-success",
+			"resource-sync-request.json",
+			"resource-sync-datamodel.json",
+			nil,
+			false,
+			nil,
+			nil,
+			nil,
+			http.StatusOK,
+			nil,
+		},
+		{
+			"sync-update-existing-resource-invalid-request",
+			"resource-sync-request-invalid.json",
+			"resource-datamodel.json",
+			nil,
+			true,
+			nil,
+			nil,
+			nil,
+			http.StatusBadRequest,
+			nil,
+		},
+		{
+			"sync-update-existing-resource-concurrency-error",
+			"resource-request.json",
+			"resource-datamodel.json",
+			nil,
+			false,
+			&store.ErrConcurrency{},
+			nil,
+			nil,
+			http.StatusInternalServerError,
+			&store.ErrConcurrency{},
+		},
+		{
+			"sync-update-existing-resource-save-error",
+			"resource-request.json",
+			"resource-datamodel.json",
+			nil,
+			false,
+			&store.ErrInvalid{Message: "testing initial save err"},
+			nil,
+			nil,
+			http.StatusInternalServerError,
+			&store.ErrInvalid{Message: "testing initial save err"},
+		},
+	}
+
+	for _, tt := range updateCases {
+		t.Run(tt.desc, func(t *testing.T) {
+			teardownTest, mds, msm := setupTest(t)
+			defer teardownTest(t)
+
+			reqModel := &TestResource{}
+			_ = json.Unmarshal(testutil.ReadFixture(tt.versionedInputFile), reqModel)
+
+			reqDataModel := &TestResourceDataModel{}
+			_ = json.Unmarshal(testutil.ReadFixture(tt.datamodelFile), reqDataModel)
+
+			w := httptest.NewRecorder()
+			req, err := testutil.GetARMTestHTTPRequest(context.Background(), http.MethodPatch, resourceTestHeaderFile, reqModel)
+			require.NoError(t, err)
+
+			ctx := testutil.ARMTestContextFromRequest(req)
+			sCtx := v1.ARMRequestContextFromContext(ctx)
+
+			so := &store.Object{
+				Metadata: store.Metadata{ID: sCtx.ResourceID.String()},
+				Data:     reqDataModel,
+			}
+
+			mds.EXPECT().Get(gomock.Any(), gomock.Any()).
+				Return(so, tt.getErr).
+				Times(1)
+
+			if tt.getErr == nil && !tt.skipSave {
+				mds.EXPECT().Save(gomock.Any(), gomock.Any(), gomock.Any()).
+					Return(tt.saveErr).
+					Times(1)
+			}
+
+			opts := ctrl.Options{
+				StorageClient: mds,
+				StatusManager: msm,
+			}
+
+			resourceOpts := ctrl.ResourceOptions[TestResourceDataModel]{
+				RequestConverter:  testResourceDataModelFromVersioned,
+				ResponseConverter: testResourceDataModelToVersioned,
+				UpdateFilters: []ctrl.UpdateFilter[TestResourceDataModel]{
+					testValidateRequest,
+				},
+			}
+
+			ctl, err := NewDefaultSyncPut(opts, resourceOpts)
+			require.NoError(t, err)
+
+			resp, err := ctl.Run(ctx, w, req)
+			if resp != nil {
+				_ = resp.Apply(ctx, w, req)
+				require.Equal(t, tt.rCode, w.Result().StatusCode)
+			}
+
+			if tt.rCode == http.StatusAccepted {
+				require.NoError(t, err)
+			}
+
+			if tt.rErr != nil {
+				require.ErrorIs(t, tt.rErr, err)
+			}
+		})
+	}
+}
