d9bf1d29d39b553f4ca7821162377dc290d8b6c0
nithyatsu <nithyasu@microsoft.com>
2023-04-21 13:52:41 -0700

    wip

--- test/functional/corerp/mechanics/aws_mechanics_test.go
+++ test/functional/corerp/mechanics/aws_mechanics_test.go
@@ -16,7 +16,7 @@ import (
 )
 
 func Test_AWSRedeployWithUpdatedResourceUpdatesResource(t *testing.T) {
-	t.Skip("Skip aws mechanics test")
+	//t.Skip("Skip aws mechanics test")
 	templateFmt := "testdata/aws-mechanics-redeploy-withupdatedresource.step%d.bicep"
 	name := "radiusfunctionaltestbucket-" + uuid.New().String()
 
@@ -72,7 +72,7 @@ func Test_AWSRedeployWithUpdatedResourceUpdatesResource(t *testing.T) {
 }
 
 func Test_AWSRedeployWithCreateAndWriteOnlyPropertyUpdate(t *testing.T) {
-	t.Skip("This test will fail because step 2 is updating a create-and-write-only property.")
+	//t.Skip("This test will fail because step 2 is updating a create-and-write-only property.")
 	name := "my-db"
 	templateFmt := "testdata/aws-mechanics-redeploy-withcreateandwriteonlypropertyupdate.step%d.bicep"
 
