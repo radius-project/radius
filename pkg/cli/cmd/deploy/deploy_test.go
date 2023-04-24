056b64f8edff5231445d5b4a9fbed2ba711795c0
nithyatsu <nithyasu@microsoft.com>
2023-04-24 10:39:51 -0700

    add aws provider UT

--- pkg/cli/cmd/deploy/deploy_test.go
+++ pkg/cli/cmd/deploy/deploy_test.go
@@ -177,7 +177,7 @@ func Test_Validate(t *testing.T) {
 }
 
 func Test_Run(t *testing.T) {
-	t.Run("Environment-scoped deployment", func(t *testing.T) {
+	t.Run("Environment-scoped deployment with az provider", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
 
@@ -198,6 +198,81 @@ func Test_Run(t *testing.T) {
 		providers := &v20220315privatepreview.Providers{
 			Azure: &v20220315privatepreview.ProvidersAzure{
 				Scope: to.Ptr("/subscriptions/test-subId/resourceGroups/test-rg"),
+			},
+		}
+
+		filePath := "app.bicep"
+		progressText := fmt.Sprintf(
+			"Deploying template '%v' into environment '%v' from workspace '%v'...\n\n"+
+				"Deployment In Progress...", filePath, radcli.TestEnvironmentName, workspace.Name)
+
+		options := deploy.Options{
+			Providers:      providers,
+			EnvironmentID:  fmt.Sprintf("/planes/radius/local/resourceGroups/%s/providers/applications.core/environments/%s", radcli.TestEnvironmentName, radcli.TestEnvironmentName),
+			Workspace:      *workspace,
+			Parameters:     map[string]map[string]any{},
+			CompletionText: "Deployment Complete",
+			ProgressText:   progressText,
+			Template:       map[string]any{},
+		}
+
+		deployMock := deploy.NewMockInterface(ctrl)
+		deployMock.EXPECT().
+			DeployWithProgress(gomock.Any(), options).
+			DoAndReturn(func(ctx context.Context, o deploy.Options) (clients.DeploymentResult, error) {
+				// Capture options for verification
+				options = o
+				return clients.DeploymentResult{}, nil
+			}).
+			Times(1)
+
+		outputSink := &output.MockOutput{}
+		runner := &Runner{
+			Bicep:  bicep,
+			Deploy: deployMock,
+			Output: outputSink,
+
+			FilePath:        filePath,
+			EnvironmentID:   fmt.Sprintf("/planes/radius/local/resourceGroups/%s/providers/applications.core/environments/%s", radcli.TestEnvironmentName, radcli.TestEnvironmentName),
+			EnvironmentName: radcli.TestEnvironmentName,
+			Parameters:      map[string]map[string]any{},
+			Workspace:       workspace,
+			Providers:       providers,
+		}
+
+		err := runner.Run(context.Background())
+		require.NoError(t, err)
+
+		// Deployment is scoped to env
+		require.Equal(t, "", options.ApplicationID)
+		require.Equal(t, runner.EnvironmentID, options.EnvironmentID)
+
+		// All of the output in this command is being done by functions that we mock for testing, so this
+		// is always empty.
+		require.Empty(t, outputSink.Writes)
+	})
+
+	t.Run("Environment-scoped deployment with aws provider", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		bicep := bicep.NewMockInterface(ctrl)
+		bicep.EXPECT().
+			PrepareTemplate("app.bicep").
+			Return(map[string]any{}, nil).
+			Times(1)
+
+		workspace := &workspaces.Workspace{
+			Connection: map[string]any{
+				"kind":    "kubernetes",
+				"context": "kind-kind",
+			},
+			Name: "kind-kind",
+		}
+
+		providers := &v20220315privatepreview.Providers{
+			Aws: &v20220315privatepreview.ProvidersAws{
+				Scope: to.Ptr("/accounts/test-accountId/regions/test-region"),
 			},
 		}
 
