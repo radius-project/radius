1b339f93b7550920b965adccb92d814f50b13cba
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-04-24 18:35:00 -0700

    Make Recipe parameters a separate table for `rad recipe show` (#5469)
    # Description
    
    Breaks recipe output into two tables, one for Recipe and one for
    paramters
    
    
    ```
    go run . recipe show --name redis-azure
    RECIPE NAME  TYPE                           TEMPLATE
    redis-azure  Applications.Link/redisCaches  radius.azurecr.io/recipes/rediscaches/azure:1.0
    
    PARAMETER NAME  TYPE      DEFAULT VALUE               MIN       MAX
    capacity        int       0                           0         6
    family          string    C                           -         -
    location        string    [resourceGroup().location]  -         -
    name            string    Basic                       -         -
    ```
    
    ## Issue reference
    
    Partially addresses #5304
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [x] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [x] Extended the documentation / Created issue for it
    
    ---------
    
    Co-authored-by: Karishma Chawla <kachawla@microsoft.com>

--- pkg/cli/cmd/recipe/show/show.go
+++ pkg/cli/cmd/recipe/show/show.go
@@ -8,7 +8,6 @@ package show
 import (
 	"context"
 	"fmt"
-	"sort"
 
 	"github.com/project-radius/radius/pkg/cli"
 	"github.com/project-radius/radius/pkg/cli/cmd/commonflags"
@@ -124,66 +123,69 @@ func (r *Runner) Run(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-
-	var recipeParams []EnvironmentRecipe
-	keys := make([]string, 0, len(recipeDetails.Parameters))
-
-	for k := range recipeDetails.Parameters {
-		keys = append(keys, k)
+	recipe := Recipe{
+		Name:         r.RecipeName,
+		LinkType:     *recipeDetails.LinkType,
+		TemplatePath: *recipeDetails.TemplatePath,
 	}
 
-	// to keep order of parameters consistent - sort.
-	sort.Strings(keys)
-	var paramDetailIndex = 0
-	for _, paramName := range keys {
-		paramDetails, ok := recipeDetails.Parameters[paramName].(map[string]any)
-		if !ok {
-			return fmt.Errorf("parameter details for parameter %s are formatted incorrectly", paramName)
+	err = r.Output.WriteFormatted(r.Format, recipe, objectformats.GetRecipeTableFormat())
+	if err != nil {
+		return err
+	}
+
+	r.Output.LogInfo("")
+
+	var recipeParams []RecipeParameter
+
+	for parameter := range recipeDetails.Parameters {
+		values := recipeDetails.Parameters[parameter].(map[string]any)
+
+		paramItem := RecipeParameter{
+			Name:         parameter,
+			DefaultValue: "-",
+			MaxValue:     "-",
+			MinValue:     "-",
 		}
 
-		var paramDetailValueIndex = 0
-		for paramDetailName, paramDetailValue := range paramDetails {
-			var recipe EnvironmentRecipe
-			if paramDetailIndex == 0 && paramDetailValueIndex == 0 {
-				recipe = EnvironmentRecipe{
-					RecipeName:           r.RecipeName,
-					LinkType:             *recipeDetails.LinkType,
-					TemplatePath:         *recipeDetails.TemplatePath,
-					ParameterName:        paramName,
-					ParameterDetailName:  paramDetailName,
-					ParameterDetailValue: paramDetailValue,
-				}
-			} else if paramDetailValueIndex == 0 {
-				recipe = EnvironmentRecipe{
-					ParameterName:        paramName,
-					ParameterDetailName:  paramDetailName,
-					ParameterDetailValue: paramDetailValue,
-				}
-			} else {
-				recipe = EnvironmentRecipe{
-					ParameterDetailName:  paramDetailName,
-					ParameterDetailValue: paramDetailValue,
-				}
+		for paramDetailName, paramDetailValue := range values {
+			switch paramDetailName {
+			case "type":
+				paramItem.Type = paramDetailValue.(string)
+			case "defaultValue":
+				paramItem.DefaultValue = paramDetailValue
+			case "maxValue":
+				paramItem.MaxValue = fmt.Sprintf("%v", paramDetailValue.(float64))
+			case "minValue":
+				paramItem.MinValue = fmt.Sprintf("%v", paramDetailValue.(float64))
 			}
-
-			recipeParams = append(recipeParams, recipe)
-			paramDetailValueIndex += 1
 		}
-		paramDetailIndex += 1
+
+		recipeParams = append(recipeParams, paramItem)
 	}
+
 	err = r.Output.WriteFormatted(r.Format, recipeParams, objectformats.GetRecipeParamsTableFormat())
 	if err != nil {
 		return err
 	}
 
+	if len(recipeParams) == 0 {
+		r.Output.LogInfo("No parameters available")
+	}
+
 	return nil
 }
 
-type EnvironmentRecipe struct {
-	RecipeName           string      `json:"recipeName,omitempty"`
-	LinkType             string      `json:"linkType,omitempty"`
-	TemplatePath         string      `json:"templatePath,omitempty"`
-	ParameterName        string      `json:"parameterName,omitempty"`
-	ParameterDetailName  string      `json:"parameterDetailName,omitempty"`
-	ParameterDetailValue interface{} `json:"parameterDetailValue,omitempty"`
+type RecipeParameter struct {
+	Name         string      `json:"name,omitempty"`
+	DefaultValue interface{} `json:"defaultValue,omitempty"`
+	Type         string      `json:"type,omitempty"`
+	MaxValue     string      `json:"maxValue,omitempty"`
+	MinValue     string      `json:"minValue,omitempty"`
+}
+
+type Recipe struct {
+	Name         string `json:"name,omitempty"`
+	LinkType     string `json:"linkType,omitempty"`
+	TemplatePath string `json:"templatePath,omitempty"`
 }
--- pkg/cli/cmd/recipe/show/show_test.go
+++ pkg/cli/cmd/recipe/show/show_test.go
@@ -14,9 +14,11 @@ import (
 	"github.com/project-radius/radius/pkg/cli/clients"
 	"github.com/project-radius/radius/pkg/cli/connections"
 	"github.com/project-radius/radius/pkg/cli/framework"
+	"github.com/project-radius/radius/pkg/cli/objectformats"
 	"github.com/project-radius/radius/pkg/cli/output"
 	"github.com/project-radius/radius/pkg/cli/workspaces"
 	"github.com/project-radius/radius/pkg/corerp/api/v20220315privatepreview"
+	"github.com/project-radius/radius/pkg/linkrp"
 	"github.com/project-radius/radius/pkg/to"
 	"github.com/project-radius/radius/test/radcli"
 	"github.com/stretchr/testify/require"
@@ -83,24 +85,45 @@ func Test_Run(t *testing.T) {
 		t.Run("Success", func(t *testing.T) {
 			ctrl := gomock.NewController(t)
 
-			envRecipes := v20220315privatepreview.EnvironmentRecipeProperties{
+			envRecipe := v20220315privatepreview.EnvironmentRecipeProperties{
 				LinkType:     to.Ptr("Applications.Link/mongoDatabases"),
 				TemplatePath: to.Ptr("testpublicrecipe.azurecr.io/bicep/modules/mongodatabases:v1"),
 				Parameters: map[string]any{
 					"throughput": map[string]any{
-						"type": "float64",
-						"max":  float64(800),
+						"type":     "float64",
+						"maxValue": float64(800),
 					},
 					"sku": map[string]any{
 						"type": "string",
 					},
 				},
 			}
+			recipe := Recipe{
+				Name:         "cosmosDB",
+				LinkType:     linkrp.MongoDatabasesResourceType,
+				TemplatePath: "testpublicrecipe.azurecr.io/bicep/modules/mongodatabases:v1",
+			}
+			recipeParams := []RecipeParameter{
+				{
+					Name:         "throughput",
+					Type:         "float64",
+					MaxValue:     "800",
+					MinValue:     "-",
+					DefaultValue: "-",
+				},
+				{
+					Name:         "sku",
+					Type:         "string",
+					MaxValue:     "-",
+					MinValue:     "-",
+					DefaultValue: "-",
+				},
+			}
 
 			appManagementClient := clients.NewMockApplicationsManagementClient(ctrl)
 			appManagementClient.EXPECT().
 				ShowRecipe(gomock.Any(), gomock.Any(), gomock.Any()).
-				Return(envRecipes, nil).Times(1)
+				Return(envRecipe, nil).Times(1)
 
 			outputSink := &output.MockOutput{}
 
@@ -114,52 +137,24 @@ func Test_Run(t *testing.T) {
 
 			err := runner.Run(context.Background())
 			require.NoError(t, err)
-			output := outputSink.Writes[0].(output.FormattedOutput)
-			skuType := false
-			throughputType := false
-			throughputMax := false
-			outputParams := output.Obj.([]EnvironmentRecipe)
-			require.Equal(t, 3, len(outputParams))
-			for i, envRecipeObj := range output.Obj.([]EnvironmentRecipe) {
-				if i == 0 {
-					require.Equal(t, "cosmosDB", envRecipeObj.RecipeName)
-					require.Equal(t, "Applications.Link/mongoDatabases", envRecipeObj.LinkType)
-					require.Equal(t, "testpublicrecipe.azurecr.io/bicep/modules/mongodatabases:v1", envRecipeObj.TemplatePath)
-				} else {
-					require.Equal(t, "", envRecipeObj.RecipeName)
-					require.Equal(t, "", envRecipeObj.LinkType)
-					require.Equal(t, "", envRecipeObj.TemplatePath)
-				}
-				if envRecipeObj.ParameterName == "sku" && envRecipeObj.ParameterDetailName == "type" {
-					require.Equal(t, "string", envRecipeObj.ParameterDetailValue)
-					skuType = true
-				}
 
-				if envRecipeObj.ParameterName == "throughput" {
-					if envRecipeObj.ParameterDetailName == "type" {
-						require.Equal(t, "float64", envRecipeObj.ParameterDetailValue)
-						throughputType = true
-					}
-					if envRecipeObj.ParameterDetailName == "max" {
-						require.Equal(t, float64(800), envRecipeObj.ParameterDetailValue)
-						throughputMax = true
-					}
-				}
-
-				if envRecipeObj.ParameterName == "" {
-					if envRecipeObj.ParameterDetailName == "type" && !throughputType {
-						require.Equal(t, "float64", envRecipeObj.ParameterDetailValue)
-						throughputType = true
-					}
-					if envRecipeObj.ParameterDetailName == "max" && !throughputMax {
-						require.Equal(t, float64(800), envRecipeObj.ParameterDetailValue)
-						throughputMax = true
-					}
-				}
+			expected := []any{
+				output.FormattedOutput{
+					Format:  "table",
+					Obj:     recipe,
+					Options: objectformats.GetRecipeTableFormat(),
+				},
+				output.LogOutput{
+					Format: "",
+				},
+				output.FormattedOutput{
+					Format:  "table",
+					Obj:     recipeParams,
+					Options: objectformats.GetRecipeParamsTableFormat(),
+				},
 			}
-			require.True(t, skuType)
-			require.True(t, throughputType)
-			require.True(t, throughputMax)
+			require.Equal(t, expected, outputSink.Writes)
+
 		})
 	})
 }
