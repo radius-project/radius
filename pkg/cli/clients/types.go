a0bef0d61b2db6172cd5875cdf9c62924929ce37
nithyatsu <nithyasu@microsoft.com>
2023-05-05 08:12:42 -0700

    wip

--- pkg/cli/clients/types.go
+++ pkg/cli/clients/types.go
@@ -6,21 +6,27 @@
 package clients
 
 type Providers struct {
-	// Azure represents the configuration for the Azure IAC provider used during deployment. This field is optional.
-	Azure  *AzureProvider
-	AWS    *AWSProvider
+	// Azure provider information. This field is optional.
+	Azure *AzureProvider
+	// AWS provider information. This field is optional.
+	AWS *AWSProvider
+	// Radius provider information.
 	Radius *RadiusProvider
 }
 
 type AzureProvider struct {
+	// Scope is the target level for deploying the Azure resources.
 	Scope string
 }
 
 type AWSProvider struct {
+	// Scope is the target level for deploying the AWS resources.
 	Scope string
 }
 
 type RadiusProvider struct {
+	// Currently, we must provide an environment ID for deploying applications.
 	EnvironmentID string
+	// ApplicationID is the ID of the application to be deployed. This is optional.
 	ApplicationID string
 }
