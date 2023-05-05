198bc5a141e89d340d47cfdbaef54a909f4ebda0
karishma-chawla <74574173+karishma-chawla@users.noreply.github.com>
2023-05-05 12:49:04 -0700

    Implement driver to install terraform, run terrafor init and apply

--- pkg/recipes/driver/terraform.go
+++ pkg/recipes/driver/terraform.go
@@ -8,18 +8,26 @@ package driver
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/go-logr/logr"
-	"github.com/hashicorp/go-version"
+	"github.com/google/uuid"
 	"github.com/hashicorp/hc-install/product"
 	"github.com/hashicorp/hc-install/releases"
 	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/project-radius/radius/pkg/recipes"
+	"github.com/project-radius/radius/pkg/ucp/util"
+)
+
+const (
+	installDirRoot = "/terraform/install"
+	workingDirRoot = "/terraform/exec"
 )
 
 var _ Driver = (*terraformDriver)(nil)
 
-// NewTerraformDriver creates a new instance of Driver to execute a Terraform recipe.
+// NewTerraformDriver creates a new instance of driver to execute a Terraform recipe.
 func NewTerraformDriver() Driver {
 	return &terraformDriver{}
 }
@@ -31,27 +39,60 @@ func (d *terraformDriver) Execute(ctx context.Context, configuration recipes.Con
 	logger := logr.FromContextOrDiscard(ctx)
 	logger.Info(fmt.Sprintf("Deploying recipe: %q, template: %q", recipe.Name, definition.TemplatePath))
 
-	// Generate Terraform configuration
-
-	// Terraform init
-	installer := &releases.ExactVersion{
-		Product: product.Terraform,
-		Version: version.Must(version.NewVersion("1.0.6")),
+	// Create Terraform installation directory
+	installDir := filepath.Join(installDirRoot, util.NormalizeStringToLower(recipe.ResourceID), uuid.NewString())
+	logger.Info(fmt.Sprintf("Creating Terraform install directory: %q", installDir))
+	if err := os.MkdirAll(installDir, os.ModePerm); err != nil {
+		return nil, fmt.Errorf("failed to create directory for terraform installation for resource %q: %w", recipe.ResourceID, err)
 	}
+	defer os.RemoveAll(installDir)
 
-	_, err := installer.Install(context.Background())
+	// Install Terraform
+	// Using latest version, revisit this if we want to use a specific version.
+	installer := &releases.LatestVersion{
+		Product:    product.Terraform,
+		InstallDir: installDir,
+	}
+	// We should look into checking if an exsiting installation of Terraform is available.
+	// For initial iteration we will always install Terraform. Optimizations can be made later.
+	execPath, err := installer.Install(ctx)
 	if err != nil {
+		return nil, fmt.Errorf("failed to install terraform for resource %q: %w", recipe.ResourceID, err)
+	}
+	// TODO check if anything else is needed to clean up Terraform installation.
+	defer installer.Remove(ctx)
+
+	// Create working directory for Terraform execution
+	workingDir := filepath.Join(workingDirRoot, util.NormalizeStringToLower(recipe.ResourceID), uuid.NewString())
+	logger.Info(fmt.Sprintf("Creating Terraform working directory: %q", workingDir))
+	if err := os.Mkdir(workingDir, os.ModePerm); err != nil {
+		return nil, fmt.Errorf("failed to create working directory for terraform execution for Radius resource %q. %w", recipe.ResourceID, err)
+	}
+	defer os.RemoveAll(workingDir)
+
+	if err := d.initAndApply(ctx, recipe.ResourceID, workingDir, execPath); err != nil {
 		return nil, err
 	}
-	_, err = tfexec.NewTerraform("", "")
-	if err != nil {
-		return nil, err
-	}
-
-	// Terraform import to load resource group
-	// Get context parameter
-	// Terraform apply with recipe input params and context param
-	// Terraform destroy
 
 	return &recipes.RecipeOutput{}, nil
+}
+
+// Runs Terraform init and apply in the provided working directory.
+func (d *terraformDriver) initAndApply(ctx context.Context, resourceID, workingDir, execPath string) error {
+	tf, err := tfexec.NewTerraform(workingDir, execPath)
+	if err != nil {
+		return err
+	}
+
+	// Initialize Terraform
+	if err := tf.Init(ctx); err != nil {
+		return fmt.Errorf("terraform init failure. Radius resource %q. %w", resourceID, err)
+	}
+
+	// Apply Terraform configuration
+	if err := tf.Apply(ctx); err != nil {
+		return fmt.Errorf("terraform apply failure. Radius resource %q. %w", resourceID, err)
+	}
+
+	return nil
 }
