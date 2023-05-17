d225626cee2163a27cd39e639f156955e8b5ee68
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2023-05-17 10:29:22 -0700

    more fixes  after rebase

--- build/util.mk
+++ build/util.mk
@@ -9,7 +9,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ------------------------------------------------------------.
+# ------------------------------------------------------------
 
 # go-install-tool will 'go install' any package $2 if it is missing from $1
 define go-install-tool
