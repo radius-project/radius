dc168fcbad1274aee745b3007cf1d9e505783d7e
sk593 <shruthikumar@microsoft.com>
2023-05-19 11:18:48 -0700

    fix merge issues

--- pkg/linkrp/backend/service.go
+++ pkg/linkrp/backend/service.go
@@ -98,7 +98,7 @@ func (s *Service) Run(ctx context.Context) error {
 	engine := engine.NewEngine(engine.Options{
 		ConfigurationLoader: configLoader,
 		Drivers: map[string]driver.Driver{
-			recipes.DriverBicep: driver.NewBicepDriver(clientOptions, deploymentEngineClient),
+			recipes.TemplateKindBicep: driver.NewBicepDriver(clientOptions, deploymentEngineClient),
 		},
 	})
 
