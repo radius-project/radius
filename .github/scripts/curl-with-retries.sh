973c35255f532ed4699f04a84bc678e5631a2d28
Yetkin Timocin <ytimocin@microsoft.com>
2023-05-22 18:24:38 -0700

    Adding retries to bicep download (#5578)
    # Description
    
    There seems to be some kind of flakiness around the Download Bicep steps
    of our CI/CD pipeline. Updating the two places that initiate the
    Download Bicep step with a new retry logic.
    
    ## Issue reference
    Fixes: #5581 
    
    ## Checklist
    
    Please make sure you've completed the relevant tasks for this PR, out of
    the following list:
    
    * [x] Code compiles correctly
    * [ ] Adds necessary unit tests for change
    * [ ] Adds necessary E2E tests for change
    * [x] Unit tests passing
    * [x] Extended the documentation / Created issue for it

--- .github/scripts/curl-with-retries.sh
+++ .github/scripts/curl-with-retries.sh
@@ -1,4 +1,37 @@
 # See https://everything.curl.dev/usingcurl/downloads/retry
 #
 # Unfortunately we can't use --retry-all-errors as the agent does not support it.
-curl $@ --retry 5
+
+# Number of retries
+MAX_RETRIES=5
+
+# Retry delay in seconds
+RETRY_DELAY=5
+
+# Retry loop
+RETRY_COUNT=0
+while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+    # Download the file with retries and resume capability
+    curl --retry $MAX_RETRIES --retry-delay $RETRY_DELAY --continue-at - $@
+
+    # Check if the download was successful
+    if [ $? -eq 0 ]; then
+        echo "File downloaded successfully"
+        break
+    fi
+
+    # Download failed, increase the retry count
+    RETRY_COUNT=$((RETRY_COUNT + 1))
+
+    # Check if there are more retries left
+    if [ $RETRY_COUNT -lt $MAX_RETRIES ]; then
+        # Retry after delay
+        echo "Retrying in $RETRY_DELAY seconds..."
+        sleep $RETRY_DELAY
+    fi
+done
+
+# Check if the maximum number of retries exceeded
+if [ $RETRY_COUNT -eq $MAX_RETRIES ]; then
+    echo "Maximum number of retries exceeded"
+fi
