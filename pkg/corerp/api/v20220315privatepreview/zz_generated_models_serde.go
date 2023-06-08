dcebd5072f73a27c4549aa65842d064af80eda93
willdavsmith <willdavsmith@gmail.com>
2023-06-08 13:06:33 -0700

    Removing hostname from gateway.properties.tls

--- pkg/corerp/api/v20220315privatepreview/zz_generated_models_serde.go
+++ pkg/corerp/api/v20220315privatepreview/zz_generated_models_serde.go
@@ -1547,7 +1547,6 @@ func (g *GatewayPropertiesHostname) UnmarshalJSON(data []byte) error {
 func (g GatewayPropertiesTLS) MarshalJSON() ([]byte, error) {
 	objectMap := make(map[string]interface{})
 	populate(objectMap, "certificateFrom", g.CertificateFrom)
-	populate(objectMap, "hostname", g.Hostname)
 	populate(objectMap, "minimumProtocolVersion", g.MinimumProtocolVersion)
 	populate(objectMap, "sslPassthrough", g.SSLPassthrough)
 	return json.Marshal(objectMap)
@@ -1564,9 +1563,6 @@ func (g *GatewayPropertiesTLS) UnmarshalJSON(data []byte) error {
 		switch key {
 		case "certificateFrom":
 				err = unpopulate(val, "CertificateFrom", &g.CertificateFrom)
-				delete(rawMsg, key)
-		case "hostname":
-				err = unpopulate(val, "Hostname", &g.Hostname)
 				delete(rawMsg, key)
 		case "minimumProtocolVersion":
 				err = unpopulate(val, "MinimumProtocolVersion", &g.MinimumProtocolVersion)
