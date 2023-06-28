fc26acaf9ce13d4951cf1c44991ac1f444ed8261
willdavsmith <willdavsmith@gmail.com>
2023-06-28 13:29:12 -0700

    Renaming test

--- test/functional/corerp/resources/extender_test.go
+++ test/functional/corerp/resources/extender_test.go
@@ -93,3 +93,70 @@ func Test_Extender_Recipe(t *testing.T) {
 
 	test.Test(t)
 }
+
+func Test_Extender_RecipeAWS(t *testing.T) {
+	awsAccountId := os.Getenv("AWS_ACCOUNT_ID")
+	awsRegion := os.Getenv("AWS_REGION")
+	// Error the test if the required environment variables are not set
+	// for running locally set the environment variables
+	if awsAccountId == "" || awsRegion == "" {
+		t.Error("This test needs the env variables AWS_ACCOUNT_ID and AWS_REGION to be set")
+	}
+
+	template := "testdata/corerp-resources-extenders-aws-s3-recipe.bicep"
+	name := "corerp-resources-extenders-aws-s3-recipe"
+	appName := "corerp-resources-extenders-aws-s3-recipe-app"
+	bucketName := generateS3BucketName()
+
+	test := corerp.NewCoreRPTest(t, name, []corerp.TestStep{
+		{
+			Executor: step.NewDeployExecutor(
+				template,
+				fmt.Sprintf("bucketName=%s", bucketName),
+				functional.GetAWSAccountId(),
+				functional.GetAWSRegion(),
+				functional.GetRecipeRegistry(),
+				functional.GetRecipeVersion(),
+			),
+			CoreRPResources: &validation.CoreRPResourceSet{
+				Resources: []validation.CoreRPResource{
+					{
+						Name: "corerp-resources-extenders-aws-s3-recipe-env",
+						Type: validation.EnvironmentsResource,
+					},
+					{
+						Name: "corerp-resources-extenders-aws-s3-recipe-app",
+						Type: validation.ApplicationsResource,
+					},
+					{
+						Name: "corerp-resources-extenders-aws-s3-recipe",
+						Type: validation.ExtendersResource,
+						App:  appName,
+						OutputResources: []validation.OutputResourceResponse{
+							{
+								Provider: resourcemodel.ProviderAWS,
+								LocalID:  "RecipeResource0",
+							},
+						},
+					},
+				},
+			},
+			AWSResources: &validation.AWSResourceSet{
+				Resources: []validation.AWSResource{
+					{
+						Name:       bucketName,
+						Type:       validation.AWSS3BucketResourceType,
+						Identifier: bucketName,
+						Properties: map[string]any{
+							"BucketName": bucketName,
+						},
+						SkipDeletion: true, // will be deleted by the recipe
+					},
+				},
+			},
+			SkipObjectValidation: true,
+		},
+	})
+
+	test.Test(t)
+}
