4b16086b7f30ef9e5f39f63e8e5f8fee52ceba48
Young Bu Park <youngp@microsoft.com>
2023-08-04 14:22:49 -0700

    add testdata

---
+++ pkg/recipes/terraform/testdata/.terraform/modules/test-module-recipe-context/variables.tf
@@ -0,0 +1,36 @@
+variable "context" {
+  description = "This variable contains Radius recipe context."
+  type        = object({
+    resource = object({
+      name = string
+      id = string
+      type = string
+    })
+    application = object({
+      name = string
+      id = string
+    })
+    environment = object({
+      name = string
+      id = string
+    })
+    runtime = object({
+      name = string
+      id = string
+    })
+    azure = object({
+      resourceGroup = object({
+        name = string
+        id = string
+      })
+      subscription = object({
+        subscriptionId = string
+        id = string
+      })
+    })
+    aws = object({
+      region = string
+      account = string
+    })
+  })
+}
