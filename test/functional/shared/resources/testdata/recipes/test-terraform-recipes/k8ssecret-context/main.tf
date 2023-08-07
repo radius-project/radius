800cfef72459338cbfdaadeeeccbe76c564092ca
Young Bu Park <youngp@microsoft.com>
2023-08-07 10:18:07 -0700

    wip

--- test/functional/shared/resources/testdata/recipes/test-terraform-recipes/k8ssecret-context/main.tf
+++ test/functional/shared/resources/testdata/recipes/test-terraform-recipes/k8ssecret-context/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.3"
+      version = ">= 2.0"
     }
   }
 }
@@ -11,6 +11,8 @@ resource "kubernetes_secret" "recipe-context" {
   metadata {
     name = var.context.resource.name
     namespace = var.context.runtime.kubernetes.namespace
+
+    # Add labels to enable functional test recognize the created resource.
     labels = {
       "radius.dev/application" = var.context.application.name
       "radius.dev/resource" =  var.context.resource.name
@@ -20,7 +22,8 @@ resource "kubernetes_secret" "recipe-context" {
   data = {
     "resource.id" = base64encode(var.context.resource.id)
     "resource.type" = base64encode(var.context.resource.type)
-    "azure.subscription_id" = base64encode(var.context.runtime.azure.subscription.subscriptionId)
+    "azure.subscription_id" = base64encode(var.context.azure.subscription.subscriptionId)
+    # Serialize the entire recipe context object. Functional test code will decode and assert the entire values.
     "recipe_context" = base64encode(jsonencode(var.context))
   }
 
