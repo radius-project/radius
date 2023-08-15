4baa20f14bfee2b1febeef963404bd216d411791
willdavsmith <willdavsmith@gmail.com>
2023-08-15 08:33:00 -0700

    PR Comments

--- .github/scripts/release-create-tags-and-branches.sh
+++ .github/scripts/release-create-tags-and-branches.sh
@@ -18,29 +18,21 @@
 
 set -xe
 
-# Comma-separated list of repositories to create release branches and tags for
-# (e.g. "radius,bicep,deployment-engine,recipes")
-REPOSITORIES=$1
+# Repository to create tags and branches for
+# (e.g. radius)
+REPOSITORY=$1
 
 # Comma-separated list of tag versions 
 # (e.g. v0.2.0,v0.1.0)
 VERSIONS=$2
-
-if [[ -z "$REPOSITORIES" ]]; then
-  echo "Error: REPOSITORIES is not set."
-  exit 1
-fi
 
 if [[ -z "$VERSIONS" ]]; then
   echo "Error: VERSIONS is not set."
   exit 1
 fi
 
-# Create the tags and branches for each repository
-for REPOSITORY in $(echo $REPOSITORIES | sed "s/,/ /g")
+# Create the tags and branches for each version
+for VERSION in $(echo $VERSIONS | sed "s/,/ /g")
 do
-  for VERSION in $(echo $VERSIONS | sed "s/,/ /g")
-  do
-    sh .github/scripts/release-create-tag-and-branch.sh $REPOSITORY $VERSION
-  done
+  sh .github/scripts/release-create-tag-and-branch.sh $REPOSITORY $VERSION
 done
