2b052dfc2763e5433b6db8fbbc5aa4fca390eab8
Vishwanath Hiremath <vhiremath@microsoft.com>
2023-08-24 22:10:05 -0700

    adding changes to verify secret object

--- test/validation/k8s.go
+++ test/validation/k8s.go
@@ -60,6 +60,7 @@ type K8sObject struct {
 	Labels               map[string]string
 	Kind                 string
 	SkipLabelValidation  bool
+	ResourceName         string
 }
 
 // NewK8sPodForResource creates a new K8sObject with Kind set to "Pod" and the selector labels for the pod
@@ -121,6 +122,18 @@ func NewK8sSecretForResource(application string, name string) K8sObject {
 		},
 		Kind:   "Secret",
 		Labels: kuberneteskeys.MakeSelectorLabels(application, name),
+	}
+}
+
+// NewK8sSecretForResourceWithResourceName creates a K8sObject for a secret with the Labels set to the application and name.
+func NewK8sSecretForResourceWithResourceName(resourceName string) K8sObject {
+	return K8sObject{
+		GroupVersionResource: schema.GroupVersionResource{
+			Group:    "",
+			Version:  "v1",
+			Resource: "secrets",
+		},
+		Kind: "Secret",
 	}
 }
 
@@ -522,7 +535,7 @@ func matchesActualLabels(expectedResources []K8sObject, actualResources []unstru
 	remaining := []K8sObject{}
 
 	for _, expectedResource := range expectedResources {
-		if expectedResource.SkipLabelValidation {
+		if expectedResource.SkipLabelValidation && expectedResource.Kind != "Secret" {
 			continue
 		}
 		resourceExists := false
@@ -531,6 +544,12 @@ func matchesActualLabels(expectedResources []K8sObject, actualResources []unstru
 				resourceExists = true
 				actualResources = append(actualResources[:idx], actualResources[idx+1:]...)
 				break
+			} else if expectedResource.Kind == "Secret" && expectedResource.SkipLabelValidation {
+				if actualResource.GetName() == expectedResource.ResourceName {
+					resourceExists = true
+					actualResources = append(actualResources[:idx], actualResources[idx+1:]...)
+					break
+				}
 			}
 		}
 		if !resourceExists {
