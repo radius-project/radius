b9b9e29c209858931bfe3e524dd64325233f7b10
Young Bu Park <youngp@microsoft.com>
2023-09-10 21:12:41 -0700

    merge

--- pkg/corerp/renderers/container/manifest.go
+++ pkg/corerp/renderers/container/manifest.go
@@ -33,6 +33,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 )
 
@@ -227,4 +228,35 @@ func patchPodSpec(sourceSpec *corev1.PodSpec, patchSpec []byte) (*corev1.PodSpec
 	}
 
 	return patched, nil
+}
+
+func mergeLabelSelector(base *metav1.LabelSelector, cur *metav1.LabelSelector) *metav1.LabelSelector {
+	if base == nil {
+		base = &metav1.LabelSelector{}
+	}
+
+	return &metav1.LabelSelector{
+		MatchLabels:      labels.Merge(base.MatchLabels, cur.MatchLabels),
+		MatchExpressions: append(base.MatchExpressions, cur.MatchExpressions...),
+	}
+}
+
+func mergeObjectMeta(base metav1.ObjectMeta, cur metav1.ObjectMeta) metav1.ObjectMeta {
+	return metav1.ObjectMeta{
+		Name:        cur.Name,
+		Namespace:   cur.Namespace,
+		Labels:      labels.Merge(base.Labels, cur.Labels),
+		Annotations: labels.Merge(base.Annotations, cur.Annotations),
+	}
+}
+
+func getObjectMeta(base metav1.ObjectMeta, appName, resourceName, resourceType string, options renderers.RenderOptions) metav1.ObjectMeta {
+	cur := metav1.ObjectMeta{
+		Name:        kubernetes.NormalizeResourceName(resourceName),
+		Namespace:   options.Environment.Namespace,
+		Labels:      renderers.GetLabels(options, appName, resourceName, resourceType),
+		Annotations: renderers.GetAnnotations(options),
+	}
+
+	return mergeObjectMeta(base, cur)
 }
--- pkg/corerp/renderers/container/manifest_test.go
+++ pkg/corerp/renderers/container/manifest_test.go
@@ -49,6 +49,160 @@ var (
 	testOptions = &renderers.RenderOptions{Environment: renderers.EnvironmentOptions{Namespace: "test-ns"}}
 )
 
+func TestMergeLabelSelector(t *testing.T) {
+	labelMergeTests := []struct {
+		name     string
+		base     *metav1.LabelSelector
+		cur      *metav1.LabelSelector
+		expected *metav1.LabelSelector
+	}{
+		{
+			name: "base is nil",
+			base: nil,
+			cur: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"key1": "value1",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "key2",
+						Operator: metav1.LabelSelectorOpIn,
+						Values:   []string{"value2"},
+					},
+				},
+			},
+			expected: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"key1": "value1",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "key2",
+						Operator: metav1.LabelSelectorOpIn,
+						Values:   []string{"value2"},
+					},
+				},
+			},
+		},
+		{
+			name: "base includes matchLabels",
+			base: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"key1": "value1",
+				},
+			},
+			cur: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"key2": "value2",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "key2",
+						Operator: metav1.LabelSelectorOpIn,
+						Values:   []string{"value2"},
+					},
+				},
+			},
+			expected: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "key2",
+						Operator: metav1.LabelSelectorOpIn,
+						Values:   []string{"value2"},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range labelMergeTests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := mergeLabelSelector(tc.base, tc.cur)
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestMergeObjectMeta(t *testing.T) {
+	mergeObjectMetaTests := []struct {
+		name     string
+		base     metav1.ObjectMeta
+		cur      metav1.ObjectMeta
+		expected metav1.ObjectMeta
+	}{
+		{
+			name: "base is empty",
+			base: metav1.ObjectMeta{},
+			cur: metav1.ObjectMeta{
+				Name:      "name",
+				Namespace: "namespace",
+				Labels: map[string]string{
+					"key1": "value1",
+				},
+				Annotations: map[string]string{
+					"key2": "value2",
+				},
+			},
+			expected: metav1.ObjectMeta{
+				Name:      "name",
+				Namespace: "namespace",
+				Labels: map[string]string{
+					"key1": "value1",
+				},
+				Annotations: map[string]string{
+					"key2": "value2",
+				},
+			},
+		},
+		{
+			name: "override name and namespace",
+			base: metav1.ObjectMeta{
+				Name:      "base",
+				Namespace: "base namespace",
+				Labels: map[string]string{
+					"key1": "value1",
+				},
+				Annotations: map[string]string{
+					"key1": "value1",
+				},
+			},
+			cur: metav1.ObjectMeta{
+				Name:      "name",
+				Namespace: "namespace",
+				Labels: map[string]string{
+					"key2": "value2",
+				},
+				Annotations: map[string]string{
+					"key2": "value2",
+				},
+			},
+			expected: metav1.ObjectMeta{
+				Name:      "name",
+				Namespace: "namespace",
+				Labels: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+				Annotations: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+			},
+		},
+	}
+
+	for _, tc := range mergeObjectMetaTests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := mergeObjectMeta(tc.base, tc.cur)
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
 func TestFetchBaseManifest(t *testing.T) {
 	manifestTests := []struct {
 		name     string
