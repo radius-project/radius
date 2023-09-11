20c992296af73175501bf063fcb8fec8035215be
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-09-11 12:41:28 -0700

    Fix JSONPath mapping

--- pkg/cli/objectformats/objectformats.go
+++ pkg/cli/objectformats/objectformats.go
@@ -78,11 +78,11 @@ func GetResourceGroupTableFormat() output.FormatterOptions {
 		Columns: []output.Column{
 			{
 				Heading:  "ID",
-				JSONPath: "{ .id }",
+				JSONPath: "{ .ID }",
 			},
 			{
 				Heading:  "Name",
-				JSONPath: "{ .name }",
+				JSONPath: "{ .Name }",
 			},
 		},
 	}
