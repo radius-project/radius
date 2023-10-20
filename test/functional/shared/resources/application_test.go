89a1e2227168bf90f2e783ad94c7170ded9f3697
nithyatsu <nithyasu@microsoft.com>
2023-10-20 13:05:54 -0700

    wip

--- test/functional/shared/resources/application_test.go
+++ test/functional/shared/resources/application_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"sort"
 	"testing"
 
 	"github.com/radius-project/radius/test/functional"
@@ -66,7 +67,67 @@ func Test_ApplicationGraph(t *testing.T) {
 	template := "testdata/corerp-resources-application-graph.bicep"
 	name := "corerp-application-simple"
 	appNamespace := "corerp-application-simple"
-	expectedGraphResp := v20231001preview.ApplicationsClientGetGraphResponse{}
+
+	frontCntrID := "/planes/radius/local/resourcegroups/default/providers/Applications.Core/containers/http-front-ctnr-simple"
+	frontCntrName := "http-front-ctnr-simple"
+	frontCntrType := "Applications.Core/containers"
+
+	backCntrID := "/planes/radius/local/resourcegroups/default/providers/Applications.Core/containers/http-back-ctnr-simple"
+	backCntrName := "http-back-ctnr-simple"
+	backCntrType := "Applications.Core/containers"
+
+	backRteID := "/planes/radius/local/resourcegroups/default/providers/Applications.Core/httpRoutes/http-back-rte-simple"
+	backRteName := "http-back-rte-simple"
+	backRteType := "Applications.Core/httpRoutes"
+
+	ProvisioningStateSuccess := "Succeeded"
+	DirectionInbound := v20231001preview.DirectionInbound
+	DirectionOutbound := v20231001preview.DirectionOutbound
+
+	expectedGraphResp := v20231001preview.ApplicationsClientGetGraphResponse{
+		ApplicationGraphResponse: v20231001preview.ApplicationGraphResponse{
+			Resources: []*v20231001preview.ApplicationGraphResource{
+				{
+					ID:                &frontCntrID,
+					Name:              &frontCntrName,
+					Type:              &frontCntrType,
+					ProvisioningState: &ProvisioningStateSuccess,
+					OutputResources:   []*v20231001preview.ApplicationGraphOutputResource{},
+					Connections: []*v20231001preview.ApplicationGraphConnection{
+						{
+							Direction: &DirectionInbound,
+							ID:        &backRteID,
+						},
+					},
+				},
+				{
+					ID:                &backCntrID,
+					Name:              &backCntrName,
+					Type:              &backCntrType,
+					ProvisioningState: &ProvisioningStateSuccess,
+					OutputResources:   []*v20231001preview.ApplicationGraphOutputResource{},
+					Connections: []*v20231001preview.ApplicationGraphConnection{
+						{
+							Direction: &DirectionOutbound,
+							ID:        &backRteID,
+						},
+					},
+				},
+				{
+					ID:                &backRteID,
+					Name:              &backRteName,
+					Type:              &backRteType,
+					ProvisioningState: &ProvisioningStateSuccess,
+					Connections: []*v20231001preview.ApplicationGraphConnection{
+						{
+							Direction: &DirectionInbound,
+							ID:        &backRteID,
+						},
+					},
+				},
+			},
+		},
+	}
 
 	test := shared.NewRPTest(t, name, []shared.TestStep{
 		{
@@ -114,9 +175,56 @@ func Test_ApplicationGraph(t *testing.T) {
 				res, err := appGraphClient.GetGraph(ctx, "corerp-application-simple", map[string]any{}, nil)
 				//require(res, )
 				require.NoError(t, err)
+
+				sort.Slice(expectedGraphResp.ApplicationGraphResponse.Resources, func(i, j int) bool {
+					return *expectedGraphResp.ApplicationGraphResponse.Resources[i].ID < *expectedGraphResp.ApplicationGraphResponse.Resources[j].ID
+				})
+
+				sort.Slice(res.ApplicationGraphResponse.Resources, func(i, j int) bool {
+					return *res.ApplicationGraphResponse.Resources[i].ID < *res.ApplicationGraphResponse.Resources[j].ID
+				})
+
+				//iterate over each of the element in res.ApplicationGraphResponse.Resources and compare with expectedGraphResp.ApplicationGraphResponse.Resources
+				for i, resResource := range res.ApplicationGraphResponse.Resources {
+					expectedResource := expectedGraphResp.ApplicationGraphResponse.Resources[i]
+
+					// Compare the ID field of the two resources
+					if *resResource.ID != *expectedResource.ID {
+						t.Errorf("Unexpected ID: got %v, want %v", *resResource.ID, *expectedResource.ID)
+					}
+
+					// Compare the Name field of the two resources
+					if resResource.Name != expectedResource.Name {
+						t.Errorf("Unexpected Name: got %v, want %v", resResource.Name, expectedResource.Name)
+					}
+
+					// Compare the Type field of the two resources
+					if resResource.Type != expectedResource.Type {
+						t.Errorf("Unexpected Type: got %v, want %v", resResource.Type, expectedResource.Type)
+					}
+
+					// Compare the Connections field of the two resources
+					if len(resResource.Connections) != len(expectedResource.Connections) {
+						t.Errorf("Unexpected number of Connections: got %v, want %v", len(resResource.Connections), len(expectedResource.Connections))
+					} else {
+						for j, resConn := range resResource.Connections {
+							expectedConn := expectedResource.Connections[j]
+
+							// Compare the ID field of the two connections
+							if *resConn.ID != *expectedConn.ID {
+								t.Errorf("Unexpected Connection ID: got %v, want %v", *resConn.ID, *expectedConn.ID)
+							}
+
+							// Compare the Name field of the two connections
+							if resConn.Direction != expectedConn.Direction {
+								t.Errorf("Unexpected Connection Name: got %v, want %v", resConn.Direction, expectedConn.Direction)
+							}
+						}
+					}
+				}
+
 				s, _ := json.MarshalIndent(res, "", "\t")
 				fmt.Print(string(s))
-				require.Equal(t, expectedGraphResp, res)
 
 			},
 		},
