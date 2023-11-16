4832c3e7ec836c96e7c93d89d5e91fd9ace56b9e
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2023-11-16 14:49:10 -0800

    Use environments instead of ok-to-test for approval. Use workflow_run to make it work via forks
    Signed-off-by: vinayada1 <28875764+vinayada1@users.noreply.github.com>

--- .github/scripts/radius-bot.js
+++ .github/scripts/radius-bot.js
@@ -44,9 +44,6 @@ async function handleIssueCommentCreate({ github, context }) {
         case '/assign':
             await cmdAssign(github, issue, isFromPulls, username);
             break;
-        case '/ok-to-test':
-            await cmdOkToTest(github, issue, isFromPulls, username);
-            break;
         default:
             console.log(`[handleIssueCommentCreate] command ${command} not found, exiting.`);
             break;
@@ -75,70 +72,4 @@ async function cmdAssign(github, issue, isFromPulls, username) {
         issue_number: issue.number,
         assignees: [username],
     });
-}
-
-/**
- * Trigger e2e test for the pull request.
- * @param {*} github GitHub object reference
- * @param {*} issue GitHub issue object
- * @param {boolean} isFromPulls is the workflow triggered by a pull request?
- * @param {string} username is the user who trigger the command
- */
-async function cmdOkToTest(github, issue, isFromPulls, username) {
-    if (!isFromPulls) {
-        console.log('[cmdOkToTest] only pull requests supported, skipping command execution.');
-        return;
-    }
-
-    // Check if the user has permission to trigger e2e test with an issue comment
-    const org = 'radius-project';
-    console.log(`Checking team membership for: ${username}`);
-    const isMember = await checkTeamMembership(github, org, process.env.TEAM_SLUG, username);
-    if (!isMember) {
-        console.log(`${username} is not a member of the ${teamSlug} team.`);
-        return;
-    }
-
-    // Get pull request
-    const pull = await github.rest.pulls.get({
-        owner: issue.owner,
-        repo: issue.repo,
-        pull_number: issue.number,
-    });
-
-    if (pull && pull.data) {
-        // Get commit id and repo from pull head
-        const testPayload = {
-            pull_head_ref: pull.data.head.sha,
-            pull_head_repo: pull.data.head.repo.full_name,
-            command: 'ok-to-test',
-            issue: issue,
-        };
-
-        console.log('Creating repository dispatch event for e2e test');
-
-        // Fire repository_dispatch event to trigger e2e test
-        await github.rest.repos.createDispatchEvent({
-            owner: issue.owner,
-            repo: issue.repo,
-            event_type: 'functional-tests',
-            client_payload: testPayload,
-        });
-
-        console.log(`[cmdOkToTest] triggered E2E test for ${JSON.stringify(testPayload)}`);
-    }
-}
-
-async function checkTeamMembership(github, org, teamSlug, username) {
-    try {
-        const response = await github.rest.teams.getMembershipForUserInOrg({
-            org: org,
-            team_slug: teamSlug,
-            username: username,
-        });
-        return response.data.state === 'active';
-    } catch (error) {
-        console.log(`error: ${error}`)
-        return false;
-    }
 }
