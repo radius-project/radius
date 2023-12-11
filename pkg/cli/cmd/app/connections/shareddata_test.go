3f9ba1f30322985eddbadad47fb69b9b9e903d9f
nithyatsu <nithyasu@microsoft.com>
2023-12-11 14:13:17 -0800

    edit existing test

--- pkg/cli/cmd/app/connections/shareddata_test.go
+++ pkg/cli/cmd/app/connections/shareddata_test.go
@@ -17,15 +17,23 @@ limitations under the License.
 package connections
 
 import (
+	corerpv20231001preview "github.com/radius-project/radius/pkg/corerp/api/v20231001preview"
 	resources_kubernetes "github.com/radius-project/radius/pkg/ucp/resources/kubernetes"
 )
 
-// This file contains shared variables and functions used in tests.
+// This file contains shared variables, constants and functions used in tests.
+const containerResourceType = "Applications.Core/containers"
+const redisResourceType = "Applications.Datastores/redisCaches"
+const provisioningStateSuccess = "Success"
 
+var directionOutbound = corerpv20231001preview.DirectionOutbound
+var directionInbound = corerpv20231001preview.DirectionInbound
 var environmentResourceID = "/planes/radius/local/resourceGroups/test-group/providers/Applications.Core/environments/test-env"
 var applicationResourceID = "/planes/radius/local/resourceGroups/test-group/providers/Applications.Core/applications/test-app"
 var containerResourceID = "/planes/radius/local/resourceGroups/test-group/providers/Applications.Core/containers/webapp"
 var redisResourceID = "/planes/radius/local/resourceGroups/test-group/providers/Applications.Datastores/redisCaches/redis"
+var containerResourceName = "webapp"
+var redisResourceName = "redis"
 
 var awsMemoryDBResourceID = "/planes/aws/aws/accounts/00000000/regions/us-west-2/providers/AWS.MemoryDB/Cluster/redis-aqbjixghynqgg"
 var azureRedisCacheResourceID = "/planes/azure/azure/subscriptions/00000000/resourceGroups/azure-group/providers/Microsoft.Cache/Redis/redis"
