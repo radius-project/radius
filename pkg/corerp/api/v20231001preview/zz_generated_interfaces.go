b658e6bfa0508d3a57719d173175f0d204125245
Young Bu Park <youngp@microsoft.com>
2024-01-03 12:37:12 -0800

    add aci compute resource
    Signed-off-by: Young Bu Park <youngp@microsoft.com>
    Signed-off-by: sk593 <shruthikumar@microsoft.com>

--- pkg/corerp/api/v20231001preview/zz_generated_interfaces.go
+++ pkg/corerp/api/v20231001preview/zz_generated_interfaces.go
@@ -7,16 +7,26 @@ package v20231001preview
 // EnvironmentComputeClassification provides polymorphic access to related types.
 // Call the interface's GetEnvironmentCompute() method to access the common type.
 // Use a type switch to determine the concrete type.  The possible types are:
-// - *EnvironmentCompute, *KubernetesCompute
+// - *AzureContainerInstanceCompute, *EnvironmentCompute, *KubernetesCompute
 type EnvironmentComputeClassification interface {
 	// GetEnvironmentCompute returns the EnvironmentCompute content of the underlying type.
 	GetEnvironmentCompute() *EnvironmentCompute
 }
 
+// EnvironmentComputeUpdateClassification provides polymorphic access to related types.
+// Call the interface's GetEnvironmentComputeUpdate() method to access the common type.
+// Use a type switch to determine the concrete type.  The possible types are:
+// - *AzureContainerInstanceComputeUpdate, *EnvironmentComputeUpdate, *KubernetesComputeUpdate
+type EnvironmentComputeUpdateClassification interface {
+	// GetEnvironmentComputeUpdate returns the EnvironmentComputeUpdate content of the underlying type.
+	GetEnvironmentComputeUpdate() *EnvironmentComputeUpdate
+}
+
 // ExtensionClassification provides polymorphic access to related types.
 // Call the interface's GetExtension() method to access the common type.
 // Use a type switch to determine the concrete type.  The possible types are:
-// - *DaprSidecarExtension, *Extension, *KubernetesMetadataExtension, *KubernetesNamespaceExtension, *ManualScalingExtension
+// - *AzureContainerInstanceExtension, *DaprSidecarExtension, *Extension, *KubernetesMetadataExtension, *KubernetesNamespaceExtension,
+// - *ManualScalingExtension
 type ExtensionClassification interface {
 	// GetExtension returns the Extension content of the underlying type.
 	GetExtension() *Extension
