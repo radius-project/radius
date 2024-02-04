164ba9fedf1ec5d6c981a63ecc60c215f44269ba
Young Bu Park <youngp@microsoft.com>
2024-02-03 23:34:02 -0800

    updated
    Signed-off-by: Young Bu Park <youngp@microsoft.com>
    Signed-off-by: sk593 <shruthikumar@microsoft.com>

--- pkg/corerp/handlers/azure_containerlb.go
+++ pkg/corerp/handlers/azure_containerlb.go
@@ -20,10 +20,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
 	"github.com/radius-project/radius/pkg/azure/armauth"
 	"github.com/radius-project/radius/pkg/to"
+	"github.com/radius-project/radius/pkg/ucp/ucplog"
 )
 
 func NewAzureContainerLoadBalancerHandler(arm *armauth.ArmConfig) ResourceHandler {
@@ -35,6 +37,7 @@ type azureContainerLoadBalancerHandler struct {
 }
 
 func (handler *azureContainerLoadBalancerHandler) Put(ctx context.Context, options *PutOptions) (map[string]string, error) {
+	logger := ucplog.FromContextOrDiscard(ctx)
 	lb, ok := options.Resource.CreateResource.Data.(*armnetwork.LoadBalancer)
 	if !ok {
 		return nil, errors.New("cannot parse load balancer")
@@ -54,68 +57,103 @@ func (handler *azureContainerLoadBalancerHandler) Put(ctx context.Context, optio
 	}
 	loadBalancersClient := networkClientFactory.NewLoadBalancersClient()
 
+	var lastErr error
+	for retry := 0; retry < 5; retry++ {
+		lbresp, err := loadBalancersClient.Get(ctx, resourceGroupName, vnetID.Name(), nil)
+		if err != nil {
+			return nil, err
+		}
+
+		updateRequired := false
+
+		found := false
+		for _, conf := range lbresp.LoadBalancer.Properties.FrontendIPConfigurations {
+			if to.String(conf.Name) == *lb.Properties.FrontendIPConfigurations[0].Name {
+				found = true
+				break
+			}
+		}
+		if !found {
+			updateRequired = true
+			lbresp.LoadBalancer.Properties.FrontendIPConfigurations = append(lbresp.LoadBalancer.Properties.FrontendIPConfigurations, lb.Properties.FrontendIPConfigurations...)
+		}
+
+		found = false
+		for _, pool := range lbresp.LoadBalancer.Properties.BackendAddressPools {
+			if to.String(pool.Name) == *lb.Properties.BackendAddressPools[0].Name {
+				found = true
+				break
+			}
+		}
+		if !found {
+			updateRequired = true
+			lbresp.LoadBalancer.Properties.BackendAddressPools = append(lbresp.LoadBalancer.Properties.BackendAddressPools, lb.Properties.BackendAddressPools...)
+		}
+
+		found = false
+		for _, r := range lbresp.LoadBalancer.Properties.LoadBalancingRules {
+			if to.String(r.Name) == *lb.Properties.LoadBalancingRules[0].Name {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			updateRequired = true
+			lbresp.LoadBalancer.Properties.LoadBalancingRules = append(lbresp.LoadBalancer.Properties.LoadBalancingRules, lb.Properties.LoadBalancingRules...)
+		}
+
+		found = false
+		for _, pr := range lbresp.LoadBalancer.Properties.Probes {
+			if to.String(pr.Name) == *lb.Properties.Probes[0].Name {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			updateRequired = true
+			lbresp.LoadBalancer.Properties.Probes = append(lbresp.LoadBalancer.Properties.Probes, lb.Properties.Probes...)
+		}
+
+		if updateRequired {
+			poller, err := loadBalancersClient.BeginCreateOrUpdate(ctx, resourceGroupName, to.String(lb.Name), lbresp.LoadBalancer, nil)
+			if err != nil {
+				logger.Info("failed to update load balancer", "retry", retry, "error", err.Error())
+				fmt.Printf("failed to update load balancer: %s, retry: %d", err.Error(), retry)
+				lastErr = err
+				time.Sleep(60 * time.Second)
+				continue
+			}
+
+			_, err = poller.PollUntilDone(ctx, nil)
+			if err != nil {
+				logger.Info("failed to poll load balancer update", "retry", retry, "error", err.Error())
+				fmt.Printf("failed to poll load balancer: %s, retry: %d", err.Error(), retry)
+				lastErr = err
+				// Retry if the request was cancelled by the following errors:
+				// {\n  \"status\": \"Canceled\",\n  \"error\": {\n    \"code\": \"Canceled\",\n    \"message\": \"Operation was canceled.\",\n    \"details\": [\n      {\n        \"code\": \"CanceledAndSupersededDueToAnotherOperation\",\n        \"message\": \"Operation PutLoadBalancerOperation (52037801-74e8-4660-b359-39055aa02d7c) was canceled and superseded by operation PutLoadBalancerOperation (bb71e36a-3f38-4762-b28e-1cc2fbb44ca9).\"\n      }\n    ]\n  }\n}\n
+				// This happens when multiple container deployment updates LB. Needs to have the better way.
+				time.Sleep(60 * time.Second)
+				continue
+			}
+		}
+		lastErr = nil
+		break
+	}
+
+	if lastErr != nil {
+		return nil, lastErr
+	}
+
 	lbresp, err := loadBalancersClient.Get(ctx, resourceGroupName, vnetID.Name(), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	found := false
-	for _, conf := range lbresp.LoadBalancer.Properties.FrontendIPConfigurations {
-		if to.String(conf.Name) == *lb.Properties.FrontendIPConfigurations[0].Name {
-			found = true
-			break
-		}
-	}
-	if !found {
-		lbresp.LoadBalancer.Properties.FrontendIPConfigurations = append(lbresp.LoadBalancer.Properties.FrontendIPConfigurations, lb.Properties.FrontendIPConfigurations...)
-	}
-
-	found = false
-	for _, pool := range lbresp.LoadBalancer.Properties.BackendAddressPools {
-		if to.String(pool.Name) == *lb.Properties.BackendAddressPools[0].Name {
-			found = true
-			break
-		}
-	}
-	if !found {
-		lbresp.LoadBalancer.Properties.BackendAddressPools = append(lbresp.LoadBalancer.Properties.BackendAddressPools, lb.Properties.BackendAddressPools...)
-	}
-
-	found = false
-	for _, r := range lbresp.LoadBalancer.Properties.LoadBalancingRules {
-		if to.String(r.Name) == *lb.Properties.LoadBalancingRules[0].Name {
-			found = true
-			break
-		}
-	}
-	if !found {
-		lbresp.LoadBalancer.Properties.LoadBalancingRules = append(lbresp.LoadBalancer.Properties.LoadBalancingRules, lb.Properties.LoadBalancingRules...)
-	}
-
-	found = false
-	for _, pr := range lbresp.LoadBalancer.Properties.Probes {
-		if to.String(pr.Name) == *lb.Properties.Probes[0].Name {
-			found = true
-			break
-		}
-	}
-	if !found {
-		lbresp.LoadBalancer.Properties.Probes = append(lbresp.LoadBalancer.Properties.Probes, lb.Properties.Probes...)
-	}
-
-	poller, err := loadBalancersClient.BeginCreateOrUpdate(ctx, resourceGroupName, to.String(lb.Name), lbresp.LoadBalancer, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := poller.PollUntilDone(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	hostname := ""
 	if len(lb.Properties.FrontendIPConfigurations) > 0 {
-		for _, conf := range resp.Properties.FrontendIPConfigurations {
+		for _, conf := range lbresp.Properties.FrontendIPConfigurations {
 			if to.String(conf.Name) == to.String(lb.Properties.FrontendIPConfigurations[0].Name) {
 				hostname = to.String(conf.Properties.PrivateIPAddress)
 			}
@@ -142,55 +180,77 @@ func (handler *azureContainerLoadBalancerHandler) Delete(ctx context.Context, op
 
 	loadBalancersClient := networkClientFactory.NewLoadBalancersClient()
 
-	lbresp, err := loadBalancersClient.Get(ctx, resourceGroupName, vnetID.Name(), nil)
-	if err != nil {
-		return err
-	}
-
-	appName := options.Resource.ID.Name()
-
-	deletedFrontend := []*armnetwork.FrontendIPConfiguration{}
-	for _, conf := range lbresp.LoadBalancer.Properties.FrontendIPConfigurations {
-		if to.String(conf.Name) == appName {
-			continue
+	var lastErr error
+	for retry := 0; retry < 5; retry++ {
+		lbresp, err := loadBalancersClient.Get(ctx, resourceGroupName, vnetID.Name(), nil)
+		if err != nil {
+			return err
 		}
 
-		deletedFrontend = append(deletedFrontend, conf)
-	}
-	lbresp.LoadBalancer.Properties.FrontendIPConfigurations = deletedFrontend
+		appName := options.Resource.ID.Name()
+		updateRequired := false
 
-	backendaddr := []*armnetwork.BackendAddressPool{}
-	for _, pool := range lbresp.LoadBalancer.Properties.BackendAddressPools {
-		if to.String(pool.Name) == appName {
-			continue
+		deletedFrontend := []*armnetwork.FrontendIPConfiguration{}
+		for _, conf := range lbresp.LoadBalancer.Properties.FrontendIPConfigurations {
+			if to.String(conf.Name) == appName {
+				updateRequired = true
+				continue
+			}
+
+			deletedFrontend = append(deletedFrontend, conf)
 		}
-		backendaddr = append(backendaddr, pool)
-	}
-	lbresp.LoadBalancer.Properties.BackendAddressPools = backendaddr
+		lbresp.LoadBalancer.Properties.FrontendIPConfigurations = deletedFrontend
 
-	lbrs := []*armnetwork.LoadBalancingRule{}
-	for _, lbrule := range lbresp.LoadBalancer.Properties.LoadBalancingRules {
-		if to.String(lbrule.Name) == appName {
-			continue
+		backendaddr := []*armnetwork.BackendAddressPool{}
+		for _, pool := range lbresp.LoadBalancer.Properties.BackendAddressPools {
+			if to.String(pool.Name) == appName {
+				updateRequired = true
+				continue
+			}
+			backendaddr = append(backendaddr, pool)
 		}
-		lbrs = append(lbrs, lbrule)
-	}
-	lbresp.LoadBalancer.Properties.LoadBalancingRules = lbrs
+		lbresp.LoadBalancer.Properties.BackendAddressPools = backendaddr
 
-	probe := []*armnetwork.Probe{}
-	for _, p := range lbresp.LoadBalancer.Properties.Probes {
-		if to.String(p.Name) == appName {
-			continue
+		lbrs := []*armnetwork.LoadBalancingRule{}
+		for _, lbrule := range lbresp.LoadBalancer.Properties.LoadBalancingRules {
+			if to.String(lbrule.Name) == appName {
+				updateRequired = true
+				continue
+			}
+			lbrs = append(lbrs, lbrule)
 		}
-		probe = append(probe, p)
-	}
-	lbresp.LoadBalancer.Properties.Probes = probe
+		lbresp.LoadBalancer.Properties.LoadBalancingRules = lbrs
 
-	poller, err := loadBalancersClient.BeginCreateOrUpdate(ctx, resourceGroupName, to.String(lbresp.Name), lbresp.LoadBalancer, nil)
-	if err != nil {
-		return err
+		probe := []*armnetwork.Probe{}
+		for _, p := range lbresp.LoadBalancer.Properties.Probes {
+			if to.String(p.Name) == appName {
+				updateRequired = true
+				continue
+			}
+			probe = append(probe, p)
+		}
+		lbresp.LoadBalancer.Properties.Probes = probe
+
+		if updateRequired {
+			poller, err := loadBalancersClient.BeginCreateOrUpdate(ctx, resourceGroupName, to.String(lbresp.Name), lbresp.LoadBalancer, nil)
+			if err != nil {
+				fmt.Printf("failed to update load balancer: %s, retry: %d", err, retry)
+				lastErr = err
+				time.Sleep(60 * time.Second)
+				continue
+			}
+
+			_, err = poller.PollUntilDone(ctx, nil)
+			if err != nil {
+				fmt.Printf("failed to poll load balancer: %s, retry: %d", err, retry)
+				lastErr = err
+				time.Sleep(60 * time.Second)
+				continue
+			}
+		}
+		lastErr = nil
+		break
 	}
 
-	_, err = poller.PollUntilDone(ctx, nil)
-	return err
+	return lastErr
 }
