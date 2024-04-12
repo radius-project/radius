ac367eeb2d5053b95f10948f639e1173940f58cc
sk593 <shruthikumar@microsoft.com>
2024-04-12 15:31:43 -0700

    update generator code to use bicep-types
    Signed-off-by: sk593 <shruthikumar@microsoft.com>

--- hack/bicep-types-radius/src/autorest.bicep/src/main.ts
+++ hack/bicep-types-radius/src/autorest.bicep/src/main.ts
@@ -16,13 +16,9 @@
 
 import { AutoRestExtension, AutorestExtensionHost, startSession } from "@autorest/extension-base";
 import { generateTypes } from "./type-generator";
-import { orderBy } from 'lodash';
-import { ResourceType } from "./types"
 import { CodeModel, codeModelSchema } from "@autorest/codemodel";
-import { writeJson } from './writers/json';
-import { writeMarkdown } from "./writers/markdown";
-import { writeTableMarkdown } from "./writers/markdown-table";
 import { getProviderDefinitions } from "./resources";
+import { writeTypesJson, writeMarkdown } from "bicep-types";
 
 export async function processRequest(host: AutorestExtensionHost) {
   try {
@@ -40,18 +36,10 @@ export async function processRequest(host: AutorestExtensionHost) {
       const outFolder = `${namespace}/${apiVersion}`.toLowerCase();
 
       // write types.json
-      host.writeFile({ filename: `${outFolder}/types.json`, content: writeJson(types) });
+      host.writeFile({ filename: `${outFolder}/types.json`, content: writeTypesJson(types) });
 
       // writer types.md
-      host.writeFile({ filename: `${outFolder}/types.md`, content: writeMarkdown(namespace, apiVersion, types) });
-
-      // writer resource types
-      const resourceTypes = orderBy(types.filter(t => t instanceof ResourceType) as ResourceType[], x => x.Name.split('@')[0].toLowerCase());
-      for (const resourceType of resourceTypes) {
-        const filename = resourceType.Name.split('/')[1].split('@')[0].toLowerCase();
-        host.writeFile({ filename: `${outFolder}/docs/${filename}.md`, content: writeTableMarkdown(namespace, apiVersion, [resourceType], types) });
-      }
-
+      host.writeFile({ filename: `${outFolder}/types.md`, content: writeMarkdown(types, `${namespace} @ ${apiVersion}`) });
     }
 
     session.info(`autorest.bicep took ${Date.now() - start}ms`);
