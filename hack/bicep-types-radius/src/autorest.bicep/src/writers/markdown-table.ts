43403fd165806e02cd941693a88356beaca186e5
sk593 <shruthikumar@microsoft.com>
2024-06-14 14:45:23 -0700

    Update bicep generator to use bicep-types and new contract (#7506)
    # Description
    
    Update bicep generator to use bicep-types and new contract
    
    ## Type of change
    
    <!--
    
    Please select **one** of the following options that describes your
    change and delete the others. Clearly identifying the type of change you
    are making will help us review your PR faster, and is used in authoring
    release notes.
    
    If you are making a bug fix or functionality change to Radius and do not
    have an associated issue link please create one now.
    
    -->
    
    - This pull request fixes a bug in Radius and has an approved issue
    (issue link required).
    - This pull request adds or changes features of Radius and has an
    approved issue (issue link required).
    - This pull request is a minor refactor, code cleanup, test improvement,
    or other maintenance task and doesn't change the functionality of Radius
    (issue link optional).
    
    <!--
    
    Please update the following to link the associated issue. This is
    required for some kinds of changes (see above).
    
    -->
    
    Fixes: #issue_number
    
    ---------
    
    Signed-off-by: sk593 <shruthikumar@microsoft.com>

--- hack/bicep-types-radius/src/autorest.bicep/src/writers/markdown-table.ts
+++ hack/bicep-types-radius/src/autorest.bicep/src/writers/markdown-table.ts
@@ -14,47 +14,68 @@
 // limitations under the License.
 // ------------------------------------------------------------.
 import { Dictionary, filter, keys, orderBy } from 'lodash';
-import { ArrayType, BuiltInType, DiscriminatedObjectType, getBuiltInTypeKindLabel, getObjectPropertyFlagsLabels, getScopeTypeLabels, ObjectProperty, ObjectType, ResourceFunctionType, ResourceType, StringLiteralType, TypeBase, TypeBaseKind, TypeReference, UnionType } from '../types';
+import { ArrayType, BuiltInType, DiscriminatedObjectType, getBuiltInTypeKindLabel, getObjectTypePropertyFlagsLabels, ObjectTypeProperty, ObjectType, ResourceFunctionType, ResourceType, StringLiteralType, BicepType, TypeBaseKind, TypeReference, UnionType, IntegerType, StringType } from 'bicep-types';
 
-export function writeTableMarkdown(provider: string, apiVersion: string, resourceTypes: ResourceType[], types: TypeBase[]) {
+export function writeTableMarkdown(provider: string, apiVersion: string, resourceTypes: ResourceType[], types: BicepType[]) {
   let output = '';
 
-  function getTypeName(types: TypeBase[], typeReference: TypeReference): string {
-    const type = types[typeReference.Index];
-    switch (type.Type) {
+  function getTypeName(types: BicepType[], typeReference: TypeReference): string {
+    const type = types[typeReference.index];
+    switch (type.type) {
       case TypeBaseKind.BuiltInType:
-        return getBuiltInTypeKindLabel((type as BuiltInType).Kind).toLowerCase();
+        return getBuiltInTypeKindLabel((type as BuiltInType).kind).toLowerCase();
       case TypeBaseKind.ObjectType:
-        return generateAnchorLink((type as ObjectType).Name);
+        return generateAnchorLink((type as ObjectType).name);
       case TypeBaseKind.ArrayType:
-        return `${getTypeName(types, (type as ArrayType).ItemType)}[]`;
+        return getArrayTypeName(types, (type as ArrayType));
       case TypeBaseKind.ResourceType:
-        return (type as ResourceType).Name;
+        return (type as ResourceType).name;
       case TypeBaseKind.ResourceFunctionType: {
         const functionType = type as ResourceFunctionType;
-        return `${functionType.Name} (${functionType.ResourceType}@${functionType.ApiVersion})`;
+        return `${functionType.name} (${functionType.resourceType}@${functionType.apiVersion})`;
       }
       case TypeBaseKind.UnionType: {
-        const elements = (type as UnionType).Elements.map(x => getTypeName(types, x));
+        const elements = (type as UnionType).elements.map(x => getTypeName(types, x));
         return elements.sort().join(' | ');
       }
       case TypeBaseKind.StringLiteralType:
-        return `'${(type as StringLiteralType).Value}'`;
+        return `'${(type as StringLiteralType).value}'`;
       case TypeBaseKind.DiscriminatedObjectType:
-        return generateAnchorLink((type as DiscriminatedObjectType).Name);
+        return generateAnchorLink((type as DiscriminatedObjectType).name);
+      case TypeBaseKind.AnyType:
+        return 'any';
+      case TypeBaseKind.NullType:
+        return 'null';
+      case TypeBaseKind.BooleanType:
+        return 'bool';
+      case TypeBaseKind.IntegerType:
+        return `int${getIntegerModifiers(type as IntegerType)}`;
+      case TypeBaseKind.StringType:
+        return `string${getStringModifiers(type as StringType)}`;
       default:
         throw `Unrecognized type`;
     }
+  }
+
+  function getArrayTypeName(types: BicepType[], type: ArrayType): string
+  {
+    let itemTypeName = getTypeName(types, type.itemType);
+    if (itemTypeName.indexOf(' ') != -1)
+    {
+      itemTypeName = `(${itemTypeName})`;
+    }
+
+    return `${itemTypeName}[]${formatModifiers(type.minLength !== undefined ? `minLength: ${type.minLength}` : undefined, type.maxLength !== undefined ? `maxLength: ${type.maxLength}` : undefined)}`;
   }
 
   function generateAnchorLink(name: string) {
     return `[${name}](#${name.replace(/[^a-zA-Z0-9-]/g, '').toLowerCase()})`;
   }
 
-  function writeTypeProperty(types: TypeBase[], name: string, property: ObjectProperty) {
-    const flagsString = property.Flags ? `${getObjectPropertyFlagsLabels(property.Flags).join(', ')}` : '';
-    const descriptionString = property.Description ? property.Description : '';
-    writeTableEntry(name, getTypeName(types, property.Type), flagsString, descriptionString);
+  function writeTypeProperty(types: BicepType[], name: string, property: ObjectTypeProperty) {
+    const flagsString = property.flags ? `${getObjectTypePropertyFlagsLabels(property.flags).join(', ')}` : '';
+    const descriptionString = property.description ? property.description : '';
+    writeTableEntry(name, getTypeName(types, property.type), flagsString, descriptionString);
   }
 
   function writeTableHeading(){
@@ -84,35 +105,35 @@ export function writeTableMarkdown(provider: string, apiVersion: string, resourc
     output += '\n';
   }
 
-  function findTypesToWrite(types: TypeBase[], typesToWrite: TypeBase[], typeReference: TypeReference) {
+  function findTypesToWrite(types: BicepType[], typesToWrite: BicepType[], typeReference: TypeReference) {
     function processTypeLinks(typeReference: TypeReference, skipParent: boolean) {
       // this is needed to avoid circular type references causing stack overflows
-      if (typesToWrite.indexOf(types[typeReference.Index]) === -1) {
+      if (typesToWrite.indexOf(types[typeReference.index]) === -1) {
         if (!skipParent) {
-          typesToWrite.push(types[typeReference.Index]);
+          typesToWrite.push(types[typeReference.index]);
         }
 
         findTypesToWrite(types, typesToWrite, typeReference);
       }
     }
 
-    const type = types[typeReference.Index];
-    switch (type.Type) {
+    const type = types[typeReference.index];
+    switch (type.type) {
       case TypeBaseKind.ArrayType: {
         const arrayType = type as ArrayType;
-        processTypeLinks(arrayType.ItemType, false);
+        processTypeLinks(arrayType.itemType, false);
 
         return;
       }
       case TypeBaseKind.ObjectType: {
         const objectType = type as ObjectType;
 
-        for (const key of sortedKeys(objectType.Properties)) {
-          processTypeLinks(objectType.Properties[key].Type, false);
+        for (const key of sortedKeys(objectType.properties)) {
+          processTypeLinks(objectType.properties[key].type, false);
         }
 
-        if (objectType.AdditionalProperties) {
-          processTypeLinks(objectType.AdditionalProperties, false);
+        if (objectType.additionalProperties) {
+          processTypeLinks(objectType.additionalProperties, false);
         }
 
         return;
@@ -120,12 +141,12 @@ export function writeTableMarkdown(provider: string, apiVersion: string, resourc
       case TypeBaseKind.DiscriminatedObjectType: {
         const discriminatedObjectType = type as DiscriminatedObjectType;
 
-        for (const key of sortedKeys(discriminatedObjectType.BaseProperties)) {
-          processTypeLinks(discriminatedObjectType.BaseProperties[key].Type, false);
+        for (const key of sortedKeys(discriminatedObjectType.baseProperties)) {
+          processTypeLinks(discriminatedObjectType.baseProperties[key].type, false);
         }
 
-        for (const key of sortedKeys(discriminatedObjectType.Elements)) {
-          const element = discriminatedObjectType.Elements[key];
+        for (const key of sortedKeys(discriminatedObjectType.elements)) {
+          const element = discriminatedObjectType.elements[key];
           // Don't display discriminated object elements as individual types
           processTypeLinks(element, true);
         }
@@ -139,27 +160,27 @@ export function writeTableMarkdown(provider: string, apiVersion: string, resourc
     return orderBy(keys(dictionary), k => k.toLowerCase(), 'asc');
   }
 
-  function writeComplexType(types: TypeBase[], type: TypeBase, nesting: number, includeHeader: boolean) {
-    switch (type.Type) {
+  function writeComplexType(types: BicepType[], type: BicepType, nesting: number, includeHeader: boolean) {
+    switch (type.type) {
       case TypeBaseKind.ResourceType: {
         const resourceType = type as ResourceType;
         writeHeading(nesting, `Top-Level Resource`);
         // temporarily removing scope as it's not applicable
         // writeBullet("Valid Scope(s)", `${getScopeTypeLabels(resourceType.ScopeType).join(', ') || 'Unknown'}`);
-        writeComplexType(types, types[resourceType.Body.Index], nesting, false);
+        writeComplexType(types, types[resourceType.body.index], nesting, false);
 
         return;
       }
       case TypeBaseKind.ResourceFunctionType: {
         const resourceFunctionType = type as ResourceFunctionType;
-        writeHeading(nesting, `Function ${resourceFunctionType.Name} (${resourceFunctionType.ResourceType}@${resourceFunctionType.ApiVersion})`);
+        writeHeading(nesting, `Function ${resourceFunctionType.name} (${resourceFunctionType.resourceType}@${resourceFunctionType.apiVersion})`);
         writeNewLine();
-        writeBullet("Resource", resourceFunctionType.ResourceType);
-        writeBullet("ApiVersion", resourceFunctionType.ApiVersion);
-        if (resourceFunctionType.Input) {
-          writeBullet("Input", getTypeName(types, resourceFunctionType.Input));
+        writeBullet("Resource", resourceFunctionType.resourceType);
+        writeBullet("ApiVersion", resourceFunctionType.apiVersion);
+        if (resourceFunctionType.input) {
+          writeBullet("Input", getTypeName(types, resourceFunctionType.input));
         }
-        writeBullet("Output", getTypeName(types, resourceFunctionType.Output));
+        writeBullet("Output", getTypeName(types, resourceFunctionType.output));
 
         writeNewLine();
         return;
@@ -167,28 +188,28 @@ export function writeTableMarkdown(provider: string, apiVersion: string, resourc
       case TypeBaseKind.ObjectType: {
         const objectType = type as ObjectType;
         if (includeHeader) {
-          writeHeading(nesting, objectType.Name);
+          writeHeading(nesting, objectType.name);
         }
 
         writeNewLine();
         writeHeading(nesting + 1, "Properties");
         writeNewLine();
 
-        if (Object.keys(objectType.Properties).length === 0) {
+        if (Object.keys(objectType.properties).length === 0) {
           writeBullet("none", "");
           writeNewLine();
         }
         else {
           writeTableHeading();
-          for (const key of sortedKeys(objectType.Properties)) {
-            writeTypeProperty(types, key, objectType.Properties[key]);
+          for (const key of sortedKeys(objectType.properties)) {
+            writeTypeProperty(types, key, objectType.properties[key]);
           }
         }
 
-        if (objectType.AdditionalProperties) {
+        if (objectType.additionalProperties) {
           writeHeading(nesting + 1, "Additional Properties");
           writeNewLine();
-          writeBullet("Additional Properties Type", getTypeName(types, objectType.AdditionalProperties));
+          writeBullet("Additional Properties Type", getTypeName(types, objectType.additionalProperties));
         }
 
         writeNewLine();
@@ -197,31 +218,31 @@ export function writeTableMarkdown(provider: string, apiVersion: string, resourc
       case TypeBaseKind.DiscriminatedObjectType: {
         const discriminatedObjectType = type as DiscriminatedObjectType;
         if (includeHeader) {
-          writeHeading(nesting, discriminatedObjectType.Name);
+          writeHeading(nesting, discriminatedObjectType.name);
           writeNewLine();
         }
 
-        writeBullet("Discriminator", discriminatedObjectType.Discriminator);
+        writeBullet("Discriminator", discriminatedObjectType.discriminator);
         writeNewLine();
 
         writeHeading(nesting + 1, "Base Properties");
         writeNewLine();
-        if (Object.keys(discriminatedObjectType.BaseProperties).length === 0) {
+        if (Object.keys(discriminatedObjectType.baseProperties).length === 0) {
           writeBullet("none", "");
           writeNewLine();
         }
         else {
           writeTableHeading();
-          for (const propertyName of sortedKeys(discriminatedObjectType.BaseProperties)) {
-            writeTypeProperty(types, propertyName, discriminatedObjectType.BaseProperties[propertyName]);
+          for (const propertyName of sortedKeys(discriminatedObjectType.baseProperties)) {
+            writeTypeProperty(types, propertyName, discriminatedObjectType.baseProperties[propertyName]);
           }
         }
 
         writeNewLine();
         
-        for (const key of sortedKeys(discriminatedObjectType.Elements)) {
-          const element = discriminatedObjectType.Elements[key];
-          writeComplexType(types, types[element.Index], nesting + 1, true);
+        for (const key of sortedKeys(discriminatedObjectType.elements)) {
+          const element = discriminatedObjectType.elements[key];
+          writeComplexType(types, types[element.index], nesting + 1, true);
         }
 
         writeNewLine();
@@ -230,23 +251,23 @@ export function writeTableMarkdown(provider: string, apiVersion: string, resourc
     }
   }
 
-  function generateMarkdown(provider: string, apiVersion: string, types: TypeBase[]) {
+  function generateMarkdown(provider: string, apiVersion: string, types: BicepType[]) {
 
-    const resourceFunctionTypes = orderBy(types.filter(t => t instanceof ResourceFunctionType) as ResourceFunctionType[], x => x.Name.split('@')[0].toLowerCase());    
-    const filteredFunctionTypes = resourceFunctionTypes.filter(x => resourceTypes.some(y => x.ResourceType.toLowerCase() === y.Name.split('@')[0].toLowerCase()));
-    const typesToWrite: TypeBase[] = [...resourceTypes, ...filteredFunctionTypes];
+    const resourceFunctionTypes = orderBy(types.filter(t => t.type == TypeBaseKind.ResourceFunctionType) as ResourceFunctionType[], x => x.name.split('@')[0].toLowerCase());    
+    const filteredFunctionTypes = resourceFunctionTypes.filter(x => resourceTypes.some(y => x.resourceType.toLowerCase() === y.name.split('@')[0].toLowerCase()));
+    const typesToWrite: BicepType[] = [...resourceTypes, ...filteredFunctionTypes];
 
     for (const resourceType of resourceTypes) {
-      findTypesToWrite(types, typesToWrite, resourceType.Body);
+      findTypesToWrite(types, typesToWrite, resourceType.body);
     }
 
     for (const resourceFunctionType of filteredFunctionTypes) {
-      if (resourceFunctionType.Input) {
-        typesToWrite.push(types[resourceFunctionType.Input.Index]);
-        findTypesToWrite(types, typesToWrite, resourceFunctionType.Input);
+      if (resourceFunctionType.input) {
+        typesToWrite.push(types[resourceFunctionType.input.index]);
+        findTypesToWrite(types, typesToWrite, resourceFunctionType.input);
       }
-      typesToWrite.push(types[resourceFunctionType.Output.Index]);
-      findTypesToWrite(types, typesToWrite, resourceFunctionType.Output);
+      typesToWrite.push(types[resourceFunctionType.output.index]);
+      findTypesToWrite(types, typesToWrite, resourceFunctionType.output);
     }
 
     for (const type of typesToWrite) {
@@ -257,4 +278,24 @@ export function writeTableMarkdown(provider: string, apiVersion: string, resourc
   }
 
   return generateMarkdown(provider, apiVersion, types);
+}
+
+function getIntegerModifiers(type: IntegerType): string
+{
+  return formatModifiers(type.minValue !== undefined ? `minValue: ${type.minValue}` : undefined,
+    type.maxValue !== undefined ? `maxValue: ${type.maxValue}` : undefined);
+}
+
+function getStringModifiers(type: StringType): string
+{
+  return formatModifiers(type.sensitive ? 'sensitive' : undefined,
+    type.minLength !== undefined ? `minLength: ${type.minLength}` : undefined,
+    type.maxLength !== undefined ? `maxLength: ${type.maxLength}` : undefined,
+    type.pattern !== undefined ? `pattern: "${type.pattern.replace('"', '\\"')}"` : undefined);
+}
+
+function formatModifiers(...modifiers: Array<string | undefined>): string
+{
+  const modifierString = modifiers.filter(modifier => !!modifier).join(', ');
+  return modifierString.length > 0 ? ` {${modifierString}}` : modifierString;
 }
