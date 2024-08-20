8310bdbcd2ad433c88f6e41933c28118f01c9213
karishma-chawla <74574173+karishma-chawla@users.noreply.github.com>
2024-08-20 15:52:17 -0700

    Remove unused test artifacts
    Signed-off-by: karishma-chawla <74574173+karishma-chawla@users.noreply.github.com>

--- test/executeFunctionalTest.sh
+++ test/executeFunctionalTest.sh
@@ -31,13 +31,9 @@ adminUser='coolUser'
 adminPassword=$(uuidgen)
 resp=$(az deployment group create --resource-group $resourcegroup --template-file createAzureTestResources.bicep --parameters 'adminUsername=$adminUser' --parameters 'adminPassword=$adminPassword')
 cat resp
-export AZURE_SERVICEBUS_RESOURCE_ID=$(jq -r '.properties.outputs.namespaceId.value' <<<"${resp}")
 export AZURE_MSSQL_RESOURCE_ID=$(jq -r '.properties.outputs.sqlServerId.value' <<<"${resp}")
 export AZURE_MSSQL_USERNAME=$adminUser
 export AZURE_MSSQL_PASSWORD=$adminPassword
-export AZURE_MONGODB_RESOURCE_ID=$(jq -r '.properties.outputs.mongoDatabaseId.value' <<<"${resp}")
-export AZURE_REDIS_RESOURCE_ID=$(jq -r '.properties.outputs.redisCacheId.value' <<<"${resp}")
-export AZURE_TABLESTORAGE_RESOURCE_ID=$(jq -r '.properties.outputs.tableStorageAccId.value' <<<"${resp}")
 export AZURE_COSMOS_MONGODB_ACCOUNT_ID=$(jq -r '.properties.outputs.cosmosMongoAccountID.value' <<<"${resp}")
 make test-functional-corerp
 make test-functional-msgrp
