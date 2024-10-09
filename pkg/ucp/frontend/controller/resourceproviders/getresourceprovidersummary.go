9a9a4d2fe76809667f64e8c4154b53e152f68006
Ryan Nowak <nowakra@gmail.com>
2024-10-09 10:12:07 -0700

    Implementation of resource providers and resource types registration (#7967)
    # Description
    
    This change implements the API functionality for registering resource
    providers and related child-types through the Radius API. This is the
    first step towards making the Radius API surface extensible. For this
    first step there is no consumer of this data or these APIs. Subsequent
    changes will consume the data and implement the user-facing
    functionality.
    
    What's possible in this commit:
    
    - CRUDL operations on resource provider and related types like:
      - resource type
      - api version (child of resource type)
      - location
    
    Additionally, this change implements the resource provider "summary"
    API. The summary API provides a combined view of the most useful data
    from each resource provider and its children.
    
    ---
    
    For reviewers there are a few important notes:
    
    - Routing in UCP is complex, and error prone to work on. I abandoned the
    existing code and rewrote it in a form that's more native to go-chi.
    This was a significant improvement and made it much easier to debug the
    code. UCP has good integration tests so I'm not very concerned about
    regressions.
    - This is our first use-case for child resources in Radius. I used the
    async controllers to implement cascading deletion.
    - The "summary" API uses cached data. The async controllers for resource
    provider and other types update the cache. This is our first use of this
    pattern, but it felt right to me.
    
    ## Type of change
    
    
    - This pull request adds or changes features of Radius and has an
    approved issue (issue link required).
    
    
    Part of: #6688
    
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>

---
+++ pkg/ucp/frontend/controller/resourceproviders/getresourceprovidersummary.go
@@ -0,0 +1,130 @@
+/*
+Copyright 2023 The Radius Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package resourceproviders
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	http "net/http"
+	"strings"
+
+	v1 "github.com/radius-project/radius/pkg/armrpc/api/v1"
+	armrpc_controller "github.com/radius-project/radius/pkg/armrpc/frontend/controller"
+	armrpc_rest "github.com/radius-project/radius/pkg/armrpc/rest"
+	"github.com/radius-project/radius/pkg/middleware"
+	"github.com/radius-project/radius/pkg/ucp/datamodel"
+	"github.com/radius-project/radius/pkg/ucp/datamodel/converter"
+	"github.com/radius-project/radius/pkg/ucp/resources"
+	"github.com/radius-project/radius/pkg/ucp/store"
+)
+
+var _ armrpc_controller.Controller = (*GetResourceProviderSummary)(nil)
+
+// GetResourceProviderSummary is the controller implementation to get the list of resources stored in a resource group.
+type GetResourceProviderSummary struct {
+	armrpc_controller.Operation[*datamodel.ResourceProviderSummary, datamodel.ResourceProviderSummary]
+}
+
+// NewGetResourceProviderSummary creates a new controller for listing resources stored in a resource group.
+func NewGetResourceProviderSummary(opts armrpc_controller.Options) (armrpc_controller.Controller, error) {
+	return &GetResourceProviderSummary{
+		Operation: armrpc_controller.NewOperation(opts,
+			armrpc_controller.ResourceOptions[datamodel.ResourceProviderSummary]{
+				RequestConverter:  converter.ResourceProviderSummaryDataModelFromVersioned,
+				ResponseConverter: converter.ResourceProviderSummaryDataModelToVersioned,
+			},
+		),
+	}, nil
+}
+
+// Run implements controller.Controller.
+func (r *GetResourceProviderSummary) Run(ctx context.Context, w http.ResponseWriter, req *http.Request) (armrpc_rest.Response, error) {
+	relativePath := middleware.GetRelativePath(r.Options().PathBase, req.URL.Path)
+
+	scope, name, err := r.extractScopeAndName(relativePath)
+	if err != nil {
+		return nil, err
+	}
+
+	// First check if the plane exists.
+	_, err = r.StorageClient().Get(ctx, scope.String())
+	if errors.Is(err, &store.ErrNotFound{}) {
+		return armrpc_rest.NewNotFoundResponse(scope), nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	// Next, construct a resource id for the summary.
+	id, err := datamodel.ResourceProviderSummaryIDFromParts(scope.String(), name)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := r.StorageClient().Get(ctx, id.String())
+	if errors.Is(err, &store.ErrNotFound{}) {
+		// If we fail to find the summary, then use the relative path as the target.
+		message := fmt.Sprintf("the resource provider with name '%s' was not found", name)
+		return armrpc_rest.NewNotFoundMessageResponse(message), nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	response, err := r.createResponse(ctx, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return armrpc_rest.NewOKResponse(response), nil
+}
+
+func (r *GetResourceProviderSummary) extractScopeAndName(relativePath string) (resources.ID, string, error) {
+	// Trim a trailing slash if it exists.
+	relativePath = strings.TrimSuffix(relativePath, "/")
+
+	// NOTE: the URL path should be something like: /planes/radius/local/providers/Applications.Test.
+	//
+	// This is NOT a valid resource id, so we can't use the parser for it.
+	//
+	// Instead we trim this to just /planes/radius/local and keep the Applications.Test part separate.
+	lastSeparator := strings.LastIndex(relativePath, resources.SegmentSeparator)
+	if lastSeparator == -1 {
+		// This probably can't happen, but let's not panic.
+		return resources.ID{}, "", errors.New("invalid URL path")
+	}
+
+	name := relativePath[lastSeparator+1:]
+	scope, err := resources.ParseScope(
+		strings.TrimSuffix(
+			strings.TrimSuffix(relativePath[0:lastSeparator], resources.SegmentSeparator), resources.SegmentSeparator+resources.ProvidersSegment))
+	if err != nil {
+		return resources.ID{}, "", err
+	}
+
+	return scope, name, nil
+}
+
+func (r *GetResourceProviderSummary) createResponse(ctx context.Context, result *store.Object) (any, error) {
+	serviceCtx := v1.ARMRequestContextFromContext(ctx)
+
+	summary := datamodel.ResourceProviderSummary{}
+	err := result.As(&summary)
+	if err != nil {
+		return nil, err
+	}
+
+	return converter.ResourceProviderSummaryDataModelToVersioned(&summary, serviceCtx.APIVersion)
+}
---
+++ pkg/ucp/integrationtests/resourceproviders/summary_test.go
@@ -0,0 +1,110 @@
+/*
+Copyright 2023 The Radius Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resourceproviders
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/radius-project/radius/pkg/to"
+	"github.com/radius-project/radius/pkg/ucp/api/v20231001preview"
+	"github.com/radius-project/radius/pkg/ucp/frontend/api"
+	"github.com/radius-project/radius/pkg/ucp/integrationtests/testserver"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ResourceProviderSummary_Lifecycle(t *testing.T) {
+	server := testserver.StartWithETCD(t, api.DefaultModules)
+	defer server.Close()
+
+	createRadiusPlane(server)
+
+	// List should begin empty
+	response := server.MakeRequest(http.MethodGet, resourceProviderSummaryCollectionURL, nil)
+	response.EqualsEmptyList()
+
+	// Getting a specific resource provider should return 404 with the correct resource ID.
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsErrorCode(404, "NotFound")
+	require.Equal(t, fmt.Sprintf("the resource provider with name '%s' was not found", resourceProviderNamespace), response.Error.Error.Message)
+	require.Equal(t, "", response.Error.Error.Target)
+
+	createResourceProvider(server)
+
+	// List should now contain a resource provider.
+	expected := v20231001preview.ResourceProviderSummary{
+		Name:          to.Ptr(resourceProviderNamespace),
+		Locations:     map[string]map[string]any{},
+		ResourceTypes: map[string]*v20231001preview.ResourceProviderSummaryResourceType{},
+	}
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryCollectionURL, nil)
+	response.EqualsValue(200, map[string]any{
+		"value": []any{expected},
+	})
+
+	// Getting a specific resource provider should return the resource provide summary.
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsValue(200, expected)
+
+	// Now we'll iteratively add/remove elements and verify the summary is updated.
+	createResourceType(server)
+	expected.ResourceTypes[resourceTypeName] = &v20231001preview.ResourceProviderSummaryResourceType{
+		APIVersions:       map[string]map[string]any{},
+		DefaultAPIVersion: to.Ptr("2025-01-01"),
+	}
+
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsValue(200, expected)
+
+	createAPIVersion(server)
+	expected.ResourceTypes[resourceTypeName].APIVersions["2025-01-01"] = map[string]any{}
+
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsValue(200, expected)
+
+	createLocation(server)
+	expected.Locations["east"] = map[string]any{}
+
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsValue(200, expected)
+
+	deleteAPIVersion(server)
+	delete(expected.ResourceTypes[resourceTypeName].APIVersions, "2025-01-01")
+
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsValue(200, expected)
+
+	deleteResourceType(server)
+	delete(expected.ResourceTypes, resourceTypeName)
+
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsValue(200, expected)
+
+	deleteLocation(server)
+	delete(expected.Locations, "east")
+
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsValue(200, expected)
+
+	deleteResourceProvider(server)
+
+	response = server.MakeRequest(http.MethodGet, resourceProviderSummaryURL, nil)
+	response.EqualsErrorCode(404, "NotFound")
+	require.Equal(t, fmt.Sprintf("the resource provider with name '%s' was not found", resourceProviderNamespace), response.Error.Error.Message)
+	require.Equal(t, "", response.Error.Error.Target)
+}
