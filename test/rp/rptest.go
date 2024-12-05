a37e6704ea5258605c219f2249ac21cc8c745bd2
nithyatsu <nithyasu@microsoft.com>
2024-12-05 15:43:42 -0800

    wip

--- test/rp/rptest.go
+++ test/rp/rptest.go
@@ -25,7 +25,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
 	"github.com/aws/aws-sdk-go-v2/service/cloudcontrol"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	ucp_aws "github.com/radius-project/radius/pkg/ucp/aws"
 	"github.com/stretchr/testify/require"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
@@ -185,15 +188,14 @@ func NewRPTestOptions(t *testing.T) RPTestOptions {
 
 	cfg, err := awsconfig.LoadDefaultConfig(ctx)
 	require.NoError(t, err)
+	//var awsClient ucp_aws.AWSCloudControlClient = cloudcontrol.NewFromConfig(cfg)
+
+	stsClient := sts.NewFromConfig(cfg)
+	roleARN := "arn:aws:iam::179022619019:role/radius_func_test"
+	creds := stscreds.NewAssumeRoleProvider(stsClient, roleARN)
+	cfg.Credentials = aws.NewCredentialsCache(creds)
 	var awsClient ucp_aws.AWSCloudControlClient = cloudcontrol.NewFromConfig(cfg)
 
-	/*
-		stsClient := sts.NewFromConfig(cfg)
-		roleARN := "arn:aws:iam::179022619019:role/radius_func_test"
-		creds := stscreds.NewAssumeRoleProvider(stsClient, roleARN)
-		cfg.Credentials = aws.NewCredentialsCache(creds)
-		var awsClient ucp_aws.AWSCloudControlClient = cloudcontrol.NewFromConfig(cfg)
-	*/
 	return RPTestOptions{
 		TestOptions:      test.NewTestOptions(t),
 		Workspace:        workspace,
