4d483d11998e93714fdcddde9a87a3e8ab979123
lakshmimsft <ljavadekar@microsoft.com>
2024-12-09 15:14:11 -0800

    draft
    Signed-off-by: lakshmimsft <ljavadekar@microsoft.com>

---
+++ pkg/ucp/ucpclient/registermanifests_test.go
@@ -0,0 +1,46 @@
+/*
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package ucpclient
+
+/*
+import (
+	"context"
+	"testing"
+
+	"github.com/radius-project/radius/pkg/ucp/hostoptions"
+	"github.com/radius-project/radius/pkg/ucp/server"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegisterManifests(t *testing.T) {
+	ctx := context.Background()
+
+	// Setup temporary manifest directory and files
+	manifestDir := "testdata"
+
+	// Create sample manifest files in manifestDir
+	// ...
+	options := &server.Options{
+		Config: &hostoptions.UCPConfig{
+			Manifests: hostoptions.ManifestConfig{
+				ManifestDirectory: manifestDir,
+			},
+		},
+	}
+
+	// Setup fake UCP client with fake servers
+	fakeUCPClient, err := NewFakeUCPClient()
+	require.NoError(t, err)
+
+
+}
+*/
