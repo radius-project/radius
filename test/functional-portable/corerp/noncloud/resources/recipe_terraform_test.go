b1d84da215913a2ab575714f1a2563ab54bb611a
Karishma Chawla <kachawla@microsoft.com>
2025-03-12 16:57:36 -0700

    Update Terraform Postgres Recipe Test
    Signed-off-by: Karishma Chawla <kachawla@microsoft.com>

--- test/functional-portable/corerp/noncloud/resources/recipe_terraform_test.go
+++ test/functional-portable/corerp/noncloud/resources/recipe_terraform_test.go
@@ -139,11 +139,14 @@ func Test_TerraformRecipe_KubernetesRedis(t *testing.T) {
 //
 // - Create an extender resource using a Terraform recipe that deploys Postgres on Kubernetes.
 // - The recipe deployment creates a Kubernetes deployment and a Kubernetes service and a postgres db.
+//
+// The purpose of this test is to validate that the Terraform provider config referencing secret store
+// resource is passed correctly during recipe deployment.
 func Test_TerraformRecipe_KubernetesPostgres(t *testing.T) {
 	template := "testdata/corerp-resources-terraform-postgres.bicep"
 	appName := "corerp-resources-terraform-pg-app"
 	envName := "corerp-resources-terraform-pg-env"
-	extenderName := "pgs-resources-terraform-pgsapp"
+	extenderName := "corerp-resources-terraform-pg"
 	secretName := "pgs-secretstore"
 	secretResourceName := appName + "/" + secretName
 	userName := "postgres"
@@ -196,31 +199,6 @@ func Test_TerraformRecipe_KubernetesPostgres(t *testing.T) {
 				},
 			},
 			SkipObjectValidation: true,
-			PostStepVerify: func(ctx context.Context, t *testing.T, test rp.RPTest) {
-				secret, err := test.Options.K8sClient.CoreV1().Secrets(secretNamespace).
-					Get(ctx, secretPrefix+secretSuffix, metav1.GetOptions{})
-				require.NoError(t, err)
-				require.Equal(t, secretNamespace, secret.Namespace)
-				require.Equal(t, secretPrefix+secretSuffix, secret.Name)
-
-				pg, err := test.Options.ManagementClient.GetResource(ctx, "Applications.Core/extenders", extenderName)
-				require.NoError(t, err)
-				require.NotNil(t, pg)
-				status := pg.Properties["status"].(map[string]any)
-				recipe := status["recipe"].(map[string]interface{})
-				require.Equal(t, "terraform", recipe["templateKind"].(string))
-				expectedTemplatePath := strings.Replace(testutil.GetTerraformRecipeModuleServerURL()+"/postgres.zip", "moduleServer=", "", 1)
-				require.Equal(t, expectedTemplatePath, recipe["templatePath"].(string))
-
-				// At present, it is not possible to verify the template version in functional tests
-				// This is verified by UTs though
-
-				// Manually delete Kubernetes the secret that stores the Terraform state file now. The next step in the test will be the deletion
-				// of the portable resource that uses this secret for Terraform recipe. This is to verify that the test and portable resource
-				// deletion will not fail even though the secret is already deleted.
-				err = test.Options.K8sClient.CoreV1().Secrets(secretNamespace).Delete(ctx, secretPrefix+secretSuffix, metav1.DeleteOptions{})
-				require.NoError(t, err)
-			},
 		},
 	})
 
