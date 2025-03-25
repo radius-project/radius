872b41e7810fd2eb8c03c8ecd008a0e4c1b3f913
willdavsmith <willdavsmith@gmail.com>
2025-03-25 13:22:52 -0700

    PR
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>

--- build/install-bicep.sh
+++ build/install-bicep.sh
@@ -5,16 +5,43 @@
 # This is used to build the Bicep container image, and is called automatically
 # by the `make build-bicep` and `make docker-build-bicep` commands.
 
-# Usage: ./install-bicep.sh <release-channel> <output-dir>
+# Usage: ./install-bicep.sh <release-channel> <output-dir> <arch>
+# Example: ./install-bicep.sh edge ./output amd64
+
+# Also, the script accepts the following environment variables:
+# - RADIUS_BICEP_TYPES_REGISTRY: The registry for the Radius Bicep types. Default is "br:biceptypes.azurecr.io".
+# - RADIUS_BICEP_TYPES_VERSION: The version for the Radius Bicep types. Default is "latest".
+# - AWS_BICEP_TYPES_REGISTRY: The registry for the AWS Bicep types. Default is "br:biceptypes.azurecr.io".
+# - AWS_BICEP_TYPES_VERSION: The version for the AWS Bicep types. Default is "latest".
 
 REL_CHANNEL=$1
 OUTPUT_DIR=$2
 ARCH=$3
 
+if [ -z "$REL_CHANNEL" ]; then
+  echo "Release channel is required. Please provide it as the first argument."
+  exit 1
+fi
+
+if [ -z "$OUTPUT_DIR" ]; then
+  echo "Output directory is required. Please provide it as the second argument."
+  exit 1
+fi
+
+if [ -z "$ARCH" ]; then
+  echo "Architecture is required. Please provide it as the third argument."
+  exit 1
+fi
+
 # Radius Bicep types uses latest tag
 if [ "$REL_CHANNEL" = "edge" ]; then
   REL_CHANNEL="latest"
 fi
+
+RADIUS_BICEP_TYPES_REGISTRY=${RADIUS_BICEP_TYPES_REGISTRY:-"br:biceptypes.azurecr.io"}
+RADIUS_BICEP_TYPES_VERSION=${RADIUS_BICEP_TYPES_VERSION:-$REL_CHANNEL}
+AWS_BICEP_TYPES_REGISTRY=${AWS_BICEP_TYPES_REGISTRY:-"br:biceptypes.azurecr.io"}
+AWS_BICEP_TYPES_VERSION=${AWS_BICEP_TYPES_VERSION:-$REL_CHANNEL}
 
 # Check if curl is installed
 if ! command -v curl &> /dev/null
@@ -31,12 +58,13 @@ cat <<EOF > $OUTPUT_DIR/bicepconfig.json
     "extensibility": true
   },
   "extensions": {
-    "radius": "br:biceptypes.azurecr.io/radius:${REL_CHANNEL}",
-    "aws": "br:biceptypes.azurecr.io/aws:${REL_CHANNEL}"
+    "radius": "br:${RADIUS_BICEP_TYPES_REGISTRY}/radius:${RADIUS_BICEP_TYPES_VERSION}",
+    "aws": "br:${AWS_BICEP_TYPES_REGISTRY}/aws:${AWS_BICEP_TYPES_VERSION}"
   }
 }
 EOF
 
+# Bicep CLI uses x64 or arm64
 BICEP_ARCH="x64"
 if [ "$ARCH" = "arm64" ]; then
   BICEP_ARCH="arm64"
