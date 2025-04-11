4f6b54ac28afc6a7e8f24fb3a9102ee1ccaa46cc
ytimocin <ytimocin@microsoft.com>
2025-04-11 15:13:35 -0700

    Updating rad version command to also show control plane version
    Signed-off-by: ytimocin <ytimocin@microsoft.com>

--- pkg/cli/output/writer.go
+++ pkg/cli/output/writer.go
@@ -21,6 +21,8 @@ import (
 	"io"
 )
 
+//go:generate mockgen -typed -destination=./mock_writer.go -package=output -self_package github.com/radius-project/radius/pkg/cli/output github.com/radius-project/radius/pkg/cli/output Interface
+
 // Interface is a mockable interface for writing cli output
 type Interface interface {
 	// LogInfo logs a message that is displayed to the user by default.
