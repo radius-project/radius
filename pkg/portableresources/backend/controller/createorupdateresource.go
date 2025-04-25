lakshmimsft/radius-mainfork

update dynamicrp
Signed-off-by: lakshmimsft <ljavadekar@microsoft.com>

pkg/portableresources/backend/controller/createorupdateresource.go
@@ -85,13 +85,12 @@ func (c *CreateOrUpdateResource[P, T]) Run(ctx context.Context, req *ctrl.Reques
 	}
 
 	// Now we're ready to process recipes (if needed).
-
+	recipeDataModel, supportsRecipes := any(data).(datamodel.RecipeDataModel)
 	recipeOutput, err := c.executeRecipeIfNeeded(ctx, data, previousOutputResources, config.Simulated)
 	if err != nil {
 		if recipeError, ok := err.(*recipes.RecipeError); ok {
 			logger.Error(err, fmt.Sprintf("failed to execute recipe. Encountered error while processing %s ", recipeError.ErrorDetails.Target))
 			// Set the deployment status to the recipe error code.
-			recipeDataModel := any(data).(datamodel.RecipeDataModel)
 			recipeDataModel.GetRecipe().DeploymentStatus = util.RecipeDeploymentStatus(recipeError.DeploymentStatus)
 			update := &database.Object{
 				Metadata: database.Metadata{
@@ -119,15 +118,11 @@ func (c *CreateOrUpdateResource[P, T]) Run(ctx context.Context, req *ctrl.Reques
 		}
 	}
 
-	recipeDataModel, supportsRecipes := any(data).(datamodel.RecipeDataModel)
 	if supportsRecipes {
 		recipeData := recipeDataModel.GetRecipe()
 		if recipeData != nil {
 			recipeData.DeploymentStatus = util.Success
 			recipeDataModel.SetRecipe(recipeData)
-		}
-		if recipeOutput != nil && recipeOutput.Status != nil {
-			setRecipeStatus(data, *recipeOutput.Status)
 		}
 	}
 
@@ -179,14 +174,4 @@ func (c *CreateOrUpdateResource[P, T]) executeRecipeIfNeeded(ctx context.Context
 		PreviousState: prevState,
 		Simulated:     simulated,
 	})
-}
-
-// setRecipeStatus sets the recipe status for the given resource model.
-// It retrieves the resource metadata from the provided model, deep copies the current resource status,
-// updates the Recipe field with the supplied recipeStatus, and then applies the updated status back to the resource.
-func setRecipeStatus[P rpv1.RadiusResourceModel](data P, recipeStatus rpv1.RecipeStatus) {
-	rm := data.ResourceMetadata()
-	status := rm.GetResourceStatus().DeepCopyRecipeStatus()
-	status.Recipe = &recipeStatus
-	rm.SetResourceStatus(status)
 }

radius-project/radius

pkg/portableresources/backend/controller/createorupdateresource.go
@@ -85,13 +85,12 @@ func (c *CreateOrUpdateResource[P, T]) Run(ctx context.Context, req *ctrl.Reques
 	}
 
 	// Now we're ready to process recipes (if needed).
-
+	recipeDataModel, supportsRecipes := any(resource).(datamodel.RecipeDataModel)
 	recipeOutput, err := c.executeRecipeIfNeeded(ctx, resource, previousOutputResources, config.Simulated)
 	if err != nil {
 		if recipeError, ok := err.(*recipes.RecipeError); ok {
 			logger.Error(err, fmt.Sprintf("failed to execute recipe. Encountered error while processing %s ", recipeError.ErrorDetails.Target))
 			// Set the deployment status to the recipe error code.
-			recipeDataModel := any(resource).(datamodel.RecipeDataModel)
 			recipeDataModel.GetRecipe().DeploymentStatus = util.RecipeDeploymentStatus(recipeError.DeploymentStatus)
 			update := &database.Object{
 				Metadata: database.Metadata{
@@ -119,7 +118,6 @@ func (c *CreateOrUpdateResource[P, T]) Run(ctx context.Context, req *ctrl.Reques
 		}
 	}
 
-	recipeDataModel, supportsRecipes := any(resource).(datamodel.RecipeDataModel)
 	if supportsRecipes {
 		recipeData := recipeDataModel.GetRecipe()
 		if recipeData != nil {