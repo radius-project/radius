b01390bc60ed99e80aa8e05a52a442390d3d547e
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-05-26 11:34:14 -0700

    simplify
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- build/workflow.mk
+++ build/workflow.mk
@@ -16,47 +16,16 @@
 
 ##@ GitHub Workflows
 
-WORKFLOW_SCRIPT_DIR := build
-WORKFLOW_SCRIPT := $(WORKFLOW_SCRIPT_DIR)/workflow-helper.sh
+WORKFLOW_SCRIPT := ./build/workflow.sh
 
-# Default branch to current branch if not specified
-BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
+.PHONY: workflow-disable-all
+workflow-disable-all: ## Disable all workflows in the current repo
+	@bash $(WORKFLOW_SCRIPT) disable-all
 
-.PHONY: workflow-dispatch
-workflow-dispatch: ## Dispatch a workflow by name. Usage: make workflow-dispatch NAME=<workflow-file> [BRANCH=<branch>]
-	@if [ -z "$(NAME)" ]; then \
-		echo "Error: NAME parameter is required. Usage: make workflow-dispatch NAME=<workflow-file> [BRANCH=<branch>]"; \
-		exit 1; \
-	fi
-	@bash $(WORKFLOW_SCRIPT) dispatch "$(NAME)" "$(BRANCH)"
-
-.PHONY: workflow
-workflow: workflow-dispatch ## Alias for workflow-dispatch
-
-.PHONY: workflow-list
-workflow-list: ## List all workflows in the repository
-	@bash $(WORKFLOW_SCRIPT) list
-
-.PHONY: workflow-disable
-workflow-disable: ## Disable a workflow by name. Usage: make workflow-disable NAME=<workflow-name>
-	@if [ -z "$(WORKFLOW_NAME)" ]; then \
-		echo "Error: WORKFLOW_NAME parameter is required. Usage: make workflow-disable WORKFLOW_NAME=<workflow file name>"; \
-		exit 1; \
-	fi
-	@bash $(WORKFLOW_SCRIPT) disable "$(WORKFLOW_NAME)"
-
-.PHONY: workflow-disable-triggered
-workflow-disable-triggered: ## Disable all workflows triggered by events (push, PR, timers)
-	@bash $(WORKFLOW_SCRIPT) disable-triggered
-
-.PHONY: workflow-enable
-workflow-enable: ## Enable a workflow by name. Usage: make workflow-enable NAME=<workflow-name>
-	@if [ -z "$(WORKFLOW_NAME)" ]; then \
-		echo "Error: WORKFLOW_NAME parameter is required. Usage: make workflow-enable WORKFLOW_NAME=<workflow-name>"; \
-		exit 1; \
-	fi
-	@bash $(WORKFLOW_SCRIPT) enable "$(WORKFLOW_NAME)"
+.PHONY: workflow-enable-all
+workflow-enable-all: ## Enable all workflows in the current repo
+	@bash $(WORKFLOW_SCRIPT) enable-all
 
 .PHONY: workflow-delete-all-runs
-workflow-delete-all-runs: ## Delete all workflow runs in the repository
+workflow-delete-all-runs: ## Delete all workflow runs in the repository. NOTE: This is a destructive operation and cannot be undone.
 	@bash $(WORKFLOW_SCRIPT) delete-all-runs
