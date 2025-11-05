94b4c1e099860a0fbbb191e5d9bca3a9a3296f42
Vishwanath Hiremath <vhiremath@microsoft.com>
2025-11-05 14:13:35 -0800

    fix copyright formatting
    Signed-off-by: Vishwanath Hiremath <vhiremath@microsoft.com>

--- pkg/cli/cmd/recipepack/delete/delete.go
+++ pkg/cli/cmd/recipepack/delete/delete.go
@@ -1,5 +1,3 @@
-package delete
-
 /*
 Copyright 2023 The Radius Authors.
 
@@ -15,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+package delete
 
 import (
 	"context"
