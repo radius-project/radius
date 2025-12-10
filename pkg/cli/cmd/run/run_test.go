75ead0d79748fe243f2cdc188174c42aa80c9e6d
Karishma Chawla <kachawla@microsoft.com>
2025-12-10 13:52:23 -0800

    Fix existing unit tests
    Signed-off-by: Karishma Chawla <kachawla@microsoft.com>

--- pkg/cli/cmd/run/run_test.go
+++ pkg/cli/cmd/run/run_test.go
@@ -24,7 +24,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 
-	"github.com/radius-project/radius/pkg/cli/bicep"
 	"github.com/radius-project/radius/pkg/cli/clients"
 	deploycmd "github.com/radius-project/radius/pkg/cli/cmd/deploy"
 	"github.com/radius-project/radius/pkg/cli/config"
@@ -67,6 +66,10 @@ func Test_Validate(t *testing.T) {
 				Config:         configWithWorkspace,
 			},
 			ConfigureMocks: func(mocks radcli.ValidateMocks) {
+				mocks.Bicep.EXPECT().
+					PrepareTemplate("app.bicep").
+					Return(map[string]any{}, nil).
+					Times(1)
 				mocks.ApplicationManagementClient.EXPECT().
 					GetEnvironment(gomock.Any(), "prod").
 					Return(v20231001preview.EnvironmentResource{}, nil).
@@ -88,6 +91,10 @@ func Test_Validate(t *testing.T) {
 				},
 			},
 			ConfigureMocks: func(mocks radcli.ValidateMocks) {
+				mocks.Bicep.EXPECT().
+					PrepareTemplate("app.bicep").
+					Return(map[string]any{}, nil).
+					Times(1)
 				mocks.ApplicationManagementClient.EXPECT().
 					GetEnvironment(gomock.Any(), "prod").
 					Return(v20231001preview.EnvironmentResource{}, nil).
@@ -103,6 +110,10 @@ func Test_Validate(t *testing.T) {
 				Config:         configWithWorkspace,
 			},
 			ConfigureMocks: func(mocks radcli.ValidateMocks) {
+				mocks.Bicep.EXPECT().
+					PrepareTemplate("app.bicep").
+					Return(map[string]any{}, nil).
+					Times(1)
 				mocks.ApplicationManagementClient.EXPECT().
 					GetEnvironment(gomock.Any(), "/planes/radius/local/resourceGroups/test-resource-group/providers/Applications.Core/environments/test-environment").
 					Return(v20231001preview.EnvironmentResource{}, nil).
@@ -140,12 +151,6 @@ func Test_Run(t *testing.T) {
 	// We should revisit the test strategy if the code paths deviate sigificantly.
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-
-	bicep := bicep.NewMockInterface(ctrl)
-	bicep.EXPECT().
-		PrepareTemplate("app.bicep").
-		Return(map[string]any{}, nil).
-		Times(1)
 
 	deployOptionsChan := make(chan deploy.Options, 1)
 	deployMock := deploy.NewMockInterface(ctrl)
@@ -259,7 +264,6 @@ func Test_Run(t *testing.T) {
 	}
 	runner := &Runner{
 		Runner: deploycmd.Runner{
-			Bicep:  bicep,
 			Deploy: deployMock,
 			Output: outputSink,
 			ConnectionFactory: &connections.MockFactory{
@@ -270,6 +274,7 @@ func Test_Run(t *testing.T) {
 			ApplicationName:     "test-application",
 			EnvironmentNameOrID: radcli.TestEnvironmentName,
 			Parameters:          map[string]map[string]any{},
+			Template:            map[string]any{}, // Template is prepared in Validate
 			Workspace:           workspace,
 			Providers:           providers,
 		},
@@ -335,12 +340,6 @@ func Test_Run_NoDashboard(t *testing.T) {
 	// This is the same test as above, but without expecting the dashboard portforward to be started.
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-
-	bicep := bicep.NewMockInterface(ctrl)
-	bicep.EXPECT().
-		PrepareTemplate("app.bicep").
-		Return(map[string]any{}, nil).
-		Times(1)
 
 	deployOptionsChan := make(chan deploy.Options, 1)
 	deployMock := deploy.NewMockInterface(ctrl)
@@ -434,7 +433,6 @@ func Test_Run_NoDashboard(t *testing.T) {
 	}
 	runner := &Runner{
 		Runner: deploycmd.Runner{
-			Bicep:  bicep,
 			Deploy: deployMock,
 			Output: outputSink,
 			ConnectionFactory: &connections.MockFactory{
@@ -445,6 +443,7 @@ func Test_Run_NoDashboard(t *testing.T) {
 			ApplicationName:     "test-application",
 			EnvironmentNameOrID: radcli.TestEnvironmentName,
 			Parameters:          map[string]map[string]any{},
+			Template:            map[string]any{}, // Template is prepared in Validate
 			Workspace:           workspace,
 			Providers:           providers,
 		},
