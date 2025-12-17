cc0000586dedc5d29930e473362f22aaba7291f6
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-12-17 03:19:12 +0000

    fix: reorder parameters in NewGenericResourcesClient calls for consistency
    Signed-off-by: Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>

--- test/functional-portable/corerp/noncloud/api_test.go
+++ test/functional-portable/corerp/noncloud/api_test.go
@@ -65,10 +65,10 @@ func Test_ResourceList(t *testing.T) {
 		if strings.HasPrefix(resourceType, "Radius.Core") {
 			radiusCoreClientOptions := *clientOptions
 			radiusCoreClientOptions.APIVersion = "2025-08-01-preview"
-			client, err = generated.NewGenericResourcesClient(resourceGroupScope, resourceType, &aztoken.AnonymousCredential{}, &radiusCoreClientOptions)
+			client, err = generated.NewGenericResourcesClient(resourceType, resourceGroupScope, &aztoken.AnonymousCredential{}, &radiusCoreClientOptions)
 			require.NoError(t, err)
 		} else {
-			client, err = generated.NewGenericResourcesClient(resourceGroupScope, resourceType, &aztoken.AnonymousCredential{}, clientOptions)
+			client, err = generated.NewGenericResourcesClient(resourceType, resourceGroupScope, &aztoken.AnonymousCredential{}, clientOptions)
 			require.NoError(t, err)
 		}
 		pager := client.NewListByRootScopePager(nil)
