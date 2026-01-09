36e561d4e539a458346a09b816d32f590fe549b3
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2026-01-09 10:53:48 -0800

    chore(lint): update golangci-lint
    Signed-off-by: Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,18 +6,18 @@ set -e
 npm install -g @typespec/compiler
 
 # Adding workspace as safe directory to avoid permission issues
-git config --global --add safe.directory /workspaces/radius 
+git config --global --add safe.directory /workspaces/radius
 
 # Install the binary form of golangci-lint, as recommended
 # https://golangci-lint.run/welcome/install/#local-installation
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v2.5.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v2.7.2
 
 # Other go tools
-go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.17.0 
+go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.17.0
 go install go.uber.org/mock/mockgen@v0.4.0
 
 # Prerequisites for Code Generation, see https://github.com/radius-project/radius/tree/main/docs/contributing/contributing-code/contributing-code-prerequisites#code-generation
-cd typespec || exit 
-npm ci 
+cd typespec || exit
+npm ci
 npm install -g autorest@3.7.2
 npm install -g oav@4.0.2
