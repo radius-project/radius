fe3f82fe4947692db541524735ac1b5e63554928
sk593 <shruthikumar@microsoft.com>
2026-04-15 22:57:41 -0700

    revert
    Signed-off-by: sk593 <shruthikumar@microsoft.com>

--- web/browser-extension/src/shared/github-client.ts
+++ web/browser-extension/src/shared/github-client.ts
@@ -952,7 +952,7 @@ jobs:
 
       - name: Install Radius CLI
         run: |
-          wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash -s edge
+          wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash
           rad version
 
       - name: Install Terraform
